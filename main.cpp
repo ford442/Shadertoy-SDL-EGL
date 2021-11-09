8bf062128cc52eae7d8c192ee74ce8a079136440
Noah Cohn <noahc42@gmail.com>
2021-11-08 21:41:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,41 +7,34 @@
 #include <GLES3/gl3.h>
 #include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
-#include <SDL2/SDL.h>
-#include "SDL2/SDL_config.h"
-#include <projectM.hpp>
-#include <pthread.h>
 
 Uint8 * stm;
 static EGLDisplay display;
 static EGLContext contextegl;
 static EGLSurface surface;
 static EmscriptenWebGLContextAttributes attr;
-const float FPS=60;
 static SDL_AudioDeviceID dev;
 static struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
-typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}
-projectMApp;projectMApp app;
+SDL_Window *win;
+SDL_GLContext *glCtx;
 
 static void renderFrame(){
-app.pm->renderFrame();
-// glFlush();
-eglSwapBuffers(display,surface);
-auto sndat=reinterpret_cast<short*>(stm);
-app.pm->pcm()->addPCM16Data(sndat,1024);
+  
+glClearColor(0.0,1.0,0.0,1.0);
+glClear(GL_COLOR_BUFFER_BIT);
 }
 
-static void chngt(){
+static void strt(){
 static const EGLint attribut_list[]={
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 EGL_NONE
 };
 static const EGLint attribute_list[]={
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
-EGL_RED_SIZE,16,
-EGL_GREEN_SIZE,16,
-EGL_BLUE_SIZE,16,
-EGL_ALPHA_SIZE,16,
+EGL_RED_SIZE,8,
+EGL_GREEN_SIZE,8,
+EGL_BLUE_SIZE,8,
+EGL_ALPHA_SIZE,8,
 EGL_STENCIL_SIZE,8,
 EGL_DEPTH_SIZE,24,
 EGL_NONE
@@ -62,11 +55,16 @@ attr.antialias=0;
 attr.premultipliedAlpha=0;
 attr.preserveDrawingBuffer=0;
 emscripten_webgl_init_context_attributes(&attr);
+  
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#canvas",&attr);
+
 EGLConfig eglconfig=NULL;
 EGLint config_size,major,minor;
+  
 display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  
 eglInitialize(display,&major,&minor);
+
 if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
 if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
 }
@@ -74,63 +72,31 @@ EGLint anEglCtxAttribs2[]={
 EGL_CONTEXT_CLIENT_VERSION,3,
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 EGL_NONE};
+  
 contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
+
 if(contextegl==EGL_NO_CONTEXT){
 }
 else{
+  
 surface=eglCreateWindowSurface(display,eglconfig,NULL,attribut_list);
+  
 eglMakeCurrent(display,surface,surface,contextegl);
+  
 glClearColor(0.0,0.0,0.0,0.0);
 glClear(GL_COLOR_BUFFER_BIT);
 }}
+  
 emscripten_webgl_make_context_current(ctx);
+
 int width=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
 int height=width;
-app.win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
-app.glCtx=&contextegl;
-SDL_SetWindowTitle(app.win,"1ink.us - Lazuli");
+win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
+glCtx=&contextegl;
+SDL_SetWindowTitle(win,"1ink.us - Shadertoy");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-app.settings.meshX=96;
-app.settings.meshY=96;
-app.settings.textureSize=2048;
-app.settings.fps=FPS;
-app.settings.windowWidth=width;
-app.settings.windowHeight=width;
-app.settings.smoothPresetDuration=22;
-app.settings.presetDuration=44;
-app.settings.beatSensitivity=1.0;
-app.settings.aspectCorrection=0;
-app.settings.easterEgg=0;
-app.settings.shuffleEnabled=0;
-app.settings.softCutRatingsEnabled=1;
-app.settings.presetURL="/presets";  
-app.pm=new projectM(app.settings);
-printf("Init ProjectM\n");
-app.pm->selectRandom(true);
-printf("Select random preset.\n");
-app.pm->projectM_resetGL(width,height);
-printf("Reseting GL.\n");
-DIR *m_dir;
-if((m_dir=opendir("/"))==NULL){printf("error opening /\n");
-}
-else{
-struct dirent *dir_entry;
-while((dir_entry=readdir(m_dir))!=NULL){
-printf("%s\n",dir_entry->d_name);
-}}
-for(uint i=0;i<app.pm->getPlaylistSize();i++){
-printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
-}
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
-}
-static void swtcht(){
-app.pm->selectRandom(true);
-printf("Select random preset.\n");
-}
-static void lckt(){
-app.pm->setPresetLock(true);
-printf("Preset locked.\n");
 }
 static void cls_aud(){
 if(dev!=0){
@@ -187,24 +153,13 @@ extern "C" {
 void pl(){
 plt();
 }
-void chng(){
-chngt();
+void str(){
+strt();
 }
-void lck(){
-lckt();
-}
-void swtch(){
-swtcht();
-}
-EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w$=parseInt(document.getElementById("iwid").innerHTML,10);let h$=parseInt(document.getElementById("ihig").innerHTML,10);w$=Math.round(w$);h$=Math.round(h$);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 }
 int main(){
 EM_ASM({
 FS.mkdir('/snd');
-FS.mkdir('/textures');
-FS.mkdir('/presets');
 });
-app.done=0;
-ma();
-return PROJECTM_SUCCESS;
+return 1;
 }
