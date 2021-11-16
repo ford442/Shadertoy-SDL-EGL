b976e2c5458448c1a85c5ef812ca44852833c21e
Noah Cohn <noahc42@gmail.com>
2021-11-16 14:09:04 -0800

    Update combine.cpp

--- combine.cpp
+++ combine.cpp
@@ -214,7 +214,9 @@ void str(){
 strt();
 }}
 
+/*
 static char* read_file_into_str(const char *filename) {
+char *result = NULL;
 long length = 0;
 FILE *file = fopen(filename, "r");
 if(file) {
@@ -229,7 +231,7 @@ if(status != 0) {
 fclose(file);
 return NULL;
 }
-auto result = malloc((length+1) * sizeof(char));
+result = malloc((length+1) * sizeof(char));
 if(result) {
 size_t actual_length = fread(result, sizeof(char), length , file);
 result[actual_length++] = '\0';
@@ -239,10 +241,62 @@ return result;
 }
 return NULL;
 }
+*/
 
 int main(){
 EM_ASM({
 FS.mkdir('/snd');
+document.getElementById('btn2').addEventListener('click',function(){
+let pth=document.getElementById('path').innerHTML;
+let pth2=document.getElementById('path2').innerHTML;
+let pth3=document.getElementById('path3').innerHTML;
+let pth4=document.getElementById('path4').innerHTML;
+let ff=new XMLHttpRequest();
+let ff2=new XMLHttpRequest();
+let ff3=new XMLHttpRequest();
+let ff4=new XMLHttpRequest();
+ff.open("GET",pth,true);
+ff.responseType="arraybuffer";
+ff.onload=function(oEvent){
+var arrayBuffer=ff.response;
+if(arrayBuffer){
+var fil=new Uint8ClampedArray(arrayBuffer);
+FS.writeFile('/shader1.glsl',fil);
+console.log('File: shader4.glsl.');
+}}
+ff.send(null);
+ff2.open("GET",pth2,true);
+ff2.responseType="arraybuffer";
+ff2.onload=function(oEvent){
+var arrayBuffer2=ff2.response;
+if(arrayBuffer2){
+var fil=new Uint8ClampedArray(arrayBuffer2);
+FS.writeFile('/shader2.glsl',fil);
+console.log('File: shader4.glsl.');
+}}
+ff2.send(null);
+ff3.open("GET",pth3,true);
+ff3.responseType="arraybuffer";
+ff3.onload=function(oEvent){
+var arrayBuffer3=ff3.response;
+if(arrayBuffer3){
+var fil=new Uint8ClampedArray(arrayBuffer3);
+FS.writeFile('/shader3.glsl',fil);
+console.log('File: shader4.glsl.');
+}}
+ff3.send(null);
+ff4.open("GET",pth4,true);
+ff4.responseType="arraybuffer";
+ff4.onload=function(oEvent){
+var arrayBuffer4=ff4.response;
+if(arrayBuffer4){
+var fil=new Uint8ClampedArray(arrayBuffer4);
+FS.writeFile('/shader4.glsl',fil);
+console.log('File: shader4.glsl.');
+}}
+ff4.send(null);
+});
+  
 });
 int window_x = -1;
 int window_y = -1;
@@ -254,7 +308,7 @@ texture_files[i] = NULL;
 char *program_source = NULL;
 int selected_option = -1;
 int selected_index = 0;
-program_source = read_file_into_str(optarg);
+program_source = EM_ASM({return FS.readFile('shader1.glsl');});
 default_fragment_shader = program_source;
 select_gles3();
 return 1;
