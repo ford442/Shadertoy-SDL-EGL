f3050acffc6106cd060abe86a4620292c023fd7f
Noah Cohn <noahc42@gmail.com>
2021-11-16 15:13:55 -0800

    Create fs.js

---
+++ fs.js
@@ -0,0 +1,13 @@
+
+var openFile=function(event){
+var input=event.target;
+var reader=new FileReader();
+reader.onload=function(){
+var arrayBuffer=reader.result;
+var fil=new Uint8ClampedArray(arrayBuffer);
+var filnm="/shader1.glsl";
+FS.writeFile(filnm,fil);
+console.log('File: '+input.files[0].name);
+};
+reader.readAsArrayBuffer(input.files[0]);
+};
