4695e465a79c2f6c7b494b056a78d2c04140570f
Noah Cohn <noahc42@gmail.com>
2021-11-16 14:16:40 -0800

    Update combine.cpp

--- combine.cpp
+++ combine.cpp
@@ -308,7 +308,7 @@ texture_files[i] = NULL;
 char *program_source = NULL;
 int selected_option = -1;
 int selected_index = 0;
-auto void srcc = EM_ASM({return FS.readFile('shader1.glsl',{ encoding: 'utf8' });});
+auto void* srcc = EM_ASM({return FS.readFile('shader1.glsl',{ encoding: 'utf8' });});
 program_source = &srcc;
 default_fragment_shader = program_source;
 select_gles3();
