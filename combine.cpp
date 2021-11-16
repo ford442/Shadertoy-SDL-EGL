01515d58c77498d99e658eda33ff06a7ffcc11c2
Noah Cohn <noahc42@gmail.com>
2021-11-16 13:52:44 -0800

    Update combine.cpp

--- combine.cpp
+++ combine.cpp
@@ -216,7 +216,7 @@ strt();
 
 static char* read_file_into_str(const char *filename) {
 long length = 0;
-char *result = NULL;
+auto  *result = NULL;
 FILE *file = fopen(filename, "r");
 if(file) {
 int status = fseek(file, 0, SEEK_END);
