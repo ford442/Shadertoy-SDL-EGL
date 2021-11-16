a0b73a02dc4939198cc83f6a86cfcf484c221cf7
Noah Cohn <noahc42@gmail.com>
2021-11-16 13:39:39 -0800

    Update combine.cpp

--- combine.cpp
+++ combine.cpp
@@ -45,54 +45,53 @@ SDL_Window *win;
 SDL_GLContext *glCtx;
 
 static void select_gles3() {
-    common_shader_header = common_shader_header_gles3;
-    vertex_shader_body = vertex_shader_body_gles3;
-    fragment_shader_header = fragment_shader_header_gles3;
-    fragment_shader_footer = fragment_shader_footer_gles3;
+common_shader_header = common_shader_header_gles3;
+vertex_shader_body = vertex_shader_body_gles3;
+fragment_shader_header = fragment_shader_header_gles3;
+fragment_shader_footer = fragment_shader_footer_gles3;
 }
 
 static GLuint compile_shader(GLenum type, GLsizei nsources, const char **sources){
-    GLuint shader;
-    GLint success, len;
-    GLsizei i, srclens[nsources];
-    char *log;
-    for (i = 0; i < nsources; ++i)
-        srclens[i] = (GLsizei)strlen(sources[i]);
-    shader = glCreateShader(type);
-    glShaderSource(shader, nsources, sources, srclens);
-    glCompileShader(shader);
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-          printf("Error compiling shader.\n");
-    }
-    return shader;
+GLuint shader;
+GLint success, len;
+GLsizei i, srclens[nsources];
+char *log;
+for (i = 0; i < nsources; ++i)
+srclens[i] = (GLsizei)strlen(sources[i]);
+shader = glCreateShader(type);
+glShaderSource(shader, nsources, sources, srclens);
+glCompileShader(shader);
+glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+if (!success) {
+printf("Error compiling shader.\n");
 }
-
-static void renderFrame(float ttime){
-    static const GLfloat vertices[] = {
-        -1.0f, -1.0f,
-        1.0f, -1.0f,
-        -1.0f, 1.0f,
-        1.0f, 1.0f,
-    };
-
-    if(uniform_gtime >= 0)
-        glUniform1f(uniform_gtime, abstime);
-    if(uniform_time >= 0)
-        glUniform1f(uniform_time, abstime);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glEnableVertexAttribArray(attrib_position);
-    glVertexAttribPointer(attrib_position, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+return shader;
+}
+float abstime;
+static void renderFrame(){
+abstime = SDL_GetTicks() / 1000;
+static const GLfloat vertices[] = {
+-1.0f, -1.0f,
+1.0f, -1.0f,
+-1.0f, 1.0f,
+1.0f, 1.0f,
+};
+if(uniform_gtime >= 0)
+glUniform1f(uniform_gtime, abstime);
+if(uniform_time >= 0)
+glUniform1f(uniform_time, abstime);
+glClearColor(0.0f, 0.0f, 0.0f, 1.0);
+glClear(GL_COLOR_BUFFER_BIT);
+glEnableVertexAttribArray(attrib_position);
+glVertexAttribPointer(attrib_position, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 static void strt(){
-
-  GLuint vtx, frag;
-    const char *sources[4];
-    char* log;
-    GLint success, len;
+GLuint vtx, frag;
+const char *sources[4];
+char* log;
+GLint success, len;
 
 static const EGLint attribut_list[]={
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
@@ -152,7 +151,7 @@ glCtx=&contextegl;
 SDL_SetWindowTitle(win,"1ink.us - Shadertoy");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-
+SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
 
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
@@ -216,51 +215,48 @@ strt();
 }}
 
 static char* read_file_into_str(const char *filename) {
-    long length = 0;
-    char *result = NULL;
-    FILE *file = fopen(filename, "r");
-    if(file) {
-        int status = fseek(file, 0, SEEK_END);
-        if(status != 0) {
-            fclose(file);
-            return NULL;
-        }
-        length = ftell(file);
-        status = fseek(file, 0, SEEK_SET);
-        if(status != 0) {
-            fclose(file);
-            return NULL;
-        }
-        result = malloc((length+1) * sizeof(char));
-        if(result) {
-            size_t actual_length = fread(result, sizeof(char), length , file);
-            result[actual_length++] = '\0';
-        } 
-        fclose(file);
-        return result;
-    }
-    return NULL;
+long length = 0;
+char *result = NULL;
+FILE *file = fopen(filename, "r");
+if(file) {
+int status = fseek(file, 0, SEEK_END);
+if(status != 0) {
+fclose(file);
+return NULL;
+}
+length = ftell(file);
+status = fseek(file, 0, SEEK_SET);
+if(status != 0) {
+fclose(file);
+return NULL;
+}
+result = malloc((length+1) * sizeof(char));
+if(result) {
+size_t actual_length = fread(result, sizeof(char), length , file);
+result[actual_length++] = '\0';
+} 
+fclose(file);
+return result;
+}
+return NULL;
 }
 
 int main(){
 EM_ASM({
 FS.mkdir('/snd');
 });
- bool fullscreen = false;
-    int window_width = 640;
-    int window_height = 360;
-    int window_x = -1;
-    int window_y = -1;
-    int temp_val = 0;
-    const char* texture_files[4];
-    for (int i=0; i<4; ++i) {
-        texture_files[i] = NULL;
-    }
-    char *program_source = NULL;
-    int selected_option = -1;
-    int selected_index = 0;
-    program_source = read_file_into_str(optarg);
-    default_fragment_shader = program_source;
-    select_gles3();
+int window_x = -1;
+int window_y = -1;
+int temp_val = 0;
+const char* texture_files[4];
+for (int i=0; i<4; ++i) {
+texture_files[i] = NULL;
+}
+char *program_source = NULL;
+int selected_option = -1;
+int selected_index = 0;
+program_source = read_file_into_str(optarg);
+default_fragment_shader = program_source;
+select_gles3();
 return 1;
 }
