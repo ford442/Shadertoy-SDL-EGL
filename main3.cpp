ffef05f46ffccb0d0cd22e0f6cab5df3207019cc
Noah Cohn <noahc42@gmail.com>
2021-11-17 09:50:17 -0800

    Create main3.cpp

---
+++ main3.cpp
@@ -0,0 +1,166 @@
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl3.h>
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#include <SDL2/SDL.h>
+
+Uint8 * stm;
+static EGLDisplay display;
+static EGLContext contextegl;
+static EGLSurface surface;
+static EmscriptenWebGLContextAttributes attr;
+static SDL_AudioDeviceID dev;
+static struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
+SDL_Window *win;
+SDL_GLContext *glCtx;
+
+static void renderFrame(){
+  
+glClearColor(0.0,1.0,0.0,1.0);
+glClear(GL_COLOR_BUFFER_BIT);
+}
+
+static void strt(){
+static const EGLint attribut_list[]={
+EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
+EGL_NONE
+};
+static const EGLint attribute_list[]={
+EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+EGL_RED_SIZE,8,
+EGL_GREEN_SIZE,8,
+EGL_BLUE_SIZE,8,
+EGL_ALPHA_SIZE,8,
+EGL_STENCIL_SIZE,8,
+EGL_DEPTH_SIZE,24,
+EGL_NONE
+};
+SDL_GL_SetAttribute( SDL_GL_RED_SIZE,5);
+SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,5);
+SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,5);
+SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE,8);
+SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE,8);
+SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE,8);
+SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,8);
+SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,24);
+SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,1);
+attr.alpha=1;
+attr.stencil=1;
+attr.depth=1;
+attr.antialias=0;
+attr.premultipliedAlpha=0;
+attr.preserveDrawingBuffer=0;
+emscripten_webgl_init_context_attributes(&attr);
+  
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#canvas",&attr);
+
+EGLConfig eglconfig=NULL;
+EGLint config_size,major,minor;
+  
+display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  
+eglInitialize(display,&major,&minor);
+
+if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
+if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
+}
+EGLint anEglCtxAttribs2[]={
+EGL_CONTEXT_CLIENT_VERSION,3,
+EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+EGL_NONE};
+  
+contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
+
+if(contextegl==EGL_NO_CONTEXT){
+}
+else{
+  
+surface=eglCreateWindowSurface(display,eglconfig,NULL,attribut_list);
+  
+eglMakeCurrent(display,surface,surface,contextegl);
+  
+glClearColor(0.0,0.0,0.0,0.0);
+glClear(GL_COLOR_BUFFER_BIT);
+}}
+  
+emscripten_webgl_make_context_current(ctx);
+
+int width=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
+int height=width;
+win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
+glCtx=&contextegl;
+SDL_SetWindowTitle(win,"1ink.us - Shadertoy");
+SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
+SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
+emscripten_set_main_loop((void (*)())renderFrame,0,0);
+}
+static void cls_aud(){
+if(dev!=0){
+SDL_PauseAudioDevice(dev,SDL_TRUE);
+SDL_CloseAudioDevice(dev);
+dev=0;
+}}
+static void qu(int rc){
+SDL_Quit();
+exit(rc);
+}
+static void opn_aud(){
+dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){
+SDL_FreeWAV(wave.snd);
+qu(2);
+}
+SDL_PauseAudioDevice(dev,SDL_FALSE);
+}
+static void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
+int lft;
+wptr=wave.snd+wave.pos;
+lft=wave.slen-wave.pos;
+while (lft<=len){
+SDL_memcpy(stm,wptr,lft);
+stm+=lft;
+len-=lft;
+wptr=wave.snd;
+lft=wave.slen;
+wave.pos=0;
+}
+SDL_memcpy(stm,wptr,len);
+wave.pos+=len;
+}
+static void plt(){
+cls_aud();
+char flnm[1024];
+SDL_FreeWAV(wave.snd);
+SDL_Quit();
+SDL_SetMainReady();
+if (SDL_Init(SDL_INIT_AUDIO)<0){
+qu(1);
+}
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
+qu(1);
+}
+wave.pos=0;
+wave.spec.callback=bfr;
+opn_aud();
+}
+extern "C" {
+void pl(){
+plt();
+}
+void str(){
+strt();
+}
+}
+int main(){
+EM_ASM({
+FS.mkdir('/snd');
+});
+return 1;
+}
