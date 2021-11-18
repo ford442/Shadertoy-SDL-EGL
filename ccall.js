dd31cdde51eee7d99fad117aeadc7caaa84e8f03
Noah Cohn <noahc42@gmail.com>
2021-11-17 16:08:22 -0800

    Update ccall.js

--- ccall.js
+++ ccall.js
@@ -18,3 +18,17 @@ document.getElementById('shut').innerHTML=2;
 document.getElementById('di').click();
 Module.ccall("str");
 });
+
+
+document.getElementById('cue').addEventListener('onchange',function(event){
+let input=event.target;
+let reader=new FileReader();
+reader.onload=function(){
+let arrayBuffer=reader.result;
+let fil=new Uint8ClampedArray(arrayBuffer);
+let filnm="/shader1.glsl";
+FS.writeFile(filnm,fil);
+console.log('File: '+input.files[0].name);
+}
+reader.readAsArrayBuffer(input.files[0]);
+}
