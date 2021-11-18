fcddfa6cf9204a7a70f7121d56ce7eb42ace1d00
Noah Cohn <noahc42@gmail.com>
2021-11-17 19:01:49 -0800

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,14 +1,13 @@
 
-document.getElementById('cue').addEventListener('onchange',function(event){
+var openFile=function(event){
 var input=event.target;
 var reader=new FileReader();
 reader.onload=function(){
 var arrayBuffer=reader.result;
 var fil=new Uint8ClampedArray(arrayBuffer);
-var filnm="/shader1.glsl";
-console.log("Wrote: /shader1.glsl");
+var filnm="/"+input.files[0].name;
 FS.writeFile(filnm,fil);
 console.log('File: '+input.files[0].name);
 };
 reader.readAsArrayBuffer(input.files[0]);
-});
+};
