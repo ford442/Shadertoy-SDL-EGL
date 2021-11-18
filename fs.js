d9a25c15666189d04a4bb24c1221746e3725bad0
Noah Cohn <noahc42@gmail.com>
2021-11-17 16:07:25 -0800

    Update fs.js

--- fs.js
+++ fs.js
@@ -8,6 +8,6 @@ let fil=new Uint8ClampedArray(arrayBuffer);
 let filnm="/shader1.glsl";
 FS.writeFile(filnm,fil);
 console.log('File: '+input.files[0].name);
-};
+}
 reader.readAsArrayBuffer(input.files[0]);
 }
