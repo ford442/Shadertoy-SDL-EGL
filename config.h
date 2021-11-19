9a797209c2a6b1e8cc658173b730a99e87ae7978
Noah Cohn <noahc42@gmail.com>
2021-11-19 04:06:21 -0800

    Update config.h

--- config.h
+++ config.h
@@ -4,10 +4,7 @@
  * Configuration attributes for EGL context. See eglChooseConfig.
 */
 #include <getopt.h>
-
-
 static const char options_string[] = "?fw:h:s:l0:1:2:3:x:y:k:";
-
 static struct option long_options[] = {
     {"source", required_argument, 0, 's'},
     {"texture0", required_argument, 0, '0'},
@@ -16,25 +13,15 @@ static struct option long_options[] = {
     {"texture3", required_argument, 0, '3'},
     {0, 0, 0, 0}
 };
-
-/*
- * Settings for OpenGL ES 2.0 with GLSL 100 (legacy) and OpenGL ES 3.0 with GLSL 300 ES
- */
-
-
-
 // OpenGL ES 3.0 / GLSL 300 es
-
 static const char common_shader_header_gles3[] =
-    "#version 300 es\n"
-    "precision highp float;\n";
-
+    "#version 300 es \n"
+    "precision highp float; \n";
 static const char vertex_shader_body_gles3[] =
     "layout(location = 0) in vec4 iPosition;"
     "void main() {"
-    "  gl_Position=iPosition;"
-    "}\n";
-
+    "gl_Position=iPosition;"
+    "} \n";
 static const char fragment_shader_header_gles3[] =
     "uniform vec3 iResolution;"
     "uniform float iGlobalTime;" // legacy
@@ -42,26 +29,22 @@ static const char fragment_shader_header_gles3[] =
     "uniform float iChannelTime[4];"
     "uniform vec4 iMouse;"
     "uniform vec4 iDate;"
-    "uniform float iSampleRate;"
     "uniform vec3 iChannelResolution[4];"
     "uniform sampler2D iChannel0;"
     "uniform sampler2D iChannel1;"
     "uniform sampler2D iChannel2;"
     "uniform sampler2D iChannel3;"
-    "out vec4 fragColor;\n";
-
+    "out vec4 fragColor; \n";
 static const char fragment_shader_footer_gles3[] =
-    "\nvoid main(){mainImage(fragColor, gl_FragCoord.xy);}";
-
+    "\n void main(){mainImage(fragColor, gl_FragCoord.xy);} \n";
 /*
  * Standard ShaderToy Shader
  * https://www.shadertoy.com/new#
 */
-
-static char *default_fragment_shader =
-    "void mainImage( out vec4 fragColor, in vec2 fragCoord )"
+static char *default_fragment_shader[] =
+    "\n void mainImage( out vec4 fragColor, in vec2 fragCoord )"
     "{"
-    "    vec2 uv = fragCoord/iResolution.xy;"
-    "    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));"
-    "    fragColor = vec4(col,1.0);"
-    "}";
+    "vec2 uv = fragCoord/iResolution.xy;"
+    "vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));"
+    "fragColor = vec4(col,1.0);"
+    "} \n";
