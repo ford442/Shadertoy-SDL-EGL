af4635c5adf42b0b38f6e7544583c4119c9302c6
Noah Cohn <noahc42@gmail.com>
2021-11-19 04:26:26 -0800

    Update main4.cpp

--- main4.cpp
+++ main4.cpp
@@ -10,7 +10,52 @@
 #include <emscripten/html5.h>
 #include "SDL2/SDL_config.h"
 #include <SDL2/SDL.h>
-#include "config.h"
+#include <getopt.h>
+
+static const char options_string[] = "?fw:h:s:l0:1:2:3:x:y:k:";
+static struct option long_options[] = {
+    {"source", required_argument, 0, 's'},
+    {"texture0", required_argument, 0, '0'},
+    {"texture1", required_argument, 0, '1'},
+    {"texture2", required_argument, 0, '2'},
+    {"texture3", required_argument, 0, '3'},
+    {0, 0, 0, 0}
+};
+// OpenGL ES 3.0 / GLSL 300 es
+static const char common_shader_header_gles3[] =
+    "#version 300 es \n"
+    "precision highp float; \n";
+static const char vertex_shader_body_gles3[] =
+    "layout(location = 0) in vec4 iPosition;"
+    "void main() {"
+    "gl_Position=iPosition;"
+    "} \n";
+static const char fragment_shader_header_gles3[] =
+    "uniform vec3 iResolution;"
+    "uniform float iGlobalTime;" // legacy
+    "uniform float iTime;"
+    "uniform float iChannelTime[4];"
+    "uniform vec4 iMouse;"
+    "uniform vec4 iDate;"
+    "uniform vec3 iChannelResolution[4];"
+    "uniform sampler2D iChannel0;"
+    "uniform sampler2D iChannel1;"
+    "uniform sampler2D iChannel2;"
+    "uniform sampler2D iChannel3;"
+    "out vec4 fragColor; \n";
+static const char fragment_shader_footer_gles3[] =
+    "\n void main(){mainImage(fragColor, gl_FragCoord.xy);} \n";
+/*
+ * Standard ShaderToy Shader
+ * https://www.shadertoy.com/new#
+*/
+static const char default_fragment_shader[] =
+    "\n void mainImage( out vec4 fragColor, in vec2 fragCoord )"
+    "{"
+    "vec2 uv = fragCoord/iResolution.xy;"
+    "vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));"
+    "fragColor = vec4(col,1.0);"
+    "} \n";
 static SDL_AudioDeviceID dev;
 static EGLDisplay display;
 static EGLContext contextegl;
@@ -102,7 +147,7 @@ const char* ssrc = "/shader1.glsl";
 static char *program_source = NULL;
 int selected_option = -1;
 int selected_index = 0;
-static char *ttxt[] = 
+static const char ttxt[] =
 "vec2 fluid(vec2 uv1){"
 "vec2 uv = uv1;"
 "float t = iTime;"
