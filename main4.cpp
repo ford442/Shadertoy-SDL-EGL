2aa723b68325593b89462d2cf91b243b37b59e69
Noah Cohn <noahc42@gmail.com>
2021-11-22 02:29:09 -0800

    Update main4.cpp

--- main4.cpp
+++ main4.cpp
@@ -116,8 +116,6 @@ glUniform1f(uniform_gtime,abstime);
 glDrawArrays(GL_TRIANGLE_STRIP,0,4);
 eglSwapBuffers(display,surface);
 }
-
-static void strt(){
 static const EGLint attribut_list[]={
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 EGL_NONE
@@ -134,15 +132,42 @@ EGL_DEPTH_SIZE,24,
 EGL_BUFFER_SIZE,32,
 EGL_NONE
 };
-char *program_source=NULL;
-char *fileloc="/shader/shader1.toy";
+static void strt(){
 static const char *default_fragment_shader=
-"NULL"
-"NULL";
-program_source=(char*)read_file_into_str(fileloc);
-default_fragment_shader=program_source;
-SDL_Log("Got Shader: %s",fileloc);
-SDL_Log("Shader: %s",read_file_into_str(fileloc));
+"vec2 fold = vec2(-0.5, -0.5);"
+"vec2 translate = vec2(1.5);"
+"float scale = 1.25;"
+"vec3 hsv(float h,float s,float v) {"
+"return mix(vec3(3.1),clamp((abs(fract(h+vec3(3.,2.,1.)/3.)*6.-3.)-1.),0.,1.),s)*v;"
+"}"
+"vec2 rotate(vec2 p, float a){"
+"return vec2(p.x*cos(a)-p.y*sin(a), p.x*sin(a)+p.y*cos(a));"
+"}"
+"void mainImage( out vec4 fragColor, in vec2 fragCoord ) {"
+"vec2 p = -1.0 + 2.0*fragCoord.xy/iResolution.xy;"
+"p.x *= iResolution.x/iResolution.y;"
+"p *= 0.182;"
+"float x = p.y;"
+"p = abs(mod(p, 4.0) - 2.0);"
+"for(int i = 28; i > 0; i--){"
+"p = abs(p - fold) + fold;"
+"p = p*scale - translate;"
+"p = rotate(p, 3.14159/(0.10+sin(iTime*0.0005+float(i)*0.5000001)*0.4999+0.5+(10./iTime)+sin(iTime)/100.));"
+"}"
+"float i = x*x + atan(p.y, p.x) + iTime*0.02;"
+"float h = floor(i*4.0)/8.0 + 1.107;"
+"h += smoothstep(-0.1, 0.8, mod(i*2.0/5.0, 1.0/4.0)*900.0)/0.010 - 0.5;"
+"fragColor=vec4(hsv(h, 1.0, smoothstep(-3.0, 3.0, length(p)*1.0)), 2);"
+"}";
+// char *program_source=NULL;
+// char *fileloc="/shader/shader1.toy";
+// static const char *default_fragment_shader=
+// "NULL"
+// "NULL";
+// program_source=(char*)read_file_into_str(fileloc);
+// default_fragment_shader=program_source;
+// SDL_Log("Got Shader: %s",fileloc);
+// SDL_Log("Shader: %s",read_file_into_str(fileloc));
 GLuint vtx,frag,vbo;
 const char *sources[4];
 // const char *log;
