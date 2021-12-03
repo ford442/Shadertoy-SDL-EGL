d69fd2af57baf56619a9f3332c9213cd049e0a10
Noah Cohn <noahc42@gmail.com>
2021-12-03 14:59:58 -0800

    Update glsl.cpp

--- glsl.cpp
+++ glsl.cpp
@@ -101,7 +101,6 @@ return shader;
 
 static void renderFrame(){
 int x, y;
-GLuint vbo,vbu;
 Uint32 buttons;
 SDL_PumpEvents();
 glClear(GL_COLOR_BUFFER_BIT);
@@ -112,15 +111,25 @@ if((buttons & SDL_BUTTON_LMASK)!=0){
 // mouseLPressed=0.0f;
 }
 float abstime=(float)(round(SDL_GetTicks()/100));
-glGenBuffers(1,&vbo);
-glBindBuffer(GL_ARRAY_BUFFER,vbo);
-glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-glGenVertexArrays(1,&vbu);
-glBindVertexArray(vbu);
-glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,0);
+
+float vertices[] = {
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+};
+
+unsigned int VBO, VAO;
+glGenVertexArrays(1, &VAO);
+glGenBuffers(1, &VBO);
+glBindVertexArray(VAO);
+glBindBuffer(GL_ARRAY_BUFFER, VBO);
+glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 glEnableVertexAttribArray(0);
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+glEnableVertexAttribArray(1);
 glUseProgram(shader_program);
-glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+glDrawArrays(GL_TRIANGLES, 0, 3);
 eglSwapBuffers(display,surface);
 }
 
