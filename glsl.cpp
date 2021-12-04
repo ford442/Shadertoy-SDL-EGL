5ccad3eccd5fb3146e13ca8fd01e277fcefb181b
Noah Cohn <noahc42@gmail.com>
2021-12-03 18:40:38 -0800

    Update glsl.cpp

--- glsl.cpp
+++ glsl.cpp
@@ -103,10 +103,13 @@ return shader;
 static GLfloat vertices[] = {
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   
+         0.1f, -0.1f, 0.1f,  1.0f, 0.0f, 1.0f,
+        -0.2f, -0.2f, 0.2f,  0.0f, 1.0f, 0.3f,
+         0.7f,  0.7f, 0.0f,  0.4f, 0.0f, 1.0f
 };
 static GLfloat ink[]={1.0f,0.0f,0.0f,1.0f};
-
+int sides=3;
 static void renderFrame(){
 GLuint VBO,VAO;
 float white;
@@ -134,7 +137,9 @@ vertices[3]=white;
 vertices[0]=vertices[0]-(white*0.01);
 vertices[1]=vertices[1]-(white*0.01);
 vertices[2]=vertices[2]-(white*0.01);
+sides=6;
 }else{
+sides=3;
 mouseLPressed=0.0f;
 vertices[7]=-0.5f;
 vertices[1]=-0.5f;
@@ -154,12 +159,12 @@ glGenBuffers(1, &VBO);
 glBindVertexArray(VAO);
 glBindBuffer(GL_ARRAY_BUFFER,VBO);
 glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
-glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+glVertexAttribPointer(0,sides,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
 glEnableVertexAttribArray(0);
-glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
+glVertexAttribPointer(1,sides,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
 glEnableVertexAttribArray(1);
 glUseProgram(shader_program);
-glDrawArrays(GL_TRIANGLES,0,3);
+glDrawArrays(GL_TRIANGLES,0,sides);
 eglSwapBuffers(display,surface);      
 }
 
