ad12fd32da123960c1291abab7887a152f1e7959
Noah Cohn <noahc42@gmail.com>
2021-12-03 21:20:35 -0800

    Update main7.cpp

--- main7.cpp
+++ main7.cpp
@@ -124,7 +124,7 @@ mouseLPressed=1.0f;
 }else{
 mouseLPressed=0.0f;
 }
-GLclampf abstime=(double)SDL_GetTicks()/1000;
+GLint64 abstime=(double)SDL_GetTicks()/1000;
 glGenBuffers(1,&vbo);
 glBindBuffer(GL_ARRAY_BUFFER,vbo);
 glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
