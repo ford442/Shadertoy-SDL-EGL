a5ff2254797d5d061f8b9c19894aa94befb0f47e
Noah Cohn <noahc42@gmail.com>
2021-12-04 23:24:47 -0800

    Update glsl.cpp

--- glsl.cpp
+++ glsl.cpp
@@ -163,7 +163,7 @@ vertices[0]=vertices[32]-white;
 vertices[3]=vertices[1]-white;
 vertices[2]=vertices[2]-white;
 for(a=0;a>(mouseX*100);a++){
-vertices[(a*6)+1]=white;
+vertices[(360/a)*6]=white;
 }
 }else{
 mouseLPressed=0.0f;
