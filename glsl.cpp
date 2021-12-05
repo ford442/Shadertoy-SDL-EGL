a2dc15d01052eda79a714b58a1c9e1b48fe9b52e
Noah Cohn <noahc42@gmail.com>
2021-12-04 22:48:36 -0800

    Update glsl.cpp

--- glsl.cpp
+++ glsl.cpp
@@ -177,13 +177,13 @@ glGenVertexArrays(1,&VAO);
 glGenBuffers(1,&VBO);
 glBindVertexArray(VAO);
 glBindBuffer(GL_ARRAY_BUFFER,VBO);
-glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
+glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
 glEnableVertexAttribArray(0);
 glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
 glEnableVertexAttribArray(1);
 glUseProgram(shader_program);
-glDrawArrays(GL_TRIANGLE_STRIP,0,10);
+glDrawArrays(GL_TRIANGLE_STRIP,0,360);
 eglSwapBuffers(display,surface);      
 }
 
