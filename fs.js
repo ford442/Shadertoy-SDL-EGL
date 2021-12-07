2a77f7d156847a29bc81e3c367cfaf4bb1786c28
Noah Cohn <noahc42@gmail.com>
2021-12-06 17:03:34 -0800

    Update fs.js

--- fs.js
+++ fs.js
@@ -22,7 +22,7 @@ ff2.onload=function(oEvent){
 var arrayBuffer2=ff2.response;
 if(arrayBuffer2){
 var fil=new Uint8ClampedArray(arrayBuffer2);
-FS.writeFile('/shader/shader2.toy',fil);
+FS.writeFile('/shader/shader1.bfa',fil);
 }}
 ff2.send(null);
 ff3.open("GET",pth3,true);
