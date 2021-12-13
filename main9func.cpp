ffee2b27476506559159004d4137254c04d5e263
Noah Cohn <noahc42@gmail.com>
2021-12-12 19:33:20 -0800

    Update main9func.cpp

--- main9func.cpp
+++ main9func.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 using namespace std::chrono;
-using std::string;
+
 struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
 steady_clock::time_point t1;
 SDL_AudioDeviceID dev;
