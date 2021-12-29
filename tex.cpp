7f375b60b46707b0a1f76520221497348dea1eca
Noah Cohn <noahc42@gmail.com>
2021-12-28 23:46:03 -0800

    Update tex.cpp

--- tex.cpp
+++ tex.cpp
@@ -160,7 +160,7 @@ mouseLPressed=0.0f;
 }
 t2=high_resolution_clock::now();
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-glDrawBuffers(s4,attt);
+glDrawBuffers(4,attt);
 duration<long double>time_spana=duration_cast<duration<long double>>(t2-t1);
 Ttime=time_spana.count();
 glUniform1f(uniform_time,Ttime);
