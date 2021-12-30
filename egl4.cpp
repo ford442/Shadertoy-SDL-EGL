2e1fb3989cd46e1d85b53e28c006a62f377bf273
Noah Cohn <noahc42@gmail.com>
2021-12-29 21:35:02 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -1,13 +1,12 @@
-// #include <cstring>
-// #include <cstdarg>
-// #include <cstdio>
-// #include <cstdlib>
+#include <cstring>
+#include <cstdarg>
+#include <cstdlib>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES3/gl3.h>
 #include <emscripten.h>
 #include <emscripten/html5.h>
- #include <iostream>
+#include <iostream>
 #include <ctime>
 
 using namespace std;
