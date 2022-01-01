bffb7a6e3dcf2f0a4e62b5b1ad6c9acd99ef2172
Noah Cohn <noahc42@gmail.com>
2021-12-31 19:26:28 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -99,7 +99,7 @@ Vertex vertices[]={{-1.0,-1.0,0.0,1.0},{-1.0,1.0,0.0,1.0},{1.0,-1.0,1.0,1.0},{1.
 GLubyte Indices[]={0,1,2,2,1,3};
 const size_t BufferSize=sizeof(vertices);
 const size_t VertexSize=sizeof(vertices[0]);
-char8_t *fileloc="/shader/shader1.toy";
+char8_t fileloc="/shader/shader1.toy";
 const char *sources[4];
 // const char *texture_files[4];
 char *result=NULL;
