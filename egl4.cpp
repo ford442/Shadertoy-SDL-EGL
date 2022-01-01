826a7389cae71e10802f16627ec83700a028d519
Noah Cohn <noahc42@gmail.com>
2021-12-31 20:00:35 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -54,7 +54,8 @@ char8_t fileloc[20]=u8"/shader/shader1.toy";
 char8_t *sources[4];
 char8_t *result=NULL;
 long length=0;
-static const char8_t *read_file(const char *filename){
+
+static char8_t *read_file(const char *filename){
 FILE *file=fopen(filename,"r");
 if(file){
 int status=fseek(file,0,SEEK_END);
