76e43598b424dc4385999475ed9dff2b1b34cd75
Noah Cohn <noahc42@gmail.com>
2021-12-31 21:01:48 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -200,7 +200,7 @@ glReleaseShaderCompiler();
 
 static void strt(){
 S=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
-glUseProgram(shader_program);
+glUseProgram(&shader_program);
 glGenBuffers(v1,&EBO);
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(Indices),Indices,GL_STATIC_DRAW);
