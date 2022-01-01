f30ff8311fb82376996492007c76ce9275d40f6e
Noah Cohn <noahc42@gmail.com>
2021-12-31 22:52:07 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -147,8 +147,8 @@ nanosleep(&req,&rem);
 
 static void gets(){
 EM_ASM({
-document.getElementById("canvas").height=parseInt(document.getElementById('pmhig').innerHTML,10)*4;
-document.getElementById("canvas").width=parseInt(document.getElementById('pmhig').innerHTML,10)*4;
+document.getElementById("canvas").height=parseInt(document.getElementById('pmhig').innerHTML,10)*2;
+document.getElementById("canvas").width=parseInt(document.getElementById('pmhig').innerHTML,10)*2;
 });
 }
 
@@ -193,7 +193,7 @@ glReleaseShaderCompiler();
 }
 
 static void strt(){
-S=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10)*32;});
+S=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10)*2;});
 glUseProgram(shader_program);
 glGenBuffers(v1,&EBO);
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
