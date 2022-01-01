c2bb55af475584979c75cc40a27ed6f8251a5c96
Noah Cohn <noahc42@gmail.com>
2021-12-31 19:21:04 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -16,6 +16,8 @@
 using namespace std;
 using namespace std::chrono;
 
+char8_t test;
+
 high_resolution_clock::time_point t1,t2;
 GLuint EBO,FBO,tex2d[4],shader_program,shader,frame,attrib_position,sampler_channel[4];
 GLuint VBO,VAO,vtx,frag,uniform_frame,uniform_time,uniform_res,uniform_mouse;
