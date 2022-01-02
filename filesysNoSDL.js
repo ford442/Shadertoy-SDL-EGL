1f480528687b24841bb3ddd4b5734c3f3b5f8ad8
Noah Cohn <noahc42@gmail.com>
2022-01-01 21:34:43 -0800

    Update filesysNoSDL.js

--- filesysNoSDL.js
+++ filesysNoSDL.js
@@ -1,10 +1,10 @@
 let bz=new BroadcastChannel('bez');
 document.getElementById('btn').addEventListener('click',function(){
-let hi=window.innerHeight;
-let wi=window.innerWidth;
-document.getElementById('pmhig').innerHTML=parseInt(window.innerHeight,10);
+let hi=Math.round(window.innerHeight);
+let wi=Math.round(window.innerWidth);
+document.getElementById('pmhig').innerHTML=hi;
 document.getElementById("ihig").innerHTML=hi;
-document.getElementById("iwid").innerHTML=hi;
+document.getElementById("iwid").innerHTML=wi;
 document.getElementById("circle").width=wi;
 document.getElementById("circle").height=hi;
 document.getElementById("di").click();
@@ -13,11 +13,10 @@ data:222
 });});
 
 document.getElementById('btn2').addEventListener('click',function(){
-document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
-document.getElementById("canvas").height=window.innerHeight;
-document.getElementById("canvas").width=window.innerHeight;
+// document.getElementById("canvas").height=window.innerHeight;
+// document.getElementById("canvas").width=window.innerHeight;
 document.getElementById('btn3').style.border="5px solid green";
 document.getElementById('btn2').style.border="5px solid red";
 document.getElementById('btn2').style.background="red";
@@ -34,7 +33,7 @@ document.getElementById('btn7').style="background-color:red;position:absolute;di
 document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;"
 });
 document.getElementById('btn6').addEventListener('click',function(){
-let midl=Math.round((window.innerWidth*0.5)-(window.innerHeight*0.5));
+let midl=(window.innerWidth*0.5)-(window.innerHeight*0.5);
 document.getElementById("contain2").style="pointer-events:none;z-index:999993;height:"+window.innerHeight+"px;width:"+window.innerHeight+"px;position:absolute;top:0px;left:"+midl+"px;";
 document.getElementById("canvas").style="position:absolute;left:0;top:0;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:pixelated;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
 document.getElementById('ihig').innerHTML=Math.round(window.innerHeight);
