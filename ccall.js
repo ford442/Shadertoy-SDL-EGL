822fb97887d9c415b1e021307abbc692adcc408d
Noah Cohn <noahc42@gmail.com>
2022-01-02 00:46:34 -0800

    Update ccall.js

--- ccall.js
+++ ccall.js
@@ -8,15 +8,15 @@ document.getElementById("canvas").style="height100vh;width:100vh;image-rendering
 });
 
 document.getElementById('btn7').addEventListener('click',function(){
-document.getElementById("canvas").height=2160;
-document.getElementById("canvas").width=2160;
+document.getElementById("canvas").height=4320;
+document.getElementById("canvas").width=4320;
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn6').style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
-document.getElementById('pmhig').innerHTML=2160;
+document.getElementById('pmhig').innerHTML=4320;
 Module.ccall("str");
 });
 
