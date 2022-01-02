e59ec0f5249d5b503c813229757d548027ea398d
Noah Cohn <noahc42@gmail.com>
2022-01-01 23:59:45 -0800

    Update ccall.js

--- ccall.js
+++ ccall.js
@@ -4,12 +4,8 @@ Module.ccall('str');
 });
 
 document.getElementById('btn7').addEventListener('click',function(){
-document.getElementById("contain2").height=1440;
-document.getElementById("contain2").width=1440;
 document.getElementById("canvas").height=1440;
 document.getElementById("canvas").width=1440;
-document.getElementById("wrap").height=1440;
-document.getElementById("wrap").width=1440;
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
