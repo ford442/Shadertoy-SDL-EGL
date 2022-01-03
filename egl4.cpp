6aca72dd99b2ca97d6a6ce035b120168cf2e1ac8
Noah Cohn <noahc42@gmail.com>
2022-01-03 00:18:26 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -164,7 +164,7 @@ attr.majorVersion=2;
 attr.minorVersion=0;
 ctx=emscripten_webgl_create_context("#canvas",&attr);
 display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
-eglInitialize(display,3,0);
+eglInitialize(display,&v3,&v0);
 eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size);
 eglBindAPI(EGL_OPENGL_ES_API);
 contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
