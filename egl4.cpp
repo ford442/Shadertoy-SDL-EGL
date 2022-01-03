fbf5ea8ddb54fa94ab424dff40fbee1c0c414c93
Noah Cohn <noahc42@gmail.com>
2022-01-02 23:18:44 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -60,6 +60,8 @@ const char vertex_shader_body_gles3[]=
 const char fragment_shader_header_gles3[]=
 "uniform vec3 iResolution;"
 "uniform float iTime;"
+"uniform float iTimeDelta;"
+"uniform float iFrameRate;"
 "uniform vec4 iMouse;"
 "uniform sampler2D iChannel0;"
 "uniform sampler2D iChannel1;"
@@ -114,11 +116,11 @@ duration<long double>time_spanb=duration_cast<duration<long double>>(t2-t3);
 duration<long double>time_spana=duration_cast<duration<long double>>(t2-t1);
 Ttime=time_spana.count();
 Dtime=time_spanb.count();
-// fps=1.0f/Dtime;
+fps=1.0f/Dtime;
 glUniform1f(uniform_time,Ttime);
 glUniform1i(uniform_frame,frame);
-// glUniform1f(uniform_dtime,Dtime);
-// glUniform1f(uniform_fps,fps);
+glUniform1f(uniform_dtime,Dtime);
+glUniform1f(uniform_fps,fps);
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 glDrawElements(GL_TRIANGLES,v6,GL_UNSIGNED_BYTE,Indices);
 eglSwapBuffers(display,surface);
@@ -202,8 +204,8 @@ glBindVertexArray(VAO);
 glVertexAttribPointer(attribute_position,v4,GL_FLOAT,GL_TRUE,VertexSize,0);
 glEnableVertexAttribArray(attribute_position);
  
-// glVertexAttribPointer(shader_color,v4,GL_UNSIGNED_BYTE,GL_TRUE,sizeof(float)*7,&v0);
-// glEnableVertexAttribArray(shader_color);
+glVertexAttribPointer(shader_color,v4,GL_UNSIGNED_BYTE,GL_TRUE,sizeof(float)*7,0);
+glEnableVertexAttribArray(shader_color);
 
 /*
 glGenTextures(v4,tex2d);
@@ -254,12 +256,12 @@ sampler_channel[1]=glGetUniformLocation(shader_program,"iChannel1");
 sampler_channel[2]=glGetUniformLocation(shader_program,"iChannel2");
 sampler_channel[3]=glGetUniformLocation(shader_program,"iChannel3");
 uniform_time=glGetUniformLocation(shader_program,"iTime");
-// uniform_dtime=glGetUniformLocation(shader_program,"iTimeDelta");
+uniform_dtime=glGetUniformLocation(shader_program,"iTimeDelta");
 uniform_frame=glGetUniformLocation(shader_program,"iFrame");
 uniform_res=glGetUniformLocation(shader_program,"iResolution");
 uniform_mouse=glGetUniformLocation(shader_program,"iMouse");
-// uniform_fps=glGetUniformLocation(shader_program,"iFrameRate");
-// shader_color=glGetUniformLocation(shader_program,"fragColor");
+uniform_fps=glGetUniformLocation(shader_program,"iFrameRate");
+shader_color=glGetUniformLocation(shader_program,"fragColor");
 glUniform3f(uniform_res,F,F,F);
 /*
 glUniform1i(sampler_channel[0],v0);
