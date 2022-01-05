7253ef775d21727eb5eb95b75b3d7d7ad4402046
Noah Cohn <noahc42@gmail.com>
2022-01-05 05:18:27 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -264,8 +264,8 @@ glDisable(GL_DITHER);
 glEnable(GL_SCISSOR_TEST);
 glScissor(v0,v0,S,S);
 glDisable(GL_SCISSOR_TEST);
-// glClearColor(F0,F0,F0,F);
-// glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+glClearColor(0.0,0.0,0.0,1.0);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 t1=high_resolution_clock::now();
 emscripten_set_main_loop((void(*)())renderFrame,0,0);
 }
