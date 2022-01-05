b1419592e7c9bbb5b99697e56155cddc71fe2123
Noah Cohn <noahc42@gmail.com>
2022-01-05 04:03:06 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -259,7 +259,7 @@ glUniform1i(sampler_channel[1],v0);
 glUniform1i(sampler_channel[2],v0);
 glUniform1i(sampler_channel[3],v0);
 */
-glViewport(v0,v0,S,S);
+// glViewport(v0,v0,S,S);
 glDisable(GL_BLEND);
 // glEnable(GL_CULL_FACE);
 // glFrontFace(GL_CW);
