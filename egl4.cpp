d65243b9223731e463dc624cfa6b3838ef1d7aaf
Noah Cohn <noahc42@gmail.com>
2022-01-07 01:57:18 -0800

    Update egl4.cpp

--- egl4.cpp
+++ egl4.cpp
@@ -50,9 +50,9 @@ const char common_shader_header_gles3[]=
 // "precision highp sampler2D;"
 // "precision highp int;\n";
 const char vertex_shader_body_gles3[]=
-"layout(location=0)in highp vec4 iPosition;void main(){gl_Position=iPosition;} \n";
+"layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;} \n";
 const char fragment_shader_header_gles3[]=
-"uniform highp vec3 iResolution;uniform float iTime;uniform vec4 iMouse;uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;out highp vec4 fragColor;\n";
+"uniform vec3 iResolution;uniform float iTime;uniform vec4 iMouse;uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;out highp vec4 fragColor;\n";
 const char fragment_shader_footer_gles3[]=
 "\n void main(){mainImage(fragColor,gl_FragCoord.xy);} \n";
 const char* common_shader_header=common_shader_header_gles3;
