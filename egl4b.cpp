5c7049c969e819382425bb388e935f206ca80c00
Noah Cohn <noahc42@gmail.com>
2022-01-09 15:42:28 -0800

    Update egl4b.cpp

--- egl4b.cpp
+++ egl4b.cpp
@@ -41,7 +41,7 @@ static GLfloat Fm2=-2.0f;
 GLfloat fps;
 typedef struct{GLfloat XYZW[4];}Vertex;
 Vertex vertices[]={{Fm1,Fm1,Fm1,Fm1},{F,Fm1,Fm1,Fm1},{F,F,Fm1,Fm1},{Fm1,F,Fm1,Fm1},{Fm1,Fm1,Fm1,F},{F,Fm1,Fm1,F},{F,F,Fm1,F},{Fm1,F,Fm1,F}};
-GLubyte Indices[]={4,1,2,2,3,4,5,1,4,4,8,5,2,6,7,7,3,2,5,8,7,7,6,4,1,5,2,5,6};
+GLubyte Indices[]={3,0,1,1,2,3,4,0,3,3,7,4,1,5,6,6,2,1,4,7,6,6,5,3,0,4,1,4,5};
 static const char *fileloc="/shader/shader1.toy";
 const char *sources[4];
 char8_t *result=NULL;
