c9118463d8bd479b09308f6210e93a1773bfead1
Noah Cohn <noahc42@gmail.com>
2022-01-10 10:52:15 -0800

    Update egl4b.cpp

--- egl4b.cpp
+++ egl4b.cpp
@@ -111,7 +111,7 @@ frame++;
 static void strt(){
 S=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
 static const EGLint attribut_list[]={ EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
-,EGL_NONE};
+EGL_NONE};
 static const EGLint anEglCtxAttribs2[]={
 EGL_CONTEXT_CLIENT_VERSION,v3,
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FIXED_EXT,
