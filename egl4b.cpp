70e9059be7083ead95e1e81c38295d9b890e7bec
Noah Cohn <noahc42@gmail.com>
2022-02-03 21:38:12 -0800

    Update egl4b.cpp

--- egl4b.cpp
+++ egl4b.cpp
@@ -184,9 +184,7 @@ glUseProgram(shader_program);
 glDeleteShader(vtx);
 glDeleteShader(frag);
 glReleaseShaderCompiler();
-attrib_position=glGetAttribLocation(shader_program,"iPosition");
-glEnableVertexAttribArray(attrib_position);
-glVertexAttribPointer(attrib_position,v4,GL_FLOAT,GL_TRUE,0,(void*)0);
+
 glGenBuffers(v1,&EBO);
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(Indices),Indices,GL_STATIC_DRAW);
@@ -195,6 +193,10 @@ glBindVertexArray(VCO);
 glGenBuffers(v1,&VBO);
 glBindBuffer(GL_ARRAY_BUFFER,VBO);
 glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+attrib_position=glGetAttribLocation(shader_program,"iPosition");
+glEnableVertexAttribArray(attrib_position);
+glVertexAttribPointer(attrib_position,v4,GL_FLOAT,GL_TRUE,0,(void*)0);
 
 /*
 glGenBuffers(1,&CBO);
