c93f54b6a82b9a0c68b5e017544ac51537d09433
Noah Cohn <noahc42@gmail.com>
2022-02-16 03:39:02 -0800

    Update filesys.js

--- filesys.js
+++ filesys.js
@@ -53,7 +53,7 @@ document.getElementById('btn').style="background-color:red;position:absolute;dis
 document.getElementById('btn6').addEventListener('click',function(){
 document.getElementById("drop").style="background-color:rgba(0,0,0,1);z-index:999992;height:100vh;width:auto;position:absolute;text-align:center;";
 let midl=Math.round((window.innerWidth*0.5)-(window.innerHeight*0.5));
-document.getElementById("contain2").style="pointer-events:none;z-index:999993;height:"+window.innerHeight+"px;width:"+window.innerHeight+"px;position:absolute;top:0px;left:"+midl+"px;";
+document.getElementById("contain2").style="pointer-events:none;height:"+window.innerHeight+"px;width:"+window.innerHeight+"px;position:absolute;top:0px;left:"+midl+"px;";
 document.getElementById("canvas").style="position:absolute;left:0;top:0;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:auto;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
 document.getElementById('ihig').innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
