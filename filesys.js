3325736980091a7806f92c71c39e419a3837dd9e
Noah Cohn <noahc42@gmail.com>
2022-02-16 19:04:36 -0800

    Update filesys.js

--- filesys.js
+++ filesys.js
@@ -26,9 +26,11 @@ document.getElementById("circle").width=window.innerWidth;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("contain2").width=window.innerHeight;
 document.getElementById("contain2").height=window.innerHeight;
+
 document.getElementById('btn3').addEventListener('click',function(){
 window.open('./flac/');
 });
+
 document.getElementById('btn2').addEventListener('click',function(){
 document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
@@ -44,6 +46,7 @@ document.getElementById('btn').style.border="5px solid green";
 document.getElementById('btn7').style.border="5px solid green";
 document.getElementById('di').click();
 });
+
 document.getElementById('btn').addEventListener('click',function(){
 document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
@@ -52,21 +55,24 @@ document.getElementById('btn6').style="background-color:yellow;position:absolute
 document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;"
 });
+
 document.getElementById('btn6').addEventListener('click',function(){
-document.getElementById("drop").style="background-color:rgba(0,0,0,1);z-index:999992;height:100vh;width:auto;position:absolute;text-align:center;";
+document.getElementById("bcanvas").style="background-color:rgba(1,1,1,1);z-index:999992;height:100vh;width:100vh;position:absolute;top:0;text-align:center;";
 let midl=Math.round((window.innerWidth*0.5)-(window.innerHeight*0.5));
 document.getElementById("contain2").style="pointer-events:none;height:"+window.innerHeight+"px;width:"+window.innerHeight+"px;position:absolute;top:0px;left:"+midl+"px;";
-document.getElementById("scanvas").style="position:absolute;left:0;top:0;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:auto;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
+document.getElementById("scanvas").style="position:absolute;top:0;background-color:rgba(1,1,1,0);z-index:999993;image-rendering:auto;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
 document.getElementById('ihig').innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
 document.getElementById('di').click();
 });
+
 document.getElementById('btn7').addEventListener('click',function(){
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
-document.getElementById("drop").style="background-color:rgba(0,0,0,1);z-index:999994;height:100vh;width:auto;position:absolute;text-align:center;";
+document.getElementById("bcanvas").style="background-color:rgba(1,1,1,1);z-index:999994;height:100vh;width:auto;position:absolute;text-align:center;";
+document.getElementById("scanvas").style="background-color:rgba(1,1,1,0);z-index:999994;height:100vh;width:auto;position:absolute;text-align:center;";
 document.getElementById('btn6').style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
