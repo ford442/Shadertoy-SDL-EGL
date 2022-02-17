e3fbc5674227df687716b3c5a45cfc7f2591e9c2
Noah Cohn <noahc42@gmail.com>
2022-02-17 04:23:12 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -398,20 +398,21 @@ EM_JS(void,ma,(),{
 const canvas=document.getElementById("bcanvas");
 const contx=canvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:true,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:true});
 const v=document.getElementById("mv");
+const alph=document.getElementById("alf").innerHTYML;
 let d=S();if(d)d();d=S();function S(){
-let w$=document.getElementById('iwid').innerHTML;
+let w$=doalfcument.getElementById('iwid').innerHTML;
 let h$=document.getElementById('ihig').innerHTML;
 const g=new GPU({canvas:canvas,webGl:contx});
 let Rn=document.getElementById("frate").innerHTML;
 let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);
 let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];
-const t=g.createKernel(function(v,alf){const P=v[this.thread.y][this.thread.x];
-return[P[0],P[1],P[2],0.7];}).setTactic("speed").setPipeline(true).setOutput(o);
-const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];
-this.color(p[0],p[1],p[2],0.7);}).setTactic("speed").setGraphical(true).setOutput(o);
+const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];
+return[P[0],P[1],P[2]];}).setTactic("speed").setPipeline(true).setOutput(o);
+const r=g.createKernel(function(f,alf){const p=f[this.thread.y][this.thread.x];
+this.color(p[0],p[1],p[2],alf);}).setTactic("speed").setGraphical(true).setOutput(o);
 let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));
-$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);
-function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);
+$.set(t(v),0);r(t($),alph);$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);
+function M(){if(T){return;}r(t($),alph);$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);
 let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();
 document.getElementById("di").onclick=function(){
 T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}
