edf4c43f52dd7ab7052a1cfaedf5a47f55a53c02
Noah Cohn <noahc42@gmail.com>
2022-02-22 01:48:15 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -394,7 +394,9 @@ wave.spec.callback=bfr;
 opn_aud();
 }
 
-EM_JS(void,ma,(),{
+void ma(){
+EM_ASM({
+// EM_JS(void,ma,(),{
 const bcanvas=document.getElementById("bcanvas");
 const contx=bcanvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:false});
 const v=document.getElementById("mv");
@@ -406,17 +408,23 @@ let Rn=document.getElementById("frate").innerHTML;
 let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);
 let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];
 const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];
-return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o);
+return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
 const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];
-this.color(p[0],p[1],p[2],(1.0-((p[0]+p[1]+p[2])/6)));}).setTactic("precision").setGraphical(true).setOutput(o);
+this.color(p[0],p[1],p[2],(1.0-((p[0]+p[1]+p[2])/6)));}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput(o);
 let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));
 $.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);
-function M(){if(T){return;}r(t($));$.set(t(v),0); // dupctx.drawImage(bcanvas,0,0);dupEctx.drawImage(scanvas,0,0);
+function M(){if(T){return;}r(t($));$.set(t(v),0);
 let mq=((ms*f)/R);let k=Math.floor(mq);
 let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();
 document.getElementById("di").onclick=function(){
-T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}
+let w$=document.getElementById('iwid').innerHTML;
+let h$=document.getElementById('ihig').innerHTML;
+o=[w$,h$];
+t.setOutput(o);
+r.setOutput(o);
+T=true;S();};return()=>{T=true;};}
 });
+}
 
 extern "C" {
 void str(){
