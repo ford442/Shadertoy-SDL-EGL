ac8882423c05f6850f62c84ac9fa3458202bdf2b
Noah Cohn <noahc42@gmail.com>
2022-02-22 03:20:37 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -395,11 +395,9 @@ opn_aud();
 }
 
 EM_JS(void,ma,(),{
-  
 var w$=document.getElementById('iwid').innerHTML;
 var h$=document.getElementById('ihig').innerHTML;
 var o=[w$,h$];
-
 const bcanvas=document.getElementById("bcanvas");
 const contx=bcanvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:false});
 const v=document.getElementById("mv");
@@ -408,16 +406,11 @@ const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];
 return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
 const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];
 this.color(p[0],p[1],p[2],(1.0-((p[0]+p[1]+p[2])/3)));}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput(o);
-
-
 let d=S();if(d)d();d=S();function S(){
-  
 var w$=document.getElementById('iwid').innerHTML;
 var h$=document.getElementById('ihig').innerHTML;
 let Rn=document.getElementById("frate").innerHTML;
-let l=(w$*h$*4);
-let m=((l/65536)+1);
-m=Math.floor(m);
+let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);
 let W=new WebAssembly.Memory({initial:m});
 let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));
 $.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);
@@ -425,14 +418,9 @@ function M(){if(T){return;}r(t($));$.set(t(v),0);
 let mq=((ms*f)/R);let k=Math.floor(mq);
 let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();
 document.getElementById("di").onclick=function(){ 
-  T=true;
-
 var w$=document.getElementById('iwid').innerHTML;
 var h$=document.getElementById('ihig').innerHTML;
-var o=[w$,h$];
-t.setOutput(o);
-r.setOutput(o);
-
+var o=[w$,h$];t.setOutput(o);r.setOutput(o);T=true;
 S();};return()=>{T=true;};}
 });
 
