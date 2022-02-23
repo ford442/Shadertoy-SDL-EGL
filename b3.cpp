024efbc0e6e7dac3159e18947b59b25adbb2e869
Noah Cohn <noahc42@gmail.com>
2022-02-23 12:08:48 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -401,13 +401,8 @@ var o=[w$,h$];
 const bcanvas=document.getElementById("bcanvas");
 const contx=bcanvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:false});
 const v=document.getElementById("mv");
-  
-function ave(a,b,c){
-const va=1.0-((((a+b+c)/3)-0.7542)*4);
-return va;
-}  
                        
-const g=new GPU({canvas:bcanvas,webGl:contx,functions:[ave],nativeFunctions:[{name:'aver',source:`float aver(float a,float b,float c){return 1.0-((((a+b+c)/3)-0.7542)*4);});
+const g=new GPU({canvas:bcanvas,webGl:contx,nativeFunctions:[{name:'aver',source:`float aver(float a,float b,float c){return 1.0-((((a+b+c)/3)-0.7542)*4);});
 var t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2],aver(P[0],P[1],P[2])];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
 var r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2],p[3]);}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput(o);
 let d=S();if(d)d();d=S();function S(){
