8939e8c6e30f8e69a8e2c95f7e3fa079d21b0ea2
Noah Cohn <noahc42@gmail.com>
2022-02-25 10:26:46 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -402,19 +402,20 @@ let h$=document.getElementById('ihig').innerHTML;
 let o=[w$,h$];
 const bcanvas=document.getElementById("bcanvas");
 const contx=bcanvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:false});
-var v=document.getElementById("mv");
+const v=document.getElementById("mv");
 const g=new GPU({canvas:bcanvas,webGl:contx});
 var t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x];
-const aveg=1.0-((((P[0]+P[1]+P[2])/3)-0.75)*(((P[0]+P[1]+P[2])/3)-0.75)*4.0);return[P[0],P[1],P[2],(aveg)];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
+const aveg=1.0-(Math.sqrt(((P[0]+P[1]+P[2])/3)-0.75)*4.0);return[P[0],P[1],P[2],(aveg)];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
 var r=g.createKernel(function(f){
 const p=f[this.thread.y][this.thread.x];
 this.color(p[0],p[1],p[2],p[3]);}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput(o);
-
-
-  
 let d=S();if(d)d();d=S();function S(){
-  let l=(1920*1080*4);let m=((l/65536)+1);m=Math.floor(m);
+$w=document.getElementById('iwid').innerHTML;
+o=[$w,h$];
+t.setOutput(o);
+r.setOutput(o);
+let l=($w*$h*4);let m=((l/65536)+1);m=Math.floor(m);
 let W1=new WebAssembly.Memory({initial:m});
 let W2=new WebAssembly.Memory({initial:m});
 let W3=new WebAssembly.Memory({initial:m});
@@ -431,12 +432,10 @@ let $5=new Uint8ClampedArray(W5.buffer,0,l);
 let $6=new Uint8ClampedArray(W6.buffer,0,l);
 let $7=new Uint8ClampedArray(W7.buffer,0,l);
 let $8=new Uint8ClampedArray(W8.buffer,0,l);
-$w=document.getElementById('iwid').innerHTML;
-
-v=document.getElementById("mv");
-o=[$w,h$];
-t.setOutput(o);
 let T=false;
+$8.set(t(v),0);
+$7.set(t(v),0);
+$6.set(t(v),0);
 r(t($8));
 $1.set(t(v),0);
 r(t($7));
@@ -444,7 +443,6 @@ $2.set(t(v),0);
 r(t($6));
 $3.set(t(v),0);
 let $F=1;
-r.setOutput(o);
 function M(){
 if(T)
 {return;
@@ -491,11 +489,6 @@ $F=2;
 setTimeout(function(){M();},16.666);}
 M();
 document.getElementById("di").onclick=function(){
-
-$8.set(t(v),0);
-$7.set(t(v),0);
-$6.set(t(v),0);
-
 T=true;
 S();};return()=>{T=true;};}
 });
