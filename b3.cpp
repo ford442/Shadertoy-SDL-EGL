212c2afb06d6fb0c1a6c059747147766510056bc
Noah Cohn <noahc42@gmail.com>
2022-02-26 09:11:34 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -412,7 +412,7 @@ var t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x+this.constants.blnk-this.constants.nblnk];
 let aveg=1.0-((((P[0]+P[1]+P[2])/3)-0.75)*(((P[0]+P[1]+P[2])/3)*4.0));return[P[0],P[1],P[2],(aveg)];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setConstants({blnk:blank$,nblnk:nblank$}).setOutput(o);
 var r=g.createKernel(function(f){
-const p=f[this.thread.y][this.thread.x-this.constants.rblnk];
+const p=f[this.thread.y][this.thread.x+this.constants.rblnk];
 this.color(p[0],p[1],p[2],p[3]);}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setConstants({rblnk:rblank$}).setOutput(o);
 let d=S();if(d)d();d=S();function S(){
 $w=document.getElementById('iwid').innerHTML;
