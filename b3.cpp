2985dd2959baf0538ef9922a7fb78d32399b5365
Noah Cohn <noahc42@gmail.com>
2022-02-26 05:32:19 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -399,13 +399,16 @@ opn_aud();
 EM_JS(void,ma,(),{
 let w$=document.getElementById('iwid').innerHTML;
 let h$=document.getElementById('ihig').innerHTML;
-let o=[w$,h$];
+let io=[w$,h$];
+let ro=[h$,h$];
+let blank=(h$-w$)*0.5;
+let rblank=h$-w$;
 const bcanvas=document.getElementById("bcanvas");
 const contx=bcanvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,minorVersion:0,desynchronized:false});
 let v=document.getElementById("mv");
 const g=new GPU({canvas:bcanvas,webGl:contx});
 var t=g.createKernel(function(v){
-const P=v[this.thread.y][this.thread.x];
+const P=v[this.thread.y][this.thread.x+blank];
 let aveg=1.0-((((P[0]+P[1]+P[2])/3)-0.75)*(((P[0]+P[1]+P[2])/3)*4.0));return[P[0],P[1],P[2],(aveg)];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput(o);
 var r=g.createKernel(function(f){
 const p=f[this.thread.y][this.thread.x];
