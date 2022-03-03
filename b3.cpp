15888df89b142d650aa427b258a7670011904e77
Noah Cohn <noahc42@gmail.com>
2022-03-02 22:35:19 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -428,7 +428,7 @@ var t=g.createKernel(function(v){
 var P=v[this.thread.y][this.thread.x+this.constants.blnk];
 let aveg=((P[0]+P[1]+P[2])/3);
 if(aveg>this.constants.max){setMin(aveg)};if(aveg<this.constants.min){setMax(aveg)};
-return[P[0],P[1],P[2],aveg];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setConstants({blnk:blank$,min:min$,max:max$}).setOutput(o);
+return[P[0],P[1],P[2],aveg];}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setConstants({blnk:blank$,min:min$,max:max$}).setFunctions([setMin],[setMax]).setOutput(o);
 var r=g.createKernel(function(f){
 var p=f[this.thread.y][this.thread.x-this.constants.nblnk];
 this.color(p[0],p[1],p[2],1.0-((p[3]-(this.constants.max-this.constants.min-p[3]+this.constants.min))*(1.0/(1.0-p[3]))));}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setConstants({nblnk:nblank$,max:max$,min:min$}).setOutput(o);
