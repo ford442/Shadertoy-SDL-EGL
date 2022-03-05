24888c96097d84d42ea459e594882fe15416a4f3
Noah Cohn <noahc42@gmail.com>
2022-03-04 18:36:09 -0800

    Update b3.cpp

--- b3.cpp
+++ b3.cpp
@@ -411,15 +411,15 @@ let v=document.getElementById("mv");
 let g=new GPU({canvas:bcanvas,webGl:contx});
 let blank$=Math.max(((w$-h$)/2),0);
 let nblank$=Math.max((h$-w$),0);
-let avgs=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+var avgs=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
 function avvg(){
 avgs[0]=(avgs[1]+avgs[2]+avgs[3]+avgs[4]+avgs[5]+avgs[6]+avgs[7]+avgs[8])/8;
   console.log(avgs);
   console.log(min$);
   console.log(max$);
 }
-let min$=0.0;
-let max$=1.0;
+var min$=0.0;
+var max$=1.0;
 function setMin(a,b){
 return Math.min(a,b);
 }
@@ -489,6 +489,8 @@ $1.set(t(vv),0);
 max$=$1v[22];
 min$=$1v[22];
   console.log($1v.getUint8(22));
+  console.log($1.getUint8(22));
+  console.log(W1.getUint8(22));
 // avvg();
 $2.set(t(vv),0);
 // avgs[2]=($2.filter((_,i) => i % 4 == 3)).reduce(avgg)/(l/4);
