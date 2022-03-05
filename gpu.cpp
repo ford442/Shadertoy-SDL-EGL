088b2ef0d78696a448e4eccef560cb73f4119573
Noah Cohn <noahc42@gmail.com>
2022-03-05 03:46:56 -0800

    Update gpu.cpp

--- gpu.cpp
+++ gpu.cpp
@@ -464,7 +464,7 @@ var $1=new Uint8ClampedArray(W1.buffer,0,l);
 
 let T=false;
 let vv=document.getElementById("mv");
-$1.set([a(vv)],0);
+$1.set([A(vv)],0);
         console.log($1.buffer);
         
   
