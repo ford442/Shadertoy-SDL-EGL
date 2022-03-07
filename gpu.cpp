2f23966ab88c97111afd40d33cd69c21b0636a81
Noah Cohn <noahc42@gmail.com>
2022-03-07 15:58:53 -0800

    Update gpu.cpp

--- gpu.cpp
+++ gpu.cpp
@@ -561,10 +561,10 @@ if($F==1){
 var $r1=R($1);
 r(t($r1));
  var llc=$r1.toArray();
- var nn=new Float32Array($r1,0,1);
+ var nn=new Float32Array($r1,8,8,8);
  var ou=parseFloat(nn);
   console.log(ou);
-  var nn=new Float32Array(llc,0,1);
+  var nn=new Float32Array(llc,8,8,8);
  var ou=parseFloat(nn);
   console.log(ou);
 var $$5=R(vv);
