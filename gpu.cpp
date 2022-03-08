23ae9797eab1ca1779d34304c55e2ad8f64e1ab8
Noah Cohn <noahc42@gmail.com>
2022-03-07 20:25:39 -0800

    Update gpu.cpp

--- gpu.cpp
+++ gpu.cpp
@@ -474,7 +474,7 @@ function adds(ac,a){
 return ac+a;
 }
 function avvg(){
-var fave=new Float32Array(W1,0,la);
+var fave=new Float32Array(W1.buffer,0,la);
   avgs.set([fave[0]],0);
 min$.set([Math.min(...fave)],0);
 avg$.set([avgs.reduce(adds,0)/8]);
