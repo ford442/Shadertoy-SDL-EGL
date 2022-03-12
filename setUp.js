e8ac17ef6d0387f5e5d683682a2cdfcbbf7c89c7
Noah Cohn <noahc42@gmail.com>
2022-03-12 02:57:39 -0800

    Update setUp.js

--- setUp.js
+++ setUp.js
@@ -6,8 +6,5 @@ document.getElementById("circle").height=window.innerHeight;
 document.getElementById("wrap").style.lineheight=window.innerHeight+"px";
 document.getElementById("di").click();
 },500);
-setTimeout(function(){
-document.getElementById("btn2").click();
-},500);
 }
 
