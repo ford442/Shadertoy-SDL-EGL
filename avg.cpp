0e23cbfbdbd7341b364cce15b8b431cc639a860a
Noah Cohn <noahc42@gmail.com>
2022-03-12 16:20:04 -0800

    Update avg.cpp

--- avg.cpp
+++ avg.cpp
@@ -415,7 +415,7 @@ let g=new GPU({canvas:bcanvas,webGl:contx});
 let R=g.createKernel(function(tv){
 const P=tv[this.thread.y][this.thread.x];
 const aveg=(P[0]+P[1]+P[2])/3;
-return [0.75,0.0,0.0,aveg];
+return [0.0,0.0,0.0,aveg];
 }).setTactic("speed").setDynamicOutput(true).setArgumentTypes(['HTMLVideo']).setOutput(o);
 
 let t=g.createKernel(function(v){
