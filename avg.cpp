4735518e78cc0b144764e5f7f1ca4ad411c8dbef
Noah Cohn <noahc42@gmail.com>
2022-03-12 22:59:29 -0800

    Update avg.cpp

--- avg.cpp
+++ avg.cpp
@@ -355,7 +355,8 @@ return [P[0],P[1],P[2],avgg];
 
 let t=g.createKernel(function(v){
 var P=v[this.thread.y][this.thread.x];
-let aveg=1.0-((((P[0]+P[1]+P[2])/3)-(this.constants.avg))*(((P[0]+P[1]+P[2])/3)*(1.0/(1.0-this.constants.avg))));
+var av$=(P[0]+P[1]+P[2])/3;
+var aveg=1.0-(((av$)-(this.constants.avg))*((av$)*(1.0/(1.0-this.constants.avg))));
 return[P[0],P[1],P[2],aveg];
 }).setTactic("speed").setPipeline(true).setArgumentTypes(['HTMLVideo']).setDynamicOutput(true).setConstants({avg:avag}).setOutput(o);
 
