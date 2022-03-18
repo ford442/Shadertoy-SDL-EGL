ce816ed0beed418737cc3ad2e76057a9bde8c6fc
Noah Cohn <noahc42@gmail.com>
2022-03-17 22:08:37 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -59,7 +59,8 @@ EGLConfig eglconfig=NULL;
 EmscriptenWebGLContextAttributes attr;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 struct timespec rem;
-struct timespec req={0,16666666};
+// struct timespec req={0,16666666};
+struct timespec req={0,33100000};
 EMSCRIPTEN_RESULT ret;
 typedef struct{GLfloat XYZW[4];}Vertex;
 static Vertex vertices[]={{Fm1,Fm1,F,F},{F,Fm1,F,F},{F,F,F,F},{Fm1,F,F,F},{Fm1,Fm1,Fm1,F},{F,Fm1,Fm1,F},{F,F,Fm1,F},{Fm1,F,F,F}};
@@ -164,7 +165,7 @@ uniforms(mouseX,mouseY,Ttime,iFrame);
 emscripten_webgl_make_context_current(ctx);
 glDrawElements(GL_TRIANGLES,36,GL_UNSIGNED_BYTE,Indices);
 // glFinish();
-// nanosleep(&req,&rem);
+ nanosleep(&req,&rem);
 iFrame++;
 }
 
