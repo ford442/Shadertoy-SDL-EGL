27804f0663643a2eb91baaee1af3a23ea6274573
Noah Cohn <noahc42@gmail.com>
2022-03-19 00:50:14 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -383,11 +383,11 @@ let r=g.createKernel(function(f){
 var p=f[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
 var alph=((((this.constants.fmax-this.constants.fmin)*0.75)+this.constants.fmin)+(((this.constants.amax-this.constants.amin)*0.75)+this.constants.amin)+(((1.0-(this.constants.fmin/2))*0.75))+((1.0-(this.constants.amax/2)*0.75))+((0.75-(0.75*(this.constants.favg-p[3])/(this.constants.amax-this.constants.aavg))))+((this.constants.aavg+0.75)/2))/6;
 var aveg=1.0-(((p[3])-(alph))*((p[3])*(1.0/(1.0-alph))));
-
-var red=p[0]+(((0.1-p[3])/(0.1-p[3]))*(this.constants.aavg-p[0]));
-var green=p[1]+(((0.1-p[3])/(0.1-p[3]))*(this.constants.aavg-p[1]));
-var blue=p[2]+(((0.1-p[3])/(0.1-p[3]))*(this.constants.aavg-p[2]));
-
+if(p[3]<0.12){
+p[0]=this.constants.aavg;
+p[1]=this.constants.aavg;
+p[2]=this.constants.aavg;
+}
 this.color(red,green,blue,aveg);
 }).setTactic("speed").setGraphical(true).setArgumentTypes(['HTMLVideo']).setDynamicOutput(true).setOutput([w$,h$]);
 
