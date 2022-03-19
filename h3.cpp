d34a686aaf7fe5e79e101ce1e5e5c26d1c84f3b7
Noah Cohn <noahc42@gmail.com>
2022-03-19 07:21:46 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -54,6 +54,7 @@ EGLSurface surface;
 EGLContext contextegl;
 GLsizei i;
 GLfloat fps;
+float timeSpeed=0.5;
 EGLint config_size,major,minor,attrib_position;
 EGLConfig eglconfig=NULL;
 EmscriptenWebGLContextAttributes attr;
@@ -145,6 +146,7 @@ glUniform4f(uniform_mouse,(Size*xx),(Size*yy),mX,mY);
 }else{
 clickLoc=true;
 }
+time=time*timeSpeed;
 glUniform1f(uniform_time,time);
 glUniform1i(uniform_frame,fram);
 }
@@ -185,6 +187,11 @@ aLoc[F+20]=max;
 aLoc[0]=(aLoc[1]+aLoc[2]+aLoc[3]+aLoc[4]+aLoc[5]+aLoc[6]+aLoc[7]+aLoc[8])/8;
 aLoc[10]=(aLoc[11]+aLoc[12]+aLoc[13]+aLoc[14]+aLoc[15]+aLoc[16]+aLoc[17]+aLoc[18])/8;
 aLoc[20]=(aLoc[21]+aLoc[22]+aLoc[23]+aLoc[24]+aLoc[25]+aLoc[26]+aLoc[27]+aLoc[28])/8;
+if (aLoc[0]>0.5){
+timeSpeed=1.0+aLoc[0];
+}else{
+timeSpeed=1.0-aLoc[0];
+}
 }
 
 static void strt(){
