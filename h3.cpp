d9ae2bd40d893cb8323c16dfca1095160014d9a4
Noah Cohn <noahc42@gmail.com>
2022-03-19 10:20:34 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -168,8 +168,7 @@ glUniform1i(uniform_frame,fram);
 static void renderFrame(){
 eglSwapBuffers(display,surface);
 t2=high_resolution_clock::now();
-duration<double>jtime=duration_cast<duration<double>>(t2);
-Dtime=jtime.count();
+Dtime=t2.count();
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 duration<double>time_spana=duration_cast<duration<double>>(t2-t1);
 Ttime=time_spana.count();
