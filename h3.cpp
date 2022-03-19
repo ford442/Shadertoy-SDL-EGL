fdfe8314ab1ad8f56d4ebc3ad2be0035f8d9c87f
Noah Cohn <noahc42@gmail.com>
2022-03-19 07:49:05 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -155,7 +155,7 @@ eglSwapBuffers(display,surface);
 t2=high_resolution_clock::now();
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 duration<double>time_spana=duration_cast<duration<double>>(t2-t1);
-Ttime=time_spana.count()*timeSpeed;
+Ttime=time_spana.count()/timeSpeed;
 ret=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_callback);
 ret=emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_callback);
 ret=emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_callback);
