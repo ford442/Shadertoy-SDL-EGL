2e4971f3478808a4a7ac45e35ee9e1d59170b86b
Noah Cohn <noahc42@gmail.com>
2022-03-20 06:49:01 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -402,7 +402,7 @@ this.color(p[0],p[1],p[2],aveg);
 
 let rA=g.createKernel(function(fa){
 var pd=fa[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
-var avrg=p[3]/2;
+var avrg=pd[3]/2;
 var red=1.0-pd[0];
 var green=1.0-pd[1];
 var blue=1.0-pd[2];
