829ab70c9b2b455428da15903d4ea22f569e43d5
Noah Cohn <noahc42@gmail.com>
2022-03-19 18:49:27 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-MyModule =
-{
-canvas: (function() {
-var aCanvas = document.getElementById('canvas');
-  //  aGlCtx = aCanvas.getContext ('webgl2',  { alpha: true, premultipliedAlpha: false, depth: false, antialias: false, preserveDrawingBuffer: false } ); 
-return aCanvas;
-})(),
-};
-const OccViewerModuleInitialized = createModule(MyModule);
