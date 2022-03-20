e1611b58046a4acd1bd43d061f4500ebf3bb5c32
Noah Cohn <noahc42@gmail.com>
2022-03-20 11:54:26 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -19,6 +19,7 @@
 #include <unistd.h>
 #include <chrono>
 #include <SDL2/SDL.h>
+#include <numeric>
 
 using namespace std;
 using namespace std::chrono;
@@ -153,7 +154,7 @@ glUniform1i(uniform_frame,fram);
 static void renderFrame(){
 eglSwapBuffers(display,surface);
 t2=high_resolution_clock::now();
-glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 duration<double>time_spana=duration_cast<duration<double>>(t2-t1);
 Ttime=time_spana.count();
 ret=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_callback);
@@ -166,7 +167,7 @@ uniforms(mouseX,mouseY,Ttime,iFrame);
 emscripten_webgl_make_context_current(ctx);
 glDrawElements(GL_TRIANGLES,36,GL_UNSIGNED_BYTE,Indices);
  glFinish();
-// nanosleep(&req,&rem);
+ nanosleep(&req,&rem);
 iFrame++;
 }
 
@@ -181,11 +182,19 @@ if(min>dat[i]&&dat[i]>0){min=dat[i];}
 }
 sum=sum/leng;
 aLoc[F]=sum;
-aLoc[F+10]=min;
-aLoc[F+20]=max;
-aLoc[0]=(aLoc[1]+aLoc[2]+aLoc[3]+aLoc[4]+aLoc[5]+aLoc[6]+aLoc[7]+aLoc[8])/8;
-aLoc[10]=(aLoc[11]+aLoc[12]+aLoc[13]+aLoc[14]+aLoc[15]+aLoc[16]+aLoc[17]+aLoc[18])/8;
-aLoc[20]=(aLoc[21]+aLoc[22]+aLoc[23]+aLoc[24]+aLoc[25]+aLoc[26]+aLoc[27]+aLoc[28])/8;
+aLoc[F+100]=min;
+aLoc[F+200]=max;
+float avgSum=0.0;
+float minSum=0.0;
+float maxSum=0.0;
+for (int i=1;i<33;i=i){
+avgSum+=dat[i];
+minSum+=dat[i+100];
+maxSum+=dat[i+200];
+}
+aLoc[0]=avgSum/32;
+aLoc[100]=minSum/32;
+aLoc[200]=maxSum/32;
 }
 
 static void strt(){
@@ -284,14 +293,14 @@ uniform_frame=glGetUniformLocation(shader_program,"iFrame");
 uniform_res=glGetUniformLocation(shader_program,"iResolution");
 uniform_mouse=glGetUniformLocation(shader_program,"iMouse");
 glUniform2f(uniform_res,Size,Size);
- glEnable(GL_BLEND);
- glBlendFunc(GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
- glBlendFuncSeparate(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA,GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
+glEnable(GL_BLEND);
+glBlendFunc(GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
+glBlendFuncSeparate(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA,GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
 glEnable(GL_DEPTH_TEST);
 glDepthFunc(GL_LESS);
 glClearColor(F0,F0,F0,F);
 glViewport(0,0,S,S);
-glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 t1=high_resolution_clock::now();
 emscripten_set_main_loop((void(*)())renderFrame,0,0);
 }
@@ -352,7 +361,7 @@ var w$=parseInt(document.getElementById('wid').innerHTML,10);
 var h$=parseInt(document.getElementById('hig').innerHTML,10);
 vv=document.getElementById("mv");
 let $H=Module.HEAPF32.buffer;
-var agav=new Float32Array($H,82933000,30);
+var agav=new Float32Array($H,82933000,500);
 var sz=(h$*h$)/8;
 var avag=0.750;
 var min=1.0;
@@ -402,12 +411,13 @@ this.color(p[0],p[1],p[2],aveg);
 
 let rA=g.createKernel(function(fa){
 var pd=fa[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
-var avrg=(1.0-Ave(pd[0],pd[1],pd[2])-0.8)*(Ave(pd[0],pd[1],pd[2])*5.0);
+var avrg=((1.0-Ave(pd[0],pd[1],pd[2])-0.75)*(Ave(pd[0],pd[1],pd[2])*4.0))/2;
 this.color(pd[0],pd[1],pd[2],avrg);
 }).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
+let $F=1;
 
 let d=S();if(d)d();d=S();function S(){
-var agav=new Float32Array($H,82933000,30);
+var agav=new Float32Array($H,82933000,500);
 var w$=parseInt(document.getElementById('wid').innerHTML,10);
 var h$=parseInt(document.getElementById('hig').innerHTML,10);
 var vv=document.getElementById("mv");
@@ -418,106 +428,21 @@ var la=h$*h$*4;
 var al=w$*h$*8;
 var sz=(h$*h$)/8;
 R.setOutput([sz]);
-var point1=0;
-var $1=new Float32Array($H,point1,la);
-var point2=1*la;
-var $2=new Float32Array($H,point2,la);
-var point3=2*la;
-var $3=new Float32Array($H,point3,la);
-var point4=3*la;
-var $4=new Float32Array($H,point4,la);
-var point5=4*la;
-var $5=new Float32Array($H,point5,la);
-var point6=5*la;
-var $6=new Float32Array($H,point6,la);
-var point7=6*la;
-var $7=new Float32Array($H,point7,la);
-var point8=7*la;
-var $8=new Float32Array($H,point8,la);
-var point9=8*la;
-var $9=new Float32Array($H,82944000,sz);
-let $F=1;
-r.setConstants({nblnk:nblank$,blnk:blank$,favg:agav[$F],fmin:agav[$F+10],fmax:agav[$F+20],amin:agav[10],amax:agav[20],aavg:agav[0]});
-t.setConstants({nblnk:nblank$,blnk:blank$});
-rA.setConstants({nblnk:nblank$,blnk:blank$});
-var $$1=t(vv);
-$1.set($$1);
-$2.set($$1);
-$3.set($$1);
-$4.set($$1);
+
+var point1=0; var $1=new Float32Array($H,point1,la); var point2=1*la; var $2=new Float32Array($H,point2,la); var point3=2*la; var $3=new Float32Array($H,point3,la); var point4=3*la; var $4=new Float32Array($H,point4,la); var point5=4*la; var $5=new Float32Array($H,point5,la); var point6=5*la; var $6=new Float32Array($H,point6,la); var point7=6*la; var $7=new Float32Array($H,point7,la); var point8=7*la; var $8=new Float32Array($H,point8,la); var point9=8*la; var $9=new Float32Array($H,point9,la); var point10=9*la; var $10=new Float32Array($H,point10,la); var point11=10*la; var $11=new Float32Array($H,point11,la); var point12=11*la; var $12=new Float32Array($H,point12,la); var point13=12*la; var $13=new Float32Array($H,point13,la); var point14=13*la; var $14=new Float32Array($H,point14,la); var point15=14*la; var $15=new Float32Array($H,point15,la); var point16=15*la; var $16=new Float32Array($H,point16,la); var point17=16*la; var $17=new Float32Array($H,point17,la); var point18=17*la; var $18=new Float32Array($H,point18,la); var point19=18*la; var $19=new Float32Array($H,point19,la); var point20=19*la; var $20=new Float32Array($H,point20,la); var point21=20*la; var $21=new Float32Array($H,point21,la); var point22=21*la; var $22=new Float32Array($H,point22,la); var point23=22*la; var $23=new Float32Array($H,point23,la); var point24=23*la; var $24=new Float32Array($H,point24,la); var point25=24; var $25=new Float32Array($H,point25,la); var point26=25*la; var $26=new Float32Array($H,point26,la); var point27=26*la; var $27=new Float32Array($H,point27,la); var point28=27*la; var $28=new Float32Array($H,point28,la); var point29=28*la; var $29=new Float32Array($H,point29,la); var point30=29*la; var $30=new Float32Array($H,point30,la); var point31=30*la; var $31=new Float32Array($H,point31,la); var point32=31*la; var $32=new Float32Array($H,point32,la); var point9=8*la; var $B=new Float32Array($H,82944000,sz);
+
 var T=false;
 
 function M(){
 rA.setConstants({nblnk:nblank$,blnk:blank$});
 t.setConstants({nblnk:nblank$,blnk:blank$});
-r.setConstants({nblnk:nblank$,blnk:blank$,favg:agav[$F],fmin:agav[$F+10],fmax:agav[$F+20],amin:agav[10],amax:agav[20],aavg:agav[0]});
+r.setConstants({nblnk:nblank$,blnk:blank$,favg:agav[$F],fmin:agav[$F+100],fmax:agav[$F+200],amin:agav[100],amax:agav[200],aavg:agav[0]});
 if(T){return;}
-if($F==8){
-var $r8=t($8);
-rA($r8);
-r($r8);
-var $$4=t(vv);
-$4.set($$4);
-$F=1;
-}
-if($F==7){ 
-var $r7=t($7);
-rA($r7);
-r($r7);
-var $$3=t(vv);
-$3.set($$3);
-$F=8;
-}
-if($F==6){  
-var $r6=t($6);
-rA($r6);
-r($r6);
-var $$2=t(vv);
-$2.set($$2);
-$F=7;
-}
-if($F==5){  
-var $r5=t($5);
-rA($r5);
-r($r5);
-var $$1=t(vv);
-$1.set($$1);
-$F=6;
-}
-if($F==4){  
-var $r4=t($4);
-rA($r4);
-r($r4);
-var $$8=t(vv);
-$8.set($$8);
-$F=5;
-}
-if($F==3){  
-var $r3=t($3);
-rA($r3);
-r($r3);
-var $$7=t(vv);
-$7.set($$7);
-$F=4;
-}  
-if($F==2){
-var $r2=t($2);
-rA($r2);
-r($r2);
-var $$6=t(vv);
-$6.set($$6);
-$F=3;
-}
-if($F==1){
-var $r1=t($1);
-rA($r1);
-r($r1);
-var $$5=t(vv);
-$5.set($$5);
-$F=2;
-}
+
+if($F==32){ var $r32=t($32); rA($r32); r($r32); var $$16=t(vv); $16.set($$16); $F=1; } if($F==31){ var $r31=t($31); rA($r31); r($r31); var $$15=t(vv); $15.set($$15); $F=32; } if($F==30){ var $r30=t($30); rA($r30); r($r30); var $$14=t(vv); $14.set($$14); $F=31; } if($F==29){ var $r29=t($29); rA($r29); r($r29); var $$13=t(vv); $13.set($$13); $F=30; } if($F==28){ var $r28=t($28); rA($r28); r($r28); var $$12=t(vv); $12.set($$12); $F=29; } if($F==27){ var $r27=t($27); rA($r27); r($r27); var $$11=t(vv); $11.set($$11); $F=28; } if($F==26){ var $r26=t($26); rA($r26); r($r26); var $$10=t(vv); $10.set($$10); $F=27; } if($F==25){ var $r25=t($25); rA($r25); r($r25); var $$9=t(vv); $9.set($$9); $F=26; } if($F==24){ var $r24=t($24); rA($r24); r($r24); var $$8=t(vv); $8.set($$8); $F=25; } if($F==23){ var $r23=t($23); rA($r23); r($r23); var $$7=t(vv); $7.set($$7); $F=24; } if($F==22){ var $r22=t($22); rA($r22); r($r22); var $$6=t(vv); $6.set($$6); $F=23; } if($F==21){ var $r21=t($21); rA($r21); r($r21); var $$5=t(vv); $5.set($$5); $F=22; } if($F==20){ var $r20=t($20); rA($r20); r($r20); var $$4=t(vv); $4.set($$4); $F=21; } if($F==19){ var $r19=t($19); rA($r19); r($r19); var $$3=t(vv); $3.set($$3); $F=20; } if($F==18){ var $r18=t($18); rA($r18); r($r18); var $$2=t(vv); $2.set($$2); $F=19; } if($F==17){ var $r17=t($17); rA($r17); r($r17); var $$1=t(vv); $1.set($$1); $F=18; } if($F==16){ var $r16=t($16); rA($r16); r($r16); var $$32=t(vv); $32.set($$32); $F=17; } if($F==15){ var $r15=t($15); rA($r15); r($r15); var $$31=t(vv); $31.set($$31); $F=16; } if($F==14){ var $r14=t($14); rA($r14); r($r14); var $$30=t(vv); $30.set($$30); $F=15; } if($F==13){ var $r13=t($13); rA($r13); r($r13); var $$29=t(vv); $29.set($$29); $F=14; } if($F==12){ var $r12=t($12); rA($r12); r($r12); var $$28=t(vv); $28.set($$28); $F=13; } if($F==11){ var $r11=t($11); rA($r11); r($r11); var $$27=t(vv); $27.set($$27); $F=12; } if($F==10){ var $r10=t($10); rA($r10); r($r10); var $$26=t(vv); $26.set($$26); $F=11; } if($F==9){ var $r9=t($9); rA($r9); r($r9); var $$25=t(vv); $25.set($$25); $F=10; } if($F==8){ var $r8=t($8); rA($r8); r($r8); var $$24=t(vv); $24.set($$24); $F=9; } if($F==7){ var $r7=t($7); rA($r7); r($r7); var $$23=t(vv); $23.set($$23); $F=8; } if($F==6){ var $r6=t($6); rA($r6); r($r6); var $$22=t(vv); $22.set($$22); $F=7; } if($F==5){ var $r5=t($5); rA($r5); r($r5); var $$21=t(vv); $21.set($$21); $F=6; } if($F==4){ var $r4=t($4); rA($r4); r($r4); var $$20=t(vv); $20.set($$20); $F=5; } if($F==3){ var $r3=t($3); rA($r3); r($r3); var $$19=t(vv); $19.set($$19); $F=4; } if($F==2){ var $r2=t($2); rA($r2); r($r2); var $$18=t(vv); $18.set($$18); $F=3; } if($F==1){ var $r1=t($1); rA($r1); r($r1); var $$17=t(vv); $17.set($$17); $F=2; } 
+ 
 var $bb=R(vv);
-$9.set($bb,0,sz);
+$B.set($bb,0,sz);
 setTimeout(function(){
 Module.ccall('nano',null,['Number'],['Number'],['Number'],['Number'],[$F],[sz],[82944000],[82933000]);
 M();
