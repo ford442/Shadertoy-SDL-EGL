95966a3f77b88415b2123cddf91f2cf8ee8623f6
Noah Cohn <noahc42@gmail.com>
2022-03-20 10:24:57 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -402,7 +402,7 @@ this.color(p[0],p[1],p[2],aveg);
 
 let rA=g.createKernel(function(fa){
 var pd=fa[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
-var avrg=(Ave(pd[0],pd[1],pd[2])-0.8)*(Ave(pd[0],pd[1],pd[2])*5.0);
+var avrg=(1.0-Ave(pd[0],pd[1],pd[2]))-0.8)*(Ave(pd[0],pd[1],pd[2])*5.0);
 this.color(pd[0],pd[1],pd[2],avrg);
 }).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
 
