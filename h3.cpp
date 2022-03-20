f23b92a0ab756e278cbda1788fe065bb65bface2
Noah Cohn <noahc42@gmail.com>
2022-03-20 07:40:49 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -396,14 +396,14 @@ var $amin=this.constants.amin;
 var $favg=this.constants.favg;
 var $aavg=this.constants.aavg;
 var alph=Alphe($fmax,$fmin,$amax,$amin,$favg,$aavg,p[3]);
-var aveg=(Aveg(p[3],alph))*0.75;
+var aveg=Aveg(p[3],alph);
 this.color(p[0],p[1],p[2],aveg);
 }).setTactic("precision").setGraphical(true).setArgumentTypes(['HTMLVideo']).setDynamicOutput(true).setOutput([w$,h$]);
 
 let rA=g.createKernel(function(fa){
 var pd=fa[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
-var avrg=pd[3]/4;
-this.color(pd[0],pd[1],pd[2],avrg);
+var avrg=pd[3];
+this.color(pd[0],pd[1],pd[2],0.05);
 }).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
 
 let d=S();if(d)d();d=S();function S(){
