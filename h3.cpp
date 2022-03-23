dbd1cc8bf42c0a7a98e3ba482ef628db109079d6
Noah Cohn <noahc42@gmail.com>
2022-03-23 02:41:49 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -411,7 +411,7 @@ var gRB=Aveg(avRB,0.5);
 var aG=Math.max(gG-gRB,0.0);
 var gA=Aveg(aG,0.25);
 var gAlph=Math.max(gA,gRB);
-var aVgg=Math.max((Ave(pd[0],pd[1],pd[2])-0.666),0.0);
+var aVgg=Math.max((Ave(pd[0],pd[1],pd[2])-0.33),0.0);
 var aV=gAlph+(aVgg*6.6);
 this.color(pd[0],pd[1],pd[2],aV);
 }).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
