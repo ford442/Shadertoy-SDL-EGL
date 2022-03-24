da8b7ee795cdb2591ec8d3331e9a3b2754231fc1
Noah Cohn <noahc42@gmail.com>
2022-03-24 06:27:33 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -324,14 +324,17 @@ glEnable(GL_DEPTH_TEST);
 glDepthFunc(GL_LESS);
 glClearColor(F0,F0,F0,F);
 glViewport(0,0,S,S);
-  
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+
 solidColor=create_texture();
 unsigned int whitePixel=0xFFFFFFFFu;
-glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,1,1,0,GL_RGBA,GL_UNSIGNED_BYTE,&whitePixel);
-glBindTexture(GL_TEXTURE_2D,TEX);
+   glBindTexture(GL_TEXTURE_2D,TEX);
 glActiveTexture(GL_TEXTURE0);
+glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,1,1,0,GL_RGBA,GL_UNSIGNED_BYTE,&whitePixel);
+
 glUniform2f(sampler_channel_res[0],Size,Size);
 glUniform1i(sampler_channel[0],0);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 
 t1=high_resolution_clock::now();
 emscripten_set_main_loop((void(*)())renderFrame,0,0);
