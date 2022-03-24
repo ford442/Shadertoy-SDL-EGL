4a460ec287422c869f1f658ee7e623664161e01e
Noah Cohn <noahc42@gmail.com>
2022-03-24 05:19:08 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -317,6 +317,11 @@ uniform_frame=glGetUniformLocation(shader_program,"iFrame");
 uniform_res=glGetUniformLocation(shader_program,"iResolution");
 uniform_mouse=glGetUniformLocation(shader_program,"iMouse");
 glUniform2f(uniform_res,Size,Size);
+  glUniform2f(sampler_channel_res[0],Size,Size);
+  glUniform2f(sampler_channel_res[1],Size,Size);
+  glUniform2f(sampler_channel_res[2],Size,Size);
+  glUniform2f(sampler_channel_res[3],Size,Size);
+
 glEnable(GL_BLEND);
 glBlendFunc(GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
 glBlendFuncSeparate(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA,GL_ONE,GL_ONE_MINUS_SRC_ALPHA);
@@ -328,10 +333,11 @@ glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
   
 solidColor=create_texture();
 unsigned int whitePixel=0xFFFFFFFFu;
-glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,1,1,0,GL_RGBA,GL_UNSIGNED_BYTE,&whitePixel);
-glBindTexture(GL_TEXTURE_2D,texture);
+  
+  glBindTexture(GL_TEXTURE_2D,texture);
 glActiveTexture(GL_TEXTURE0);
-glUniform2f(sampler_channel_res[0],Size,Size);
+glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,S,S,0,GL_RGBA,GL_UNSIGNED_BYTE,&whitePixel);
+
 glUniform1i(sampler_channel[0],0);
   
 t1=high_resolution_clock::now();
