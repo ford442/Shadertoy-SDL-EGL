84d95a81c25e28bbb9ad0b5cbb77d849c93df014
Noah Cohn <noahc42@gmail.com>
2022-03-26 00:50:53 -0700

    Update h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -75,13 +75,13 @@ long length=0;
 static const char common_shader_header_gles3[]=
 "#version 300 es \n precision highp float;precision mediump int;precision mediump sampler3D;precision highp sampler2D;";
 static const char vertex_shader_body_gles3[]=
-"\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n\0";
+"\n layout(location=0)in vec4 iPosition;void main(){fragColor=fragColor*texture(iChannel3,uv);gl_Position=iPosition;}\n\0";
 static const char fragment_shader_header_gles3[]=
 "\n uniform vec3 iChannelResolution;uniform vec3 iResolution;uniform float iTime;uniform vec4 iMouse;"
 "uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
 "out vec4 fragColor;\n";
 static const char fragment_shader_footer_gles3[]=
-"\n fragColor=fragColor*texture(iChannel3,uv);void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
+"\n void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
 static const char* common_shader_header=common_shader_header_gles3;
 static const char* vertex_shader_body=vertex_shader_body_gles3;
 static const char* fragment_shader_header=fragment_shader_header_gles3;
