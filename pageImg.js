93c092b26f86d2556e0f3da3b6df0e2f97751d61
Noah Cohn <noahc42@gmail.com>
2022-05-01 22:46:20 -0700

    Update pageImg.js

--- pageImg.js
+++ pageImg.js
@@ -195,9 +195,11 @@ var canvasT=document.getElementById("mvv");
 var ctxI=canvasT.getContext("2d");
 var imgI=document.getElementById("mv");
 var siZe=parseInt(window.innerHeight,10);
+setTimeout(function(){
 ctxI.drawImage(imgI,0,0,siZe,siZe);
-document.getElementById("di").click();
 },500);
+document.getElementById("di").click();
+},250);
 setTimeout(function(){
 loada();
 },$ldt);}
