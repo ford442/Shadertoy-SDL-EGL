75dcde0051a2f24dc98c513b481171813872ff15
Noah Cohn <noahc42@gmail.com>
2022-06-02 21:57:55 -0700

    Update pageg.js

--- pageg.js
+++ pageg.js
@@ -146,12 +146,12 @@ $iwid.innerHTML=parseInt($w,10);
 document.getElementById("wrap").style.lineheight=$hg;
 document.getElementById("isrc").innerHTML=adr;
 mV.play();
-setTimeout(function(){document.getElementById("btn2").click();},2000);
-setTimeout(function(){document.getElementById("btn11").click();},2400);
-setTimeout(function(){document.getElementById("btn").click();},2800);
-setTimeout(function(){document.getElementById("btn10").click();},3800);
-setTimeout(function(){document.getElementById("btn3").click();},3500);
-setTimeout(function(){document.getElementById("btn8").click();},4000);
+setTimeout(function(){document.getElementById("btn2").click();},1000);
+setTimeout(function(){document.getElementById("btn11").click();},1400);
+setTimeout(function(){document.getElementById("btn").click();},1800);
+setTimeout(function(){document.getElementById("btn10").click();},2200);
+setTimeout(function(){document.getElementById("btn3").click();},500);
+setTimeout(function(){document.getElementById("btn8").click();},1700);
 
 function loada(){
 loadV.addEventListener('canplay',function(){
