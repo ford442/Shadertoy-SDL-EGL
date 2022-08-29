cb4dc437f4910df2548f52cb304d24442eb4a54a
Noah Cohn <noahc42@gmail.com>
2022-08-29 00:03:48 -0700

    Update pageg.js

--- pageg.js
+++ pageg.js
@@ -146,12 +146,12 @@ $iwid.innerHTML=parseInt($w,10);
 document.getElementById("wrap").style.lineheight=$hg;
 document.getElementById("isrc").innerHTML=adr;
 mV.play();
-setTimeout(function(){document.getElementById("btn2").click();},200);
-setTimeout(function(){document.getElementById("btn11").click();},1600);
-setTimeout(function(){document.getElementById("btn").click();},1300);
+setTimeout(function(){document.getElementById("btn2").click();},400);
+setTimeout(function(){document.getElementById("btn11").click();},2200);
+setTimeout(function(){document.getElementById("btn").click();},3700);
 // setTimeout(function(){document.getElementById("btn10").click();},2200);
-setTimeout(function(){document.getElementById("btn3").click();},300);
-setTimeout(function(){document.getElementById("btn8").click();},600);
+setTimeout(function(){document.getElementById("btn3").click();},500);
+setTimeout(function(){document.getElementById("btn8").click();},2600);
 
 function loada(){
 loadV.addEventListener('canplay',function(){
