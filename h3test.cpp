affdf4be8004142e0f6d326764651c0e04561fed
Noah Cohn <noahc42@gmail.com>
2022-10-15 21:15:02 -0700

    Update h3test.cpp

--- h3test.cpp
+++ h3test.cpp
@@ -70,7 +70,7 @@ const char *sources[4];
 char8_t *result=NULL;
 long length=0;
 static const char common_shader_header_gles3[]=
-"#version 300 es \n precision highp float;precision mediump int;precision highp sampler3D;precision highp sampler2D;";
+"#version 300 es \n precision highp float;precision highp int;precision highp sampler3D;precision highp sampler2D;";
 static const char vertex_shader_body_gles3[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n\0";
 static const char fragment_shader_header_gles3[]=
