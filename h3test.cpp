82bec0f709cb21ba45b01ce7f7457e92ac2189c1
Noah Cohn <noahc42@gmail.com>
2022-10-15 20:56:35 -0700

    Update h3test.cpp

--- h3test.cpp
+++ h3test.cpp
@@ -56,10 +56,10 @@ EGLConfig eglconfig=NULL;
 EmscriptenWebGLContextAttributes attr;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 static struct timespec rem;
+static struct timespec req={0,33200000};
 // static struct timespec req={0,16600000};
 // static struct timespec req={0,15000000};
-static struct timespec req={0,10000000};
-static struct timespec reqs={0,22675};
+// static struct timespec req={0,10000000};
 EMSCRIPTEN_RESULT ret;
 typedef struct{GLfloat XYZW[4];}Vertex;
 Vertex vertices[]={{Fm1,Fm1,F,F},{F,Fm1,F,F},{F,F,F,F},{Fm1,F,F,F},{Fm1,Fm1,Fm1,F},{F,Fm1,Fm1,F},{F,F,Fm1,F},{Fm1,F,F,F}};
@@ -496,7 +496,7 @@ setTimeout(function(){
 var pointb=66*la;
 Module.ccall('nano',null,['Number'],['Number'],['Number'],['Number'],[$F],[sz],[pointb],[pointa]);
 M();
-},10);
+},33.2);
 }
 M();
 document.getElementById("di").onclick=function(){
