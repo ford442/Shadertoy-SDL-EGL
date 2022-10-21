bdb3549e813d23b791922e44bc0479b16c369f9f
Noah Cohn <noahc42@gmail.com>
2022-10-20 18:24:18 -0700

    Update pageg.js

--- pageg.js
+++ pageg.js
@@ -148,10 +148,10 @@ document.getElementById("isrc").innerHTML=adr;
 mV.play();
 setTimeout(function(){document.getElementById("btn2").click();},400);
 setTimeout(function(){document.getElementById("btn11").click();},2200);
-setTimeout(function(){document.getElementById("btn").click();},3700);
+setTimeout(function(){document.getElementById("btn").click();},2700);
 // setTimeout(function(){document.getElementById("btn10").click();},2200);
 setTimeout(function(){document.getElementById("btn3").click();},500);
-setTimeout(function(){document.getElementById("btn8").click();},2600);
+setTimeout(function(){document.getElementById("btn8").click();},1600);
 
 function loada(){
 loadV.addEventListener('canplay',function(){
