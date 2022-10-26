32c9dfbee309f603243712a63f31d3f81471c36b
Noah Cohn <noahc42@gmail.com>
2022-10-25 21:12:19 -0700

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,5 +1,5 @@
-document.getElementById('btn2').addEventListener('click',function(){
-let pth=document.getElementById('path').innerHTML;
+document.getElementById("btn2").addEventListener("click",function(){
+let pth=document.getElementById("path").innerHTML;
 let ff=new XMLHttpRequest();
 ff.open("GET",pth,true);
 ff.responseType="arraybuffer";
@@ -7,7 +7,7 @@ ff.onload=function(oEvent){
 let sarrayBuffer=ff.response;
 if(sarrayBuffer){
 let sfil=new Uint8ClampedArray(sarrayBuffer);
-FS.writeFile('/shader/shader1.toy',sfil);
+FS.writeFile("/shader/shader1.toy",sfil);
 }}
 ff.send(null);
 // console.log('Wrote 1 shader to filesystem: \n -> ',pth);
