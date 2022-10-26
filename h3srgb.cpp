9f514890b5ec62e7bf793e121345ecb40c750eb3
Noah Cohn <noahc42@gmail.com>
2022-10-26 06:02:49 -0700

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -61,7 +61,7 @@ typedef struct{GLfloat XYZW[4];}Vertex;
 Vertex vertices[]={{Fm1,Fm1,F,F},{F,Fm1,F,F},{F,F,F,F},{Fm1,F,F,F},{Fm1,Fm1,Fm1,F},{F,Fm1,Fm1,F},{F,F,Fm1,F},{Fm1,F,F,F}};
 GLubyte indc[]={3,0,1,1,2,3,4,0,3,3,7,4,1,5,6,6,2,1,4,7,6,6,5,4,2,6,6,7,3,0,4,1,1,4,5};
 static const char *fileloc="/shader/shader1.toy";
-const unsigned char *sources[4];
+const char *sources[4];
 char8_t *result=NULL;
 long length=0;
 
