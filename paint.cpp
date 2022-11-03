04e585646f1139c42bac4dfbf5028412c9779fa8
Noah Cohn <noahc42@gmail.com>
2022-11-03 00:17:02 -0700

    Update paint.cpp

--- paint.cpp
+++ paint.cpp
@@ -369,7 +369,7 @@ var Min=(4.0*(($amax-($favg-$amin))/2.0));
 var ouT=Math.max(Min,alph);
 var aveg=Aveg(p[3],ouT);
 var grr=(p[3]-1.0)*10;
-p[0]=p[0]+grr;p[1]=p[1]+grr;p[2]=p[2]+grr;}
+p[0]=p[0]+grr;p[1]=p[1]+grr;p[2]=p[2]+grr;
 this.color(p[0],p[1],p[2],1.0);
 }).setTactic("speed").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
 w$=parseInt(document.getElementById("wid").innerHTML,10);
