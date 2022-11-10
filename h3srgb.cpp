bcf21fc2c4c0574753ce76c417fe73f78d3975b9
Noah Cohn <noahc42@gmail.com>
2022-11-10 06:00:47 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -16,8 +16,7 @@
 
 #include <emscripten.h>
 #include <emscripten/html5.h>
-#include <GLES3/gl2.h>
-#include <GLES2/gl2ext.h>
+
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES3/gl3.h>
