d96ce87ddbb24937ac70062012148a0d788b2260
Noah Cohn <noahc42@gmail.com>
2022-11-15 17:12:35 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -25,6 +25,67 @@ extern "C" {
 #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
 
+#include <SDL2/SDL.h>
+SDL_AudioDeviceID dev;
+struct{Uint8* snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
+
+void cls_aud(){
+if(dev!=0){
+SDL_PauseAudioDevice(dev,SDL_TRUE);
+SDL_CloseAudioDevice(dev);
+dev=0;
+return;
+}}
+
+void qu(int rc){
+SDL_Quit();
+return;
+}
+
+void opn_aud(){
+dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){
+SDL_FreeWAV(wave.snd);
+}
+SDL_PauseAudioDevice(dev,SDL_FALSE);
+return;
+}
+
+void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
+int lft;
+wptr=wave.snd+wave.pos;
+lft=wave.slen-wave.pos;
+while (lft<=len){
+SDL_memcpy(stm,wptr,lft);
+stm+=lft;
+len-=lft;
+wptr=wave.snd;
+lft=wave.slen;
+wave.pos=0;
+}
+SDL_memcpy(stm,wptr,len);
+wave.pos+=len;
+return;
+}
+
+void plt(){
+char flnm[24];
+SDL_FreeWAV(wave.snd);
+SDL_SetMainReady();
+if (SDL_Init(SDL_INIT_AUDIO)<0){
+qu(1);
+}
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
+qu(1);
+}
+wave.pos=0;
+wave.spec.callback=bfr;
+opn_aud();
+return;
+}
+
 using namespace std;
 using namespace std::chrono;
 
@@ -485,66 +546,6 @@ T=true;
 })
 }
 
-#include <SDL2/SDL.h>
-SDL_AudioDeviceID dev;
-struct{Uint8* snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
-
-void cls_aud(){
-if(dev!=0){
-SDL_PauseAudioDevice(dev,SDL_TRUE);
-SDL_CloseAudioDevice(dev);
-dev=0;
-return;
-}}
-
-void qu(int rc){
-SDL_Quit();
-return;
-}
-
-void opn_aud(){
-dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
-if(!dev){
-SDL_FreeWAV(wave.snd);
-}
-SDL_PauseAudioDevice(dev,SDL_FALSE);
-return;
-}
-
-void SDLCALL bfr(void *unused,Uint8* stm,int len){
-Uint8* wptr;
-int lft;
-wptr=wave.snd+wave.pos;
-lft=wave.slen-wave.pos;
-while (lft<=len){
-SDL_memcpy(stm,wptr,lft);
-stm+=lft;
-len-=lft;
-wptr=wave.snd;
-lft=wave.slen;
-wave.pos=0;
-}
-SDL_memcpy(stm,wptr,len);
-wave.pos+=len;
-return;
-}
-
-void plt(){
-char flnm[24];
-SDL_FreeWAV(wave.snd);
-SDL_SetMainReady();
-if (SDL_Init(SDL_INIT_AUDIO)<0){
-qu(1);
-}
-SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
-if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
-qu(1);
-}
-wave.pos=0;
-wave.spec.callback=bfr;
-opn_aud();
-return;
-}
 
 extern "C" {
 
