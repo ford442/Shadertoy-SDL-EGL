c1a976a566c679e1552b63aa162cfc26afc029d2
Noah Cohn <noahc42@gmail.com>
2022-12-01 21:53:46 -0800

    Update cl-gpu2.js

--- cl-gpu2.js
+++ cl-gpu2.js
@@ -17374,7 +17374,7 @@ class WebGL2Kernel extends WebGLKernel {
   }
 
   initContext() {
-    const settings = {const settings={alpha:false,depth:false,stencil:false,preserveDrawingBuffer:false,premultipliedAlpha:false,desynchronized:false,lowLatency:true,powerPreference:'high-performance',};
+    const settings = {alpha:false,depth:false,stencil:false,preserveDrawingBuffer:false,premultipliedAlpha:false,desynchronized:false,lowLatency:true,powerPreference:'high-performance',};
     return this.canvas.getContext('webgl2', settings);
   }
 
