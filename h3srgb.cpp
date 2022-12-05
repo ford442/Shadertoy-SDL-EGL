a58591962d4fa234aea6b8304240ebd77b22124b
Noah Cohn <noahc42@gmail.com>
2022-12-05 10:35:45 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -516,7 +516,7 @@ EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 EGLint config_size,major,minor,atb_pos;
 iFrame=0;
 clk_l=true;
-Size=(int)EM_ASM({return parseInt(window.innerHeight);});
+Size=static_cast<int>EM_ASM({return parseInt(window.innerHeight);});
 S=(GLfloat)Size;
 // eglBindAPI(EGL_OPENGL_ES_API);
 eglBindAPI(EGL_OPENGL_API);
