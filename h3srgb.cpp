b1333ccde1fd8ffe2dfcda8fa1146459f4c2d389
Noah Cohn <noahc42@gmail.com>
2022-12-06 03:20:29 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -336,9 +336,9 @@ avgFrm(Fnum,leng,ptr,aptr);
 
 }
 
-// #define GL_GLEXT_PROTOTYPES 1
+#define GL_GLEXT_PROTOTYPES 1
 #define GL_FRAGMENT_PRECISION_HIGH 1
-// #define GL3_PROTOTYPES 1
+#define GL3_PROTOTYPES 1
 
 #include <GLES3/gl3.h>
 #include <GLES3/gl31.h>
