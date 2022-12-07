ca9ac6a131fdf85c566b5c68d1f467b703a31f6f
Noah Cohn <noahc42@gmail.com>
2022-12-07 07:24:13 -0800

    Update h3test.cpp

--- h3test.cpp
+++ h3test.cpp
@@ -198,8 +198,8 @@ for(i=64;i>0;i--){var loca=$F+1;if(loca>64){loca=1;}var locb=$Bu+1;if(locb>64){l
 var $bb=R(vv);
 
 }else if(Loop===1){
-for(i=64;i>0;i--){var loca=$F+1;if(loca>64){loca=1;}var locb=$Bu+1;if(locb>64){locb=1;}eval("if($F==="+i+"){r($r"+i+");$F="+loca+";$Bu="+locb+";}");}
-eval("var $bb=t($"+i+")");
+for(i=64;i>0;i--){var loca=$F+1;if(loca>64){loca=1;}var locb=$Bu+1;if(locb>64){locb=1;}eval("if($F==="+i+"){r($$"+i+");$F="+loca+";$Bu="+locb+";}");}
+eval("var $bb=t($$"+i+")");
 
 }
   
