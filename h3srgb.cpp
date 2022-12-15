9aebfd0cac1f826817c3f5095607e561b57a025b
Noah Cohn <noahc42@gmail.com>
2022-12-15 09:58:21 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -1,9 +1,4 @@
-#include <emscripten.h>
-#include <emscripten/html5.h>
-
-#include <webgl/webgl2.h>
-
-void avgFrm(int Fnum,int leng,float *ptr,float *aptr);
+#include <b3.hpp>
 
 void avgFrm(int Fnum,int leng,float *ptr,float *aptr){
 float max=0.0;
@@ -47,9 +42,7 @@ avgFrm(Fnum,leng,ptr,aptr);
 extern "C" {
 
 EM_JS(void,ma,(),{
-  
 "use strict";
-
 const pnnl=document.body;
 var vv=document.getElementById("mv");
 var intervalBackward;
@@ -294,22 +287,7 @@ T=true;
 
 }
 
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <cstdarg>
-#include <cmath>
-#include <cstdio>
-#include <cstdint>
-#include <cstdlib>
-#include <ctime>
-#include <chrono>
-#include <unistd.h>
-#include <SDL2/SDL.h>
 
-EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
-static const char8_t *read_file(const char *filename);
-  
 extern "C" {
 
 SDL_AudioDeviceID dev;
@@ -374,27 +352,6 @@ return;
 
 }
 
-#define GL_GLEXT_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-
-#include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3platform.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
-#include <GL/gl.h>
-#include <GL/glext.h>
-
-GLfloat x;
-GLfloat y;
-EM_BOOL ms_l;
-
-void uni(float xx,float yy,GLfloat time,EGLint fram);
-GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
-
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData){
 if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
 if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0){
@@ -409,31 +366,6 @@ y=e->clientY;
 }}
 return 0;
 }
-
-using namespace std;
-using namespace std::chrono;
-
-steady_clock::time_point t1,t2,t3;
-GLuint uni_frm,uni_tme,uni_tme_dlt,uni_res,shader;
-double Ttime,TtimeDelta;
-EGLint iFrame,iFps;
-GLsizei s4=4;
-// int v0=0,v1=1,v2=2,v3=3,v4=4,v6=6,v8=8,v10=10,v16=16,v24=24,v32=32,v64=64;
-int a,b;
-float F=1.0;
-float F0=0.0;
-float Fm1=-1.0;
-float mouseX;
-float mouseY;
-float cMouseX;
-float cMouseY;
-int Size;
-GLfloat S;
-EM_BOOL clk_l;
-GLsizei i;
-struct timespec rem;
-struct timespec req={0,16666000};
-GLuint uni_mse;
 
 void uni(float xx,float yy,GLfloat time,EGLint fram){
 GLfloat mX,mY;
@@ -455,10 +387,6 @@ glUniform1f(uni_tme,time);
 glUniform1i(uni_frm,fram);
 return;
 }
-
-const char *sources[4];
-GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
-GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 
 void renderFrame(){
 glClear(GL_STENCIL_BUFFER_BIT);
