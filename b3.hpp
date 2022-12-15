9e5c0369774754e86c9f82601dbb8307bd2cc80d
Noah Cohn <noahc42@gmail.com>
2022-12-15 09:58:03 -0800

    Update b3.hpp

--- b3.hpp
+++ b3.hpp
@@ -1,1 +1,85 @@
 
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+#include <webgl/webgl2.h>
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdarg>
+#include <cmath>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <unistd.h>
+#include <SDL2/SDL.h>
+
+void avgFrm(int Fnum,int leng,float *ptr,float *aptr);
+
+EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
+
+static const char8_t *read_file(const char *filename);
+  
+#define GL_GLEXT_PROTOTYPES 1
+#define GL_FRAGMENT_PRECISION_HIGH 1
+#define GL3_PROTOTYPES 1
+
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
+#include <GLES3/gl3platform.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+
+GLfloat x;
+GLfloat y;
+EM_BOOL ms_l;
+
+void uni(float xx,float yy,GLfloat time,EGLint fram);
+
+GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+
+using namespace std;
+using namespace std::chrono;
+
+steady_clock::time_point t1,t2,t3;
+GLuint uni_frm,uni_tme,uni_tme_dlt,uni_res,shader;
+double Ttime,TtimeDelta;
+EGLint iFrame,iFps;
+GLsizei s4=4;
+// int v0=0,v1=1,v2=2,v3=3,v4=4,v6=6,v8=8,v10=10,v16=16,v24=24,v32=32,v64=64;
+int a,b;
+float F=1.0;
+float F0=0.0;
+float Fm1=-1.0;
+float mouseX;
+float mouseY;
+float cMouseX;
+float cMouseY;
+int Size;
+GLfloat S;
+EM_BOOL clk_l;
+GLsizei i;
+struct timespec rem;
+struct timespec req={0,16666000};
+GLuint uni_mse;
+
+const char *sources[4];
+GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
+GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
+
+void renderFrame();
+
+GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+
+void strt();
+void str();
+void pl();
+void b3();
