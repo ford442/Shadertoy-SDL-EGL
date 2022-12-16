fed4d8d10d3a1cca50b153973b394672834540a4
Noah Cohn <noahc42@gmail.com>
2022-12-16 09:22:14 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -69,19 +69,21 @@ let intervalLoop;
 let stp,a,b,f;
 
 function backForth(stp){
-let a=(stp/1000.0)-0.75;
-let b=(stp/1000.0)+0.75;
+let a=(stp/1000.0)-0.888888;
+let b=(stp/1000.0)+0.88888;
 f=true;
 intervalLoop=setInterval(function(){
 if(f==true){
 if(vv.currentTime<=b){
-vv.currentTime+=0.016666;
+// vv.currentTime+=0.016666;
+vv.fastSeek(vv.currentTime+0.016666);
 }else{
 f=false;
 }
 }else{
 if(vv.currentTime>=a){
-vv.currentTime-=0.016666;
+// vv.currentTime-=0.016666;
+vv.fastSeek(vv.currentTime-0.016666);
 }else{
 f=true;
 }}
