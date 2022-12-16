c809e9c2a204774e24c2c60c3f214d91c8dc1be6
Noah Cohn <noahc42@gmail.com>
2022-12-16 04:29:59 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -277,7 +277,7 @@ Module.ccall("nano",null,["Number","Number","Number","Number"],[$F,sz,pointb,poi
 
 setTimeout(function(){
 M();
-},16.6);
+},16.666666);
 }
 M();
 document.getElementById("di").onclick=function(){
@@ -412,10 +412,10 @@ mouseY=(S-y)/S;
 uni(mouseX,mouseY,Ttime,iFrame);
 glClear(GL_COLOR_BUFFER_BIT);
 glDrawElements(GL_TRIANGLES,36,GL_UNSIGNED_BYTE,indc);
-// glFlush();
-// nanosleep(&req,&rem);
+glFlush();
+nanosleep(&req,&rem);
 iFrame++;
-// glFinish();
+glFinish();
 return;
 }
 
