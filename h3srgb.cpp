9f0ee04ed2dd5cec49f6de88af02cb7d2f54467b
Noah Cohn <noahc42@gmail.com>
2022-12-18 16:46:17 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -71,14 +71,14 @@ let a=(stp/1000.0)-0.888;
 let b=(stp/1000.0)+0.888;
 f=true;
 intervalLoop=setInterval(function(){
-if(f==true){
+if(f===true){
 if(vv.currentTime<=b){
 vv.currentTime+=0.016;
 // vv.fastSeek(vv.currentTime+0.016666);
 }else{
 f=false;
 }
-if(f==false){
+if(f===false){
 if(vv.currentTime>=a){
 vv.currentTime-=0.016;
 // vv.fastSeek(vv.currentTime-0.016);
