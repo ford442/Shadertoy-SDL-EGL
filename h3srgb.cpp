3b8c4db18c8087d215ce3cdaf0779e87be11f9b7
Noah Cohn <noahc42@gmail.com>
2022-12-20 08:39:43 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -76,10 +76,10 @@ if(f==true){
 if(vv.currentTime>a){
 vv.currentTime-=0.016;
 }else{
-var f=false;
+f=false;
 }}
 else if(vv.currentTime>=b){
-var f=true;
+f=true;
 }else{
 vv.currentTime+=0.016;
 }
@@ -274,7 +274,8 @@ var loca=$F+1;
 if(loca>64){loca=1;}
 var locb=$Bu+1;
 if(locb>64){locb=1;}
-eval("if ($F=="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
+eval("if ($F=="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");}");
+eval("if ($F=="+i+"){var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
 }
 if($F%32==0){
 let $bb=R(vv);
