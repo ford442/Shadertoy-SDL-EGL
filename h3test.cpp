d0c4f8115cfc37ebabe461fb5df16ff625c26277
Noah Cohn <noahc42@gmail.com>
2022-12-27 05:31:05 -0800

    Update h3test.cpp

--- h3test.cpp
+++ h3test.cpp
@@ -281,7 +281,7 @@ gl.disable(gl.STENCIL_TEST);
   
 gl.disable(gl.SCISSOR_TEST);
 
-const g=new GPU({canvas:bcanvas,webGl:contx});
+const g=new GPU({canvas:bcanvas,webGl:gl});
 const g2=new GPU();
 const glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 /// const glslAlphe=`float Alphe(float a,float b,float c,float d,float e,float f,float g){return((0.7+(3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-e)*((1.0-g)*0.1))))))/4.0);}`;
