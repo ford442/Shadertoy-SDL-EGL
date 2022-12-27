e51ff89c6c9d4638081639811273a6429ef3b3b3
Noah Cohn <noahc42@gmail.com>
2022-12-27 10:25:25 -0800

    Update b3.hpp

--- b3.hpp
+++ b3.hpp
@@ -1,3 +1,11 @@
+#pragma once
+
+#define GL_GLEXT_PROTOTYPES 1
+#define EGL_EGLEXT_PROTOTYPES 1
+#define GL_FRAGMENT_PRECISION_HIGH 1
+#define GL3_PROTOTYPES 1
+#define GL4_PROTOTYPES 1 //maybe??
+
 #include <iomanip>
 #include <iostream>
 #include <algorithm>
@@ -11,31 +19,33 @@
 #include <chrono>
 
 #include <unistd.h>
+
 #include <SDL2/SDL.h>
+#undef main
+
 #include "SDL_config.h"
+
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
-void avgFrm(int Fnum,int leng,float *ptr,float *aptr);
-EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
-static const char8_t *read_file(const char *filename);
-
-#define GL_GLEXT_PROTOTYPES 1
-#define EGL_EGLEXT_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1 //maybe??
+#include <GL/gl.h>
+#include <GL/glext.h>
 
 #include <GLES3/gl3.h>
 #include <GLES3/gl31.h>
 #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
+
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
-#include <GL/gl.h>
-#include <GL/glext.h>
+
 #include <webgl/webgl2.h>
+
+
+void avgFrm(int Fnum,int leng,float *ptr,float *aptr);
+EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
+static const char8_t *read_file(const char *filename);
 
 GLfloat x;
 GLfloat y;
