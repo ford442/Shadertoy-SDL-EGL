509c53cc64cd181e90e21d409671408824b30fa9
Noah Cohn <noahc42@gmail.com>
2022-12-26 18:30:40 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -514,7 +514,7 @@ const char common_shader_header_gles3[]=
 "#version 300 es\n"
 "#undef HW_PERFORMANCE\n"
 "#define HW_PERFORMANCE 0\n"
-"precision highp float;precision mediump int;precision lowp sampler3D;precision mediump sampler2D;\n";
+"precision mediump float;precision mediump int;precision lowp sampler3D;precision mediump sampler2D;\n";
 const char vertex_shader_body_gles3[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
 const char fragment_shader_header_gles3[]=
