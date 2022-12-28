2ca043a66bbaa3d0436fc718d71d0b2557de97b2
Noah Cohn <noahc42@gmail.com>
2022-12-27 16:05:09 -0800

    Update b3.hpp

--- b3.hpp
+++ b3.hpp
@@ -51,11 +51,13 @@ GLfloat x;
 GLfloat y;
 EM_BOOL ms_l;
 
-void uni(float xx,float yy,GLfloat time,EGLint fram);
+void uni(float xx,float yy,GLfloat time,int fram);
 
 GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+
 using namespace std;
 using namespace std::chrono;
+
 steady_clock::time_point t1;
 steady_clock::time_point t2;
 steady_clock::time_point t3;
@@ -65,6 +67,7 @@ steady_clock::time_point t3;
 // system_clock::time_point t1;
 // system_clock::time_point t2;
 // system_clock::time_point t3;
+
 GLuint uni_mse;
 GLuint uni_frm;
 GLuint uni_tme;
@@ -72,9 +75,8 @@ GLuint uni_tme_dlt;
 GLuint uni_res;
 GLuint uni_srate;
 GLuint shader;
-
 float Ttime,TtimeDelta;
-EGLint iFrame,iFps;
+int iFrame,iFps;
 GLsizei s4=4;
 int a,b;
 float F=1.0f;
@@ -83,7 +85,6 @@ float Fm1=-1.0f;
 float D=1.0;
 float D0=0.0;
 float Dm1=-1.0;
-
 float mouseX;
 float mouseY;
 float cMouseX;
@@ -97,12 +98,15 @@ struct timespec req={0,16660000};
 const char *sources[4];
 GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
+
 void renderFrame();
+
 GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+
 extern "C"{
 void strt();
 void str();
 void pl();
 void b3();
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
-}
+};
