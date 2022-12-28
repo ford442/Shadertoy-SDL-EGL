a1533ebd559416371d7e8735ac60ee23cfb55afa
Noah Cohn <noahc42@gmail.com>
2022-12-28 00:56:44 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -68,14 +68,14 @@ return 0;
 
 };
 
-void avgFrm(int Fnum,int leng,float *ptr,float *aptr){
+void avgFrm(short int Fnum,int leng,float *ptr,float *aptr){
 float max=0.0;
 float min=1.0;
 float sum=0.0;
 float avgSum=0.0;
 float minSum=0.0;
 float maxSum=0.0;
-for (int i=0;i<leng;i++){
+for (short int i=0;i<leng;i++){
 sum+=ptr[i];
 if(max<ptr[i]){max=ptr[i];}
 if(min>ptr[i]&&ptr[i]>0){min=ptr[i];}
@@ -88,11 +88,11 @@ for(int i=33;i<65;i++){
 avgSum+=aptr[i];
 };
 aptr[0]=avgSum/32;
-for(int i=33;i<65;i++){
+for(short int i=33;i<65;i++){
 minSum+=aptr[i+100];
 };
 aptr[100]=minSum/32;
-for(int i=33;i<65;i++){
+for(short int i=33;i<65;i++){
 maxSum+=aptr[i+200];
 };
 aptr[200]=maxSum/32;
@@ -100,7 +100,7 @@ return;
 };
 
 extern "C" {
-void nano(int Fnum,int leng,float *ptr,float *aptr){avgFrm(Fnum,leng,ptr,aptr);};
+void nano(short int Fnum,int leng,float *ptr,float *aptr){avgFrm(Fnum,leng,ptr,aptr);};
 };
 
 EM_JS(void,ma,(),{
@@ -359,7 +359,7 @@ T=true;
 }};
 });
 
-void uni(float xx,float yy,GLfloat time,int fram){
+void uni(float xx,float yy,GLfloat time,short int fram){
 GLfloat mX,mY;
 if(ms_l==true){
 if(clk_l==true){
