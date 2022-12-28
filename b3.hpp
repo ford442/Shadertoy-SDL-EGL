ebf6fc35029d8f4f0be59545020042087f41db4b
Noah Cohn <noahc42@gmail.com>
2022-12-28 01:30:19 -0800

    Update b3.hpp

--- b3.hpp
+++ b3.hpp
@@ -1,10 +1,5 @@
 #pragma once
 
-#define GL_GLEXT_PROTOTYPES 1
-#define EGL_EGLEXT_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1 //maybe??
 
 #include <iostream>
 #include <algorithm>
@@ -24,23 +19,29 @@
 #include "SDL_config.h"
 
 
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+#define GL_GLEXT_PROTOTYPES 1
+#define EGL_EGLEXT_PROTOTYPES 1
+#define GL_FRAGMENT_PRECISION_HIGH 1
+#define GL3_PROTOTYPES 1
+#define GL4_PROTOTYPES 1 //maybe??
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
 #include <GLES3/gl3.h>
 #include <GLES3/gl31.h>
 #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
 
-#include <GL/gl.h>
-#include <GL/glext.h>
-
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
 
-
 #include <webgl/webgl2.h>
 
-#include <emscripten.h>
-#include <emscripten/html5.h>
 
 void avgFrm(short int Fnum,int leng,float *ptr,float *aptr);
 
