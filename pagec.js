b22b68d15d7061e6f9bdb3d435a576b87ae863ca
Noah Cohn <noahc42@gmail.com>
2022-12-28 18:41:54 -0800

    Update pagec.js

--- pagec.js
+++ pagec.js
@@ -1,58 +1,52 @@
 "use strict";
 
 document.getElementById("btn10").addEventListener("click",function(){
-    setTimeout(function(){
-        document.getElementById("scanvas").height=window.innerHeight;
-        document.getElementById("scanvas").width=window.innerHeight;
-        document.getElementById("pmhig").innerHTML=window.innerHeight;
-    },500);
+setTimeout(function(){
+document.getElementById("scanvas").height=window.innerHeight;
+document.getElementById("scanvas").width=window.innerHeight;
+document.getElementById("pmhig").innerHTML=window.innerHeight;
+},500);
 });
 
 document.getElementById("btn7").addEventListener("click",function(){
-    document.getElementById("scanvas").height=window.innerHeight*3.5;
-    document.getElementById("scanvas").width=window.innerHeight*3.5;
-    document.getElementById("pmhig").innerHTML=window.innerHeight*3.5;
-    Module.ccall("str");
-    document.getElementById("btn10").click();
-    Module.ccall("b3");
-    const bz=new BroadcastChannel('bez');
-    bz.postMessage({data:222});
+document.getElementById("scanvas").height=window.innerHeight*3.5;
+document.getElementById("scanvas").width=window.innerHeight*3.5;
+document.getElementById("pmhig").innerHTML=window.innerHeight*3.5;
+Module.ccall("str");
+document.getElementById("btn10").click();
+Module.ccall("b3");
+const bz=new BroadcastChannel('bez');
+bz.postMessage({data:222});
 });
 
 document.getElementById("btn77").addEventListener("click",function(){
-    document.getElementById("scanvas").height=2880;
-    document.getElementById("scanvas").width=2880;
-    document.getElementById("btn4").style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
-    document.getElementById("btn5").style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
-    document.getElementById("btn3").style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
-    document.getElementById("btn6").style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
-    document.getElementById("btn7").style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
-    document.getElementById("pmhig").innerHTML=2880;
-    Module.ccall("str");
-    document.getElementById("btn10").click();
+document.getElementById("scanvas").height=2880;
+document.getElementById("scanvas").width=2880;
+document.getElementById("btn3").style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById("btn7").style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById("pmhig").innerHTML=2880;
+Module.ccall("str");
+document.getElementById("btn10").click();
 });
 
-
 function pll(){
-    Module.ccall("pl");
+Module.ccall("pl");
 };
 
 const fll=new BroadcastChannel('file');
-fll.addEventListener('message',ea=> {
-    const fill=new Uint8Array(ea.data.data);
-    FS.writeFile('/snd/sample.wav',fill);
-    setTimeout(function(){
-        pll();
-    },500);
-    const shutDown=new BroadcastChannel('shutDown');
-    shutDown.postMessage({
-        data:222
-    });
+fll.addEventListener('message',ea=>{
+const fill=new Uint8Array(ea.data.data);
+FS.writeFile('/snd/sample.wav',fill);
+setTimeout(function(){
+pll();
+},500);
+const shutDown=new BroadcastChannel('shutDown');
+shutDown.postMessage({
+data:222
+});
 });
 
-var slt,$ll,r$,$w,$r,$lt,$hg,$ls,lo,mv,he,wi;
-
-var rnum,$pt;
+var $h,$pt,slt,$ll,r$,$w,$r,$lt,$hg,$ls,lo,mv,he,wi;
 
 const $iwid=document.getElementById("iwid");
 var mV=document.getElementById("mv");
@@ -274,9 +268,9 @@ function loada(){
         $ls=$ls*1000;
         $ls=Math.round($ls);
         $ls=$ls/1000;
-        rnum=Math.floor((Math.random()*($vids[0]-5))+5);
+        const rnum=Math.floor((Math.random()*($vids[0]-5))+5);
         document.getElementById("isrc").innerHTML=$vids[rnum];
-        const $h=window.innerHeight;
+        $h=window.innerHeight;
         he=document.getElementById("hig").innerHTML;
         wi=document.getElementById("wid").innerHTML;
         r$=he/$h;
