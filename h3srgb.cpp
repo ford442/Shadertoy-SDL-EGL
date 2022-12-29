1dba1ca0f8973e663943ca98ace306325d9738b8
Noah Cohn <noahc42@gmail.com>
2022-12-29 15:35:29 -0800

    Update h3srgb.cpp

--- h3srgb.cpp
+++ h3srgb.cpp
@@ -235,8 +235,8 @@ gl.disable(gl.DITHER);
  
 gl.enable(gl.BLEND);
 gl.blendFuncSeparate(gl.DST_COLOR,gl.SRC_COLOR,gl.ONE_MINUS_DST_ALPHA,gl.ONE_MINUS_CONSTANT_ALPHA);
-gl.blendEquationSeparate(gl.MAX,gl.REVERSE_SUBTRACT);
-gl.blendColor(1.0,1.0,1.0,0.777);
+gl.blendEquationSeparate(gl.MAX,gl.FUNC_ADD);
+gl.blendColor(1.0,1.0,1.0,0.3);
  
 // gl.blendColor(0.0,0.0,0.0,0.777);
 
