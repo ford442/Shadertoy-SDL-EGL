b1ee75afde8a4a88d2a7e026a3c0ee67d514837f
Noah Cohn <noahc42@gmail.com>
2022-12-31 22:51:44 -0800

    Update b3.hpp

--- b3.hpp
+++ b3.hpp
@@ -52,7 +52,7 @@ EM_BOOL ms_l;
 
 void uni(float xx,float yy,GLfloat time,short int fram);
 
-GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+GLuint compile_shader(GLenum type,GLsizei nsources,const GLchar **dsources);
 
 using namespace std;
 using namespace std::chrono;
@@ -94,13 +94,13 @@ EM_BOOL clk_l;
 GLsizei i;
 struct timespec rem;
 struct timespec req={0,16660000};
-const char *sources[4];
+const GLchar *sources[4];
 GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 
 void renderFrame();
 
-GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+GLuint compile_shader(GLenum type,GLsizei nsources,const GLchar **dsources);
 
 extern "C"{
 void strt();
