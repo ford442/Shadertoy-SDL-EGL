d88b159c2d06bc008dfd3400e7eafa30de8393cb
Noah Cohn <noahc42@gmail.com>
2023-01-05 15:03:01 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -1,10 +1,18 @@
-#include <emscripten.h>
-#include <emscripten/html5.h>
-#include <webgl/webgl2.h>
-#include <GL/gl.h>
 void avgFrm(short int,int,float *,float *);
-EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
+
 extern "C"{
 void b3();
 void nano(short int,int,float *,float *);
 };
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
+
+#define GL_GLEXT_PROTOTYPES 1
+#define GL_FRAGMENT_PRECISION_HIGH 1
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+#include <webgl/webgl2.h>
