4acead5f1e7c31dd1457c3abb0517f8fd1650098
Noah Cohn <noahc42@gmail.com>
2023-01-05 07:03:10 -0800

    Create b3sdl.h

---
+++ include/b3sdl.h
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+#undef main
+#include "SDL_config.h"
+#include <emscripten.h>
+#include <emscripten/html5.h>
+SDL_AudioDeviceID dev;
+struct{Uint8* snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
+void cls_aud();
+void qu(int rc);
+void opn_aud();
+void SDLCALL bfr(void *unused,Uint8* stm,int len);
+void plt();
+extern "C"{
+void pl();
+};
