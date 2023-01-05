5ada73cffc73edb94b761e89fc232d880d894c64
Noah Cohn <noahc42@gmail.com>
2023-01-05 14:33:52 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -2,9 +2,9 @@
 #include <emscripten/html5.h>
 #include <webgl/webgl2.h>
 #include <GL/gl.h>
-void avgFrm(short int Fnum,int leng,float *ptr,float *aptr);
-EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
+void avgFrm(short int,int,float *,float *);
+EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
 extern "C"{
 void b3();
-void nano(short int Fnum,int leng,float *ptr,float *aptr);
+void nano(short int,int,float *,float *);
 };
