756ce359a11787ed02689c3683747fbc8527578e
Noah Cohn <noahc42@gmail.com>
2023-01-05 07:59:55 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -4,6 +4,7 @@
 #include <GL/gl.h>
 void avgFrm(short int Fnum,int leng,float *ptr,float *aptr);
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
+void clrclr(GLclampf rlc,GLclampf alp);
 extern "C"{
 void b3();
 void nano(short int Fnum,int leng,float *ptr,float *aptr);
