efd459e0af8250e4c1b8fd0d1ee30a0234d66f91
Noah Cohn <noahc42@gmail.com>
2023-01-05 08:02:28 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -7,5 +7,4 @@ EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 extern "C"{
 void b3();
 void nano(short int Fnum,int leng,float *ptr,float *aptr);
-void clr(GLclampf cllr,GLclampf alp);
 };
