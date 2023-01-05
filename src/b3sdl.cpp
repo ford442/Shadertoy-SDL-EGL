fbd4393bb9a3729163baa149de73469c46ab796f
Noah Cohn <noahc42@gmail.com>
2023-01-05 07:03:07 -0800

    Create b3sdl.cpp

---
+++ src/b3sdl.cpp
@@ -0,0 +1,45 @@
+#include <include/b3sdl.h>
+void cls_aud(){if(dev!=0){SDL_PauseAudioDevice(dev,SDL_TRUE);SDL_CloseAudioDevice(dev);dev=0;return;}};
+void qu(int rc){SDL_Quit();return;};
+void opn_aud(){
+dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){SDL_FreeWAV(wave.snd);};
+SDL_PauseAudioDevice(dev,SDL_FALSE);
+return;
+}
+void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
+int lft;
+wptr=wave.snd+wave.pos;
+lft=wave.slen-wave.pos;
+while (lft<=len){
+SDL_memcpy(stm,wptr,lft);
+stm+=lft;
+len-=lft;
+wptr=wave.snd;
+lft=wave.slen;
+wave.pos=0;
+}
+SDL_memcpy(stm,wptr,len);
+wave.pos+=len;
+return;
+}
+void plt(){
+char flnm[24];
+SDL_FreeWAV(wave.snd);
+SDL_SetMainReady();
+if (SDL_Init(SDL_INIT_AUDIO)<0){
+qu(1);
+}
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
+qu(1);
+}
+wave.pos=0;
+wave.spec.callback=bfr;
+opn_aud();
+return;
+}
+extern "C" {
+void pl(){plt();return;};
+};
