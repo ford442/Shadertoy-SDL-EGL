5e0d0e65054bc38f47172e29b257ab0658611ed6
Noah Cohn <noahc42@gmail.com>
2023-01-05 07:55:33 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -1,5 +1,7 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
+#include <webgl/webgl2.h>
+#include <GL/gl.h>
 void avgFrm(short int Fnum,int leng,float *ptr,float *aptr);
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 extern "C"{
