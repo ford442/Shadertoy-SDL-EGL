e020dcd34f2223dd2463ee04994552f740b1fedb
Noah Cohn <noahc42@gmail.com>
2023-01-05 14:54:25 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -1,9 +1,8 @@
-#include <emscripten.h>
-#include <emscripten/html5.h>
-
-EMSCRIPTEN_RESULT ret;
-EM_BOOL ms_l,clk_l;
-EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
+extern "C"{
+  
+void str();
+   
+};
 
 float F=1.0f,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,Fm1=-1.0f,F0=0.0f,Ttime,TtimeDelta;
 short int iFrame,iFps,Size;
@@ -26,41 +25,24 @@ struct timespec rem;
 struct timespec req={0,15000000};
 
 #define GL_GLEXT_PROTOTYPES 1
-#define EGL_EGLEXT_PROTOTYPES 1
 #define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1 //maybe??
-
 #include <GL/gl.h>
 #include <GL/glext.h>
-#include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3platform.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
-#include <webgl/webgl2.h>
+
+extern "C"{
+  
+void clr(GLclampf,GLclampf);
+  
+};
 
 GLclampf x,y,y1y=1.0,gF=F,gF0=F0,gFm1=Fm1;
 GLfloat g1g=1.0,S;
-EGLint v0=0,v3=3;
-GLuint uni_mse,shader,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm;
 GLsizei s4=4,i;
-const GLchar *sources[4];
-GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
-GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
-GLuint compile_shader(GLenum,GLsizei,const GLchar **);
+GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res,VCO,ECO,vtx,frag,uni_mse,shader,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm;
+GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9,indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 typedef struct{GLclampf XYZW[4];}Vertex;
 const Vertex vertices[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
-GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res,VCO,ECO,vtx,frag;
-EGLDisplay display;
-EGLSurface surface;
-EGLContext contextegl;
-EmscriptenWebGLContextAttributes attr;
-EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
-EGLint config_size,major,minor,atb_pos;
-EGLConfig eglconfig;
+const GLchar *sources[4];
 
 const GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
@@ -85,9 +67,27 @@ const GLchar* vertex_shader_body=vertex_shader_body_gles3;
 const GLchar* fragment_shader_header=fragment_shader_header_gles3;
 const GLchar* fragment_shader_footer=fragment_shader_footer_gles3;
 
+void uni(float,float,GLfloat,short int);
+
+GLuint compile_shader(GLenum,GLsizei,const GLchar **);
+
+void clrclr(GLclampf,GLclampf);
+
+static const char8_t *read_file(const GLchar *);
+
+#define EGL_EGLEXT_PROTOTYPES 1
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+EGLint v0=0,v3=3,config_size,major,minor,atb_pos;
+EGLDisplay display;
+EGLSurface surface;
+EGLContext contextegl;
+EGLConfig eglconfig;
+
 const EGLint attribut_list[]={ 
-EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
-EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
 EGL_NONE};
 
@@ -103,7 +103,7 @@ EGL_NONE};
 const EGLint attribute_list[]={
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
-EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
+// EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
 //  EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
 EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT,
 EGL_CONFORMANT,EGL_OPENGL_BIT,
@@ -126,18 +126,20 @@ EGL_SAMPLES,128,
 EGL_NONE
 };
 
-void uni(float,float,GLfloat,short int);
+#define GL3_PROTOTYPES 1
+#define GL4_PROTOTYPES 1 //maybe??
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
+#include <GLES3/gl3platform.h>
 
-GLuint compile_shader(GLenum,GLsizei,const GLchar **);
+#include <webgl/webgl2.h>
 
-void clrclr(GLclampf,GLclampf);
+#include <emscripten.h>
+#include <emscripten/html5.h>
 
-static const char8_t *read_file(const GLchar *);
-
-extern "C"{
-  
-void str();
-  
-void clr(GLclampf,GLclampf);
-  
-};
+EmscriptenWebGLContextAttributes attr;
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
+EMSCRIPTEN_RESULT ret;
+EM_BOOL ms_l,clk_l;
+EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
