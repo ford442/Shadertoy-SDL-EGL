39f44acb4d1d06ccdeceda590ec4e053c8b5154d
Noah Cohn <noahc42@gmail.com>
2023-01-05 15:06:48 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -7,7 +7,7 @@ void str();
 float F=1.0f,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,Fm1=-1.0f,F0=0.0f,Ttime,TtimeDelta;
 short int iFrame,iFps,Size;
 double wi,hi;
-const char *fileloc="/shader/shader1.toy";
+const char * fileloc="/shader/shader1.toy";
 
 void renderFrame();
 
@@ -42,7 +42,7 @@ GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res,VCO,ECO,vtx,frag,uni_mse,shader,un
 GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9,indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 typedef struct{GLclampf XYZW[4];}Vertex;
 const Vertex vertices[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
-const GLchar *sources[4];
+const GLchar * sources[4];
 
 const GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
@@ -62,10 +62,10 @@ const GLchar fragment_shader_header_gles3[]=
 const GLchar fragment_shader_footer_gles3[]=
 "\n void main(){mainImage(fragColor,gl_FragCoord.xy);fragColor.a=1.0;}\0";
 
-const GLchar* common_shader_header=common_shader_header_gles3;
-const GLchar* vertex_shader_body=vertex_shader_body_gles3;
-const GLchar* fragment_shader_header=fragment_shader_header_gles3;
-const GLchar* fragment_shader_footer=fragment_shader_footer_gles3;
+const GLchar * common_shader_header=common_shader_header_gles3;
+const GLchar * vertex_shader_body=vertex_shader_body_gles3;
+const GLchar * fragment_shader_header=fragment_shader_header_gles3;
+const GLchar * fragment_shader_footer=fragment_shader_footer_gles3;
 
 void uni(float,float,GLfloat,short int);
 
@@ -73,7 +73,7 @@ GLuint compile_shader(GLenum,GLsizei,const GLchar **);
 
 void clrclr(GLclampf,GLclampf);
 
-static const char8_t *read_file(const GLchar *);
+static const char8_t * read_file(const GLchar *);
 
 #define EGL_EGLEXT_PROTOTYPES 1
 #include <EGL/egl.h>
