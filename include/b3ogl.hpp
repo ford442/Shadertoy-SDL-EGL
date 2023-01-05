2c43c9b5d3ec41470de46a0d7fbfec879737f872
Noah Cohn <noahc42@gmail.com>
2023-01-05 12:45:12 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -19,53 +19,27 @@ EMSCRIPTEN_RESULT ret;
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
 #include <webgl/webgl2.h>
+
 using namespace std;
 using namespace std::chrono;
-void clrclr(GLclampf rlc,GLclampf alp);
-static const char8_t *read_file(const GLchar *filename);
-GLclampf x;
-GLclampf y;
-GLclampf y1y=1.0;
-GLfloat g1g=1.0;
-EM_BOOL ms_l;
-void uni(float xx,float yy,GLfloat time,short int fram);
-GLuint compile_shader(GLenum type,GLsizei nsources,const GLchar **dsources);
+
+GLclampf x,y,y1y=1.0,gF=F,gF0=F0,gFm1=Fm1;
+GLfloat g1g=1.0,S;
+EGLint v0=0,v3=3;
+EM_BOOL ms_l,clk_l;
+GLuint uni_mse,shader,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm;
+short int iFrame,iFps,Size;
+GLsizei s4=4,i;
+double wi,hi;
+float F=1.0f,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,Fm1=-1.0f,F0=0.0f,Ttime,TtimeDelta;
 // steady_clock::time_point t1;
 // steady_clock::time_point t2;
 // steady_clock::time_point t3;
 high_resolution_clock::time_point t1;
 high_resolution_clock::time_point t2;
 high_resolution_clock::time_point t3;
-// system_clock::time_point t1;
-// system_clock::time_point t2;
-// system_clock::time_point t3;
-GLuint uni_mse;
-GLuint uni_frm;
-GLuint uni_tme;
-GLuint uni_tme_dlt;
-GLuint uni_res;
-GLuint uni_srate;
-GLuint shader;
-float Ttime,TtimeDelta;
-short int iFrame,iFps;
-GLsizei s4=4;
-// int a,b;
-float F=1.0f;
-float F0=0.0f;
-float Fm1=-1.0f;
-float D=1.0;
-float D0=0.0;
-float Dm1=-1.0;
-float mouseX;
-float mouseY;
-float cMouseX;
-float cMouseY;
-short int Size;
-GLfloat S;
-EM_BOOL clk_l;
-GLsizei i;
 struct timespec rem;
-struct timespec req={0,16666666};
+struct timespec req={0,15000000};
 const GLchar *sources[4];
 GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
@@ -73,32 +47,11 @@ void renderFrame();
 GLuint compile_shader(GLenum type,GLsizei nsources,const GLchar **dsources);
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 const char *fileloc="/shader/shader1.toy";
-EGLint v0=0,v3=3;
-GLclampf gF=F;
-GLclampf gF0=F0;
-GLclampf gFm1=Fm1;
+
 typedef struct{GLclampf XYZW[4];}Vertex;
 const Vertex vertices[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
-const GLchar common_shader_header_gles3[]=
-"#version 300 es\n"
-"#undef HW_PERFORMANCE\n"
-"#define HW_PERFORMANCE 0\n"
-"precision highp float;precision highp int;precision highp sampler3D;precision highp sampler2D;\n";
-const GLchar vertex_shader_body_gles3[]=
-"\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
-const GLchar fragment_shader_header_gles3[]=
-"\n uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
-"\n uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
-"\n uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
-"\n out vec4 fragColor;\n";
-const GLchar fragment_shader_footer_gles3[]=
-"\n void main(){mainImage(fragColor,gl_FragCoord.xy);fragColor.a=1.0;}\0";
-const GLchar* common_shader_header=common_shader_header_gles3;
-const GLchar* vertex_shader_body=vertex_shader_body_gles3;
-const GLchar* fragment_shader_header=fragment_shader_header_gles3;
-const GLchar* fragment_shader_footer=fragment_shader_footer_gles3;
-GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res;
-GLuint VCO,ECO,vtx,frag;
+
+GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res,VCO,ECO,vtx,frag;
 EGLDisplay display;
 EGLSurface surface;
 EGLContext contextegl;
@@ -106,12 +59,36 @@ EmscriptenWebGLContextAttributes attr;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 EGLint config_size,major,minor,atb_pos;
 EGLConfig eglconfig;
-double wi,hi;
+
+const GLchar common_shader_header_gles3[]=
+"#version 300 es\n"
+"#undef HW_PERFORMANCE\n"
+"#define HW_PERFORMANCE 0\n"
+"precision highp float;precision highp int;precision mediump sampler3D;precision highp sampler2D;\n";
+
+const GLchar vertex_shader_body_gles3[]=
+"\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
+
+const GLchar fragment_shader_header_gles3[]=
+"\n uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
+"\n uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
+"\n uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
+"\n out vec4 fragColor;\n";
+
+const GLchar fragment_shader_footer_gles3[]=
+"\n void main(){mainImage(fragColor,gl_FragCoord.xy);fragColor.a=1.0;}\0";
+
+const GLchar* common_shader_header=common_shader_header_gles3;
+const GLchar* vertex_shader_body=vertex_shader_body_gles3;
+const GLchar* fragment_shader_header=fragment_shader_header_gles3;
+const GLchar* fragment_shader_footer=fragment_shader_footer_gles3;
+
 const EGLint attribut_list[]={ 
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
 EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
 EGL_NONE};
+
 const EGLint anEglCtxAttribs2[]={
 EGL_CONTEXT_CLIENT_VERSION,3,
 EGL_CONTEXT_MINOR_VERSION_KHR,0,
@@ -120,6 +97,7 @@ EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
 // EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
 EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
 EGL_NONE};
+
 const EGLint attribute_list[]={
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
@@ -145,7 +123,19 @@ EGL_SAMPLE_BUFFERS,256,
 EGL_SAMPLES,128,
 EGL_NONE
 };
+
+void uni(float xx,float yy,GLfloat time,short int fram);
+
+GLuint compile_shader(GLenum type,GLsizei nsources,const GLchar **dsources);
+
+void clrclr(GLclampf rlc,GLclampf alp);
+
+static const char8_t *read_file(const GLchar *filename);
+
 extern "C"{
+  
 void str();
+  
 void clr(GLclampf cllr,GLclampf alp);
+  
 };
