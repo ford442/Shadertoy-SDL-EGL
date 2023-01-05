325fc47234ccbef678706bf5403426790118589c
Noah Cohn <noahc42@gmail.com>
2023-01-05 15:05:33 -0800

    Update b3sdl.hpp

--- include/b3sdl.hpp
+++ include/b3sdl.hpp
@@ -1,20 +1,19 @@
-
 extern "C"{
   
 #include <SDL2/SDL.h>
 #undef main
 #include "SDL_config.h"
-#include <emscripten.h>
-#include <emscripten/html5.h>
-#include <iostream>
 
 SDL_AudioDeviceID dev;
-struct{Uint8* snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
+struct{Uint8 * snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
 void cls_aud();
 void qu(int rc);
 void opn_aud();
-void SDLCALL bfr(void *,Uint8*,int);
+void SDLCALL bfr(void *,Uint8 *,int);
 void plt();
 void pl();
   
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <iostream>
 };
