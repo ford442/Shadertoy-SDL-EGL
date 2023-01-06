8d24a8602130366fc3473e958f3605498ec8cfc0
Noah Cohn <noahc42@gmail.com>
2023-01-05 19:47:47 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -48,9 +48,11 @@ const GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
 "#undef HW_PERFORMANCE\n"
 "#define HW_PERFORMANCE 0\n"
+"#define GL_FRAGMENT_PRECISION_HIGH 1\n"
 "precision highp float;precision mediump int;precision mediump sampler3D;precision mediump sampler2D;\n";
 
 const GLchar vertex_shader_body_gles3[]=
+"#define GL_FRAGMENT_PRECISION_HIGH 1\n"
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
 
 const GLchar fragment_shader_header_gles3[]=
