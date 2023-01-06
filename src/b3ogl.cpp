6bda9a793387efb066d9a5728ca4376dd75cf8cf
Noah Cohn <noahc42@gmail.com>
2023-01-06 05:48:15 -0800

    Update b3ogl.cpp

--- src/b3ogl.cpp
+++ src/b3ogl.cpp
@@ -17,7 +17,7 @@ return 1;
 
 void clrclr(GLclampf rlc,GLclampf alc,GLclampf avr){
 glBlendColor(y1y-rlc,y1y-rlc,y1y-rlc,alc);
-glClearColor(avr,avr,avr,rlc);
+glClearColor(avr,avr,avr,y1y);
 return;
 }
 
