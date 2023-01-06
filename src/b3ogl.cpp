e6889bbac645527e480023a5ae561ae602dbc342
Noah Cohn <noahc42@gmail.com>
2023-01-06 01:18:57 -0800

    Update b3ogl.cpp

--- src/b3ogl.cpp
+++ src/b3ogl.cpp
@@ -191,7 +191,7 @@ glClearDepth(D);
  //  swap alpha to use one_minus_alpha for 'source'
 glEnable(GL_BLEND);
 glBlendFuncSeparate(GL_DST_COLOR,GL_SRC_COLOR,GL_DST_COLOR,GL_SRC_ALPHA);
-glBlendEquationSeparate(GL_MAX,GL_MIN);
+glBlendEquationSeparate(GL_MAX,GL_MAX);
 glEnable(GL_SCISSOR_TEST);
 glScissor((GLint)0,(GLint)0,(GLsizei)Size,(GLsizei)Size);
 // glDisable(GL_DITHER);
