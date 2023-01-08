01aa4b60364fe6455884e9be727a48d3c957d48e
Noah Cohn <noahc42@gmail.com>
2023-01-08 04:25:30 -0800

    Update pagec.js

--- pagec.js
+++ pagec.js
@@ -123,6 +123,7 @@ function scanShaders(){
 const dxhttp=new XMLHttpRequest();
 dxhttp.onreadystatechange=function(){
 if(this.readyState==4&&this.status==200){
+ console.log(this);
 shds(this);
 }};
 dxhttp.open('GET','https://glsl.1ink.us/shaders/',true);
