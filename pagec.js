062af458e019cdda4d5c228d5272ce4600116b94
Noah Cohn <noahc42@gmail.com>
2023-01-08 11:45:30 -0800

    Update pagec.js

--- pagec.js
+++ pagec.js
@@ -252,8 +252,9 @@ vide[0].id=lo;
 vide[1].id=mv;
 document.getElementById('mv').play();
 document.getElementById('ldv').src=document.getElementById('isrc').innerHTML;
+document.getElementById('ldv').height=document.innerHeight;
+
 document.getElementById('ldv').currentTime=document.getElementById('itim').innerHTML;
- //   document.getElementById('ldv').height=document.innerHeight;
 
 document.getElementById('ldv').load();
 
