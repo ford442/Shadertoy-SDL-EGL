305af84fc51d0bba2dbfdb91b048ef3fb3b2cd57
Noah Cohn <noahc42@gmail.com>
2023-01-11 05:20:16 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -9,11 +9,3 @@ void nano(short int,int,float *,float *);
 
 void avgFrm(short int,int,float *,float *);
 
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
-
-#include <GL/gl.h>
-#include <GL/glext.h>
-
-#include <webgl/webgl2.h>
