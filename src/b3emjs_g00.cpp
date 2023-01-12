0ee3cce662f25413e329f6473e369a589bad6dd9
Noah Cohn <noahc42@gmail.com>
2023-01-12 15:11:25 -0800

    Update b3emjs_g00.cpp

--- src/b3emjs_g00.cpp
+++ src/b3emjs_g00.cpp
@@ -46,8 +46,8 @@ EM_JS(void,ma,(),{
 
 var vv=document.getElementById("mcanvas");
 
-var w$=parseInt(document.getElementById("wid").innerHTML,10);
-var h$=parseInt(document.getElementById("hig").innerHTML,10);
+var w$=1353;//parseInt(document.getElementById("wid").innerHTML,10);
+var h$=1353;//parseInt(document.getElementById("hig").innerHTML,10);
 const $H=Module.HEAPF32.buffer;
 var la=h$*h$*4;
 var pointa=77*la;
@@ -94,12 +94,12 @@ g2.addNativeFunction('Ave',glslAve,{returnType:'Number'});
 const R=g2.createKernel(function(tv){
 const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0]*0.8,Pa[1],Pa[2]*1.2);
-}).setTactic("speed").setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setOutput([sz]);
+}).setTactic("speed").setDynamicOutput(true).setArgumentTypes(["HTMLCanvas"]).setOutput([sz]);
 const t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 return[P[0],P[1],P[2],av$];
-}).setTactic("precision").setArgumentTypes(["HTMLCanvas"]).setPipeline(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setTactic("precision").setPipeline(true).setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setOutput([w$,h$]);
 const r=g.createKernel(function(f){
 const p=f[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
 const $amax=this.constants.amax;
