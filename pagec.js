213c614931696516640da8194deaa3a19a88c5f5
Noah Cohn <noahc42@gmail.com>
2023-01-12 07:46:40 -0800

    Update pagec.js

--- pagec.js
+++ pagec.js
@@ -19,7 +19,8 @@ function normalResStart(){
 setTimeout(function(){
 Module.ccall('str');},250);
 setTimeout(function(){
-Module.ccall('b3');},450);
+// Module.ccall('b3');
+},450);
 setTimeout(function(){
 const bz=new BroadcastChannel('bez');
 bz.postMessage({data:222});},50);
