3cfa4d7faf42cd3f79e919e5f4fcc83693523b7a
Noah Cohn <noahc42@gmail.com>
2023-01-12 15:32:19 -0800

    Update b3emjs_g00.cpp

--- src/b3emjs_g00.cpp
+++ src/b3emjs_g00.cpp
@@ -94,12 +94,12 @@ g2.addNativeFunction('Ave',glslAve,{returnType:'Number'});
 const R=g2.createKernel(function(tv){
 const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0]*0.8,Pa[1],Pa[2]*1.2);
-}).setTactic("precision").setPrecision('single').setArgumentTypes(["HTMLCanvas"]).setPipeline(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setTactic("speed").setOutput([sz]);
 const t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 return[P[0],P[1],P[2],av$];
-}).setTactic("precision").setPrecision('single').setArgumentTypes(["HTMLCanvas"]).setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setPrecision('single').setTactic("precision").setPipeline(true).setOutput([w$,h$]);
 const r=g.createKernel(function(f){
 const p=f[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
 const $amax=this.constants.amax;
@@ -110,7 +110,7 @@ const Min=(4.0*(($amax-($aavg-$amin))/2.0));
 const ouT=Math.max(Min,alph);
 const aveg=Aveg(p[3],ouT);
 this.color(p[0],p[1],p[2],aveg);
-}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setPrecision('single').setTactic("precision").setGraphical(true).setOutput([w$,h$]);
 // gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);
 gl.disable(gl.DITHER);
 gl.drawingBufferColorSpace='display-p3';
