2521243266d8e34ab00ccfc6d690f55e81d74cf3
Noah Cohn <noahc42@gmail.com>
2023-01-12 14:46:06 -0800

    Update b3emjs_g00.cpp

--- src/b3emjs_g00.cpp
+++ src/b3emjs_g00.cpp
@@ -46,8 +46,8 @@ EM_JS(void,ma,(),{
 
 var vv=document.getElementById("mcanvas");
 
-var w$=parseInt(document.getElementById("wid").innerHTML,10);
-var h$=parseInt(document.getElementById("hig").innerHTML,10);
+var w$=parseInt(document.innerHeight,10);
+var h$=parseInt(document.innerHeight,10);
 const $H=Module.HEAPF32.buffer;
 var la=h$*h$*4;
 var pointa=77*la;
