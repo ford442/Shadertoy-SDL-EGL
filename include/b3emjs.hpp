8e10fd4146b59b6fcca7f236964d1300a34d40bc
Noah Cohn <noahc42@gmail.com>
2023-01-13 02:36:51 -0800

    Update b3emjs.hpp

--- include/b3emjs.hpp
+++ include/b3emjs.hpp
@@ -1,6 +1,8 @@
 
 #include <emscripten.h>
 
+EmscriptenWebGLContextAttributes attr_js;
+
 extern "C"{
 void b3();
 void nano(short int,int,float *,float *);
@@ -8,3 +10,13 @@ void nano(short int,int,float *,float *);
 
 void avgFrm(short int,int,float *,float *);
 
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx_js;
+EGLContext contextegl_js;
+
+#include "../include/b3gl.hpp"
+#include "../include/b3egl.hpp"
+
+EGLDisplay display_js;
+EGLSurface surface_js;
+EGLConfig eglconfig_js;
+EGLint config_size_js,major_js,minor_js;
