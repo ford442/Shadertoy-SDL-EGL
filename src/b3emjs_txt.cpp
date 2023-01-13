bbdda7402dc83915facd2bd8e7f5d1cb471f6b2a
Noah Cohn <noahc42@gmail.com>
2023-01-12 16:53:24 -0800

    Update b3emjs_txt.cpp

--- src/b3emjs_txt.cpp
+++ src/b3emjs_txt.cpp
@@ -144,7 +144,7 @@ const Min=(4.0*(($amax-($aavg-$amin))/2.0));
 const ouT=Math.max(Min,alph);
 const aveg=Aveg(p[3],ouT);
 this.color(p[0],p[1],p[2],aveg);
-}).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setTactic("precision").setArgumentTypes(["HTMLCanvas"]).setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
 // gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);
 gl.disable(gl.DITHER);
 gl.drawingBufferColorSpace='display-p3';
