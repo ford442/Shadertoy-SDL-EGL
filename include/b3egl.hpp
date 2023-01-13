e234b8c492da59f9f6c31106c8d1964556a04e21
Noah Cohn <noahc42@gmail.com>
2023-01-13 02:22:24 -0800

    Create b3egl.hpp

---
+++ include/b3egl.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
+#include <GLES3/gl3platform.h>
+
+#define EGL_EGLEXT_PROTOTYPES 1
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
