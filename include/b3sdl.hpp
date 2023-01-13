a066af76e947190a1535ccce59d0b16bdadaa163
Noah Cohn <noahc42@gmail.com>
2023-01-13 10:14:43 -0800

    Update b3sdl.hpp

--- include/b3sdl.hpp
+++ include/b3sdl.hpp
@@ -10,16 +10,7 @@ void pl();
 
 SDL_AudioDeviceID dev;
 
-
-
 struct{Uint8 * snd;int pos;Uint32 slen;SDL_AudioSpec request;SDL_AudioSpec recieve;}wave;
-
-SDL_memset(&wave.request,0,sizeof(wave.request));
-
-wave.request.freq=44100;
-wave.request.format=AUDIO_F32;
-wave.request.channels=2;
-wave.request.samples=4096;
 
 void cls_aud();
 
