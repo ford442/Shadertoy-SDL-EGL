2513930966ea55c906f993fc02bc88b718affd27
Noah Cohn <noahc42@gmail.com>
2023-01-15 10:32:23 -0800

    Update b3emjs_g00.cpp

--- src/b3emjs_g00.cpp
+++ src/b3emjs_g00.cpp
@@ -1,5 +1,82 @@
 #include "../include/b3emjs.hpp"
 
+void egl(){
+emscripten_webgl_init_context_attributes(&attr_js);
+attr_js.alpha=EM_TRUE;
+attr_js.stencil=EM_FALSE;
+attr_js.depth=EM_TRUE;
+attr_js.antialias=EM_TRUE;
+attr_js.premultipliedAlpha=EM_FALSE;
+attr_js.preserveDrawingBuffer=EM_FALSE;
+attr_js.enableExtensionsByDefault=EM_TRUE;
+attr_js.renderViaOffscreenBackBuffer=EM_FALSE;
+attr_js.powerPreference=EM_WEBGL_POWER_PREFERENCE_HIGH_PERFORMANCE;
+attr_js.failIfMajorPerformanceCaveat=EM_FALSE;
+attr_js.majorVersion=2;
+attr_js.minorVersion=0;
+ctx_js=emscripten_webgl_create_context("#mcanvas",&attr_js);
+eglBindAPI(EGL_OPENGL_API);
+display_js=eglGetDisplay(EGL_DEFAULT_DISPLAY);
+eglInitialize(display_js,&major_js,&minor_js);
+eglChooseConfig(display_js,attribute_list,&eglconfig_js,(EGLint)1,&config_size_js);
+contextegl_js=eglCreateContext(display_js,eglconfig_js,EGL_NO_CONTEXT,anEglCtxAttribs2);
+surface_js=eglCreateWindowSurface(display_js,eglconfig_js,(NativeWindowType)0,attribut_list);
+eglMakeCurrent(display_js,surface_js,surface_js,contextegl_js);
+emscripten_webgl_make_context_current(ctx_js);
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_color_buffer_float");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_color_buffer_half_float");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_blend_equation_advanced_coherent");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_depth_texture");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_draw_buffers");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_provoking_vertex");
+emscripten_webgl_enable_extension(ctx_js,"WEBGL_webcodecs_video_frame");
+emscripten_webgl_enable_extension(ctx_js,"OES_texture_float_linear");
+emscripten_webgl_enable_extension(ctx_js,"OES_texture_half_float_linear");
+emscripten_webgl_enable_extension(ctx_js,"OES_element_index_uint");
+emscripten_webgl_enable_extension(ctx_js,"OES_fbo_render_mipmap");
+emscripten_webgl_enable_extension(ctx_js,"OES_vertex_array_object");
+emscripten_webgl_enable_extension(ctx_js,"OES_draw_buffers_indexed");
+emscripten_webgl_enable_extension(ctx_js,"OES_depth32");
+emscripten_webgl_enable_extension(ctx_js,"OES_fixed_point");
+emscripten_webgl_enable_extension(ctx_js,"OES_shader_multisample_interpolation");
+emscripten_webgl_enable_extension(ctx_js,"OES_single_precision");
+emscripten_webgl_enable_extension(ctx_js,"OES_standard_derivatives");
+emscripten_webgl_enable_extension(ctx_js,"EXT_float_blend");
+emscripten_webgl_enable_extension(ctx_js,"EXT_frag_depth");
+emscripten_webgl_enable_extension(ctx_js,"EXT_shader_texture_lod");
+emscripten_webgl_enable_extension(ctx_js,"EXT_sRGB");
+// emscripten_webgl_enable_extension(ctx,"EXT_blend_minmax");
+emscripten_webgl_enable_extension(ctx_js,"EXT_disjoint_timer_query");
+emscripten_webgl_enable_extension(ctx_js,"EXT_clip_cull_distance");
+emscripten_webgl_enable_extension(ctx_js,"EXT_disjoint_timer_query_webgl2");
+emscripten_webgl_enable_extension(ctx_js,"EXT_framebuffer_sRGB");
+emscripten_webgl_enable_extension(ctx_js,"ANGLE_instanced_arrays");
+emscripten_webgl_enable_extension(ctx_js,"KHR_parallel_shader_compile");
+emscripten_webgl_enable_extension(ctx_js,"EXT_texture_shadow_lod");
+emscripten_webgl_enable_extension(ctx_js,"NV_memory_attachment");
+emscripten_webgl_enable_extension(ctx_js,"EGL_NV_context_priority_realtime");
+emscripten_webgl_enable_extension(ctx_js,"NV_depth_nonlinear");
+emscripten_webgl_enable_extension(ctx_js,"EGL_HI_colorformats");
+emscripten_webgl_enable_extension(ctx_js,"EGL_EXT_pixel_format_float");
+emscripten_webgl_enable_extension(ctx_js,"EGL_EXT_gl_colorspace_bt2020_pq");
+emscripten_webgl_enable_extension(ctx_js,"EGL_EXT_gl_colorspace_bt2020_linear");
+emscripten_webgl_enable_extension(ctx_js,"EGL_EXT_gl_colorspace_display_p3");
+emscripten_webgl_enable_extension(ctx_js,"ARB_multisample");
+// emscripten_webgl_enable_extension(ctx_js,"ARB_robustness");
+// emscripten_webgl_enable_extension(ctx_js,"ARB_robust_buffer_access_behavior");
+// emscripten_webgl_enable_extension(ctx_js,"ARB_gl_spirv");
+emscripten_webgl_enable_extension(ctx_js,"ARB_spirv_extensions");
+emscripten_webgl_enable_extension(ctx_js,"ARB_ES2_compatibility");
+emscripten_webgl_enable_extension(ctx_js,"ARB_ES3_compatibility ");
+emscripten_webgl_enable_extension(ctx_js,"ARB_ES3_1_compatibility");
+emscripten_webgl_enable_extension(ctx_js,"ARB_ES3_2_compatibility");
+// emscripten_webgl_enable_extension(ctx_js,"EXT_gpu_shader4");
+// emscripten_webgl_enable_extension(ctx_js,"EXT_gpu_shader5");
+glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_FASTEST);
+// glDisable(GL_DITHER);
+return;
+}
+
 void avgFrm(short int Fnum,int leng,float * ptr,float * aptr){
 float max=0.0;
 float min=1.0;
@@ -44,33 +121,74 @@ EM_JS(void,ma,(),{
 
 // "use strict";
 
+const pnnl=document.body;
+var vv=document.getElementById("mcanvas");
+var intervalLoop=null;
+var f;
+var loopLoop;
+var a,b;
+var stp;
+var mmvv;
  
-var vv=document.getElementById('mcanvas');
-var w$=(document.getElementById("wid").innerHTML,10);
-var h$=(document.getElementById("hig").innerHTML,10);
-const $H=Module.HEAPF32.buffer;
+function backForth(stp){
+loopLoop=true;
+f=true;
+a=(stp-1);
+b=(stp+1);
+}
+
+function stpBackForth(){
+loopLoop=false;
+}
+
+function doKey(e){
+if(e.code=='Space'){
+e.preventDefault();
+};
+if (e.code=='KeyZ'){
+mmvv=document.getElementById("mv");
+mmvv.pause();
+stp=mmvv.currentTime;
+// console.log(stp);
+loopLoop=true;
+f=false;
+// backForth(stp);
+};
+if(e.code=='KeyX'){
+stpBackForth();
+vv.play();
+};
+}
+
+pnnl.addEventListener('keydown',doKey);
+// var w$=parseInt(document.getElementById("wid").innerHTML,10);
+// var h$=parseInt(document.getElementById("hig").innerHTML,10);
+var h$=vv.height;
+var w$=vv.width;
+const $H=Module.HEAPF64.buffer;
 var la=h$*h$*4;
 var pointa=77*la;
-var agav=new Float32Array($H,pointa,300);
-var sz=Math.round((h$*h$)/8);
+var agav=new Float64Array($H,pointa,300);
+var sz=(h$*h$)/8;
 var avag=0.750;
 var min=1.000;
 var max=0.000;
 agav.fill(avag,0,33);
 agav.fill(min,100,33);
 agav.fill(max,200,33);
-const bcanvas=document.getElementById("bcanvas");
-const gl=bcanvas.getContext("webgl2",{
+var bcanvas=document.getElementById("bcanvas");
+var gl=bcanvas.getContext("webgl2",{
 colorType:'float64',
 preferLowPowerToHighPerformance:false,
-precision:'highp',logarithmicDepthBuffer:true,
+precision:'highp',
+logarithmicDepthBuffer:true,
 colorSpace:'display-p3',
 alpha:true,
 depth:true,
-stencil:true,
+stencil:false,
 imageSmoothingEnabled:false,
-imageSmoothingQuality:'high',
-preserveDrawingBuffer:true,
+imageSmoothingQuality:'medium',
+preserveDrawingBuffer:false,
 premultipliedAlpha:false,
 desynchronized:false,
 lowLatency:true,
@@ -99,7 +217,7 @@ const t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 return[P[0],P[1],P[2],av$];
-}).setTactic("precision").setArgumentTypes(["HTMLCanvas"]).setPipeline(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setTactic("precision").setPrecision("single").setPipeline(true).setArgumentTypes(["HTMLCanvas"]).setDynamicOutput(true).setOutput([w$,h$]);
 const r=g.createKernel(function(f){
 const p=f[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
 const $amax=this.constants.amax;
@@ -110,9 +228,14 @@ const Min=(4.0*(($amax-($aavg-$amin))/2.0));
 const ouT=Math.max(Min,alph);
 const aveg=Aveg(p[3],ouT);
 this.color(p[0],p[1],p[2],aveg);
-}).setTactic("precision").setArgumentTypes(["HTMLCanvas"]).setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
-// gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);
-gl.disable(gl.DITHER);
+}).setTactic("precision").setPrecision("single").setGraphical(true).setDynamicOutput(true).setOutput([w$,h$]);
+// gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);  // <- crazy effect!
+gl.blendColor(1.0,1.0,1.0,1.0);
+gl.blendFuncSeparate(gl.DST_COLOR,gl.SRC_COLOR,gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
+gl.blendEquationSeparate(gl.FUNC_SUBTRACT,gl.MAX);
+// gl.enable(gl.BLEND);  //  webgl2 messed up effect
+// gl.unpackColorSpace='display-p3';  // very slow
+gl.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
 gl.drawingBufferColorSpace='display-p3';
 gl.getExtension('WEBGL_color_buffer_float');
 gl.getExtension('WEBGL_color_buffer_half_float');
@@ -148,15 +271,12 @@ gl.getExtension('GL_NV_memory_attachment');
 gl.getExtension('NV_depth_nonlinear');
 gl.getExtension('EXT_gl_colorspace_display_p3');
 gl.getExtension('GL_ARB_multisample');
-gl.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
-gl.blendColor(1.0,1.0,1.0,1.0);
-gl.blendFuncSeparate(gl.DST_COLOR,gl.SRC_COLOR,gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
-gl.blendEquationSeparate(gl.FUNC_SUBTRACT,gl.MAX);
-// gl.enable(gl.BLEND);
-// gl.unpackColorSpace='display-p3';  // very slow
-w$=parseInt(document.getElementById("wid").innerHTML,10);
-h$=parseInt(document.getElementById("hig").innerHTML,10);
-vv=document.getElementById('mcanvas');
+gl.disable(gl.DITHER);
+// w$=parseInt(document.getElementById("wid").innerHTML,10);
+// h$=parseInt(document.getElementById("hig").innerHTML,10);
+vv=document.getElementById("mcanvas");
+var h$=vv.height;
+var w$=vv.width;
 var blank$=Math.max((((w$-h$)*0)/2),0);
 var nblank$=Math.max((((h$-w$)*0)/2),0);
 var l=w$*h$*16;
@@ -164,16 +284,14 @@ la=h$*h$*4;
 var al=w$*h$*8;
 sz=(h$*h$)/8;
 pointa=77*la;
-agav=new Float32Array($H,pointa,300);
+agav=new Float64Array($H,pointa,300);
 R.setOutput([sz]);
- 
 for(i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);");
 }
- 
 var pointb=77*la;
-var $B=new Float32Array($H,pointb,sz);
+var $B=new Float64Array($H,pointb,sz);
 var $F=1;
 var $Bu=33;
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
@@ -181,11 +299,13 @@ t.setConstants({nblnk:nblank$,blnk:blank$});
 var $$1=t(vv);
 for(var i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);$"+j+".set($$1);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);$"+j+".set($$1);");
 }
 var d=S();if(d)d();d=S();function S(){
-w$=parseInt(document.getElementById("wid").innerHTML,10);
-h$=parseInt(document.getElementById("hig").innerHTML,10);
+// w$=parseInt(document.getElementById("wid").innerHTML,10);
+// h$=parseInt(document.getElementById("hig").innerHTML,10);
+var h$=vv.height;
+var w$=vv.width;
 var blank$=Math.max((((w$-h$)*0)/2),0);
 var nblank$=Math.max((((h$-w$)*0)/2),0);
 l=w$*h$*16;
@@ -193,19 +313,20 @@ la=h$*h$*4;
 al=w$*h$*8;
 sz=(h$*h$)/8;
 pointa=77*la;
-var agav=new Float32Array($H,pointa,300);
+var agav=new Float64Array($H,pointa,300);
 R.setOutput([sz]);
 for(var i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);");
 }
 pointb=66*la;
-var $B=new Float32Array($H,pointb,sz);
+var $B=new Float64Array($H,pointb,sz);
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
 t.setConstants({nblnk:nblank$,blnk:blank$});
 var T=false;
 function M(){
-vv=document.getElementById('mcanvas');
+vv=document.getElementById("mcanvas");
+// var lvv=document.getElementById("ldv");
 t.setConstants({nblnk:nblank$,blnk:blank$});
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
 if(T){return;}
@@ -214,8 +335,7 @@ var loca=$F+1;
 if(loca>64){loca=1;}
 var locb=$Bu+1;
 if(locb>64){locb=1;}
-eval("if ($F=="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");}");
-eval("if ($F=="+i+"){var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+"); $F="+loca+";$Bu="+locb+";}");
+eval("if ($F=="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+"); $F="+loca+";$Bu="+locb+";}");
 }
 var $bb=R(vv);
 $B.set($bb,0,sz);
@@ -225,7 +345,28 @@ Module.ccall("clr",null,["Number","Number","Number"],[agav[201],agav[101],agav[1
 Module.ccall("frm");
 setTimeout(function(){
 M();
-},16.666666)}
+if(loopLoop==true){
+if(f==true){
+if(mmvv.currentTime>(stp)){
+mmvv.currentTime-=0.013333;
+}else{
+f=false;
+if(mmvv.currentTime<(stp+2.0)){
+mmvv.currentTime+=0.013333;
+}else{
+f=true;
+}}}
+if(f==false){
+if(mmvv.currentTime<(stp+2.0)){
+mmvv.currentTime+=0.013333;
+}else{
+f=true;
+if(mmvv.currentTime>(stp)){
+mmvv.currentTime-=0.013333;
+}else{
+f=false;
+}}}}
+},13.333333)}
 M();
 document.getElementById("di").onclick=function(){
 T=true;
@@ -241,6 +382,7 @@ T=true;
 extern "C" {
 
 void b3(){
+egl();
 ma();
 return;
 }
