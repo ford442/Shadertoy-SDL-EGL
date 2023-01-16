8d498a1b590122dc202ea0b334e5fb4f0afbfd87
Noah Cohn <noahc42@gmail.com>
2023-01-15 18:08:15 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -213,7 +213,7 @@ g2.addNativeFunction('Ave',glslAve,{returnType:'Number'});
 const R=g2.createKernel(function(tv){
 const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0]*0.8,Pa[1],Pa[2]*1.2);
-}).setTactic("speed").setPrecision("single").setOptimizeFloatMemory(false).setFixIntegerDivisionAccuracy(false).setDynamicOutput(true).setArgumentTypes(["Number"]).setOutput([sz]);
+}).setTactic("speed").setPrecision("single").setOptimizeFloatMemory(true).setFixIntegerDivisionAccuracy(false).setDynamicOutput(true).setOutput([sz]);
 const t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
