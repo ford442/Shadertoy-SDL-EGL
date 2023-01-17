58a528fd04c8045a475b93132e2dcaba6473f684
Noah Cohn <noahc42@gmail.com>
2023-01-17 07:21:39 -0800

    Update b3main.cpp

--- src/b3main.cpp
+++ src/b3main.cpp
@@ -121,7 +121,7 @@ if(sarrayBuffer){
 const sfil=new Uint8ClampedArray(sarrayBuffer);
 FS.writeFile('/shader/shader1.toy',sfil);
 setTimeout(function(){
-highResStart();
+normalResStart();
 },150);
 };};
 ff.send(null);
