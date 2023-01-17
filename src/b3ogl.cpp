aa27c0ad6bcc80fb93dee2f1efcb500327f89bc9
Noah Cohn <noahc42@gmail.com>
2023-01-17 11:01:42 -0800

    Update b3ogl.cpp

--- src/b3ogl.cpp
+++ src/b3ogl.cpp
@@ -7,10 +7,8 @@ glClearColor(y1y,y1y,y1y,y1y);
 return;
 }
 
-static void uni(GLfloat xx,GLfloat yy,GLfloat time,GLint fram,GLfloat delt){
-  
+void uni(GLfloat xx,GLfloat yy,GLfloat time,GLint fram,GLfloat delt){
 // ret=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)1,mouse_call);
-
 ret=emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)1,mouse_call);
 // ret=emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)1,mouse_call);
 ret=emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)1,mouse_call);
@@ -245,7 +243,8 @@ auto t1=std::chrono::steady_clock::now();
 return;
 }
 
-EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent * e,void * userData){
+  static EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent * e,void * userData){
+    
 if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
 if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0){
 ms_l=true;
