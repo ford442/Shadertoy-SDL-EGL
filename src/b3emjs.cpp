997a8b304520a4a659cb84bde9904b508ee4399e
Noah Cohn <noahc42@gmail.com>
2023-01-17 20:50:03 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -280,7 +280,7 @@ gl.blendEquationSeparate(gl.FUNC_SUBTRACT,gl.MAX);
 // gl.unpackColorSpace='display-p3';  // very slow
 gl.drawingBufferColorSpace='display-p3';
 gl.disable(gl.DITHER);
-gl.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.NICEST);
+gl.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.FASTEST);
 gl.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
 // w$=parseInt(document.getElementById("wid").innerHTML,10);
 // h$=parseInt(document.getElementById("hig").innerHTML,10);
