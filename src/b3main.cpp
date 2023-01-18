b680b7915c293e04a2db7742f13a952b9e94981d
Noah Cohn <noahc42@gmail.com>
2023-01-17 20:57:35 -0800

    Update b3main.cpp

--- src/b3main.cpp
+++ src/b3main.cpp
@@ -3,7 +3,6 @@
 EM_JS(void,js_main,(),{
 
 "use strict";
-var $h,$pt,slt,$ll,r$,$w,$r,$lt,$hg,$ls,lo,mv,he,wi;
 
 function highResStart(){
 document.getElementById('scanvas').height=window.innerHeight*2;
@@ -194,25 +193,27 @@ snd();
 },1650);
 });
 
-const tem=document.getElementById('tim');
-const ban=document.getElementById('menuBtn');
-const sfr=document.getElementById('slideframe');
+var $h,$pt,slt,$ll,r$,$w,$r,$lt,$hg,$ls,lo,mv,he,wi;
+
+let tem=document.getElementById('tim');
+let ban=document.getElementById('menuBtn');
+let sfr=document.getElementById('slideframe');
 function grab$lt(){$lt=Math.round($lt);}
 $lt=tem.innerHTML;
 grab$lt();
-const slo=new Slideout({'panel':document.getElementById('panel'),'menu':document.getElementById('menu'),'padding':384,'tolerance':70,'easing':'cubic-bezier(.32,2,.55,.27)'});
+let slo=new Slideout({'panel':document.getElementById('panel'),'menu':document.getElementById('menu'),'padding':384,'tolerance':70,'easing':'cubic-bezier(.32,2,.55,.27)'});
 ban.addEventListener('click',function(){slo.toggle();sfr.innerHTML="";
 setTimeout(function(){
 grab$lt();slt=$lt/1000;slt=Math.round(slt);
 sfr.innerHTML='<input type='+'"te'+'xt" id'+'="time'+'slider"/'+'>';
-const tsl=new rSlider({target:'#timeslider',values:{min:0.25,max:30.00},
+let tsl=new rSlider({target:'#timeslider',values:{min:0.25,max:16.00},
 step:[0.25],labels:false,tooltip:true,scale:false,});
 grab$lt();slt=($lt/1000);slt=slt*100;slt=Math.round(slt);
 slt=slt/100;tsl.setValues(slt);
 document.getElementById('menu').addEventListener('click',function(){
 $ll=tsl.getValue();$ll=$ll*100;$ll=Math.round($ll);$ll=$ll/100;$ll=($ll*1000);tem.innerHTML=$ll;
 });
-setTimeout(function(){slt=tem.innerHTML;},8);},16);});
+setTimeout(function(){slt=tem.innerHTML;},8);},13);});
 var adr='./intro.mp4';
 wi=1920;
 he=1080;
@@ -220,8 +221,8 @@ var hii=window.innerHeight;
 document.getElementById('ihid').innerHTML=hii;
 r$=hii/he;
 $w=wi*r$;
-const $ihigB=document.getElementById('ihid');
-const $ihig=document.getElementById('ihig');
+let $ihigB=document.getElementById('ihid');
+let $ihig=document.getElementById('ihig');
 $hg=hii+'px';
 $ihig.innerHTML=parseInt(window.innerHeight,10);
 $iwid.innerHTML=parseInt($w,10);
@@ -241,7 +242,7 @@ if(e.code=='KeyZ'){lockVid=1;};
 if(e.code=='KeyX'){lockVid=0;};
 }
 
-const pnnl=document.body;
+let pnnl=document.body;
 pnnl.addEventListener('keydown',spKey);
 
 function loada(){
