1fd993fa32fda51caa1cc1be539bba73fc90739a
1ink.us <noahc42@gmail.com>
2023-01-19 12:01:29 -0800

    clean branch
    clean area

--- setUp.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function opn(){
-setTimeout(function(){
-document.getElementById('shut').innerHTML=2;
-document.getElementById('circle').width=window.innerWidth;
-document.getElementById('circle').height=window.innerHeight;
-},350)}
-opn();
