37ce528a0c9d63336732a9cf92635dc4f83ecae5
Noah Cohn <noahc42@gmail.com>
2023-01-18 17:07:06 -0800

    Update b3sdl.hpp

--- include/b3sdl.hpp
+++ include/b3sdl.hpp
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#include <pthread.h>
+#include <thread>
 #include <SDL2/SDL.h>
 
 #undef main
-
-static SDL_TLSID tls;
 
 char flnm[24];
 SDL_AudioDeviceID dev;
