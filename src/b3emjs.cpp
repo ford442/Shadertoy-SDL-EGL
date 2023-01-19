a1e570d4592dbb66a4c66a4a11db2e4eac8c9ffe
Noah Cohn <noahc42@gmail.com>
2023-01-19 09:39:30 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -3,7 +3,7 @@
 void egl(){
 emscripten_webgl_init_context_attributes(&attr_js);
 attr_js.alpha=EM_TRUE;
-attr_js.stencil=EM_FALSE;
+attr_js.stencil=EM_TRUE;
 attr_js.depth=EM_TRUE;
 attr_js.antialias=EM_TRUE;
 attr_js.premultipliedAlpha=EM_FALSE;
@@ -23,8 +23,8 @@ contextegl_js=eglCreateContext(display_js,eglconfig_js,EGL_NO_CONTEXT,anEglCtxAt
 surface_js=eglCreateWindowSurface(display_js,eglconfig_js,(NativeWindowType)0,attribut_list_js);
 eglMakeCurrent(display_js,surface_js,surface_js,contextegl_js);
 emscripten_webgl_make_context_current(ctx_js);
-emscripten_get_element_css_size("canvas",&wi,&hi);
-Size=(int)hi;
+emscripten_get_element_css_size("canvas",&wi_js,&hi_js);
+Size_js=(int)hi_js;
 emscripten_webgl_enable_extension(ctx_js,"WEBGL_color_buffer_float");
 emscripten_webgl_enable_extension(ctx_js,"WEBGL_color_buffer_half_float");
 // emscripten_webgl_enable_extension(ctx_js,"WEBGL_blend_equation_advanced_coherent");
@@ -78,11 +78,10 @@ emscripten_webgl_enable_extension(ctx_js,"GL_ARB_ES3_1_compatibility");
 emscripten_webgl_enable_extension(ctx_js,"GL_ARB_ES3_2_compatibility");
 emscripten_webgl_enable_extension(ctx_js,"GL_EXT_gpu_shader4");
 emscripten_webgl_enable_extension(ctx_js,"GL_EXT_gpu_shader5");
- glViewport((GLint)0,(GLint)0,GLsizei(Size),GLsizei(Size));
-
+glViewport((GLint)0,(GLint)0,GLsizei(Size_js),GLsizei(Size_js));
 glDisable(GL_DITHER);
-glDisable(GL_STENCIL_TEST);
-glDisable(GL_DEPTH_TEST);
+// glDisable(GL_STENCIL_TEST);
+// glDisable(GL_DEPTH_TEST);
 glHint(GL_GENERATE_MIPMAP_HINT,GL_NICEST);
 glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_NICEST);
 return;
@@ -397,11 +396,15 @@ Module.ccall("clr",null,["Number","Number","Number"],[agav[201],agav[101],agav[1
 if($F%2!=0){
 var $bb=R(vv);$B.set($bb,0,sz);
 Module.ccall("nano",null,["Number","Number","Number","Number"],[$F,sz,pointb,pointa]);
-if(sh4d==true){
+
+};
+}
+
+if($F%2!=0&&sh4d==true){
 Module.ccall("frm");
 };
-};
-}},13.333333)}
+
+},13.333333)}
 M();
 document.getElementById("di").onclick=function(){
 T=true;
