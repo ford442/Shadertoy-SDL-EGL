97fec70104d03e07e18062362381585c82bc651d
Noah Cohn <noahc42@gmail.com>
2023-01-19 15:34:08 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -2,7 +2,7 @@ extern "C"{
 void str();
 };
 
-float Ttime,TtimeDelta,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,F=1.0f,Fm1=-1.0f,F0=0.0f;
+double Ttime,TtimeDelta,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,F=1.0f,Fm1=-1.0f,F0=0.0f;
 short int iFrame,iFps,Size;
 double wi,hi;
 char * fileloc=(char*)"/shader/shader1.toy";
