02732f4cd9114bfcb258b5eccaea6be34522fbf5
Noah Cohn <noahc42@gmail.com>
2023-01-18 19:46:33 -0800

    Update b3sdl.hpp

--- include/b3sdl.hpp
+++ include/b3sdl.hpp
@@ -1,21 +1,17 @@
 #include "../include/b3emsc.hpp"
-
 #include <SDL2/SDL.h>
 #undef main
-
-char flnm[24];
-SDL_AudioDeviceID dev;
-
-struct{Uint8 * snd;int pos;Uint32 slen;SDL_AudioSpec request;}wave;
-
-void SDLCALL bfr(void *,Uint8 *,int);
-
-void plt();
+#include "SDL_config.h"
 
 extern "C"{
-
+  
+char flnm[24];
 void pl();
-
+SDL_AudioDeviceID dev;
+struct{Uint8 * snd;int pos;Uint32 slen;SDL_AudioSpec request;}wave;
+void SDLCALL bfr(void *,Uint8 *,int);
+void plt();
+  
 }
 
 #include <iostream>
