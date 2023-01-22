9c5018ef67216cdc1f8a629d9d2447c52803f7ed
Noah Cohn <noahc42@gmail.com>
2023-01-21 16:32:29 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -1,7 +1,7 @@
 extern "C"{
 void str();
 };
-
+#include <cstdint>
 double Ttime,TtimeDelta,cMouseY,cMouseX,mouseY,mouseX,Dm1=-1.0,D0=0.0,D=1.0,F=1.0f,Fm1=-1.0f,F0=0.0f;
 int_fast32_t iFrame,iFps,Size;
 double wi,hi;
