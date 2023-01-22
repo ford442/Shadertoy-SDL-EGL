4f17c6f9e70ab10e73d1617878d0acacac0d875b
Noah Cohn <noahc42@gmail.com>
2023-01-21 21:54:41 -0800

    Update b3emjs_g00.cpp

--- src/b3emjs_g00.cpp
+++ src/b3emjs_g00.cpp
@@ -288,7 +288,7 @@ var $F=1;
 var $Bu=33;
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
 t.setConstants({nblnk:nblank$,blnk:blank$});
-var vv=document.getElementById("mcanvas");
+var vv=document.getElementsByClassName('mapsConsumerUiSceneInternalCoreScene__canvas widget-scene-canvas');
 var $$1=t(vv);
 for(var i=0;i<65;i++){
 var j=i+1;
