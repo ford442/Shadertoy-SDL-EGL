2496333afeb2033700e2408e2b661ec3b4ad48e5
Noah Cohn <noahc42@gmail.com>
2023-01-26 12:23:44 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -56,7 +56,7 @@ const GLchar vertex_shader_body_gles3[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
 const GLchar fragment_shader_header_gles3[]=
 "\n uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
-"\n uniform float iTime;uniform float iTimeDelta;in float iAlpha;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
+"\n uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
 "\n uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
 "\n out vec4 fragColor;\n";
 const GLchar fragment_shader_footer_gles3[]=
