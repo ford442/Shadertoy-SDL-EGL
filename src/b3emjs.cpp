05d2b05d0342a1acdee4d27f054ccb2c0a95a6b7
Noah Cohn <noahc42@gmail.com>
2023-01-26 15:49:31 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -242,7 +242,7 @@ var loca=$F+1;
 if(loca>64){loca=1;}
 var locb=$Bu+1;
 if(locb>64){locb=1;}
- if($F==i)(
+ if($F==i){
 eval("var $r"+i+"=t($"+i+");r($r"+i+");var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+",0,la);$F="+loca+";$Bu="+locb+";");
   }
 }
