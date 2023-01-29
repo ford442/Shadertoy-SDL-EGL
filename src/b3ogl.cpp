e8b4bb67bcf77e3cebc7263ce7c416a3518a0037
Noah Cohn <noahc42@gmail.com>
2023-01-29 04:25:11 -0800

    Update b3ogl.cpp

--- src/b3ogl.cpp
+++ src/b3ogl.cpp
@@ -354,22 +354,24 @@ atb_pos=0;
 glBindAttribLocation(shd_prg,(GLuint)0,"iPosition");
   
 glLinkProgram(shd_prg);
-  uni_tme=glGetUniformLocation(shd_prg,"iTime");
+  glGenVertexArrays((GLsizei)1,&VCO);
+glBindVertexArray(VCO);
+atb_pos=glGetAttribLocation(shd_prg,"iPosition");
+glEnableVertexAttribArray(atb_pos);
+glVertexAttribPointer(atb_pos,(GLint)4,GL_FLOAT,GL_TRUE,(GLsizei)0,(GLvoid *)0);
+uni_tme=glGetUniformLocation(shd_prg,"iTime");
 uni_tme_dlt=glGetUniformLocation(shd_prg,"iTimeDelta");
 uni_frm=glGetUniformLocation(shd_prg,"iFrame");
 uni_fps=glGetUniformLocation(shd_prg,"iFrameRate");
 uni_res=glGetUniformLocation(shd_prg,"iResolution");
 uni_mse=glGetUniformLocation(shd_prg,"iMouse");
 uni_srate=glGetUniformLocation(shd_prg,"iSampleRate");
+  
    glViewport((GLint)0,(GLint)0,GLsizei(Size),GLsizei(Size));
    glScissor((GLint)0,(GLint)0,(GLsizei)Size,(GLsizei)Size);
+   glUseProgram(shd_prg);
   
-glUseProgram(shd_prg);
-glGenVertexArrays((GLsizei)1,&VCO);
-glBindVertexArray(VCO);
-atb_pos=glGetAttribLocation(shd_prg,"iPosition");
-glEnableVertexAttribArray(atb_pos);
-glVertexAttribPointer(atb_pos,(GLint)4,GL_FLOAT,GL_TRUE,(GLsizei)0,(GLvoid *)0);
+
 // smp_chn_res=glGetUniformLocation(shd_prg,"iChannelResolution");
 // smp_chn[0]=glGetUniformLocation(shd_prg,"iChannel0");
 // smp_chn[1]=glGetUniformLocation(shd_prg,"iChannel1");
@@ -381,9 +383,11 @@ glVertexAttribPointer(atb_pos,(GLint)4,GL_FLOAT,GL_TRUE,(GLsizei)0,(GLvoid *)0);
 glUniform1f(uni_srate,(GLfloat)44100.0);
 glUniform3f(uni_res,S,S,g1g);
 glUniform3f(smp_chn_res,S,S,g1g);
+  
 glDeleteShader(vtx);
 glDeleteShader(frag);
 glReleaseShaderCompiler();
+  
   // glDisable(GL_STENCIL_TEST);
 // glEnable(GL_SCISSOR_TEST);
 auto t1=std::chrono::steady_clock::now();
