cf6aaf5efad8f754b47c3a9509b327380e37cfad
Noah Cohn <noahc42@gmail.com>
2023-01-30 04:36:45 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -109,7 +109,7 @@ gl_js.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.NICEST);
 gl_js.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
 //    gl_js.enable(gl.SAMPLE_COVERAGE);
 // gl_js.sampleCoverage(1.0, false);
-gl_js.getExtension('WEBGL_color_buffer_float');
+gl_js.getExtension('EXT_color_buffer_float');
 gl_js.getExtension('WEBGL_color_buffer_half_float');
 gl_js.getExtension('OES_texture_float_linear');
 gl_js.getExtension('OES_texture_half_float_linear');
