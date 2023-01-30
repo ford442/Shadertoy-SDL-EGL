e967f4187547ba985ffb009cc9611c06274251ba
Noah Cohn <noahc42@gmail.com>
2023-01-29 17:21:04 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -61,13 +61,18 @@ const GLchar common_shader_header_gles3[]=
 "precision highp usampler3D;"
 "precision highp usamplerCube;"
 "precision highp usampler2DArray;"
-/* "precision highp sampler2DMS;"
+  
+  "precision highp image2D;"
+
+/* 
+
+// // "precision highp sampler2DMS;"
 "precision highp isampler2DMS;"
 "precision highp usampler2DMS;"
+
 "precision highp image2DArray;"
 "precision highp iimage2DArray;"
 "precision highp uimage2DArray;"
-"precision highp image2D;"
 "precision highp iimage2D;"
 "precision highp uimage2D;"
 "precision highp image3D;"
@@ -76,7 +81,10 @@ const GLchar common_shader_header_gles3[]=
 "precision highp imageCube;"
 "precision highp iimageCube;"
 "precision highp uimageCube;"
-"precision highp struct;"*/
+// // "precision highp struct;"
+
+*/
+  
 "precision highp samplerCubeShadow;precision highp sampler2DArrayShadow;\n";
 const GLchar vertex_shader_body_gles3[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
