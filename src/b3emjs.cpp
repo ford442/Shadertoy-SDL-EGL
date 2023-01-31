fb1f6715baebdee60daa1bb7c165bb4b7706e8ff
Noah Cohn <noahc42@gmail.com>
2023-01-31 02:55:49 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -132,7 +132,7 @@ const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0],Pa[1],Pa[2]);
 }).setTactic("speed").setDynamicOutput(true).setOptimizeFloatMemory(true).setOutput([sz]);
 const t=g.createKernel(function(v){
-const P=v[this.thread.y+this.constants.nblnk][this.thread.x+this.constants.blnk];
+const P=v[this.thread.y-this.constants.nblnk][this.thread.x-this.constants.blnk];
 // const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 const av$=Ave(P[0],P[1],P[2]);
 var minuss=(av$-0.9)*(av$/(av$-0.9));
@@ -141,7 +141,7 @@ return[P[0],P[1],P[2],av$];
 }).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput([w$,h$]);
 const r=g.createKernel(function(f){
  
-const p=f[this.thread.y+this.constants.nblnk][this.thread.x+this.constants.blnk];
+const p=f[this.thread.y-this.constants.nblnk][this.thread.x-this.constants.blnk];
 const $amax=this.constants.amax;
 const $amin=this.constants.amin;
 const $aavg=this.constants.aavg;
