a3a61105e71154c3928854b208f6863653ace4b7
Noah Cohn <noahc42@gmail.com>
2023-01-31 03:33:17 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -133,15 +133,15 @@ const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0],Pa[1],Pa[2]);
 }).setTactic("speed").setDynamicOutput(true).setOptimizeFloatMemory(true).setOutput([sz]);
 const t=g.createKernel(function(v){
-const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
+const P=v[this.thread.y][this.thread.x+this.constants.blnk-this.constants.nblnk];
 // const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 const av$=Ave(P[0],P[1],P[2]);
 var minuss=(av$-0.9)*(av$/(av$-0.9));
 av$=av$-(minuss*(av$*0.01));
 return[P[0],P[1],P[2],av$];
-}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput([w$,h$]);
+}).setTactic("precision").setPipeline(true).setDynamicOutput(true).setOutput([h$,h$]);
 const r=g.createKernel(function(f){
-const p=f[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
+const p=f[this.thread.y][this.thread.x];
 const $amax=this.constants.amax;
 const $amin=this.constants.amin;
 const $aavg=this.constants.aavg;
