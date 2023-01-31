19a0c948cd9452ff934f3b13b5edb2f482aaeed9
Noah Cohn <noahc42@gmail.com>
2023-01-31 03:54:11 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -131,7 +131,7 @@ const Pa=tv[this.thread.y][this.thread.x*4];
 return Ave(Pa[0],Pa[1],Pa[2]);
 }).setTactic("speed").setDynamicOutput(true).setOptimizeFloatMemory(true).setOutput([sz]);
 const t=g.createKernel(function(v){
-const P=v[this.thread.y][this.thread.x+this.constants.blnk-this.constants.nblnk];
+const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 // const av$=Ave(P[0]*0.8,P[1],P[2]*1.2);
 const av$=Ave(P[0],P[1],P[2]);
 var minuss=(av$-0.9)*(av$/(av$-0.9));
