f784987b45d53806f35a188a3f36d3aa64808353
Noah Cohn <noahc42@gmail.com>
2023-01-31 21:31:29 -0800

    Update b3ogl.hpp

--- include/b3ogl.hpp
+++ include/b3ogl.hpp
@@ -1,3 +1,8 @@
+#include <cstdint>
+
+int_fast32_t Size,iFrame,iFps;
+double wi_js,hi_js;
+int_fast32_t Size_js;
 double wi,hi;
 float TtimeDelta,cMouseY,cMouseX,mouseY,mouseX,F=1.0f,Fm1=-1.0f,F0=0.0f;
 double Ttime,Dm1=-1.0,D0=0.0,D=1.0;
@@ -34,7 +39,6 @@ void b3_egl();
 };
 
 GLclampf avrg,drk,brt;
-
 GLclampf mX,mY;
 GLclampf mm,nn;
 GLuint atb_pos;
@@ -49,9 +53,9 @@ const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu
 const GLchar * sources[4];
 const GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
-"\n #undef HW_PERFORMANCE\n"
-"\n #define HW_PERFORMANCE 0\n"
-"\n precision highp float;precision highp sampler3D;precision highp sampler2D;"
+"#undef HW_PERFORMANCE\n"
+"#define HW_PERFORMANCE 0\n"
+"precision highp float;precision highp sampler3D;precision highp sampler2D;"
 "precision highp samplerCube;precision highp sampler2DArray;precision highp sampler2DShadow;"
 "precision highp isampler2D;"
 "precision highp isampler3D;"
@@ -61,33 +65,14 @@ const GLchar common_shader_header_gles3[]=
 "precision highp usampler3D;"
 "precision highp usamplerCube;"
 "precision highp usampler2DArray;"
- /*  
-  "precision highp image2D;"
-// // "precision highp sampler2DMS;"
-"precision highp isampler2DMS;"
-"precision highp usampler2DMS;"
-"precision highp image2DArray;"
-"precision highp iimage2DArray;"
-"precision highp uimage2DArray;"
-"precision highp iimage2D;"
-"precision highp uimage2D;"
-"precision highp image3D;"
-"precision highp iimage3D;"
-"precision highp uimage3D;"
-"precision highp imageCube;"
-"precision highp iimageCube;"
-"precision highp uimageCube;"
-// // "precision highp struct;"
-*/
-  
 "precision highp samplerCubeShadow;precision highp sampler2DArrayShadow;\n";
 const GLchar vertex_shader_body_gles3[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
 const GLchar fragment_shader_header_gles3[]=
-"\n uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
-"\n uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
-"\n uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
-"\n out vec4 fragColor;\n";
+"uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
+"uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
+"uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
+"out vec4 fragColor;\n";
 const GLchar fragment_shader_footer_gles3[]=
 "\n void main(){mainImage(fragColor,gl_FragCoord.xy);}\0";
 const GLchar * common_shader_header=common_shader_header_gles3;
@@ -105,7 +90,6 @@ char32_t * read_file(const GLchar *);
 
 #include "../include/b3egl.hpp"
 
-int_fast32_t Size,iFrame,iFps;
 
 EGLDisplay display;
 EGLSurface surface;
@@ -115,7 +99,7 @@ EGLint config_size,major,minor;
 
 EGLint const attribut_list[]={ 
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
-EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
 // EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
@@ -123,12 +107,12 @@ EGL_NONE
 };
 
 EGLint const anEglCtxAttribs2[]={
-EGL_CONTEXT_CLIENT_VERSION,(EGLint)4,
-EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)6,
-EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT, 
+EGL_CONTEXT_CLIENT_VERSION,(EGLint)3,
+EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)0,
+// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT, 
 EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_HIGH_IMG,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
 EGL_NONE
 };
 
@@ -137,21 +121,21 @@ EGLint const attribute_list[]={
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
 //  EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
-EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT,
-EGL_CONFORMANT,EGL_OPENGL_BIT,
+// EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT,
+// EGL_CONFORMANT,EGL_OPENGL_BIT,
 /// EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER,
 // EGL_BIND_TO_TEXTURE_RGBA, EGL_TRUE,
-EGL_CONFIG_CAVEAT,EGL_NONE,
-EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
-EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
+// EGL_CONFIG_CAVEAT,EGL_NONE,
+// EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+// EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
 EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
-EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
-EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
+// EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
+// EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
  EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
 EGL_TRANSPARENT_RED_VALUE,(EGLint)32,
 EGL_TRANSPARENT_GREEN_VALUE,(EGLint)32,
 EGL_TRANSPARENT_BLUE_VALUE,(EGLint)32,
-EGL_NATIVE_RENDERABLE,EGL_TRUE,
+// EGL_NATIVE_RENDERABLE,EGL_TRUE,
 EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_HIGH_IMG,
 EGL_RED_SIZE,(EGLint)32,
 EGL_GREEN_SIZE,(EGLint)32,
@@ -174,25 +158,22 @@ EGLSurface surface_js;
 EGLConfig eglconfig_js;
 EGLint config_size_js,major_js,minor_js;
 
-double wi_js,hi_js;
-int_fast32_t Size_js;
-
 EGLint const attribut_list_js[]={ 
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
-EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
 // EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
 EGL_NONE
 };
 
 EGLint const anEglCtxAttribs2_js[]={
-EGL_CONTEXT_CLIENT_VERSION,(EGLint)4,
-EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)6,
+EGL_CONTEXT_CLIENT_VERSION,(EGLint)3,
+EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)0,
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT, 
 EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
 EGL_NONE
 };
 
@@ -201,21 +182,21 @@ EGLint const attribute_list_js[]={
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
 // EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
-EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT,
-EGL_CONFORMANT,EGL_OPENGL_BIT,
+// EGL_RENDERABLE_TYPE,EGL_OPENGL_BIT,
+// EGL_CONFORMANT,EGL_OPENGL_BIT,
 // EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER,
 //  EGL_CONFIG_CAVEAT,EGL_NONE,
 // EGL_BIND_TO_TEXTURE_RGBA, EGL_TRUE,
-EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
-EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
+// EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+// EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
 // EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
-EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
-EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
+// EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
+// EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
 EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
 EGL_TRANSPARENT_RED_VALUE,(EGLint)32,
 EGL_TRANSPARENT_GREEN_VALUE,(EGLint)32,
 EGL_TRANSPARENT_BLUE_VALUE,(EGLint)32,
-EGL_NATIVE_RENDERABLE,EGL_TRUE,
+// EGL_NATIVE_RENDERABLE,EGL_TRUE,
 EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
 EGL_RED_SIZE,(EGLint)32,
 EGL_GREEN_SIZE,(EGLint)32,
@@ -225,7 +206,7 @@ EGL_DEPTH_SIZE,(EGLint)0,
 EGL_STENCIL_SIZE,(EGLint)0,
 EGL_BUFFER_SIZE,(EGLint)64,
 EGL_SAMPLE_BUFFERS,(EGLint)64,
-EGL_SAMPLES,(EGLint)512,
+EGL_SAMPLES,(EGLint)256,
 EGL_NONE
 };
 
@@ -233,7 +214,7 @@ EGL_NONE
 
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 EmscriptenWebGLContextAttributes attr;
-EMSCRIPTEN_RESULT ret;
+EMSCRIPTEN_RESULT retCl,retMu,retMd,retMv,retSa,retSb,retSc;
 EM_BOOL ms_l,clk_l;
 
 EM_BOOL mouse_call_click(int,const EmscriptenMouseEvent *,void *);
