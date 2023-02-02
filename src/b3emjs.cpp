3e30bf44208c8cfecdbdacec6773352b889c738b
Noah Cohn <noahc42@gmail.com>
2023-02-01 18:56:38 -0800

    Update b3emjs.cpp

--- src/b3emjs.cpp
+++ src/b3emjs.cpp
@@ -149,7 +149,7 @@ const ouT=Math.max(Min,alph);
 const aveg=Aveg(p[3],ouT);
 this.color(p[0],p[1],p[2],aveg);
 }).setTactic("precision").setGraphical(true).setDynamicOutput(true).setOutput([$S,$S]);
-
+/*
 gl_js.getExtension('WEBGL_color_buffer_float');
 gl_js.getExtension('WEBGL_color_buffer_half_float');
 gl_js.getExtension('OES_texture_float_linear');
@@ -186,7 +186,7 @@ gl_js.getExtension('EXT_gl_colorspace_display_p3');
 gl_js.getExtension('EXT_gl_colorspace_display_p3_linear');
 gl_js.getExtension('EXT_gl_colorspace_bt2020_linear');
 gl_js.getExtension('ARB_multisample');
-
+ */
 // gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);  // <- crazy effect!
 gl_js.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.NICEST);
 gl_js.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
