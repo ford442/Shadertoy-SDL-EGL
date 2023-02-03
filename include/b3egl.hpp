0e967d9113fd224d6d5725af1d71c23eb208ed6e
Noah Cohn <noahc42@gmail.com>
2023-02-03 03:48:35 -0800

    Update b3egl.hpp

--- include/b3egl.hpp
+++ include/b3egl.hpp
@@ -2,16 +2,14 @@
 
 #include <cstdint>
 
-#include <popcntintrin.h>
-// #include <mmintrin.h> //  MMX
+#include <wasm_simd128.h>
 #include <xmmintrin.h> // SSE
 #include <emmintrin.h> // SSE2
 #include <pmmintrin.h> // SSE3
+#include <popcntintrin.h>
 #include <tmmintrin.h> // SSSE3
 #include <nmmintrin.h>  //  SSE 4.2 lib
 #include <avxintrin.h>  // AVX
-
-#include <wasm_simd128.h>
 
 #include <unistd.h>
 // #include <webgl/webgl2.h>
