6b35c8175476e69ed9d202921c1be85473597fd9
Noah Cohn <noahc42@gmail.com>
2023-02-02 20:31:31 -0800

    Update b3main.cpp

--- src/b3main.cpp
+++ src/b3main.cpp
@@ -232,10 +232,10 @@ document.getElementById('acanvas').width=window.innerHeight;
 document.getElementById('acanvas').height=window.innerHeight;
 
 setTimeout(function(){
-Module.ccall('b3_egl');
+// Module.ccall('fire_egl');
 },150);
 setTimeout(function(){
-Module.ccall('b3_egl');
+// Module.ccall('b3_egl');
 },350);
 setTimeout(function(){
 Module.ccall('b3');
