cd56ac7603c300ce43c2df9cd151b063e7362e71
Noah Cohn <noahc42@gmail.com>
2023-02-03 05:23:35 -0800

    Update b3main.cpp

--- src/b3main.cpp
+++ src/b3main.cpp
@@ -332,16 +332,16 @@ document.getElementById('acanvas').width=window.innerHeight;
 document.getElementById('acanvas').height=window.innerHeight;
 
 setTimeout(function(){
-// Module.ccall('fire_egl');
+Module.ccall('fire_egl');
 },150);
 setTimeout(function(){
-// Module.ccall('b3_egl');
+Module.ccall('b3_egl');
 },350);
 setTimeout(function(){
-// Module.ccall('b3');
+Module.ccall('b3');
 },550);
 setTimeout(function(){
-// Module.ccall('str');
+Module.ccall('str');
 },750);
 setTimeout(function(){
 document.getElementById('shut').innerHTML=2;
@@ -446,4 +446,4 @@ FS.mkdir("/shader");
 js_main();
 return 0;
 
-}
+};
