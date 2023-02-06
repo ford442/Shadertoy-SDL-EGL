6b0c6df1f6754c9302f078d658db1cf688e8347d
Noah Cohn <noahc42@gmail.com>
2023-02-06 05:51:23 -0800

    Create egl.hpp

---
+++ include/shader/egl.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+#include <wasm_simd128.h>
+#include <xmmintrin.h>
+#include <immintrin.h> 
+#include <nmmintrin.h>
+#include <unistd.h>
+
+#include <webgl/webgl2.h>
+
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
+#include <GLES3/gl3platform.h>
+
+#define EGL_EGLEXT_PROTOTYPES 1
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+
+// #include <emscripten/html5.h>
+// #include <emscripten/html5_webgl.h>
