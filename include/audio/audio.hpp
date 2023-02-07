aeca53e2873bf779d5e5c7423138432f2814388f
Noah Cohn <noahc42@gmail.com>
2023-02-07 03:37:05 -0800

    Update audio.hpp

--- include/audio/audio.hpp
+++ include/audio/audio.hpp
@@ -1,1 +1,32 @@
+#include <SDL2/SDL.h>
+#undef main
+#include "SDL_config.h"
 
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdarg>
+#include <cmath>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <cfloat>
+#include <climits>
+
+#include <iostream>
+
+char flnm[24];
+SDL_AudioDeviceID dev;
+struct{Uint8 * snd;int_fast32_t pos;Uint32 slen;SDL_AudioSpec request;}wave;
+
+void SDLCALL bfr(void *,Uint8 *,int_fast32_t);
+  
+void plt();
+
+extern "C"{  
+  
+void pl();
+  
+};
