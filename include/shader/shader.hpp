c25919bce4cddbf46966361c582bc50958bfd5fd
Noah Cohn <noahc42@gmail.com>
2023-02-07 10:52:39 -0800

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -78,8 +78,8 @@ char32_t * read_file(const GLchar *);
 
 #include "../../include/shader/egl.hpp"
 
-int_fast32_t iFrame,iFps,Size;
-// int iFrame,iFps,Size;
+// int_fast32_t iFrame,iFps,Size;
+int iFrame,iFps,Size;
 
 EGLDisplay display;
 EGLSurface surface;
