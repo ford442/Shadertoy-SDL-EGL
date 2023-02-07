de3e9a321f1198dce01bbfcd90a64b28db543608
Noah Cohn <noahc42@gmail.com>
2023-02-07 07:25:01 -0800

    Update shader.cpp

--- src/shader/shader.cpp
+++ src/shader/shader.cpp
@@ -47,7 +47,7 @@ glUniform1i(uni_frm,fram);
 return;
 }
 
-void renderFrame(void){
+void renderFrame(){
 auto t3=t2;
 auto t2=std::chrono::steady_clock::now();
 std::chrono::duration<float>time_spanb=duration_cast<std::chrono::duration<float>>(t2-t3);
