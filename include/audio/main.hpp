c6350718994767dbfd4b0094b2bd82881556c2cc
Noah Cohn <noahc42@gmail.com>
2023-02-07 03:45:40 -0800

    Update main.hpp

--- include/audio/main.hpp
+++ include/audio/main.hpp
@@ -1,19 +1,1 @@
 #include <emscripten.h>
-
-#include <SDL2/SDL.h>
-
-char flnm[24];
-SDL_AudioDeviceID dev;
-struct{Uint8 * snd;int_fast32_t pos;Uint32 slen;SDL_AudioSpec request;}wave;
-
-void SDLCALL bfr(void *,Uint8 *,int_fast32_t);
-  
-void plt();
-
-extern "C"{  
-  
-void pl();
-  
-};
-
-#include <iostream>
