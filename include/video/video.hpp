ca9f22e5caab8ba4d46aff7346940b57f0021dba
Noah Cohn <noahc42@gmail.com>
2023-02-08 12:59:50 -0800

    Update video.hpp

--- include/video/video.hpp
+++ include/video/video.hpp
@@ -1,3 +1,12 @@
+void avgFrm(short int Fnum,int leng,float *ptr,float *aptr);
+
+extern "C"{
+
+void b3();
+void nano(short int Fnum,int leng,float *ptr,float *aptr);
+
+}
+
 #include <emscripten.h>
 
 #include <emscripten/html5.h>
