5f3c7f9e51ec95721621fa61c7d19db6bb8e44ec
Noah Cohn <noahc42@gmail.com>
2023-02-10 18:30:08 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -168,7 +168,7 @@ var ouT=Math.max(Min,alph);
 var aveg=Aveg(p[3],ouT);
 this.color(p[0],p[1],p[2],aveg);
 }).setTactic("precision").setGraphical(true).setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]}).setOutput([$S,$S]);
-gl.getExtension('EXT_color_buffer_float');
+// gl.getExtension('EXT_color_buffer_float');
 // gl.getExtension('WEBGL_color_buffer_float');
 // gl.getExtension('WEBGL_color_buffer_half_float');
 // gl.getExtension('OES_texture_float_linear');
