aa59828df31b32d092c4879e2350f881b08463b8
Noah Cohn <noahc42@gmail.com>
2023-02-11 15:56:56 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -229,7 +229,7 @@ t.setConstants({nblnk:nblank$,blnk:blank$});
 
 for(var i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);var $$1=t(vv);var $"+j+".set($$1);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);var $$1=t(vv);$"+j+".set($$1);");
 }
 var d=S();if(d)d();d=S();function S(){
 $S=parseInt(window.innerHeight,10);
