6b69eea3e708dda25ddaf0563c48314e03dcdf6d
Noah Cohn <noahc42@gmail.com>
2023-02-11 22:14:07 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -144,8 +144,8 @@ xrCompatible:false,
 majorVersion:2,
 minorVersion:0
 });
-var g=new GPU({mode:'gpu',canvas:bcanvas,webGl:gl});
-var g2=new GPU({mode:'gpu'});
+var g=new GPU({mode:'webgl2',canvas:bcanvas,webGl:gl});
+var g2=new GPU({mode:'webgl2'});
 var glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 var glslAlphe=`float Alphe(float a,float b,float f,float g){return(((3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-f)*((1.0-g)*0.1))))))/3.0);}`;
 var glslAveg=`float Aveg(float a,float b){return(0.999-(((a)-(b))*((a)*(0.999/(0.999-b)))));}`;
@@ -175,8 +175,8 @@ var Min=(4.0*(($amax-($aavg-$amin))/2.0));
 var ouT=Math.max(Min,alph);
 var aveg=Aveg(p[3],ouT);
   var silvrr=Ave(p[0],p[1],p[2]);
-// this.color((1.0-silvrr),(1.0-silvrr),(1.0-p[2]),aveg);
-this.color(p[0],p[1],p[2],aveg);
+this.color((1.0-silvrr),(1.0-silvrr),(1.0-p[2]),aveg);
+// this.color(p[0],p[1],p[2],aveg);
 }).setTactic("precision").setDynamicOutput(true).setArgumentTypes(["HTMLVideo"]).setGraphical(true).setOutput([$S,$S]);
 // gl.getExtension('EXT_color_buffer_float');
 // gl.getExtension('WEBGL_color_buffer_float');
