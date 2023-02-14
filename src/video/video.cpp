3d6fd70500ef7bd4892dbe8c8c5a112340cb862d
Noah Cohn <noahc42@gmail.com>
2023-02-13 18:38:31 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -146,7 +146,7 @@ xrCompatible:false,
 majorVersion:2,
 minorVersion:0
 });
-var g=new GPU(canvas:bcanvas,webGl:gl});
+var g=new GPU({canvas:bcanvas,webGl:gl});
 var g2=new GPU();
 var glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 var glslAlphe=`float Alphe(float a,float b,float f,float g){return(((3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-f)*((1.0-g)*0.1))))))/3.0);}`;
