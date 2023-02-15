8271e08bd4cca292bc33bc4d2cb09f7bba41fd15
Noah Cohn <noahc42@gmail.com>
2023-02-15 07:33:24 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -231,8 +231,8 @@ var max=0.000;
 agav.fill(avag,0,33);
 agav.fill(min,100,33);
 agav.fill(max,200,33);
-var blank$=Math.max((((w$-h$)*0.0)/2.0),0);
-var nblank$=Math.max((((h$-w$)*0.0)/2.0),0);
+var blank$=Math.max((((w$-h$)*1.0)/2.0),0);
+var nblank$=Math.max((((h$-w$)*1.0)/2.0),0);
 let bCan=document.getElementById("bcanvas");
 let gl=bCan.getContext("webgl2",{
 colorType:'float64',
@@ -345,8 +345,8 @@ this.color(p[0],p[1],p[2],aveg);
 w$=parseInt(document.getElementById("wid").innerHTML,10);
 h$=parseInt(document.getElementById("hig").innerHTML,10);
 vv=document.getElementById("mv");
-var blank$=Math.max((((w$-h$)*0.0)/2.0),0);
-var nblank$=Math.max((((h$-w$)*0.0)/2.0),0);
+var blank$=Math.max((((w$-h$)*1.0)/2.0),0);
+var nblank$=Math.max((((h$-w$)*1.0)/2.0),0);
 la=h$*w$*2;
 sz=(h$*w$)/8;
 pointa=77*la;
@@ -373,8 +373,8 @@ vv=document.getElementById("mv");
 w$=parseInt(document.getElementById("wid").innerHTML,10);
 h$=parseInt(document.getElementById("hig").innerHTML,10);
 
-var blank$=Math.max((((w$-h$)*0.0)/2.0),0);
-var nblank$=Math.max((((h$-w$)*0.0)/2.0),0);
+var blank$=Math.max((((w$-h$)*1.0)/2.0),0);
+var nblank$=Math.max((((h$-w$)*1.0)/2.0),0);
 $S=parseInt(window.innerHeight,10);
 // t.setOutput([$S,$S]);
 // r.setOutput([$S,$S]);
