afab028351b237c2a1ab4672ffbd57bc1adf3691
Noah Cohn <noahc42@gmail.com>
2023-02-15 10:08:53 -0800

    Update main_simd.cpp

--- src/vanilla/main_simd.cpp
+++ src/vanilla/main_simd.cpp
@@ -5,7 +5,9 @@ float simd_test(float a[]){
 v128_t ab=wasm_f32x4_splat(a[0]);
 wasm_v128_store(0,ab);
 // wasm_v128_store(0,a);
-v128_t b=wasm_v128_load(0);
+// v128_t b=wasm_v128_load(0);
+v128_t b=wasm_v32x4_load_splat(0);
+  
 float c=wasm_f32x4_extract_lane(b,0);
 return c;
 }
