9546fffeec72d3c6521b8060db92da4db59c310d
Noah Cohn <noahc42@gmail.com>
2023-02-15 10:39:34 -0800

    Update main_simd.cpp

--- src/vanilla/main_simd.cpp
+++ src/vanilla/main_simd.cpp
@@ -32,10 +32,10 @@ document.getElementById('di').click();
 },950);
 var tsta=document.getElementById('smd').innerHTML;
 var tst=[tsta,tsta,tsta,tsta,tsta,tsta,tsta,tsta];
-  const $H=Module.HEAPF64.buffer;
- const $P=Module.HEAPF64.subarray(0,2);
-  $P.set(tst,0);
-  console.log($H);
+//  const $H=Module.HEAPF64.buffer;
+// const $P=Module.HEAPF64.subarray(0,2);
+//  $P.set(tst,0);
+//  console.log($H);
 var reslt=Module.ccall('js_simd',"number",["array"],[tst]);
 console.log(reslt);
 }
