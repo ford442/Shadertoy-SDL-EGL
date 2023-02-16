7318a713decda8ea4c72cbe72e682df0cd59af76
Noah Cohn <noahc42@gmail.com>
2023-02-15 20:50:25 -0800

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -43,9 +43,9 @@ const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu
 const GLchar * sources[4];
 const GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
-// "#define AA 1\n"
 "#pragma optionNV(fastmath off)\n"
 "#pragma optionNV(fastprecision off)\n"
+"#pragma optionNV(unroll none)\n"
 "#undef HW_PERFORMANCE\n"
 "#define HW_PERFORMANCE 0\n"
 "#extension GL_ARB_gpu_shader_fp64 : enable\n"
