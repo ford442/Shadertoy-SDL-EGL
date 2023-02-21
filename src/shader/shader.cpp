9d87760aedfcfdb4eaa4a6ae00d8002afe28aafc
Noah Cohn <noahc42@gmail.com>
2023-02-21 00:35:17 -0800

    Update shader.cpp

--- src/shader/shader.cpp
+++ src/shader/shader.cpp
@@ -182,7 +182,7 @@ emscripten_webgl_enable_extension(ctx,"EGL_HI_colorformats");
   
 emscripten_webgl_enable_extension(ctx,"EGL_EXT_pixel_format_float");
   
-emscripten_webgl_enable_extension(ctx,"EGL_KHR_gl_colorspace");
+/// emscripten_webgl_enable_extension(ctx,"EGL_KHR_gl_colorspace");
 /// emscripten_webgl_enable_extension(ctx,"EGL_EXT_gl_colorspace_scrgb");
 /// emscripten_webgl_enable_extension(ctx,"EGL_EXT_gl_colorspace_scrgb_linear");
 /// emscripten_webgl_enable_extension(ctx,"EGL_EXT_gl_colorspace_bt2020_pq");
