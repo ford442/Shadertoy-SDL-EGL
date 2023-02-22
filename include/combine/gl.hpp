6de7fa12867384ed18601b33e67159d52a047720
Noah Cohn <noahc42@gmail.com>
2023-02-21 19:39:27 -0800

    Create gl.hpp

---
+++ include/combine/gl.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GL_SILENCE_DEPRECATION
+
+#define GL3_PROTOTYPES 1
+#define GL4_PROTOTYPES 1
+#define GL_FRAGMENT_PRECISION_HIGH 1
+
+#include <GL/gl.h>
+#define GL_GLEXT_PROTOTYPES 1
+#include <GL/glext.h>
