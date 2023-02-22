d57a983ef38a1da14223c621c5403951abd50b84
Noah Cohn <noahc42@gmail.com>
2023-02-22 12:22:54 -0800

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -13,9 +13,9 @@
 
 #define GL_GLES_PROTOTYPES 1
 #include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3platform.h>
+// #include <GLES3/gl31.h>
+// #include <GLES3/gl32.h>
+// #include <GLES3/gl3platform.h>
 
 #define EGL_EGL_PROTOTYPES 1
 #define EGL_EGLEXT_PROTOTYPES
