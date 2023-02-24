afb1c74cac1d438af01e710c0f487644da4326fe
Noah Cohn <noahc42@gmail.com>
2023-02-23 16:03:35 -0800

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -48,6 +48,7 @@ const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu
 GLchar * sources[4];
 GLchar common_shader_header_gles3[]=
 "#version 300 es\n"
+"#pragma STDGL(invariant all)\n"
 "#pragma optionNV(fastmath off)\n"
 "#pragma optionNV(fastprecision off)\n"
 "#pragma optionNV(unroll none)\n"
