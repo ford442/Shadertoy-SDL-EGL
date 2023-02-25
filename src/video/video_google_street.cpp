10a01594017b9af3b16ea14251da9af82fec77a4
Noah Cohn <noahc42@gmail.com>
2023-02-25 03:04:00 -0800

    Update video_google_street.cpp

--- src/video/video_google_street.cpp
+++ src/video/video_google_street.cpp
@@ -1,5 +1,23 @@
 #include "../../include/video/video_google_street.hpp"
 
+void clrclr(GLclampf rlc,GLclampf alc,GLclampf avr){
+avrg=(((avr+(y1y-rlc))/2.0)+alc);
+drk=y1y-(avr-0.5);
+brt=((y1y-rlc)-(alc-0.5));
+glBlendColor(avrg,avrg,avrg,y1y);
+glClearColor(drk,drk,drk,brt);
+return;
+}
+
+extern "C" {
+
+void clr(GLclampf cllr,GLclampf alp,GLclampf avr){
+clrclr(cllr,alp,avr);
+return;
+}
+
+}
+
 EM_BOOL mouse_call_click(int eventType,const EmscriptenMouseEvent * e,void * userData){
 if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
 if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0){
