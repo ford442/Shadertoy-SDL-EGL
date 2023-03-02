0046cdb414e4e4fbe4b2875bf4ef3a9f6662959e
Noah Cohn <noahc42@gmail.com>
2023-03-02 11:29:35 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -125,27 +125,27 @@ sum=0.0;
 avgSum=0.0;
 minSum=0.0;
 maxSum=0.0;
-for(iI=0;iI<leng;iI++){
-sum+=ptr[iI];
-if(max<ptr[iI]){max=ptr[iI];
+for(int i=0;i<leng;i++){
+sum+=ptr[i];
+if(max<ptr[i]){max=ptr[i];
 }
-if(min>ptr[iI]&&ptr[iI]>0){min=ptr[iI];
+if(min>ptr[i]&&ptr[i]>0){min=ptr[i];
 }
 }
 sum=sum/leng;
 aptr[Fnum]=sum;
 aptr[Fnum+100]=min;
 aptr[Fnum+200]=max;
-for(iI=33;iI<65;iI++){
-avgSum+=aptr[iI];
+for(int i=33;i<65;i++){
+avgSum+=aptr[i];
 }
 aptr[0]=avgSum/32;
-for(iI=33;iI<65;iI++){
-minSum+=aptr[iI+100];
+for(int i=33;i<65;i++){
+minSum+=aptr[i+100];
 }
 aptr[100]=minSum/32;
-for(iI=33;iI<65;iI++){
-maxSum+=aptr[iI+200];
+for(int i=33;i<65;i++){
+maxSum+=aptr[i+200];
 }
 aptr[200]=maxSum/32;
 return;
