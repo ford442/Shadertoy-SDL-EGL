688093e3972c6e8646544558362782303910d530
Noah Cohn <noahc42@gmail.com>
2023-03-06 13:07:44 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -221,14 +221,14 @@ sh4d=false;
 }
 const pnnl=document.body;
 pnnl.addEventListener('keydown',doKey);
-const $H=Module.HEAPF32.buffer;
-const $BB=Module.HEAPF32.buffer;
+const $H=Module.HEAPF64.buffer;
+const $BB=Module.HEAPF64.buffer;
 var $S=parseInt(window.innerHeight,10);
 var w$=parseInt(document.getElementById("wid").innerHTML,10);
 var h$=parseInt(document.getElementById("hig").innerHTML,10);
-var la=h$*w$*4;
+var la=h$*w$*8;
 var pointa=77*la;
-var agav=new Float32Array($BB,pointa,300);
+var agav=new Float64Array($BB,pointa,300);
 var sz=(h$*w$)/8;
 var avag=0.750;
 var min=1.000;
@@ -387,24 +387,24 @@ h$=parseInt(document.getElementById("hig").innerHTML,10);
 vv=document.getElementById("mv");
 var blank$=Math.max((((w$-h$)*0.0)/8.0),0);
 var nblank$=Math.max((((h$-w$)*0.0)/8.0),0);
-la=h$*w$*4;
+la=h$*w$*8;
 sz=(h$*w$)/8;
 pointa=77*la;
-agav=new Float32Array($BB,pointa,300);
+agav=new Float64Array($BB,pointa,300);
 R.setOutput([sz]);
 for(i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);");
 };
 var pointb=77*la;
-var $B=new Float32Array($BB,pointb,sz);
+var $B=new Float64Array($BB,pointb,sz);
 var $F=1;
 var $Bu=33;
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
 t.setConstants({nblnk:nblank$,blnk:blank$});
 for(var i=0;i<65;i++){
 var j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);var $$1=t(vv);$"+j+".set($$1);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);var $$1=t(vv);$"+j+".set($$1);");
 };
 var d=S();if(d)d();d=S();function S(){
 vv=document.getElementById("mv");
@@ -413,19 +413,17 @@ h$=parseInt(document.getElementById("hig").innerHTML,10);
 var blank$=Math.max((((w$-h$)*0.0)/8.0),0);
 var nblank$=Math.max((((h$-w$)*0.0)/8.0),0);
 $S=parseInt(window.innerHeight,10);
-// t.setOutput([$S,$S]);
-// r.setOutput([$S,$S]);
-la=h$*w$*4;
+la=h$*w$*8;
 sz=(h$*w$)/8;
 pointa=77*la;
-var agav=new Float32Array($BB,pointa,300);  // has to var?
+var agav=new Float64Array($BB,pointa,300);  // has to var?
 R.setOutput([sz]);
 for(var i=0;i<65;i++){
 j=i+1;
-eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);");
 };
 pointb=66*la;
-var $B=new Float32Array($BB,pointb,sz);  // has to var?
+var $B=new Float64Array($BB,pointb,sz);  // has to var?
 r.setConstants({nblnk:nblank$,blnk:blank$,amin:agav[100],amax:agav[200],aavg:agav[0]});
 t.setConstants({nblnk:nblank$,blnk:blank$});
 var T=false;
