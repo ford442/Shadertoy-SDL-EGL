e312f4baae05844831cbd2050c3b154a07ff453b
Noah Cohn <noahc42@gmail.com>
2023-03-07 04:55:50 -0800

    Update video.cpp

--- src/video/video.cpp
+++ src/video/video.cpp
@@ -294,6 +294,7 @@ t.setConstants({nblnk:nblank$,blnk:blank$});
 for(var i=0;i<65;i++){
 var j=i+1;
 eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);var $$1=t(vv);$"+j+".set($$1);");
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float64Array($H,point"+j+",la);var $$1=0.0;$"+j+".fill($$1);");
 };
 var d=S();if(d)d();d=S();function S(){
 vv=document.getElementById("mv");
