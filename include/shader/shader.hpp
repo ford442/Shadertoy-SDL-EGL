87235bd75b7ddf94546f939512262d7d3c5d8311
Noah Cohn <noahc42@gmail.com>
2023-03-09 01:40:10 -0800

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -72,7 +72,7 @@ GLchar common_shader_header_gles3[]=
 "#pragma STDGL(invariant none)\n"
 "#undef HW_PERFORMANCE\n"
 "#define HW_PERFORMANCE 0\n"
-"precision lowp int;precision lowp float;precision highp sampler3D;precision highp sampler2D;"
+"precision highp int;precision highp float;precision highp sampler3D;precision highp sampler2D;"
 "precision highp samplerCube;precision highp sampler2DArray;precision highp sampler2DShadow;"
 "precision highp isampler2D;precision highp isampler3D;precision highp isamplerCube;"
 "precision highp isampler2DArray;precision highp usampler2D;precision highp usampler3D;"
