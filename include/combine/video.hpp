ce6e8db8b48396d4e851fec1d0ed4a0c339a01de
Noah Cohn <noahc42@gmail.com>
2023-03-12 09:02:12 -0700

    Update video.hpp

--- include/combine/video.hpp
+++ include/combine/video.hpp
@@ -30,28 +30,6 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
-#define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1 //maybe??
-
-#include <webgl/webgl2.h>
-
-#define GL_GLEXT_PROTOTYPES 1
-#include <GL/gl.h>
-#include <GL/glext.h>
-
-#define GL_GLES_PROTOTYPES 1
-#include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3platform.h>
-
-#define EGL_EGL_PROTOTYPES 1
-#include <EGL/egl.h>
-#define EGL_EGLEXT_PROTOTYPES
-#include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
-
 void avgFrm(short int,int,float *,float *);
 
 float max;
