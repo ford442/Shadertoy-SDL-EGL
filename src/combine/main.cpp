a571d206ea245981692c491f8e907bce7861d1da
Noah Cohn <noahc42@gmail.com>
2023-03-11 19:41:06 -0800

    Update main.cpp

--- src/combine/main.cpp
+++ src/combine/main.cpp
@@ -154,10 +154,10 @@ setTimeout(function(){
 Module.ccall('str');
 },300);
 setTimeout(function(){
-Module.ccall('b3');
+Module.ccall('b3_egl');
 },600);
 setTimeout(function(){
-Module.ccall('b3_egl');
+Module.ccall('b3');
 },800);
 document.getElementById('shut').innerHTML=2;
 bezl.width=window.innerWidth;
