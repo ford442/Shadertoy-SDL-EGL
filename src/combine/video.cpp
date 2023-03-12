0ddaf1d6222644c660f062827f4333f37931ff47
Noah Cohn <noahc42@gmail.com>
2023-03-11 19:24:41 -0800

    Update video.cpp

--- src/combine/video.cpp
+++ src/combine/video.cpp
@@ -281,7 +281,7 @@ eval("$r"+i+"=t($"+i+");r($r"+i+");$$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+",0,la);
 $bb=R(vv);
 $B.set($bb,0,sz);
 var pointb=66*la;  // has to revar?
-  Module.ccall("frm",null,[],[]);
+Module.ccall("frm",null,[],[]);
 Module.ccall("nano",null,["Number","Number","Number","Number"],[$F,sz,pointb,pointa]);
 Module.ccall("clr",null,["Number","Number","Number"],[agav[200],agav[100],agav[0]]);
 setTimeout(function(){
