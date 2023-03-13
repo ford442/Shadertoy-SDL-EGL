545dc8a6b4921da96b4ede13f6bd8a12ea987aa1
Noah Cohn <noahc42@gmail.com>
2023-03-12 21:31:01 -0700

    Update main.hpp

--- include/audio/main.hpp
+++ include/audio/main.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+#undef FLT_EVAL_METHOD
+#define FLT_EVAL_METHOD 2
+#pragma STDC CX_LIMITED_RANGE ON
+
+#include <wasm_simd128.h>
+#include <tmmintrin.h> // ssse 3
+#include <smmintrin.h>  // sse 4.1
+#include <avxintrin.h>  // AVX
+
+#include <xmmintrin.h>
+#include <immintrin.h> 
+#include <nmmintrin.h>  //  sse 4.2
+#include <unistd.h>
+
 #include <cstdlib>
 #include <ctime>
 #include <emscripten.h>
