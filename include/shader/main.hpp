2ad03459f8b78ba62512f8451b1cb71ab5b4d0df
Noah Cohn <noahc42@gmail.com>
2023-03-12 22:02:10 -0700

    Update main.hpp

--- include/shader/main.hpp
+++ include/shader/main.hpp
@@ -4,9 +4,10 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include "../../include/shader/intrins.hpp"
-int rNd(int);
+v128_t rNd(int);
 
-int rD,Th,Rg;
+int rD,Th;
+v128_t Dr;
 
 extern"C"{
 
