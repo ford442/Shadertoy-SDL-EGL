1d95d66ccf05dd3915109246472f96f732baa322
Noah Cohn <noahc42@gmail.com>
2023-03-13 20:37:55 -0700

    Update egl.hpp

--- include/combine/egl.hpp
+++ include/combine/egl.hpp
@@ -1,19 +1,4 @@
 #pragma once
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 2
-#pragma STDC CX_LIMITED_RANGE ON
-
-#include <wasm_simd128.h>
-#include <tmmintrin.h> // ssse 3
-#include <smmintrin.h>  // sse 4.1
-#include <avxintrin.h>  // AVX
-
-#include <xmmintrin.h>
-#include <immintrin.h> 
-#include <nmmintrin.h>  //  sse 4.2
-#include <unistd.h>
 
 #include <webgl/webgl2.h>
 
