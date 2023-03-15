28a2fbbb7bee08878df33995b697dec24c25548a
Noah Cohn <noahc42@gmail.com>
2023-03-15 00:49:35 -0700

    Update gl.hpp

--- include/combine/gl.hpp
+++ include/combine/gl.hpp
@@ -4,10 +4,12 @@
 
 #ifndef GL_FRAGMENT_PRECISION_HIGH
 #define GL_FRAGMENT_PRECISION_HIGH
+#endif
 
 #include <GL/gl.h>
 
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES
+#endif
 
 #include <GL/glext.h>
