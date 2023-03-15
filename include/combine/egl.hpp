6ccf60d9d2b4f60aa44ddbd91df0fe8349415d71
Noah Cohn <noahc42@gmail.com>
2023-03-15 00:43:07 -0700

    Update egl.hpp

--- include/combine/egl.hpp
+++ include/combine/egl.hpp
@@ -6,18 +6,28 @@
 
 #ifndef GL_GLES_PROTOTYPES
 #define GL_GLES_PROTOTYPES
+#endif
+
 #include <GLES3/gl3.h>
 #include <GLES3/gl31.h>
 #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
+
 #ifndef EGL_EGL_PROTOTYPES 
 #define EGL_EGL_PROTOTYPES
+#endif
+
 #ifndef EGL_EGLEXT_PROTOTYPES
 #define EGL_EGLEXT_PROTOTYPES
+#endif
+
 #ifndef EGL_NO_PLATFORM_SPECIFIC_TYPES
 #define EGL_NO_PLATFORM_SPECIFIC_TYPES
+#endif
+
 #ifndef EGLAPI
 #define EGLAPI
+#endif
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
