93449b2515de6013a25c3d6af5a276bbe4a8dc3b
Noah Cohn <noahc42@gmail.com>
2023-03-16 17:54:35 -0700

    Update main_google_street.hpp

--- include/video/main_google_street.hpp
+++ include/video/main_google_street.hpp
@@ -3,6 +3,11 @@
 #include <ctime>
 #include <emscripten.h>
 #include <emscripten/html5.h>
+#include "../../include/video/intrins.hpp"
+
+#undef FLT_EVAL_METHOD
+#define FLT_EVAL_METHOD 0
+#pragma STDC CX_LIMITED_RANGE ON
 
 int rNd(int);
 
