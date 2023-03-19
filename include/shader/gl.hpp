ee88995bafda2c88714e1c980a061ecf863041e2
Noah Cohn <noahc42@gmail.com>
2023-03-19 07:06:30 -0700

    Update gl.hpp

--- include/shader/gl.hpp
+++ include/shader/gl.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
-#define GL_SILENCE_DEPRECATION
+// #define GL_SILENCE_DEPRECATION
 
-#define GL3_PROTOTYPES 1
-#define GL4_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
+#ifndef GL_FRAGMENT_PRECISION_HIGH
+#define GL_FRAGMENT_PRECISION_HIGH
+#endif
 
 #include <GL/gl.h>
-#define GL_GLEXT_PROTOTYPES 1
+
+#ifndef GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES
+#endif
+
 #include <GL/glext.h>
