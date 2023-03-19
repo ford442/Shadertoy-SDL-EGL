783459fac8af4673389bdb0037ba36e876975fc7
Noah Cohn <noahc42@gmail.com>
2023-03-19 06:49:03 -0700

    Update main.hpp

--- include/video/main.hpp
+++ include/video/main.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdlib>
 #include <ctime>
+#include <random>
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include "../../include/video/intrins.hpp"
@@ -8,6 +9,8 @@
 #undef FLT_EVAL_METHOD
 #define FLT_EVAL_METHOD 0
 #pragma STDC CX_LIMITED_RANGE ON
+#undef FLT_ROUNDS
+#define FLT_ROUNDS 1
 
 v128_t rNd(int);
 
