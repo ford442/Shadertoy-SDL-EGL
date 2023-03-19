fcd28512f7721d8f79bdd08817a99ef3e7103b9b
Noah Cohn <noahc42@gmail.com>
2023-03-18 23:26:29 -0700

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -21,14 +21,15 @@
 // #include <stdfloat>  //  c++23
 // double_t wi,hi;
 double wi,hi;
-float cMouseY,cMouseX,mouseY,mouseX,F=1.0f,Fm1=-1.0f;
-float_t F0=0.0f;
+float cMouseY,cMouseX,mouseY,mouseX;
+const float F=1.0f,Fm1=-1.0f;
+const float_t F0=0.0f;
 // float F0=0.0f;
 // double Ttime,Tdlt,Dm1=-1.0,D=1.0;
-long double Dm1=-1.0,D=1.0;
+const long double Dm1=-1.0,D=1.0;
 // double Dm1=-1.0,D=1.0;
 float Ttime,Tdlt;
-double_t D0=0.0;
+const double_t D0=0.0;
 // long double D0=0.0;
 // double D0=0.0;
 std::chrono::steady_clock::time_point t1;
@@ -36,7 +37,9 @@ std::chrono::steady_clock::time_point t2;
 std::chrono::steady_clock::time_point t3;
 struct timespec rem;
 struct timespec req={0,16666666};
+
 #include "../../include/shader/gl.hpp"
+
 const char * Fnm=reinterpret_cast<const char *>("/shader/shader1.toy");
 GLint fram;
 GLfloat mX,mY,mm,nn;
@@ -48,11 +51,11 @@ GLfloat g1g=F,S;
 GLsizei s4=4,i;
 GLuint EBO,VBO,shd_prg,smp_chn[4],smp_chn_res,VCO,ECO,vtx,frag,uni_mse,shader,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm,uni_fps;
 typedef struct{GLclampf XYZW[4];}Vertex;
-const Vertex vrt[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
-const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
-const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
+static volatile Vertex vrt[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
+static volatile GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
+static volatile GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 GLchar * src[4];
-GLchar cm_hdr_src[]=
+const GLchar cm_hdr_src[]=
 "#version 300 es\n"
 "#extension EGL_KHR_gl_colorspace : enable\n"
 // "#extension EGL_EXT_gl_colorspace_scrgb : enable\n"
@@ -86,24 +89,29 @@ GLchar cm_hdr_src[]=
 "precision highp isampler2DArray;precision highp usampler2D;precision highp usampler3D;"
 "precision highp usamplerCube;precision highp usampler2DArray;precision highp samplerCubeShadow;"
 "precision highp sampler2DArrayShadow;\n";
-GLchar vrt_bdy_src[]=
+const GLchar vrt_bdy_src[]=
 "\n layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n";
-GLchar frg_hdr_src[]=
+const GLchar frg_hdr_src[]=
 "uniform float iTime;uniform float iTimeDelta;uniform float iFrameRate;uniform vec4 iDate;uniform float iChannelTime[4];"
 "uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
 "uniform vec3 iChannelResolution[4];uniform vec3 iResolution;uniform vec4 iMouse;uniform float iSampleRate;"
 "out vec4 fragColor;\n";
-GLchar frg_ftr_src[]=
+const GLchar frg_ftr_src[]=
 "\n void main(){mainImage(fragColor,gl_FragCoord.xy);}\0";
-GLchar * cm_hdr=cm_hdr_src;
-GLchar * vrt_bdy=vrt_bdy_src;
-GLchar * frg_hdr=frg_hdr_src;
-GLchar * frg_ftr=frg_ftr_src;
+const GLchar * cm_hdr=cm_hdr_src;
+const GLchar * vrt_bdy=vrt_bdy_src;
+const GLchar * frg_hdr=frg_hdr_src;
+const GLchar * frg_ftr=frg_ftr_src;
+
 void uni(GLfloat,GLfloat,GLfloat,GLint,GLfloat);
+
 GLuint cmpl_shd(GLenum,GLsizei,GLchar **);
+
 GLchar * rd_fl(const char *);
+
 // char16_t * rd_fl(const char *);
 #include "../../include/shader/egl.hpp"
+
 // int_fast32_t iFrame,iwi,ihi;
 long long int iFrame,iwi,ihi;
 // long iFrame,iwi,ihi;
@@ -126,7 +134,7 @@ EGLint att_lst2[]={
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
 EGL_NONE
 };
-EGLint ctx_att[]={
+const EGLint ctx_att[]={
 EGL_CONTEXT_CLIENT_VERSION,(EGLint)3,
 EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)0,
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT, 
@@ -136,7 +144,7 @@ EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
 // EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
 EGL_NONE
 };
-EGLint att_lst[]={
+const EGLint att_lst[]={
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
 EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
@@ -165,14 +173,21 @@ EGL_SAMPLES,(EGLint)4,
 EGL_MULTISAMPLE_RESOLVE,EGL_MULTISAMPLE_RESOLVE_BOX,
 EGL_NONE
 };
+
 #include <emscripten.h>
 #include <emscripten/html5.h>
+
 EmscriptenWebGLContextAttributes attr;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
 EMSCRIPTEN_RESULT retCl,retMu,retMd,retMv,retSa,retSb,retSc;
 EM_BOOL ms_l,clk_l;
+
 EM_BOOL ms_clk(int,const EmscriptenMouseEvent *,void *);
+
 static EM_BOOL ms_mv(int,const EmscriptenMouseEvent *,void *);
+
 extern "C"{
+  
 void str();
+  
 }
