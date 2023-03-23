c91d0eae780bd9c87a67785148ec20a814474734
Noah Cohn <noahc42@gmail.com>
2023-03-22 19:49:52 -0700

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -59,7 +59,7 @@ typedef struct{GLclampf XYZW[4];}Vertex;
 Vertex vrt[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
 const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
-const GLchar * src[4];
+const GLchar * src[5];
 const GLchar cm_hdr_src[]=
 "#version 300 es\n"
 "#extension EGL_KHR_gl_colorspace : enable\n"
