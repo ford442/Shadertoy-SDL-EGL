5cb51ec78cfd55143d7fe82401685448e521edbf
Noah Cohn <noahc42@gmail.com>
2023-03-26 01:15:01 -0700

    Update gl.hpp

--- include/combine/gl.hpp
+++ include/combine/gl.hpp
@@ -3,16 +3,16 @@
 // #define GL_SILENCE_DEPRECATION
 
 #ifndef GL_FRAGMENT_PRECISION_HIGH
-#define GL_FRAGMENT_PRECISION_HIGH
+#define GL_FRAGMENT_PRECISION_HIGH 1
 #endif
 
 #include <GL/gl.h>
 
 #ifndef GL_GLEXT_PROTOTYPES
-#define GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES 1
 #endif
 
-#define GLFW_INCLUDE_GLEXT
+#define GLFW_INCLUDE_GLEXT 1
 #include <GLFW/glfw3.h>
 
 #include <GL/glext.h>
