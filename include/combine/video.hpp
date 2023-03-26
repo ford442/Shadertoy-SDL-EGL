aea07a30ac6d3f5bd37608399176b3c1111f8673
Noah Cohn <noahc42@gmail.com>
2023-03-26 01:27:24 -0700

    Update video.hpp

--- include/combine/video.hpp
+++ include/combine/video.hpp
@@ -1,3 +1,29 @@
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+#pragma STDC CX_LIMITED_RANGE OFF
+#pragma STDC FP_CONTRACT ON
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+
+double wi_js,hi_js;
+
+void egl();
+void avgFrm(short int,int,float *,float *);
+
+extern "C"{
+
+void b3_egl();
+void b3();
+void nano(short int,int,float *,float *);
+
+}
+
 #pragma once
 
 #include <cfloat>
@@ -5,13 +31,6 @@
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 #include "../../include/combine/intrins.hpp"
-
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 2
-#pragma STDC CX_LIMITED_RANGE ON
-#pragma STDC FP_CONTRACT ON
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
 
 #include <iostream>
 #include <algorithm>
@@ -21,18 +40,13 @@
 #include <cstdio>
 #include <cstdint>
 #include <cstdlib>
-#include <ctime>
-#include <chrono>
 #include <cfloat>
 #include <climits>
-
-#include <emscripten.h>
-
-double wi_js,hi_js;
 
 #include "../../include/combine/gl.hpp"
 
 GLint Size_js;
+GLclampf avrg,drk,brt,avr;
 
 #include "../../include/combine/egl.hpp"
 
@@ -97,21 +111,9 @@ EGL_MIPMAP_LEVEL,(EGLint)1,
 EGL_NONE
 };
 
-void egl();
-
-GLclampf avrg,drk,brt,avr;
-
-void avgFrm(short int,int,float *,float *);
-
+#include <emscripten.h>
 #include <emscripten/html5.h>
 
 EmscriptenWebGLContextAttributes attr_js;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx_js;
 
-extern "C"{
-
-void b3_egl();
-void b3();
-void nano(short int,int,float *,float *);
-
-}
