b61af6c93dba470e97447cb625cbcb82b0a9cbd4
Noah Cohn <noahc42@gmail.com>
2023-03-26 02:01:55 -0700

    Update main.hpp

--- include/shader/main.hpp
+++ include/shader/main.hpp
@@ -4,17 +4,20 @@ int rD,Th;
 
 #include "../../include/shader/intrins.hpp"
 
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE OFF
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+// #pragma STDC CX_LIMITED_RANGE OFF
 #pragma STDC FP_CONTRACT ON
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
-#define POSIX_TIMERS 1
-#define XOPEN_REALTIME 1
-#define POSIX_ASYNC_IO 1
-#define POSIX_PRIO_IO 1
-#define POSIX_SYNC_IO 1
+#pragma STDC FENV_ACCESS on
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
 
 v128_t rNd(int);
 
