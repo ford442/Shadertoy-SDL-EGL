3d8291a9021b61d55ede8f4cf8f243811b961be7
Noah Cohn <noahc42@gmail.com>
2023-03-26 01:57:42 -0700

    Update shader.hpp

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -6,7 +6,7 @@ void str();
 
 #undef _FLT_EVAL_METHOD
 #define _FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE OFF
+// #pragma STDC CX_LIMITED_RANGE OFF
 #pragma STDC FP_CONTRACT ON
 #define _XOPEN_REALTIME 1
 #define _POSIX_ASYNC_IO 1
@@ -16,6 +16,7 @@ void str();
 #define _POSIX_PRIORITIZED_IO 1
 #undef _FLT_ROUNDS
 #define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
 
 int iFrame;
 void Rend();
