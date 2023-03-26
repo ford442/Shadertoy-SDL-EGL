d9ea096e0cd16d4ae8a0fa0352aa9e2f6b3e2ed9
Noah Cohn <noahc42@gmail.com>
2023-03-26 04:06:58 -0700

    Update main.hpp

--- include/audio/main.hpp
+++ include/audio/main.hpp
@@ -1,3 +1,6 @@
+
+int Th,rD;
+
 #pragma once
 
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
@@ -10,8 +13,19 @@
 #include <immintrin.h> 
 #include <nmmintrin.h>  //  sse 4.2
 
-#define POSIX_TIMERS 1
-#define XOPEN_REALTIME 1
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+// #pragma STDC CX_LIMITED_RANGE OFF
+#pragma STDC FP_CONTRACT ON
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
 #include <unistd.h>
 
 extern "C"{
@@ -25,14 +39,7 @@ int r4nd(int);
 
 v128_t rNd(int);
 
-int Th,rD;
 v128_t Dr;
-
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE ON
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
 
 #include <cstdlib>
 #include <ctime>
