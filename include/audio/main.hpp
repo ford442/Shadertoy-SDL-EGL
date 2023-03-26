e63106f840ae0f112856757e5ad7b0f50f1826ae
Noah Cohn <noahc42@gmail.com>
2023-03-25 21:43:06 -0700

    Update main.hpp

--- include/audio/main.hpp
+++ include/audio/main.hpp
@@ -2,34 +2,17 @@
 
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE ON
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
-
 #include <wasm_simd128.h>
 #include <tmmintrin.h> // ssse 3
 #include <smmintrin.h>  // sse 4.1
 #include <avxintrin.h>  // AVX
-
 #include <xmmintrin.h>
 #include <immintrin.h> 
 #include <nmmintrin.h>  //  sse 4.2
+
+#define POSIX_TIMERS 1
+#define XOPEN_REALTIME 1
 #include <unistd.h>
-
-#include <cstdlib>
-#include <ctime>
-#include <random>
-
-#include <emscripten.h>
-#include <emscripten/html5.h>
-
-v128_t rNd(int);
-
-int Th,rD;
-v128_t Dr;
-std::random_device rd;
 
 extern "C"{
   
@@ -39,3 +22,23 @@ int c;
 int r4nd(int);
 
 }
+
+v128_t rNd(int);
+
+int Th,rD;
+v128_t Dr;
+
+#undef FLT_EVAL_METHOD
+#define FLT_EVAL_METHOD 0
+#pragma STDC CX_LIMITED_RANGE ON
+#undef FLT_ROUNDS
+#define FLT_ROUNDS 1
+
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+std::random_device rd;
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
