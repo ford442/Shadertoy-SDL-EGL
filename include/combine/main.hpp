5fa24ed4db95391ed99b81d6bc7e039db5a662b2
Noah Cohn <noahc42@gmail.com>
2023-03-26 01:16:16 -0700

    Update main.hpp

--- include/combine/main.hpp
+++ include/combine/main.hpp
@@ -5,15 +5,22 @@
 #include <cfloat>
 #include <random>
 #include <emscripten.h>
-#include <emscripten/html5.h>
-#include "../../include/combine/intrins.hpp"
+// #include <emscripten/html5.h>
 
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
 #pragma STDC CX_LIMITED_RANGE OFF
-#pragma STDC FP_CONTRACT OFF
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
+#pragma STDC FP_CONTRACT ON
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+
+#include "../../include/combine/intrins.hpp"
 
 v128_t rNd(int);
 int rD;
