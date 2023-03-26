ebe20f7c093b60b34d30569b0306495a0de2ac30
Noah Cohn <noahc42@gmail.com>
2023-03-26 04:07:52 -0700

    Update audio.hpp

--- include/audio/audio.hpp
+++ include/audio/audio.hpp
@@ -8,6 +8,20 @@ void pl();
   
 }
 
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+// #pragma STDC CX_LIMITED_RANGE OFF
+#pragma STDC FP_CONTRACT ON
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
+
 #include <cstdint>
 #include <SDL2/SDL.h>
 
