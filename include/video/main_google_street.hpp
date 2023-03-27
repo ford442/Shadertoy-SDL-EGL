9d56df3fe3bded71569f344ecab66b868f2db385
Noah Cohn <noahc42@gmail.com>
2023-03-26 18:27:33 -0700

    Update main_google_street.hpp

--- include/video/main_google_street.hpp
+++ include/video/main_google_street.hpp
@@ -1,13 +1,23 @@
 #pragma once
-#include <cstdlib>
-#include <ctime>
+
 #include <emscripten.h>
-#include <emscripten/html5.h>
+#include <random>
+
 #include "../../include/video/intrins.hpp"
 
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE ON
+#pragma STDC FP_CONTRACT ON
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+// #pragma STDC CX_LIMITED_RANGE OFF
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
 
 int rNd(int);
 
@@ -16,3 +26,7 @@ extern"C"{
 int r4nd(int);
 
 }
+
+#include <cstdlib>
+#include <ctime>
+// #include <emscripten/html5.h>
