c66e4b051c2a7cc4a7f086c1b6c95937307c6e2a
Noah Cohn <noahc42@gmail.com>
2023-03-26 18:25:28 -0700

    Update main.hpp

--- include/video/main.hpp
+++ include/video/main.hpp
@@ -1,23 +1,31 @@
 #pragma once
-#include <cstdlib>
-#include <ctime>
-#include <cfloat>
-#include <random>
-#include <emscripten.h>
-#include <emscripten/html5.h>
-#include "../../include/video/intrins.hpp"
 
-#undef FLT_EVAL_METHOD
-#define FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE ON
-#undef FLT_ROUNDS
-#define FLT_ROUNDS 1
+#include <emscripten.h>
+
+int Th,rD;
+
+#include <random>
+std::random_device rd;
+
+#pragma STDC FP_CONTRACT ON
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD 0
+// #pragma STDC CX_LIMITED_RANGE OFF
+#define _XOPEN_REALTIME 1
+#define _POSIX_ASYNC_IO 1
+#define _POSIX_PRIO_IO 1
+#define _POSIX_SYNC_IO 1
+#define	_XOPEN_SHM 1
+#define _POSIX_PRIORITIZED_IO 1
+#undef _FLT_ROUNDS
+#define _FLT_ROUNDS 1
+#define _POSIX_REGEXP	1
+
+#include "../../include/video/intrins.hpp"
 
 v128_t rNd(int);
 
-int Th,rD;
 v128_t Dr;
-std::random_device rd;
 
 extern "C"{
   
@@ -26,3 +34,9 @@ int c;
 int r4nd(int);
 
 }
+
+#include <cstdlib>
+#include <ctime>
+#include <cfloat>
+
+// #include <emscripten/html5.h>
