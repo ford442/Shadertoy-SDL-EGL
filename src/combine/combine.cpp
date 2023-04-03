c4e8e01b7dc2290baf5d9a3318a593b1457875da
Noah Cohn <noahc42@gmail.com>
2023-04-03 01:28:51 -0700

    Update combine.cpp

--- src/combine/combine.cpp
+++ src/combine/combine.cpp
@@ -189,16 +189,13 @@ glEnable(GL_BLEND);
 // glDisable(GL_BLEND);
 // glBlendEquationSeparate(GL_MAX,GL_FUNC_ADD);
 // glBlendFuncSeparate(GL_DST_COLOR,GL_SRC_COLOR,GL_DST_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
- 
-glBlendEquationSeparate(GL_MAX,GL_FUNC_ADD);
-glBlendFuncSeparate(GL_SRC_COLOR,GL_DST_COLOR,GL_DST_COLOR,GL_SRC_ALPHA);
+ glBlendEquationSeparate(GL_FUNC_SUBTRACT,GL_MIN);
+glBlendFuncSeparate(GL_SRC_COLOR,GL_ONE_MINUS_DST_COLOR,GL_DST_COLOR,GL_SRC_ALPHA);
  
 // glBlendEquation(GL_LIGHTEN_NV);
  
- 
-// glBlendFunc(GL_ONE_MINUS_SRC_ALPHA,GL_DST_COLOR);
- 
- 
+ // glBlendFunc(GL_ONE_MINUS_SRC_ALPHA,GL_DST_COLOR);
+  
 glClearColor((GLclampf)gF,(GLclampf)gF,(GLclampf)gF,(GLclampf)gF);
 glBlendColor((GLclampf)gF,(GLclampf)gF,(GLclampf)gF,(GLclampf)gF);
 glGenBuffers((GLsizei)1,&VBO);
