9a54ad383880eb1471ded8f11df37e764881ada0
Noah Cohn <noahc42@gmail.com>
2023-04-15 12:00:55 -0700

    Update combine.cpp

--- src/combine/combine.cpp
+++ src/combine/combine.cpp
@@ -192,7 +192,7 @@ glFrontFace(GL_CW);
 // glDisable(GL_BLEND);
 // glBlendEquationSeparate(GL_MAX,GL_FUNC_ADD);
 // glBlendFuncSeparate(GL_DST_COLOR,GL_SRC_COLOR,GL_DST_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-glBlendEquationSeparate(GL_FUNC_SUBTRACT,GL_FUNC_ADD);
+glBlendEquationSeparate(GL_FUNC_ADD,GL_MAX);
 // glBlendEquation(GL_EXCLUSION_NV);                             
 glBlendFuncSeparate(GL_SRC_COLOR,GL_ONE_MINUS_DST_COLOR,GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 // glBlendEquation(GL_LIGHTEN_NV);
