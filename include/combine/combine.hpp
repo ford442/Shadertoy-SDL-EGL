6cf2dfbbb7b6e4c16ab6ada8fb0c2925481de0c9
Noah Cohn <noahc42@gmail.com>
2023-04-17 19:36:17 -0700

    Update combine.hpp

--- include/combine/combine.hpp
+++ include/combine/combine.hpp
@@ -1,7 +1,7 @@
 // #undef _FLT_EVAL_METHOD
 // #define _FLT_EVAL_METHOD 0
 #pragma STDC CX_LIMITED_RANGE ON
-#pragma STDC FP_CONTRACT FAST
+#pragma STDC FP_CONTRACT ON
 #define _XOPEN_REALTIME 1
 #define _POSIX_ASYNC_IO 1
 #define _POSIX_PRIO_IO 1
