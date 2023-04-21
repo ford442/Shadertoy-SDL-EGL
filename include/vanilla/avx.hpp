8b08d7560b99991e1b998a76b069062aca2398f7
Noah Cohn <noahc42@gmail.com>
2023-04-21 01:26:58 -0700

    Create avx.hpp

---
+++ include/vanilla/avx.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <wasm_simd128.h>
+#include <tmmintrin.h> // ssse 3
+#include <smmintrin.h>  // sse 4.1
+#include <avxintrin.h>  // AVX
+
+#define VLEAVE _mm256_zeroupper
+
+#include <xmmintrin.h>
+#include <immintrin.h> 
+#include <nmmintrin.h>  //  sse 4.2
+#include <unistd.h>
