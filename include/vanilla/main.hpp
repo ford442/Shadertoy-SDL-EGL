75478631e268186d3cfab2cfebb4411dc3fe7326
Noah Cohn <noahc42@gmail.com>
2023-04-21 01:12:23 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -1,5 +1,6 @@
 #include <emscripten.h>
-#include "emscripten/html5_webgpu.h"
-#include "webgpu/webgpu.h"
-#include "webgpu/webgpu_cpp.h"
-#include "boost/tuple/tuple.hpp"
+#include <emscripten/html5_webgpu.h>
+#include <webgpu/webgpu.h>
+#include <webgpu/webgpu_cpp.h>
+#include <boost/tuple/tuple.hpp>
+#include <boost/timer/timer.hpp>
