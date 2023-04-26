36f83dc81be73f7aa33a79f4a7553207dea20af8
Noah Cohn <noahc42@gmail.com>
2023-04-25 23:36:04 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -58,7 +58,6 @@ const float_t F0=0.0f;
 #include <boost/chrono.hpp>
 #include <boost/chrono/time_point.hpp>
 #include <boost/chrono/system_clocks.hpp>
-#include <boost/chrono/clock_strings.hpp>
 #include <boost/chrono/typeof/boost/chrono/chrono.hpp>
 #include <boost/chrono/chrono.hpp>
 #include <boost/chrono/duration.hpp>
