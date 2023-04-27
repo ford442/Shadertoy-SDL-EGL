af9f00ad8affccb87ddc75b31fb2421647351391
Noah Cohn <noahc42@gmail.com>
2023-04-26 22:25:19 -0700

    Update audio.cpp

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -1,6 +1,6 @@
 #include "../../include/audio/audio.hpp"
 
-void SDLCALL bfr(void * unused,GLuint * stm,GLuint len){
+void SDLCALL bfr(void * unused,GLubyte * stm,GLuint len){
 // Uint8 * wptr;
 GLubyte * wptr;
 GLint lft;
