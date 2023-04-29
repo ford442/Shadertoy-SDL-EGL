d3541cd6a903a503ca2e188f02b53f0fabcdb8ce
Noah Cohn <noahc42@gmail.com>
2023-04-28 17:34:22 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -19,10 +19,13 @@ using namespace ::boost::tuples;
 using namespace std;
 
 Class funcs{
-  private:
-  int r;
-float Tdlt;
-float nn;
+
+private:
+
+int r,m;
+float cc,pp,uu,cc2,pp2,uu2,Tdlt,nn;
+v128_t aa,vv,xx,l,tt;
+
 public:
 
 float tuple_float_short(float num){
@@ -44,10 +47,7 @@ return nn;
 }
 
 float tuple_float_long(float num){
-int r;
 cc2=num,pp2=num,uu2=num;cc=num,pp=num,uu=num;
-float Tdlt;
-float nn;
 cc2=num,pp2=num,uu2=num;cc=num,pp=num,uu=num;
 for(r=0;r<100;r++){
 tie(cc,pp,uu,cc2,pp2,uu2);
@@ -65,36 +65,36 @@ return uu;
 
 GLfloat tuple_gl(GLfloat num){
 t1=std::chrono::steady_clock::now();
-GLfloat gg=num,pp=num,uu=num;
-tie(gg,pp,uu);
+GLfloat ggg=num,ppg=num,uug=num;
+tie(ggg,ppg,uug);
 t2=std::chrono::steady_clock::now();
 std::chrono::duration<double,std::chrono::milliseconds::period>time_span=std::chrono::duration<double,std::chrono::milliseconds::period>(t2-t1);
 float Tdlt=time_span.count()*1000.0;
 cout << Tdlt << endl;
-return uu;
+return uug;
 }
 
 v128_t tuple_avx(float num){
 t1=std::chrono::steady_clock::now();
-v128_t aa=wasm_i32x4_splat(num);
-v128_t vv=wasm_i32x4_splat(num);
-v128_t xx=wasm_i32x4_splat(num);
+aa=wasm_i32x4_splat(num);
+vv=wasm_i32x4_splat(num);
+xx=wasm_i32x4_splat(num);
 tie(aa,vv,xx);
 t2=std::chrono::steady_clock::now();
 std::chrono::duration<double,std::chrono::milliseconds::period>time_span=std::chrono::duration<double,std::chrono::milliseconds::period>(t2-t1);
-float Tdlt=time_span.count()*1000.0;
+Tdlt=time_span.count()*1000.0;
 cout << Tdlt << endl;
 return xx;
 }
 
 v128_t simd_test(float * a){
 t1=std::chrono::steady_clock::now();
-int m=a[0]*1000.0;
-v128_t l=wasm_i32x4_splat(m);
-v128_t tt=wasm_f32x4_add(l,l);
+m=a[0]*1000.0;
+l=wasm_i32x4_splat(m);
+tt=wasm_f32x4_add(l,l);
 t2=std::chrono::steady_clock::now();
 std::chrono::duration<float, std::chrono::milliseconds::period> time_span=std::chrono::duration<float,std::chrono::milliseconds::period>(t2-t1);
-float Tdlt=time_span.count()*1000.0;
+Tdlt=time_span.count()*1000.0;
 cout << Tdlt << endl;
 return tt;
 }
