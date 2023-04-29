f7ad1ffb7a75e073d6c1485bb1124ed6f30773f7
Noah Cohn <noahc42@gmail.com>
2023-04-29 06:57:31 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -381,7 +381,7 @@ tie(shad.EBO,shad.VBO,shad.VCO);
 tie(config_size,major,minor);
 tie(display,surface,eglconfig);
 tie(attr,ctxegl,ctx);
-tie(uni_fps,uni_srate,times.uni_frm,fram);
+tie(uni_fps,uni_srate,times.uni_frm);
 tie(times.uni_tme,times.uni_tme_dlt);
 tie(smp_chn_res,smp_chn,uni_res);
 tie(cm_hdr_src,vrt_bdy_src,frg_hdr_src,frg_ftr_src);
