050f56df47b42a70e8cb0aa655836a656c765ed2
Noah Cohn <noahc42@gmail.com>
2023-04-28 23:39:39 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -107,6 +107,227 @@ glCompileShader(shader);
 return shader;
 }
   
+};
+
+class Run
+{
+
+private:
+  
+const float_t F=1.0f,Fm1=-1.0f;
+const double_t Dm1=-1.0,D=1.0;
+const double_t D0=0.0;
+const float_t F0=0.0f;
+std::chrono::high_resolution_clock::time_point t1;
+std::chrono::high_resolution_clock::time_point t2;
+std::chrono::high_resolution_clock::time_point t3;
+  int32_t tmm=166666660;
+struct timespec rem;
+struct timespec req={0,tmm};
+
+// using namespace std::chrono;
+
+GLfloat xx;
+GLfloat yy;
+GLfloat Tm;
+GLfloat delt;
+GLdouble wi,hi;
+GLfloat Tdlt;
+GLint ele=36;
+std::chrono::duration<GLdouble,std::chrono::seconds::period>time_spana;
+GLdouble Ttime;
+std::chrono::duration<GLdouble,std::chrono::seconds::period>time_spanb;
+GLint iFrame,iFps,fram;
+GLfloat mouseY,mouseX;
+GLint Size;
+GLfloat mX,mY,mm,nn;
+GLfloat gF=F,gF0=F0,gFm1=Fm1;
+GLclampf x,y;
+GLdouble gD=1.0,gD0=0.0,gDm1=-1.0;
+GLfloat S;
+  GLuint EBO,VBO,VCO,ECO;
+GLuint uni_mse,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm,uni_fps,smp_chn_res,smp_chn[4];
+typedef struct{GLfloat XYZW[4];}Vertex;
+Vertex vrt[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
+const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
+const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
+EGLDisplay display;
+EGLSurface surface;
+EGLContext ctxegl;
+EGLConfig eglconfig;
+EGLint config_size,major,minor;
+
+const GLchar * src[4];
+
+const GLchar cm_hdr_src[]=
+"#version 300 es\n"
+"#pragma STDGL(fastmath on)\n"
+"#pragma optionNV(fastmath on)\n"
+"#pragma STDGL(fastprecision on)\n"
+"#pragma optionNV(fastprecision on)\n"
+// "#pragma STDGL(unroll none)\n"
+// "#pragma optionNV(unroll none)\n"
+"#pragma STDGL(ifcvt none)\n"
+"#pragma optionNV(ifcvt none)\n"
+// "#pragma STDGL(inline none)\n"
+// "#pragma optionNV(inline none)\n"
+"#undef HW_PERFORMANCE\n"
+"#define HW_PERFORMANCE 0\n"
+"precision mediump float;precision mediump int;\n";
+
+const GLchar vrt_bdy_src[]=
+"layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n\0";
+
+const GLchar frg_hdr_src[]=
+"precision lowp sampler3D;precision lowp sampler2D;"
+"precision lowp samplerCube;precision lowp sampler2DArray;precision lowp sampler2DShadow;"
+"precision lowp isampler2D;precision lowp isampler3D;precision lowp isamplerCube;"
+"precision lowp isampler2DArray;precision lowp usampler2D;precision lowp usampler3D;"
+"precision lowp usamplerCube;precision lowp usampler2DArray;precision lowp samplerCubeShadow;"
+"precision lowp sampler2DArrayShadow;"
+"uniform highp float iTime;uniform highp float iTimeDelta;uniform highp float iFrameRate;uniform lowp vec4 iDate;uniform lowp float iChannelTime[4];"
+"uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
+"uniform vec3 iChannelResolution[4];uniform highp vec3 iResolution;uniform vec4 iMouse;uniform highp float iSampleRate;"
+"out highp vec4 fragColor;\n";
+
+const GLchar frg_ftr_src[]=
+"void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
+
+const GLchar * cm_hdr=cm_hdr_src;
+const GLchar * vrt_bdy=vrt_bdy_src;
+const GLchar * frg_hdr=frg_hdr_src;
+const GLchar * frg_ftr=frg_ftr_src;
+  
+static const EGLint att_lst2[]={ 
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT|EGL_GL_COLORSPACE_BT2020_PQ_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
+EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_LINEAR_EXT|EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
+// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_LINEAR_EXT|EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT|EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
+// EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
+// EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
+EGL_NONE,EGL_NONE
+};
+
+static const EGLint ctx_att[]={
+EGL_CONTEXT_MAJOR_VERSION_KHR,(EGLint)4,
+EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)7,
+// EGL_CONTEXT_MAJOR_VERSION_KHR,(EGLint)3,
+// EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)0,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
+EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
+// EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_HIGH_IMG,
+EGL_NONE,EGL_NONE
+};
+static const EGLint att_lst[]={
+// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+// EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
+// EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
+// EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
+// EGL_RENDERABLE_TYPE,EGL_NONE,
+// EGL_CONFORMANT,EGL_OPENGL_ES3_BIT,
+// EGL_CONFORMANT,EGL_NONE,
+// EGL_CONFIG_CAVEAT,EGL_NONE,
+EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
+EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
+// EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
+EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
+// EGL_NATIVE_RENDERABLE,EGL_TRUE,
+EGL_RED_SIZE,(EGLint)32,
+EGL_GREEN_SIZE,(EGLint)32,
+EGL_BLUE_SIZE,(EGLint)32,
+EGL_ALPHA_SIZE,(EGLint)32,
+EGL_DEPTH_SIZE,(EGLint)24,
+EGL_STENCIL_SIZE,(EGLint)8,
+EGL_BUFFER_SIZE,(EGLint)64,
+EGL_SAMPLE_BUFFERS,(EGLint)1,
+EGL_COVERAGE_BUFFERS_NV,(EGLint)1,
+EGL_COVERAGE_SAMPLES_NV,(EGLint)1,
+EGL_SAMPLES,(EGLint)1,
+EGL_MIPMAP_LEVEL,(EGLint)1,
+EGL_MULTISAMPLE_RESOLVE,EGL_MULTISAMPLE_RESOLVE,
+EGL_NONE,EGL_NONE
+};
+
+EmscriptenWebGLContextAttributes attr;
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
+EMSCRIPTEN_RESULT retCl,retMu,retMd,retMv,retSa,retSb,retSc;
+EM_BOOL ms_l,clk_l;
+  
+public:
+
+static inline EM_BOOL ms_clk(int eventType,const EmscriptenMouseEvent * e,void * userData){
+if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
+if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0){
+ms_l=true;
+}
+if(eventType==EMSCRIPTEN_EVENT_MOUSEUP){
+ms_l=false;
+}}
+return(EM_BOOL)1;
+}
+
+static inline EM_BOOL ms_mv(int eventType,const EmscriptenMouseEvent * e,void * userData){
+if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
+if(eventType==EMSCRIPTEN_EVENT_MOUSEMOVE&&(e->movementX!=0||e->movementY!=0)){
+x=e->clientX;
+y=e->clientY;
+}}
+return (EM_BOOL)1;
+}
+
+static inline void uni(GLfloat xx,GLfloat yy,GLfloat Tm,GLint fram,GLfloat delt){
+retCl=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
+retMd=emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
+if(ms_l==true){
+retMv=emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_mv);
+retMu=emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
+if(clk_l==true){
+const GLfloat xxx=xx;
+const GLfloat yyy=yy;
+mX=1.0-(xxx*Size);
+mY=1.0-(yyy*Size);
+clk_l=false;
+}
+mm=S*xx;
+nn=S*yy;
+glUniform4f(uni_mse,mm,nn,mX,mY);
+}else{
+clk_l=true;
+}
+glUniform1f(uni_tme,Tm);
+glUniform1f(uni_tme_dlt,delt);
+glUniform1i(uni_frm,fram);
+return;
+}
+
+// static inline void(*un)(GLfloat,GLfloat,GLfloat,GLint,GLfloat){&uni};
+
+static inline void Rend(){
+iFrame++;
+t3=t2;
+t2=std::chrono::high_resolution_clock::now();
+time_spana=std::chrono::duration<GLdouble,std::chrono::seconds::period>(t2-t1);
+time_spanb=std::chrono::duration<GLdouble,std::chrono::seconds::period>(t2-t3);
+Ttime=time_spana.count();
+Tdlt=time_spanb.count();
+mouseX=x/S;
+mouseY=(S-y)/S;
+run.uni(mouseX,mouseY,Ttime,iFrame,Tdlt);
+// glClear(GL_COLOR_BUFFER_BIT);
+// glClear(GL_DEPTH_BUFFER_BIT);
+// glClear(GL_STENCIL_BUFFER_BIT);
+glDrawElements(GL_TRIANGLES,ele,GL_UNSIGNED_BYTE,indc);
+// glFlush();
+// glFinish();
+// nanosleep(&req,&rem);
+return;
+}
+  
+  
 static inline void strt(){
 tie(F,Fm1,F0);
 tie(gF,gFm1,gF0);
@@ -309,184 +530,7 @@ nanosleep(&req,&rem);
 emscripten_set_main_loop((void(*)())Rend,0,0);
 return;
 }
-};
-
-class Run
-{
-
-private:
   
-const float_t F=1.0f,Fm1=-1.0f;
-const double_t Dm1=-1.0,D=1.0;
-const double_t D0=0.0;
-const float_t F0=0.0f;
-std::chrono::high_resolution_clock::time_point t1;
-std::chrono::high_resolution_clock::time_point t2;
-std::chrono::high_resolution_clock::time_point t3;
-  int32_t tmm=166666660;
-struct timespec rem;
-struct timespec req={0,tmm};
-
-// using namespace std::chrono;
-
-GLfloat xx;
-GLfloat yy;
-GLfloat Tm;
-GLfloat delt;
-GLdouble wi,hi;
-GLfloat Tdlt;
-GLint ele=36;
-std::chrono::duration<GLdouble,std::chrono::seconds::period>time_spana;
-GLdouble Ttime;
-std::chrono::duration<GLdouble,std::chrono::seconds::period>time_spanb;
-GLint iFrame,iFps,fram;
-GLfloat mouseY,mouseX;
-GLint Size;
-GLfloat mX,mY,mm,nn;
-GLfloat gF=F,gF0=F0,gFm1=Fm1;
-GLclampf x,y;
-GLdouble gD=1.0,gD0=0.0,gDm1=-1.0;
-GLfloat S;
-  GLuint EBO,VBO,VCO,ECO;
-GLuint uni_mse,uni_srate,uni_res,uni_tme_dlt,uni_tme,uni_frm,uni_fps,smp_chn_res,smp_chn[4];
-typedef struct{GLfloat XYZW[4];}Vertex;
-Vertex vrt[]={{gFm1,gFm1,gF,gF},{gF,gFm1,gF,gF},{gF,gF,gF,gF},{gFm1,gF,gF,gF},{gFm1,gFm1,gFm1,gF},{gF,gFm1,gFm1,gF},{gF,gF,gFm1,gF},{gFm1,gF,gF,gF}};
-const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
-const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
-EGLDisplay display;
-EGLSurface surface;
-EGLContext ctxegl;
-EGLConfig eglconfig;
-EGLint config_size,major,minor;
-
-static const EGLint att_lst2[]={ 
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT|EGL_GL_COLORSPACE_BT2020_PQ_EXT,
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_EXT,
-EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_EXT,
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_LINEAR_EXT|EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT,
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SCRGB_LINEAR_EXT|EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT|EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
-// EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_BT2020_PQ_EXT,
-// EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_BT2020_LINEAR_EXT,
-EGL_NONE,EGL_NONE
-};
-
-static const EGLint ctx_att[]={
-EGL_CONTEXT_MAJOR_VERSION_KHR,(EGLint)4,
-EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)7,
-// EGL_CONTEXT_MAJOR_VERSION_KHR,(EGLint)3,
-// EGL_CONTEXT_MINOR_VERSION_KHR,(EGLint)0,
-// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
-EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
-// EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_HIGH_IMG,
-EGL_NONE,EGL_NONE
-};
-static const EGLint att_lst[]={
-// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
-// EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
-// EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
-// EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
-// EGL_RENDERABLE_TYPE,EGL_NONE,
-// EGL_CONFORMANT,EGL_OPENGL_ES3_BIT,
-// EGL_CONFORMANT,EGL_NONE,
-// EGL_CONFIG_CAVEAT,EGL_NONE,
-EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
-EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
-EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
-// EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
-EGL_COLOR_FORMAT_HI,EGL_COLOR_RGBA_HI,
-// EGL_NATIVE_RENDERABLE,EGL_TRUE,
-EGL_RED_SIZE,(EGLint)32,
-EGL_GREEN_SIZE,(EGLint)32,
-EGL_BLUE_SIZE,(EGLint)32,
-EGL_ALPHA_SIZE,(EGLint)32,
-EGL_DEPTH_SIZE,(EGLint)24,
-EGL_STENCIL_SIZE,(EGLint)8,
-EGL_BUFFER_SIZE,(EGLint)64,
-EGL_SAMPLE_BUFFERS,(EGLint)1,
-EGL_COVERAGE_BUFFERS_NV,(EGLint)1,
-EGL_COVERAGE_SAMPLES_NV,(EGLint)1,
-EGL_SAMPLES,(EGLint)1,
-EGL_MIPMAP_LEVEL,(EGLint)1,
-EGL_MULTISAMPLE_RESOLVE,EGL_MULTISAMPLE_RESOLVE,
-EGL_NONE,EGL_NONE
-};
-
-EmscriptenWebGLContextAttributes attr;
-EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx;
-EMSCRIPTEN_RESULT retCl,retMu,retMd,retMv,retSa,retSb,retSc;
-EM_BOOL ms_l,clk_l;
-  
-public:
-
-static inline EM_BOOL ms_clk(int eventType,const EmscriptenMouseEvent * e,void * userData){
-if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
-if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0){
-ms_l=true;
-}
-if(eventType==EMSCRIPTEN_EVENT_MOUSEUP){
-ms_l=false;
-}}
-return(EM_BOOL)1;
-}
-
-static inline EM_BOOL ms_mv(int eventType,const EmscriptenMouseEvent * e,void * userData){
-if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!=0){
-if(eventType==EMSCRIPTEN_EVENT_MOUSEMOVE&&(e->movementX!=0||e->movementY!=0)){
-x=e->clientX;
-y=e->clientY;
-}}
-return (EM_BOOL)1;
-}
-
-static inline void uni(GLfloat xx,GLfloat yy,GLfloat Tm,GLint fram,GLfloat delt){
-retCl=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
-retMd=emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
-if(ms_l==true){
-retMv=emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_mv);
-retMu=emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
-if(clk_l==true){
-const GLfloat xxx=xx;
-const GLfloat yyy=yy;
-mX=1.0-(xxx*Size);
-mY=1.0-(yyy*Size);
-clk_l=false;
-}
-mm=S*xx;
-nn=S*yy;
-glUniform4f(uni_mse,mm,nn,mX,mY);
-}else{
-clk_l=true;
-}
-glUniform1f(uni_tme,Tm);
-glUniform1f(uni_tme_dlt,delt);
-glUniform1i(uni_frm,fram);
-return;
-}
-
-// static inline void(*un)(GLfloat,GLfloat,GLfloat,GLint,GLfloat){&uni};
-
-static inline void Rend(){
-iFrame++;
-t3=t2;
-t2=std::chrono::high_resolution_clock::now();
-time_spana=std::chrono::duration<GLdouble,std::chrono::seconds::period>(t2-t1);
-time_spanb=std::chrono::duration<GLdouble,std::chrono::seconds::period>(t2-t3);
-Ttime=time_spana.count();
-Tdlt=time_spanb.count();
-mouseX=x/S;
-mouseY=(S-y)/S;
-run.uni(mouseX,mouseY,Ttime,iFrame,Tdlt);
-// glClear(GL_COLOR_BUFFER_BIT);
-// glClear(GL_DEPTH_BUFFER_BIT);
-// glClear(GL_STENCIL_BUFFER_BIT);
-glDrawElements(GL_TRIANGLES,ele,GL_UNSIGNED_BYTE,indc);
-// glFlush();
-// glFinish();
-// nanosleep(&req,&rem);
-return;
-}
 };
 
 
@@ -495,46 +539,6 @@ const char * Fnm=reinterpret_cast<const char *>("/shader/shader.glsl");
 // int32_t Size;
 
 GLsizei s4=4,i;
-const GLchar * src[4];
-
-const GLchar cm_hdr_src[]=
-"#version 300 es\n"
-"#pragma STDGL(fastmath on)\n"
-"#pragma optionNV(fastmath on)\n"
-"#pragma STDGL(fastprecision on)\n"
-"#pragma optionNV(fastprecision on)\n"
-// "#pragma STDGL(unroll none)\n"
-// "#pragma optionNV(unroll none)\n"
-"#pragma STDGL(ifcvt none)\n"
-"#pragma optionNV(ifcvt none)\n"
-// "#pragma STDGL(inline none)\n"
-// "#pragma optionNV(inline none)\n"
-"#undef HW_PERFORMANCE\n"
-"#define HW_PERFORMANCE 0\n"
-"precision mediump float;precision mediump int;\n";
-
-const GLchar vrt_bdy_src[]=
-"layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n\0";
-
-const GLchar frg_hdr_src[]=
-"precision lowp sampler3D;precision lowp sampler2D;"
-"precision lowp samplerCube;precision lowp sampler2DArray;precision lowp sampler2DShadow;"
-"precision lowp isampler2D;precision lowp isampler3D;precision lowp isamplerCube;"
-"precision lowp isampler2DArray;precision lowp usampler2D;precision lowp usampler3D;"
-"precision lowp usamplerCube;precision lowp usampler2DArray;precision lowp samplerCubeShadow;"
-"precision lowp sampler2DArrayShadow;"
-"uniform highp float iTime;uniform highp float iTimeDelta;uniform highp float iFrameRate;uniform lowp vec4 iDate;uniform lowp float iChannelTime[4];"
-"uniform sampler2D iChannel0;uniform sampler2D iChannel1;uniform sampler2D iChannel2;uniform sampler2D iChannel3;"
-"uniform vec3 iChannelResolution[4];uniform highp vec3 iResolution;uniform vec4 iMouse;uniform highp float iSampleRate;"
-"out highp vec4 fragColor;\n";
-
-const GLchar frg_ftr_src[]=
-"void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
-
-const GLchar * cm_hdr=cm_hdr_src;
-const GLchar * vrt_bdy=vrt_bdy_src;
-const GLchar * frg_hdr=frg_hdr_src;
-const GLchar * frg_ftr=frg_ftr_src;
 
 // static inline void uni(GLfloat,GLfloat,GLfloat,GLint,GLfloat);
 
