4504e4e89716d0e7661206f70f75b50a53f4e412
Noah Cohn <noahc42@gmail.com>
2023-04-28 19:52:55 -0700

    Update audio.cpp

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -1,52 +1,12 @@
 #include "../../include/audio/audio.hpp"
 
-static inline void SDLCALL bfr(void * unused,GLubyte * stm,GLint len){
-GLubyte * wptr;
-GLint lft;
-tie(len,lft);
-tie(stm,wptr);
-wptr=wave.snd+wave.pos;
-lft=wave.slen-wave.pos;
-while(lft<=len){
-SDL_UnlockAudioDevice(dev);
-SDL_memcpy(stm,wptr,lft);
-stm+=lft;
-len-=lft;
-wptr=wave.snd;
-lft=wave.slen;
-wave.pos=0;
-SDL_LockAudioDevice(dev);
-}
-SDL_memcpy(stm,wptr,len);
-wave.pos+=len;
-return;
-}
-
-static inline void plt(){
-tie(wave,bfr);
-tie(wave.snd,dev);
-tie(wave.pos,wave.slen,wave.request);
-SDL_memset(&wave.request,0,sizeof(wave.request));
-wave.request.freq=44100;
-wave.request.format=AUDIO_S32;
-wave.request.channels=2;
-wave.request.samples=1024;
-wave.pos=0;
-SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
-SDL_Init(SDL_INIT_AUDIO);
-SDL_LoadWAV(flnm,&wave.request,&wave.snd,&wave.slen);
-wave.request.callback=bfr;
-dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.request,NULL,0);
-SDL_PauseAudioDevice(dev,SDL_FALSE);
-return;
-}
-
-static inline void(*lp)(){&plt};
+// static inline void(*lp)(){&plt};
+Audio audio;
 
 extern "C" {
 
 void pl(){
-lp();
+audio.plt();
 return;
 }
   
