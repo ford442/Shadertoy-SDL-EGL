7db44efe84191e6d32b4812d6ac635ec34bb4f05
Noah Cohn <noahc42@gmail.com>
2023-05-01 09:15:50 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -537,7 +537,7 @@ smp_chn[3]=glGetUniformLocation(shd_prg,"iChannel3");
 glUniform1f(uni_srate,44100.0f);
 glUniform3f(uni_res,mouse.S,mouse.S,gpu.gF);
 glUniform3f(smp_chn_res,mouse.S,mouse.S,gpu.gF);
-iFps=66.6;
+iFps=60;
 glUniform1f(uni_fps,iFps);
 mouse.mm=mouse.S*0.5;
 mouse.nn=mouse.S*0.5;
