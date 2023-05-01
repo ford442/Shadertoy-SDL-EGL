83a48291f501c0f30d7961e512fdd7d35d3c14ae
Noah Cohn <noahc42@gmail.com>
2023-05-01 05:46:52 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -98,15 +98,13 @@ GLdouble gD=1.0,gD0=0.0,gDm1=-1.0;
 typedef struct{GLfloat XYZW[4];}Vertex;
 Vertex vrt[]={{gpu.gFm1,gpu.gFm1,gpu.gF,gpu.gF},{gpu.gF,gpu.gFm1,gpu.gF,gpu.gF},{gpu.gF,gpu.gF,gpu.gF,gpu.gF},{gpu.gFm1,gpu.gF,gpu.gF,gpu.gF},{gpu.gFm1,gpu.gFm1,gpu.gFm1,gpu.gF},{gpu.gF,gpu.gFm1,gpu.gFm1,gpu.gF},{gpu.gF,gpu.gF,gpu.gFm1,gpu.gF},{gpu.gFm1,gpu.gF,gpu.gF,gpu.gF}};
 
-
-
 using indi_tensor = tensor<GLubyte>;
 indi_tensor In=indi_tensor{1,10};
 
 const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 // const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 
-const GLubyte indc[]={In.at(0,3)),In.at(0,0),In.at(0,1),In.at(0,1),In.at(0,2),In.at(0,3),In.at(0,4),In.at(0,0),In.at(0,3),In.at(0,3),In.at(0,7),In.at(0,4),In.at(0,1),In.at(0,5),In.at(0,6),In.at(0,6),In.at(0,2),In.at(0,1),In.at(0,4),In.at(0,7),In.at(0,6),In.at(0,6),In.at(0,5),In.at(0,4),In.at(0,2),In.at(0,6),In.at(0,6),In.at(0,7),In.at(0,3),In.at(0,0),In.at(0,4),In.at(0,1),In.at(0,1),In.at(0,4),In.at(0,5)};
+const GLubyte indc[]={In.at(0,3),In.at(0,0),In.at(0,1),In.at(0,1),In.at(0,2),In.at(0,3),In.at(0,4),In.at(0,0),In.at(0,3),In.at(0,3),In.at(0,7),In.at(0,4),In.at(0,1),In.at(0,5),In.at(0,6),In.at(0,6),In.at(0,2),In.at(0,1),In.at(0,4),In.at(0,7),In.at(0,6),In.at(0,6),In.at(0,5),In.at(0,4),In.at(0,2),In.at(0,6),In.at(0,6),In.at(0,7),In.at(0,3),In.at(0,0),In.at(0,4),In.at(0,1),In.at(0,1),In.at(0,4),In.at(0,5)};
 
 const GLchar cm_hdr_src[500]=
 "#version 300 es\n"
