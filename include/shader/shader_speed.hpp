5bd1744b69f91e0de669974f616a83740ca8d7a0
Noah Cohn <noahc42@gmail.com>
2023-05-01 05:49:42 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -104,7 +104,6 @@ indi_tensor In=indi_tensor{1,10};
 const GLubyte gu0=0,gu1=1,gu2=2,gu3=3,gu4=4,gu5=5,gu6=6,gu7=7,gu8=8,gu9=9;
 // const GLubyte indc[]={gu3,gu0,gu1,gu1,gu2,gu3,gu4,gu0,gu3,gu3,gu7,gu4,gu1,gu5,gu6,gu6,gu2,gu1,gu4,gu7,gu6,gu6,gu5,gu4,gu2,gu6,gu6,gu7,gu3,gu0,gu4,gu1,gu1,gu4,gu5};
 
-const GLubyte indc[]={In.at(0,3),In.at(0,0),In.at(0,1),In.at(0,1),In.at(0,2),In.at(0,3),In.at(0,4),In.at(0,0),In.at(0,3),In.at(0,3),In.at(0,7),In.at(0,4),In.at(0,1),In.at(0,5),In.at(0,6),In.at(0,6),In.at(0,2),In.at(0,1),In.at(0,4),In.at(0,7),In.at(0,6),In.at(0,6),In.at(0,5),In.at(0,4),In.at(0,2),In.at(0,6),In.at(0,6),In.at(0,7),In.at(0,3),In.at(0,0),In.at(0,4),In.at(0,1),In.at(0,1),In.at(0,4),In.at(0,5)};
 
 const GLchar cm_hdr_src[500]=
 "#version 300 es\n"
@@ -377,6 +376,7 @@ In.at(0,7)=7;
 In.at(0,8)=8;
 In.at(0,9)=9;
 In.at(0,10)=10;
+const GLubyte indc[]={In.at(0,3),In.at(0,0),In.at(0,1),In.at(0,1),In.at(0,2),In.at(0,3),In.at(0,4),In.at(0,0),In.at(0,3),In.at(0,3),In.at(0,7),In.at(0,4),In.at(0,1),In.at(0,5),In.at(0,6),In.at(0,6),In.at(0,2),In.at(0,1),In.at(0,4),In.at(0,7),In.at(0,6),In.at(0,6),In.at(0,5),In.at(0,4),In.at(0,2),In.at(0,6),In.at(0,6),In.at(0,7),In.at(0,3),In.at(0,0),In.at(0,4),In.at(0,1),In.at(0,1),In.at(0,4),In.at(0,5)};
 Compile compile;
 tie(cpu.F,cpu.Fm1,cpu.F0);
 tie(gpu.gF,gpu.gFm1,gpu.gF0);
