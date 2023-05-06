bb6d5a94373d3b38ac5ea135790ec4af5ab188d3
Noah Cohn <noahc42@gmail.com>
2023-05-06 09:42:42 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -182,7 +182,7 @@ inline EM_BOOL ms_l,clk_l;
 
 using mouse_tensor=tensor<boost::atomic<float>>;
 using shad_tensor=tensor<unsigned int>;
-using prg_tensor=tensor<long unsigned int>;
+using prg_tensor=tensor<unsigned int>;
 using f_tensor=tensor<boost::atomic<float>>;
 using d_tensor=tensor<boost::atomic<long double>>;
 using v_tensor=tensor<v128_t>;
