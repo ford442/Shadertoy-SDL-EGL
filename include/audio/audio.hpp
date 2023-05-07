de82deed3f25aeff2391eb6b02d8da47dec5da28
Noah Cohn <noahc42@gmail.com>
2023-05-07 04:14:58 -0700

    Update audio.hpp

--- include/audio/audio.hpp
+++ include/audio/audio.hpp
@@ -93,7 +93,7 @@ SDL_AudioSpec request;
 
 public:
 
-static inline void snd_pos(mpz_int set){
+static inline void snd_pos(boost::atomic<int> set){
 sse.at(0,0)=wasm_i64x2_splat(set);
 sound_pos.at(0,0)=wasm_i64x2_extract_lane(sse.at(0,0),0);
 // sound_pos.at(0,0)=set;
