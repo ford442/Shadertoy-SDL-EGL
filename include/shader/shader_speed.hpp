893d1c44024bb95f9f232dd3cbfadaebe54bac67
Noah Cohn <noahc42@gmail.com>
2023-05-07 09:45:42 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -31,8 +31,8 @@
 #define BOOST_ERROR_CODE_HEADER_ONLY 1
 #define BOOST_UBLAS_MOVE_SEMANTICS
 #define BOOST_UBLAS_TYPE_CHECK 0
-BOOST_UBLAS_USE_LONG_DOUBLE
-BOOST_NO_EXCEPTIONS 
+#define BOOST_UBLAS_USE_LONG_DOUBLE
+#define BOOST_NO_EXCEPTIONS 
 
 #include <boost/cstdint.hpp>
 #include <boost/integer.hpp>
