f3b85ec88eb881f1ab2b0cc4e12fe39a22f2afec
Noah Cohn <noahc42@gmail.com>
2023-05-07 05:02:28 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -19,6 +19,7 @@
 
 #include <boost/lockfree/spsc_queue.hpp>
 #include <boost/atomic.hpp>
+#include <boost/context.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
 using namespace std::chrono_literals;
