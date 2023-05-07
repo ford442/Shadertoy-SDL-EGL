f320cc96322a4ea880597393c6e1377d76ad9a05
Noah Cohn <noahc42@gmail.com>
2023-05-07 01:22:40 -0700

    Update shader_speed.hpp

--- include/shader/shader_speed.hpp
+++ include/shader/shader_speed.hpp
@@ -5,12 +5,13 @@
 #define TRUE 1
 #endif
 
-#pragma STDC FP_CONTRACT OFF
+#pragma STDC FP_CONTRACT ON
 
 // #undef _FLT_EVAL_METHOD
 // #define _FLT_EVAL_METHOD 0
 
 #pragma STDC CX_LIMITED_RANGE ON
+
 #define _XOPEN_REALTIME 1
 #define _POSIX_ASYNC_IO 1
 #define _POSIX_PRIO_IO 1
