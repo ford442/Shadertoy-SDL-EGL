c6738bcdc83b4a4670b4a73a5429e11141a2d210
Noah Cohn <noahc42@gmail.com>
2023-05-08 04:03:05 -0700

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -6,15 +6,15 @@
 #define EGL_EGLEXT_PROTOTYPES 1
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
+// #include <EGL/eglplatform.h>
 
-#include <KHR/khrplatform.h>
+// #include <KHR/khrplatform.h>
 
 #define GL_GLES_PROTOTYPES 1
 #include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3platform.h>
+// #include <GLES3/gl31.h>
+// #include <GLES3/gl32.h>
+// #include <GLES3/gl3platform.h>
 
 // #include <webgl/webgl2.h>  //  much better performance without
 
