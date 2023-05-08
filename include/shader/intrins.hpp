5a9d366fd145f552894e15f2f7dbd159464dd382
Noah Cohn <noahc42@gmail.com>
2023-05-08 00:14:49 -0700

    Update intrins.hpp

--- include/shader/intrins.hpp
+++ include/shader/intrins.hpp
@@ -3,7 +3,6 @@
 #include <wasm_simd128.h>
 #include <tmmintrin.h> // ssse 3
 #include <smmintrin.h>  // sse 4.1
-#include <avxintrin.h>  // AVX
 
 #define VLEAVE _mm256_zeroupper
 
@@ -11,3 +10,4 @@
 #include <immintrin.h> 
 #include <nmmintrin.h>  //  sse 4.2
 #include <unistd.h>
+#include <avxintrin.h>  // AVX
