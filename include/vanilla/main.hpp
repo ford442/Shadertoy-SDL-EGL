4aca91c71842cd11c13f4baad94ab00529dab988
Noah Cohn <noahc42@gmail.com>
2023-05-07 21:15:40 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -239,7 +239,7 @@ return intrn.at(0,3);
 float noblock(float y){
 farray=y;
 return farray;
-    std::cout << at::ones({3,4}, at::CPU(at::kFloat)) << "\n";
+    std::cout << ::at::ones({3,4}, at::CPU(at::kFloat)) << "\n";
  // emscripten_set_main_loop((void(*)())mss,0,0);
 
 }
