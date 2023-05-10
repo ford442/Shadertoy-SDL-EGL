89416601a171bd54d5c3bf9e0a182df153f705eb
Noah Cohn <noahc42@gmail.com>
2023-05-09 21:43:51 -0700

    Update intrins.hpp

--- include/shader/intrins.hpp
+++ include/shader/intrins.hpp
@@ -5,11 +5,12 @@
 // #include <tmmintrin.h> // ssse 3
 // #include <smmintrin.h>  // sse 4.1
 // #include <mmintrin.h>  //  experimental
+#include <xmmintrin.h>
+
 #include <mm3dnow.h>  //  experimental
 
 #define VLEAVE _mm256_zeroupper
 
-// #include <xmmintrin.h>
 // #include <immintrin.h> 
 // #include <nmmintrin.h>  //  sse 4.2
 // #include <unistd.h>
