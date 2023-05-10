dc3ddb4a232a598d086283a78342c4e00e2ec06e
Noah Cohn <noahc42@gmail.com>
2023-05-09 21:43:22 -0700

    Update intrins.hpp

--- include/shader/intrins.hpp
+++ include/shader/intrins.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <wasm_simd128.h>
+// #include <wasm_simd128.h>
 
-#include <tmmintrin.h> // ssse 3
-#include <smmintrin.h>  // sse 4.1
-#include <mmintrin.h>  //  experimental
+// #include <tmmintrin.h> // ssse 3
+// #include <smmintrin.h>  // sse 4.1
+// #include <mmintrin.h>  //  experimental
+#include <mm3dnow.h>  //  experimental
 
 #define VLEAVE _mm256_zeroupper
 
 // #include <xmmintrin.h>
-#include <immintrin.h> 
-#include <nmmintrin.h>  //  sse 4.2
-#include <unistd.h>
-#include <avxintrin.h>  // AVX
+// #include <immintrin.h> 
+// #include <nmmintrin.h>  //  sse 4.2
+// #include <unistd.h>
+// #include <avxintrin.h>  // AVX
