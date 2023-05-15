652f712614b8c62a8e037d3b62175e859ed27217
Noah Cohn <noahc42@gmail.com>
2023-05-15 05:36:35 -0700

    Update shader_speed.cpp

--- src/shader/shader_speed.cpp
+++ src/shader/shader_speed.cpp
@@ -8,9 +8,29 @@ void str(){
 run.strt();
 return;
 }
-  
+
 void swp(){
 run.swap();
+return;
+}
+
+void ud(){
+run.uniDOWN();
+return;
+}
+
+void uu(){
+run.uniUP();
+return;
+}
+
+void vd(){
+run.viewDOWN();
+return;
+}
+
+void vu(){
+run.viewUP();
 return;
 }
 
