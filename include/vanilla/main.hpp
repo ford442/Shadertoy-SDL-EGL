70702a09fba86612c433137e244b5a057a219b9d
Noah Cohn <noahc42@gmail.com>
2023-05-16 14:34:29 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -6,7 +6,7 @@
 // #include <torch/script.h>
 // #include <torch/csrc/api/include/torch/all.h>
 // #include <ATen/ATen.h>
-#include <gmp.h>
+#include "/usr/include/x86_64-linux-gnu/gmp.h"
 
 #include <experimental/simd>
 using std::experimental::native_simd;
