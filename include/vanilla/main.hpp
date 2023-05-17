121051b168bbaafad58255156c59146875e44767
Noah Cohn <noahc42@gmail.com>
2023-05-17 13:44:21 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -159,7 +159,7 @@ private:
 boost::atomic<float>farray;
 static int r,m;
 static float cc,pp,uu,cc2,pp2,uu2,Tdlt,nn;
-v128_t aa,vv,xx,l,tt;
+static v128_t aa,vv,xx,l,tt;
 tI inte=tI{1,1};
 tF deci=tF{1,1};
 tV q=tV{1,1};
