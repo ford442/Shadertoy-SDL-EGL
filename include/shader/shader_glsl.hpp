06959aae3cb3ad9dfd625ff5bfe1fefde2017319
Noah Cohn <noahc42@gmail.com>
2023-05-18 15:08:25 -0700

    Update shader_glsl.hpp

--- include/shader/shader_glsl.hpp
+++ include/shader/shader_glsl.hpp
@@ -575,7 +575,8 @@ std::regex rgx3(repp3);
 std::string outt=std::regex_replace(finpp,rgx,frepp);
 outt=std::regex_replace(outt,rgx2,frepp2);
 std::string outt2=std::regex_replace(outt,rgx3,frepp3);
-char *cstr=(char *)outt2.c_str();
+// char *cstr=(char *)outt2.c_str();
+char *cstr=outt2.c_str();
   // ----------------------------
   
 return cstr;
