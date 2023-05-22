df33441f9e3f6c3108f33cb985af51997cc3bd47
Noah Cohn <noahc42@gmail.com>
2023-05-22 03:15:25 -0700

    Update shader.hpp

--- include/combine/shader.hpp
+++ include/combine/shader.hpp
@@ -571,11 +571,11 @@ return nullptr;
 
 }procc;
 
-void fram(){
+static void fram(){
 procc.Rend();
 }
     
-void strt(){
+static void strt(){
 typedef struct{boost::atomic<float> XYZW[4];}Vertex;
 gpu.setFloats();
 const Vertex vrt[8]={{gpu.gFm1(),gpu.gFm1(),gpu.gF(),gpu.gF()},{gpu.gF(),gpu.gFm1(),gpu.gF(),gpu.gF()},{gpu.gF(),gpu.gF(),gpu.gF(),gpu.gF()},{gpu.gFm1(),gpu.gF(),gpu.gF(),gpu.gF()},{gpu.gFm1(),gpu.gFm1(),gpu.gFm1(),gpu.gF()},{gpu.gF(),gpu.gFm1(),gpu.gFm1(),gpu.gF()},{gpu.gF(),gpu.gF(),gpu.gFm1(),gpu.gF()},{gpu.gFm1(),gpu.gF(),gpu.gF(),gpu.gF()}};
