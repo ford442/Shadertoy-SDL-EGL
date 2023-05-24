247286289359f057871253f205f1fb79fc199adf
Noah Cohn <noahc42@gmail.com>
2023-05-24 13:58:17 -0700

    Update shader_speed.h

--- include/shader/shader_speed.h
+++ include/shader/shader_speed.h
@@ -36,7 +36,12 @@
 #define BOOST_UBLAS_MOVE_SEMANTICS
 #define BOOST_UBLAS_TYPE_CHECK 0
 #define BOOST_UBLAS_USE_LONG_DOUBLE
-#define BOOST_NO_EXCEPTIONS 
+// #define BOOST_NO_EXCEPTIONS
+// #define BOOST_HAS_FLOAT128
+#define BOOST_HAS_CLOCK_GETTIME 1
+#define BOOST_HAS_NANOSLEEP 1
+#define BOOST_HAS_NRVO 1
+#define BOOST_HAS_STDINT_H 1
 
 #include <boost/integer.hpp>
 #include <boost/atomic.hpp>
