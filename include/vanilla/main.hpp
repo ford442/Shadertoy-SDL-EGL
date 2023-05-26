8793fb23c58fc523fc6c62c677198977b58c1dab
Noah Cohn <noahc42@gmail.com>
2023-05-26 03:04:26 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -121,14 +121,13 @@ userData.requestEnded=true;
 };
 wgpuInstanceRequestAdapter(instance,options,onAdapterRequestEnded,(void*)&userData);
 return userData.adapter;
-
 }
  
 float rtt(float nm){
  WGPUAdapter adapter=requestAdapter(instance,&options);
 std::cout << "Requesting device..." << std::endl;
 const WGPUDeviceDescriptor desc={};
-WGPUDevice Gdevice=requestDevice(adapter,&desc);
+WGPUDevice Gdevice=requestDevice(adapter,desc);
 std::cout << "Got device: " << Gdevice << std::endl;
 A.at(0,0)=nm;
 tensorVar B=A;
