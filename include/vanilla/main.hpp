dbf727c35b5321c133140ce8f10883ae8d264e00
Noah Cohn <noahc42@gmail.com>
2023-05-25 23:32:27 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -67,22 +67,11 @@ using tV=tensor<v128_t>;
 #include "../../include/vanilla/webgpu/emscripten/webgpu.hpp"
 
 WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const * options) {
-    // A simple structure holding the local information shared with the
-    // onAdapterRequestEnded callback.
     struct UserData {
         WGPUAdapter adapter = nullptr;
         bool requestEnded = false;
     };
     UserData userData;
-
-    // Callback called by wgpuInstanceRequestAdapter when the request returns
-    // This is a C++ lambda function, but could be any function defined in the
-    // global scope. It must be non-capturing (the brackets [] are empty) so
-    // that it behaves like a regular C function pointer, which is what
-    // wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
-    // is to convey what we want to capture through the pUserData pointer,
-    // provided as the last argument of wgpuInstanceRequestAdapter and received
-    // by the callback as its last argument.
     auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const * message, void * pUserData) {
         UserData& userData = *reinterpret_cast<UserData*>(pUserData);
         if (status == WGPURequestAdapterStatus_Success) {
@@ -92,21 +81,13 @@ WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions cons
         }
         userData.requestEnded = true;
     };
-
-    // Call to the WebGPU request adapter procedure
     wgpuInstanceRequestAdapter(
         instance /* equivalent of navigator.gpu */,
         options,
         onAdapterRequestEnded,
         (void*)&userData
     );
-
-    // In theory we should wait until onAdapterReady has been called, which
-    // could take some time (what the 'await' keyword does in the JavaScript
-    // code). In practice, we know that when the wgpuInstanceRequestAdapter()
-    // function returns its callback has been called.
     assert(userData.requestEnded);
-
     return userData.adapter;
 }
 
@@ -115,11 +96,8 @@ WGPUQueue queue;
 WGPUSwapChain swapchain;
 
 wgpu::ComputePassDescriptor computePassDesc;
-
 WGPUBindGroup bindGroup;
 wgpu::CommandEncoderDescriptor encoderDesc = wgpu::Default;
-
-
 
 class tens{
 
