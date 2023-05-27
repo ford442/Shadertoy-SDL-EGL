8839808c17e6cf942a7709cb8e614ea97f52c46b
Noah Cohn <noahc42@gmail.com>
2023-05-27 13:41:45 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -141,10 +141,10 @@ std::cout << "Got device: " << Gdevice << std::endl;
 }
 
 void init3(){
-BindGroupLayoutEntry bindings(2,Default);
+WGPUBindGroupLayoutEntry bindings(2,Default);
 bindings[0].binding=0;
-bindings[0].buffer.type=BufferBindingType::ReadOnlyStorage;
-bindings[0].visibility=ShaderStage::Compute;
+bindings[0].buffer.type=WGPUBufferBindingType::ReadOnlyStorage;
+bindings[0].visibility=WGPUShaderStage::Compute;
 bindings[1].binding=1;
 bindings[1].buffer.type=wgpu::BufferBindingType::Storage;
 bindings[1].visibility=ShaderStage::Compute;
