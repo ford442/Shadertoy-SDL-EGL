e2c64bedd19e4d3645ada8fd0576e9544505b0da
Noah Cohn <noahc42@gmail.com>
2023-05-31 13:37:22 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -228,17 +228,6 @@ return sz.at(1,0);
 }
 auto intrinsAdd=IntrinsAdd();
 
-tld szz=tld{2,2};
-std::function<v128_t(v128_t,v128_t)>IntrinsLDAdd(){
-return[](v128_t a,v128_t b){
-szz.at(0,0)=a;
-szz.at(0,1)=b;
-szz.at(1,0)=wasm_f64x2_add(a,b);
-return szz.at(1,0);
-};
-}
-auto intrinsLDAdd=IntrinsLDAdd();
-
 class tens{
 
 private:
@@ -277,17 +266,18 @@ std::cout << "-----------double---------" << std::endl;
 std::cout << fixed << setprecision(64) << "-----"<< sy.at(1,1) <<"-----" << std::endl;
 std::cout << "--------------------------" << std::endl;
 sz.at(1,1)=intrinsAdd(wasm_f64x2_splat(3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679f),wasm_f64x2_splat(3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679f));
-sey.at(1,1)=intrinsLDAdd(3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679L,3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679L);
+sey.at(1,1)=longDoubleAdd(3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679L,3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679L);
 std::cout << "------intrins-------------" << std::endl;
 float ou2=wasm_f64x2_extract_lane(sz.at(1,1),0);
 double ou3=wasm_f64x2_extract_lane(sz.at(1,1),0);
-long double ou5=wasm_f64x2_extract_lane(sey.at(1,1),0);
+long double ou5=wasm_f64x2_extract_lane(sz.at(1,1),0);
 std::cout << fixed << setprecision(64) << "--float---"<< ou2 <<"-----" << std::endl;
 std::cout << fixed << setprecision(64) << "--double---"<< ou3 <<"-----" << std::endl;
 std::cout << fixed << setprecision(64) << "--long double---"<< ou5 <<"-----" << std::endl;
 std::cout << "--------------------------" << std::endl;
 std::cout << "--------------------------" << std::endl;
-
+std::cout << "---long double add--------------" << std::endl;
+std::cout << fixed << setprecision(64) << "-----"<< sey.at(1,1) <<"-----" << std::endl;
 A.at(0,0)=nm;
 tensorVar B=A;
 lol=static_cast<float>(B.at(4,4));
