d4131caefa857c170caac98d25e82bccc1f3547d
Noah Cohn <noahc42@gmail.com>
2023-06-02 03:50:01 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -184,7 +184,7 @@ userData.device=device;
 }
 userData.requestEnded=true;
 };
-wgpuAdapterRequestDevice(adapter,adapterOptions,onDeviceRequestEnded,&userData);
+wgpuAdapterRequestDevice(adapter,deviceDescriptor,onDeviceRequestEnded,&userData);
 while(!userData.requestEnded){
 emscripten_log(EM_LOG_CONSOLE,"Waiting for device...\n");
 sleep(1);
@@ -207,7 +207,7 @@ std::cout << "Got adapter: " << adapter << std::endl;
 }
 userData.requestEnded=true;
 };
-wgpuInstanceRequestAdapter(instance,deviceDescriptor,onAdapterRequestEnded,&userData);
+wgpuInstanceRequestAdapter(instance,adapterOptions,onAdapterRequestEnded,&userData);
 return userData.adapter;
 }
 
