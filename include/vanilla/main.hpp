bee2990be59158b4a98272218339a79fcb04997a
Noah Cohn <noahc42@gmail.com>
2023-06-01 22:40:46 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -206,10 +206,10 @@ std::cout << "Got adapter: " << adapter << std::endl;
 userData.requestEnded=true;
 };
 wgpuInstanceRequestAdapter(instance,options,onAdapterRequestEnded,&userData);
-    while (!userData.requestEnded) {
-      emscripten_log(EM_LOG_CONSOLE, "Waiting for adapter...\n");
-      sleep(1);
-    }
+  //  while (!userData.requestEnded) {
+ //     emscripten_log(EM_LOG_CONSOLE, "Waiting for adapter...\n");
+  //    sleep(1);
+ //   }
 return userData.adapter;
 }
 
