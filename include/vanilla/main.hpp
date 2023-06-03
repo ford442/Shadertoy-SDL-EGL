751a71b1b6db26c9caf6d37082095c6f9449c084
Noah Cohn <noahc42@gmail.com>
2023-06-03 08:35:34 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -257,6 +257,7 @@ void init3(){
 std::cout << "get shader module" << std::endl;
 WGPUShaderModuleWGSLDescriptor shaderModuleWGSLDescriptor;
 shaderModuleWGSLDescriptor.source = "";
+shaderModuleWGSLDescriptor.chain.stype =WGPUSType_ShaderModuleWGSLDescriptor;
 WGPUShaderModuleDescriptor shaderModuleDescriptor;
 shaderModuleDescriptor.nextInChain=&shaderModuleWGSLDescriptor.chain;
 WGPUShaderModule shaderModule=wgpuDeviceCreateShaderModule(contextProperties.device,&shaderModuleDescriptor);
