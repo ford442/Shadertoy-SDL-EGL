eaf38d93a901d09242efca28c9583432eb7f7d58
Noah Cohn <noahc42@gmail.com>
2023-06-04 07:11:14 -0700

    Update main.hpp

--- include/vanilla/main.hpp
+++ include/vanilla/main.hpp
@@ -321,7 +321,7 @@ return[](float a,float b){sx.at(0,0)=a;sx.at(0,1)=b;sx.at(1,0)=sx.at(0,0)+sx.at(
 auto tensorAdd=TensorAdd();
 
 td sy=td{2,2};
-sy& operator=0.0;
+sy&=0.0;
 
 std::function<double(double,double)>DoubleAdd(){
 return[](double a,double b){
