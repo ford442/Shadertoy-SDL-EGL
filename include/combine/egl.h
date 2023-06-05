0c4e6fc8628e6687d7c839d9f99309b2523a425e
Noah Cohn <noahc42@gmail.com>
2023-06-05 00:07:57 -0700

    Update egl.h

--- include/combine/egl.h
+++ include/combine/egl.h
@@ -7,8 +7,8 @@
 #endif
 
 #include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
+// #include <GLES3/gl31.h>
+// #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
 
 #ifndef EGL_EGL_PROTOTYPES 
