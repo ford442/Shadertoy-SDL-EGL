a83271abc1ae357d7a7d07944b6c4988da304932
Noah Cohn <noahc42@gmail.com>
2023-06-09 04:14:57 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -1,20 +1,5 @@
-#include <boost/cstdfloat.hpp>  // must be first include
-#include <cfloat>
-#include <climits>
-#include <math.h>
-#include <emscripten.h>
-#include <emscripten/html5.h>
-#include <unistd.h>
-
 #define _XOPEN_SOURCE 700
 
-#pragma pack(4)
-// #pragma float_control(precise, on)  // enable precise semantics
-#pragma fenv_access(on)             // enable environment sensitivity
-// #pragma float_control(except, on)   // enable exception semantics
-
-#pragma STDC FP_CONTRACT ON
-#pragma STDC CX_LIMITED_RANGE ON
 #define _XOPEN_REALTIME 1
 #define _POSIX_ASYNC_IO 1
 #define _POSIX_PRIO_IO 1
@@ -22,65 +7,30 @@
 #define _XOPEN_SHM 1
 #define _POSIX_PRIORITIZED_IO 1
 #define _POSIX_REGEXP 1
+
 #undef _FLT_ROUNDS
 #define _FLT_ROUNDS 1
 
-#define BOOST_CHRONO_HEADER_ONLY 1
-#define BOOST_ERROR_CODE_HEADER_ONLY 1
-#define BOOST_UBLAS_MOVE_SEMANTICS
-#define BOOST_UBLAS_TYPE_CHECK 0
-#define BOOST_UBLAS_USE_LONG_DOUBLE
-#define BOOST_NO_EXCEPTIONS
+#pragma pack(4)
+#pragma fenv_access(on)
+#pragma STDC FP_CONTRACT ON
+#pragma STDC CX_LIMITED_RANGE ON
 
-#include "boost/tuple/tuple.hpp"
-#include "boost/timer/timer.hpp"
-#include "../../include/vanilla/gl.hpp"
-#include "../../include/vanilla/avx.hpp"
-#include <boost/numeric/ublas/tensor.hpp>
-// #include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/vector.hpp>
-#include <time.h>
+#include <boost/cstdfloat.hpp>  // must be first include
+
 #include <chrono>
-
-#include <boost/lockfree/spsc_queue.hpp>
-#include <boost/atomic.hpp>
-// #include <boost/context/fiber.hpp>
-// #include <boost/multiprecision/cpp_int.hpp>
-#include <boost/random.hpp>
-#include <boost/random/random_device.hpp>
-using namespace std::chrono_literals;
-using namespace ::boost::tuples;
-
-std::chrono::steady_clock::time_point t1;
-std::chrono::steady_clock::time_point t2;
-
 #include <iostream>
-
-// using namespace std;
-using namespace boost::numeric::ublas;
-using namespace boost::random;
-
-#include <experimental/simd>
-using std::experimental::native_simd;
-using float_v=std::experimental::simd<float,std::experimental::simd_abi::scalar>;
-
-using tensorVar=tensor<GLfloat>;
-using tF=tensor<GLfloat>;
-using tf=tensor<float>;
-using td=tensor<double>;
-using tld=tensor<long double>;
-using tensorVarD=tensor<GLdouble>;
-using tD=tensor<GLdouble>;
-using tI=tensor<GLint>;
-using tV=tensor<v128_t>;
-
 #include <cassert>
 #include <iomanip>
 #include <ctime>
-
 #include <stdio.h>
-
-using namespace std;
+#include <cfloat>
+#include <climits>
+#include <math.h>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <unistd.h>
+#include <functional>
 
 #include "../../lib/lib_webgpu.h"
 
@@ -121,16 +71,7 @@ float bufferSize=64*sizeof(float);
 const char *computeShader =
 "@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;"
 "@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;"
-// The function to evaluate for each element of the processed buffer
-"fn f(x: f32) -> f32 {"
-"return 2.0 * x + 0.42;"
-"}"
 "@compute @workgroup_size(32)"
 "fn computeStuff(@builtin(global_invocation_id) global_id: vec3<u32>,@builtin(local_invocation_id) local_id: vec3<u32>) {"
-    // Apply the function f to the buffer element at index id.x:
-// "outputBuffer[global_id.x] = f(inputBuffer[global_id.x]);"
 "outputBuffer[0] = inputBuffer[0];"
-// "mapBuffer[0] = outputBuffer[0];"
 "}";
-
-#include <functional>
