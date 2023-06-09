f5bd157b4ebca51de886197eba74fe222a96bf31
Noah Cohn <noahc42@gmail.com>
2023-06-09 07:19:48 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -50,7 +50,7 @@ std::cout << output << std::endl;
 wgpu_buffer_read_mapped_range(mapBuffer,output,0,&resulT,IbufferSize);
 std::cout << &resulT << std::endl;
       int * answeR;
-std::cout << boost::algorithm::unhex(resulT,answeR) << std::endl;
+std::cout << boost::algorithm::unhex(resulT,&answeR) << std::endl;
       //     std::array<double,1>outP3(std::to_array<double,1>({resulT}));           
 
            
