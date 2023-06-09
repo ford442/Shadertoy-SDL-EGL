d7753d14505c682ea3c7d55eee568087913b28c2
Noah Cohn <noahc42@gmail.com>
2023-06-09 05:58:00 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -42,8 +42,10 @@ commandBuffer=wgpu_encoder_finish(encoder);
 WGpuOnSubmittedWorkDoneCallback onComputeDone=[](WGpuQueue queue,void *userData){
 WGpuBufferMapCallback mapCallback=[](WGpuBuffer buffer,void *userData,WGPU_MAP_MODE_FLAGS mode,double_int53_t offset,double_int53_t size){
 double output=wgpu_buffer_get_mapped_range(mapBuffer,uint32_t(0),bufferSize);
+         
            
-std::array<double>outP=std::to_array<double>({&output});           
+           
+std::array<double>outP(std::to_array<double>({&output}));           
 std::cout << std&output << std::endl;
 wgpu_buffer_read_mapped_range(mapBuffer,output,0,&resulT,bufferSize);
 std::cout << &resulT << std::endl;
