adb574110e165ac0a8228be0fd4cf1df74d459ca
Noah Cohn <noahc42@gmail.com>
2023-06-09 08:08:40 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -66,7 +66,5 @@ const char * computeShader=
 "@compute@workgroup_size(1)"
 "fn computeStuff(@builtin(global_invocation_id)global_id:vec3<u32>,@builtin(local_invocation_id)local_id:vec3<u32>){"
 // "var ff: f32=inputBuffer[0];"
-"outputBuffer[0]=42;"
-"outputBuffer[2]=4200;"
-"outputBuffer[4]=42000;"
+"outputBuffer[0]=inputBuffer[0];"
 "}";
