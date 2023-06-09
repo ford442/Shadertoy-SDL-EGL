647a06f2b71374526cb239f4c8f9bbd5bc9843d3
Noah Cohn <noahc42@gmail.com>
2023-06-09 04:55:31 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -1,23 +1,5 @@
-#define _XOPEN_SOURCE 700
-
-#define _XOPEN_REALTIME 1
-#define _POSIX_ASYNC_IO 1
-#define _POSIX_PRIO_IO 1
-#define _POSIX_SYNC_IO 1
-#define _XOPEN_SHM 1
-#define _POSIX_PRIORITIZED_IO 1
-#define _POSIX_REGEXP 1
-
-#undef _FLT_ROUNDS
-#define _FLT_ROUNDS 1
-
-#pragma pack(4)
-#pragma fenv_access(on)
-#pragma STDC FP_CONTRACT ON
-#pragma STDC CX_LIMITED_RANGE ON
-
 #include <boost/cstdfloat.hpp>  // must be first include
-
+#include "../../include/vanilla/defs.h"
 #include <chrono>
 #include <iostream>
 #include <cassert>
@@ -43,43 +25,44 @@ uint32_t workgroupCount=(invocationCount+workgroupSize-1)/workgroupSize;
 std::vector<double>outputd(bufferSize/sizeof(double));
 void * resulT;
 WGPU_MAP_MODE_FLAGS mode1=0x1; // READ MODE
+void * userDataA;
 
 WGpuAdapter adapter=0;
 WGpuDevice device=0;
+WGpuQueue queue=0;
 WGpuBindGroupLayout bindGroupLayout=0;
 WGpuComputePipeline computePipeline=0;
-WGpuQueue queue=0;
-WGpuComputePassDescriptor computePassDescriptor={};
-WGpuBufferDescriptor bufferDescriptorI={bufferSize,WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_DST,false};
-WGpuBufferDescriptor bufferDescriptorO={bufferSize,WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_SRC,false};
-WGpuBufferDescriptor bufferDescriptorM={bufferSize,WGPU_BUFFER_USAGE_MAP_READ|WGPU_BUFFER_USAGE_COPY_DST,false};
-WGpuBufferBindingLayout bufferBindingLayout1={3};
-WGpuBufferBindingLayout bufferBindingLayout2={2};
-WGpuBufferBindingLayout bufferBindingLayout3={2};
 WGpuBuffer inputBuffer=0;
-WGpuShaderModuleDescriptor shaderModuleDescriptor={};
 WGpuBuffer outputBuffer=0;
 WGpuBuffer mapBuffer=0;
 WGpuBuffer uniBuffer=0;
 WGpuShaderModule cs=0;
-WGpuCommandBufferDescriptor commandBufferDescriptor={};
 WGpuCommandBuffer commandBuffer=0;
 WGpuCommandEncoder encoder=0;
 WGpuComputePassEncoder computePass=0;
-WGpuBindGroupLayoutEntry bindGroupLayoutEntries[2]={};
-WGpuBindGroupEntry bindGroupEntry[2]={};
 WGpuBindGroup bindGroup=0;
 WGpuPipelineLayout pipelineLayout=0;
+WGpuQuerySet querySet=0;
+
+WGpuComputePassDescriptor computePassDescriptor={};
+WGpuShaderModuleDescriptor shaderModuleDescriptor={};
+WGpuCommandBufferDescriptor commandBufferDescriptor={};
 WGpuCommandEncoderDescriptor commandEncoderDescriptor={};
 WGpuDeviceDescriptor deviceDescriptor={};
-WGpuQuerySet querySet=0;
-void *userDataA;
+WGpuBindGroupLayoutEntry bindGroupLayoutEntries[2]={};
+WGpuBindGroupEntry bindGroupEntry[2]={};
+WGpuBufferBindingLayout bufferBindingLayout1={3};
+WGpuBufferBindingLayout bufferBindingLayout2={2};
+WGpuBufferBindingLayout bufferBindingLayout3={2};
+WGpuBufferDescriptor bufferDescriptorI={bufferSize,WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_DST,false};
+WGpuBufferDescriptor bufferDescriptorO={bufferSize,WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_SRC,false};
+WGpuBufferDescriptor bufferDescriptorM={bufferSize,WGPU_BUFFER_USAGE_MAP_READ|WGPU_BUFFER_USAGE_COPY_DST,false};
 WGpuRequestAdapterOptions options={WGPU_POWER_PREFERENCE_HIGH_PERFORMANCE,false};
 
-const char *computeShader =
+const char * computeShader=
 "@group(0) @binding(0) var<storage,read> inputBuffer: array<f32,64>;"
 "@group(0) @binding(1) var<storage,read_write> outputBuffer: array<f32,64>;"
-"@compute @workgroup_size(32)"
+"@compute @workgroup_size(1)"
 "fn computeStuff(@builtin(global_invocation_id) global_id: vec3<u32>,@builtin(local_invocation_id) local_id: vec3<u32>) {"
-"outputBuffer[0] = inputBuffer[0];"
+"outputBuffer[0] = 5.0;"
 "}";
