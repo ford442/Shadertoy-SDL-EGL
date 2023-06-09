cd5bd262aec55d298f7a46bf8ff712f78d80df0d
Noah Cohn <noahc42@gmail.com>
2023-06-09 13:30:59 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -92,17 +92,17 @@ glGenerateMipmap(GL_TEXTURE_2D);
            // create buffer
 GLuint pbo;
 glGenBuffers(1, &pbo);
-glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
-glBufferData(GL_PIXEL_PACK_BUFFER, width*height*sizeof(unsigned char), 0, GL_STATIC_READ);
+glBindBuffer(GL_TEXTURE_BUFFER, pbo);
+glBufferData(GL_TEXTURE_BUFFER, width*height*sizeof(unsigned char), 0, GL_STATIC_READ);
 // get texture image
 glBindTexture(GL_TEXTURE_2D, texture);
 glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)(0));
 // map pixel buffer
-void * data_ptr = glMapBufferRange (GL_PIXEL_PACK_BUFFER,0,4, GL_MAP_READ_BIT );
+void * data_ptr = glMapBufferRange(GL_TEXTURE_BUFFER,0,4, GL_MAP_READ_BIT );
 std::cout << "tex data -" << std::endl;
 std::cout << &data_ptr << std::endl;
 std::cout << data_ptr << std::endl;
-glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+glUnmapBuffer(GL_TEXTURE_BUFFER);
            
 };
 wgpu_buffer_map_async(mapBuffer,mapCallback,&userDataA,mode1,uint32_t(0),IbufferSize);
