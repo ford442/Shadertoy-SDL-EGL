cbc67c101f15bb907914a173537697979581d81b
Noah Cohn <noahc42@gmail.com>
2023-06-10 14:57:55 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -314,11 +314,11 @@ bufferDescriptorM.mappedAtCreation=false;
 for(int i=0;i<input.size();++i){
 input[i]=i;
 }
-std::cout << "at create buff1 in" << std::endl;
+// std::cout << "at create buff1 in" << std::endl;
 inputBuffer=wgpu_device_create_buffer(device,&bufferDescriptorI);
-std::cout << "at create buff2 out" << std::endl;
+// std::cout << "at create buff2 out" << std::endl;
 outputBuffer=wgpu_device_create_buffer(device,&bufferDescriptorO);
-std::cout << "at create buff3 map" << std::endl;
+// std::cout << "at create buff3 map" << std::endl;
 mapBuffer=wgpu_device_create_buffer(device,&bufferDescriptorM);
 cs=wgpu_device_create_shader_module(device,&shaderModuleDescriptor);
 bindGroupLayoutEntries[0].binding=0;
