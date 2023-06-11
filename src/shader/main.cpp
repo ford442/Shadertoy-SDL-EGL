e6452ab0c4d6c98962e46ec272857bf041f61694
Noah Cohn <noahc42@gmail.com>
2023-06-11 08:07:31 -0700

    Update main.cpp

--- src/shader/main.cpp
+++ src/shader/main.cpp
@@ -2,10 +2,10 @@
 
 js JS;
 
-int32_t r4nd4(int32_t tH){
+int r4nd4(int tH){
 Rg=JS.rNd(tH);
-c=wasm_i32x4_extract_lane(Rg,0);
-return c;
+// c=wasm_i32x4_extract_lane(Rg,0);
+return Rg;
 }
 
 extern"C"{
