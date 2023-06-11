e227ea204ed5de41b0f568ce97384b5cf1b78053
Noah Cohn <noahc42@gmail.com>
2023-06-11 09:31:38 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -372,7 +372,7 @@ Colora[hh+3]=int(resulT[hh+3]);
 wgpu_buffer_map_async(mapBuffer,mapCallback,&userDataA,mode1,uint32_t(0),bufferSize);
 };
 wgpu_queue_set_on_submitted_work_done_callback(queue,onComputeDone,0);
-wgpu_queue_submit_one(queue,commandBuffer);
+wgpu_queue_submit_one_and_destroy(queue,commandBuffer);
 return;
 }
   
