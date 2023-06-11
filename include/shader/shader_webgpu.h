8a8746f62835d55bc8960b3fb771a5b2dd53790a
Noah Cohn <noahc42@gmail.com>
2023-06-11 14:04:34 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -302,7 +302,7 @@ WGPU_MAP_MODE_FLAGS mode1=0x1; // READ MODE
 void * userDataA;
 GLsizei width=256;
 GLsizei height=256;
-GLuint wtexture=0,xtexture=0,ytexture=0,ztexture=0;
+GLuint wtexture,xtexture,ytexture,ztexture;
 WGpuAdapter adapter=0;
 WGpuDevice device=0;
 WGpuQueue queue=0;
