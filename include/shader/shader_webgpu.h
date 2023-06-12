d13b7e6718c692a842e697ca218b746b4a34312f
Noah Cohn <noahc42@gmail.com>
2023-06-12 15:41:26 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -851,38 +851,7 @@ i_date.at(1,0)=dy;
 i_date.at(1,1)+=int(d_time.at(0,0));
 // glUniform4i(uni_dte,i_date.at(0,0),i_date.at(0,1),i_date.at(1,0),i_date.at(1,1));
 int tfrm=(uni_i.at(0,0)%4);
-if(uni_i.at(0,0)%30==0){
-if(shaderToySeconds%3==0){
-WGPUCompute_Run();
-// WGPUCompute_Start();
-switch(shaderToySeconds%5){
-case 0:
-glActiveTexture(GL_TEXTURE0);
-case 1:
-glActiveTexture(GL_TEXTURE0);
-case 2:
-glActiveTexture(GL_TEXTURE1);
-case 3:
-glActiveTexture(GL_TEXTURE2);
-case 4:
-glActiveTexture(GL_TEXTURE3);
-default:
-glActiveTexture(GL_TEXTURE0);
-}
-glBindTexture(GL_TEXTURE_2D,wtexture);
-glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,&Colora);
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);	
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
-// glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
-// glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-glGenerateMipmap(GL_TEXTURE_2D);
-// glUniform1i(smp_chn[0],0);
-// glUniform1i(smp_chn[1],0);
-// glUniform1i(smp_chn[2],1);
-// glUniform1i(smp_chn[3],1);
-// glBindTexture(GL_TEXTURE_2D,0);
-}else{
-// gpuStart();
+if(uni_i.at(0,0)%15==0){
 switch(tfrm){
 case 1:
 glActiveTexture(GL_TEXTURE0);
@@ -895,19 +864,65 @@ glActiveTexture(GL_TEXTURE3);
 default:
 glActiveTexture(GL_TEXTURE0);
 }
-glBindTexture(GL_TEXTURE_2D,xtexture);
+glBindTexture(GL_TEXTURE_2D,wtexture);
 glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);	
 glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
-// glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
-// glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,&Colorb);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,&ColorA);
 glGenerateMipmap(GL_TEXTURE_2D);
 glUniform1i(smp_chn[0],1);
 glUniform1i(smp_chn[1],1);
-glUniform1i(smp_chn[2],0);
-glUniform1i(smp_chn[3],0);
+glUniform1i(smp_chn[2],2);
+glUniform1i(smp_chn[3],3);
 // glBindTexture(GL_TEXTURE_2D,0);
 }
+  
+if(uni_i.at(0,0)%60==0){
+if(shaderToySeconds%2==0){
+WGPUCompute_Run();
+// WGPUCompute_Start();
+switch(shaderToySeconds%5){
+case 0:
+glActiveTexture(GL_TEXTURE0);
+case 1:
+glActiveTexture(GL_TEXTURE1);
+case 2:
+glActiveTexture(GL_TEXTURE2);
+case 3:
+glActiveTexture(GL_TEXTURE3);
+case 4:
+glActiveTexture(GL_TEXTURE0);
+default:
+glActiveTexture(GL_TEXTURE0);
+}
+glBindTexture(GL_TEXTURE_2D,wtexture);
+glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,&Colora);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);	
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+glGenerateMipmap(GL_TEXTURE_2D);
+switch(shaderToySeconds%5){
+case 0:
+glUniform1i(smp_chn[0],0);
+case 1:
+glUniform1i(smp_chn[1],0);
+case 2:
+glUniform1i(smp_chn[2],1);
+case 3:
+glUniform1i(smp_chn[3],1);
+case 4:
+glUniform1i(smp_chn[0],0);
+default:
+glUniform1i(smp_chn[0],0);
+}
+glBindTexture(GL_TEXTURE_2D,0);
+}
+  
+  /*
+  
+  else
 }else{
 switch(tfrm) {
   case 1:
@@ -937,7 +952,7 @@ glUniform1i(smp_chn[2],0);
 glUniform1i(smp_chn[3],0);}
 }
   
-/*
+
 glActiveTexture(GL_TEXTURE0);
 glUniform1i(smp_chn[0],0);
 glActiveTexture(GL_TEXTURE1);
@@ -958,11 +973,15 @@ glUniform1i(smp_chn[3],0);
 }
 }
 */
+  
+  
   // buffer frame/time
 // glBindBuffer(GL_UNIFORM_BUFFER,uniBlock);
 // glBufferSubData(GL_UNIFORM_BUFFER,8,4,&uni_i.at(0,0)); 
 // glBufferSubData(GL_UNIFORM_BUFFER,12,4,&d_time.at(0,0)); 
 // glBindBuffer(GL_UNIFORM_BUFFER,0);
+  
+  
 glUniform1i(uni_frm,uni_i.at(0,0));
 return;
 }
