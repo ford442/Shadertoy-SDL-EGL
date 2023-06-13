3ede09c0728292ce696401eb3e9022fe4375839d
Noah Cohn <noahc42@gmail.com>
2023-06-12 17:31:51 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -595,23 +595,22 @@ raf(WGPU_Device.at(0,0,0));
 
 static void ObtainedWebGpuDeviceRun(WGpuDevice result,void * userData){
 device=result;
-WGPU_Device.at(0,0,0)=result;
+// WGPU_Device.at(0,0,0)=result;
   raf2(WGPU_Device.at(0,0,0));
-
 // WGPU_Run(WGPU_Device.at(0,0,0));
 }
 
 static void ObtainedWebGpuAdapterStart(WGpuAdapter result,void * userData){
 adapter=result;
 WGPU_Adapter.at(0,0,0)=result;
+WGPU_DeviceDescriptor.at(0,0,0)=deviceDescriptor;
 WGPU_ObtainedDeviceCallback.at(0,0,0)=ObtainedWebGpuDeviceStart;
 wgpu_adapter_request_device_async(WGPU_Adapter.at(0,0,0),&WGPU_DeviceDescriptor.at(0,0,0),WGPU_ObtainedDeviceCallback.at(0,0,0),&WGPU_UserData.at(0,0,0));
 }
 
 static void ObtainedWebGpuAdapterRun(WGpuAdapter result,void * userData){
 adapter=result;
-WGPU_Adapter.at(0,0,0)=result;
-WGPU_DeviceDescriptor.at(0,0,0)=deviceDescriptor;
+// WGPU_Adapter.at(0,0,0)=result;
 WGPU_ObtainedDeviceCallback.at(0,0,1)=ObtainedWebGpuDeviceRun;
 wgpu_adapter_request_device_async(WGPU_Adapter.at(0,0,0),&WGPU_DeviceDescriptor.at(0,0,0),WGPU_ObtainedDeviceCallback.at(0,0,1),&WGPU_UserData.at(0,0,0));
 }
