94447df6a9b70e3dd22cc5cba2d5124fee44d841
Noah Cohn <noahc42@gmail.com>
2023-06-13 13:59:39 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -410,6 +410,14 @@ WGPU_BufferMappedRange.at(0,0,0)=Range;
 WGPU_ResultBuffer.at(0,0,0)=resul;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),WGPU_BufferMappedRange.at(0,0,0),uint32_t(0),WGPU_ResultBuffer.at(0,0,0),bufferSize);
 raN=rNd4(3);
+  for(int g=0;g<65536;g++){
+int hh=g*4;
+ColorA[hh]=int(WGPU_ResultBuffer.at(0,0,0)[hh]);
+ColorA[hh+1]=int(WGPU_ResultBuffer.at(0,0,0)[hh+1]);
+ColorA[hh+2]=int(WGPU_ResultBuffer.at(0,0,0)[hh+2]);
+ColorA[hh+3]=int(WGPU_ResultBuffer.at(0,0,0)[hh+3]);
+}
+}
 if(raN==0){
 for(int g=0;g<65536;g++){
 int hh=g*4;
@@ -456,6 +464,13 @@ WGPU_BufferMappedRange.at(0,0,0)=Range;
 WGPU_ResultBuffer.at(0,0,0)=resul;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),WGPU_BufferMappedRange.at(0,0,0),uint32_t(0),WGPU_ResultBuffer.at(0,0,0),bufferSize);
 raN=rNd4(3);
+  for(int g=0;g<65536;g++){
+int hh=g*4;
+ColorA[hh]=int(WGPU_ResultBuffer.at(0,0,0)[hh]);
+ColorA[hh+1]=int(WGPU_ResultBuffer.at(0,0,0)[hh+1]);
+ColorA[hh+2]=int(WGPU_ResultBuffer.at(0,0,0)[hh+2]);
+ColorA[hh+3]=int(WGPU_ResultBuffer.at(0,0,0)[hh+3]);
+}
 if(raN==0){
 for(int g=0;g<65536;g++){
 int hh=g*4;
