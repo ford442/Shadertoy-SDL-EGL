86f3d9a13e6d0377b2ebf12fd11dd1b70f0e191b
Noah Cohn <noahc42@gmail.com>
2023-06-15 14:16:33 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -61,11 +61,11 @@ unsigned char * ColorA=new unsigned char[262144*sizeof(unsigned char)];
 
 uint32_t workgroupSize=64;
 // double_int53_t DbufferSize=262144*sizeof(int);
-uint32_t DbufferSize=262144*sizeof(unsigned int);
-uint32_t bufferSize=262144*sizeof(unsigned int);
+uint64_t DbufferSize=262144*sizeof(unsigned int);
+uint64_t bufferSize=262144*sizeof(unsigned int);
 // double_int53_t DiBufferSize=1*sizeof(int);
-uint32_t DiBufferSize=1*sizeof(unsigned int);
-uint32_t iBufferSize=1*sizeof(unsigned int);
+uint64_t DiBufferSize=1*sizeof(unsigned int);
+uint64_t iBufferSize=1*sizeof(unsigned int);
 uint64_t DescriptorBufferSize=262144*sizeof(unsigned int);
 uint64_t iDescriptorBufferSize=1*sizeof(unsigned int);
 const char * Entry="computeStuff";
@@ -127,12 +127,10 @@ double Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),uint32_t(0),Dbu
 WGPU_BufferMappedRange.at(0,0,0)=Range;
 // unsigned int * WGPU_Result_Buffer=new unsigned int[262144];
     std::vector<unsigned int>WGPU_Result_Buffer(bufferSize/sizeof(unsigned int));
-
 // WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),WGPU_BufferMappedRange.at(0,0,0),uint32_t(0),&WGPU_Result_Buffer,bufferSize);
 std::cout << "Result Buffer:\n";
 std::cout << WGPU_Result_Buffer[0];
-    
     /* for(int g=0;g<65536;g++){
 int hh=g*4;
 ColorA[hh]=uint32_t(WGPU_ResultBuffer.at(0,0,0)[hh]);
