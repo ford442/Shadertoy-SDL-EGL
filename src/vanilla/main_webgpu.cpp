c885810679393628c0cb217788a8ab8d599080c8
Noah Cohn <noahc42@gmail.com>
2023-06-15 17:37:03 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -133,7 +133,7 @@ WGPU_BufferMappedRange.at(0,0,0)=Range;
 
   //  std::vector<unsigned int>WGPU_Result_Buffer(bufferSize/sizeof(unsigned int));
 WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
-wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),WGPU_BufferMappedRange.at(0,0,0),uint64_t(0),WGPU_ResultBuffer.at(0,0,0),bufferSize);
+wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),WGPU_BufferMappedRange.at(0,0,0),uint64_t(0),&WGPU_ResultBuffer.at(0,0,0),bufferSize);
 std::cout << "Result Buffer:\n";
 std::cout << WGPU_Result_Buffer[0];
     /* for(int g=0;g<65536;g++){
