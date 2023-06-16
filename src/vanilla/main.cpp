1177f6419ba3092243e2a8c9bd3c5af713c3dd65
Noah Cohn <noahc42@gmail.com>
2023-06-16 09:50:57 -0700

    Update main.cpp

--- src/vanilla/main.cpp
+++ src/vanilla/main.cpp
@@ -1,3 +1,86 @@
+
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+
+// Performs an HTTP GET and prints the response
+int beast()
+{
+    try
+    {
+
+        auto const host = "test.1ink.us";
+        auto const port = "80";
+        auto const target = "/cr1p/images/02bb.jpg";
+        int version = 11;
+
+        // The io_context is required for all I/O
+        net::io_context ioc;
+
+        // These objects perform our I/O
+        tcp::resolver resolver(ioc);
+        beast::tcp_stream stream(ioc);
+
+        // Look up the domain name
+        auto const results = resolver.resolve(host, port);
+
+        // Make the connection on the IP address we get from a lookup
+        stream.connect(results);
+
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, version};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // Send the HTTP request to the remote host
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        // Write the message to standard out
+        std::cout << res << std::endl;
+        std::cout << stream << std::endl;
+        std::cout << buffer << std::endl;
+
+        // Gracefully close the socket
+        beast::error_code ec;
+        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+        // not_connected happens sometimes
+        // so don't bother reporting it.
+        //
+        if(ec && ec != beast::errc::not_connected)
+            throw beast::system_error{ec};
+
+        // If we get here then the connection is closed gracefully
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+
 #include "../../include/vanilla/main.hpp"
 
 float cc,pp,uu;
@@ -11,6 +94,10 @@ tens Tens;
 extern "C"{
   
 float js_double(float nem){ 
+
+    beast();
+
+    
 v128_t rtt=Funcs.rnn.double_add(nem);
 float retrnI=wasm_f32x4_extract_lane(rtt,0);
 float retrnD=wasm_f32x4_extract_lane(rtt,4);
