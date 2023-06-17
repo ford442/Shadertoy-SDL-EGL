550cf45ea3dabe58b19a1c88955e6e0fb43aa579
Noah Cohn <noahc42@gmail.com>
2023-06-17 11:42:55 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -126,12 +126,9 @@ return randomNumber;
 //  output from mapping is Uint8 and takes 4 X 8bit spaces
 
 WGpuBufferMapCallback mapCallbackStart=[](WGpuBuffer buffer,void * userData,WGPU_MAP_MODE_FLAGS mode,double_int53_t offset,double_int53_t size){
-// wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),U0);
-double Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,sizeof(WGPU_Result_Buffer);
-// wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),U0,WGPU_MAX_SIZE);
+wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,sizeof(WGPU_Result_Buffer);
 WGPU_BufferMappedRange.at(0,0,0)=Range;
-// WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
-wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),Range,0,WGPU_Result_Buffer,sizeof(WGPU_Result_Buffer));
+wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,0,WGPU_Result_Buffer,sizeof(WGPU_Result_Buffer));
 std::cout << "GETTING BUFFER\n";
 // std::cout << WGPU_Result_Buffer;
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
