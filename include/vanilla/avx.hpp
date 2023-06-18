231fba2731b380ecf9fe1ed5ba6505b4b2b30ff8
Noah Cohn <noahc42@gmail.com>
2023-06-17 17:45:40 -0700

    Update avx.hpp

--- include/vanilla/avx.hpp
+++ include/vanilla/avx.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <wasm_simd128.h>
-#include <tmmintrin.h> // ssse 3
-#include <smmintrin.h>  // sse 4.1
-#include <avxintrin.h>  // AVX
+// #include <wasm_simd128.h>
+// #include <tmmintrin.h> // ssse 3
+// #include <smmintrin.h>  // sse 4.1
+// #include <avxintrin.h>  // AVX
 
 #define VLEAVE _mm256_zeroupper
 
-#include <xmmintrin.h>
+// #include <xmmintrin.h>
 #include <immintrin.h> 
-#include <nmmintrin.h>  //  sse 4.2
-#include <unistd.h>
+// #include <nmmintrin.h>  //  sse 4.2
+// #include <unistd.h>
 
 #define __m64i  __m64
 
