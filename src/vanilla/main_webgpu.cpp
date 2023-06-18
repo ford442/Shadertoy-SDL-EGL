7b7a5c1c818336603fbcdb5927d78dc231742458
Noah Cohn <noahc42@gmail.com>
2023-06-18 00:40:09 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -156,7 +156,7 @@ int raND=0;
 // int * WGPU_Result_Buffer[262144];
 // int holder[262144];
 // int WGPU_Result_Buffer=new int[262144];
-int * locate=&outputd;
+int * locate=outputd;
 
 inline int rNd4(int randomMax){
 entropySeed=(randomMax)*randomizer();
@@ -176,7 +176,7 @@ double_int53_t Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,26214
          std::cout << "before read:" << std::endl;
          std::cout << locate << std::endl;
       
- wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,0,locate,262144);
+ wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,0,&outputd,262144);
          std::cout << &locate << std::endl;
 
       
