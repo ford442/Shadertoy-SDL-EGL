440d23c5709eb015c5f9a8653a960547bdff0a84
Noah Cohn <noahc42@gmail.com>
2023-06-17 22:55:51 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -165,16 +165,12 @@ return randomNumber;
 }
 
 WGpuBufferMapCallback mapCallbackStart=[](WGpuBuffer buffer,void * userData,WGPU_MAP_MODE_FLAGS mode,double_int53_t offset,double_int53_t size){
-
-   
-double Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,262144);
+double Range=wgpu_buffer_get_mapped_range(buffer,0,262144);
 //   wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,262144);
 // WGPU_BufferMappedRange.at(0,0,0)=Range;
       //     std::cout << WGPU_BufferMappedRange.at(0,0,0) << std::endl;
 // WGPU_ResultBuffer.at(0,0,0)=&outputd;
    wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),Range,0,&outputd,262144);
-
-  
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
 return;
 };
