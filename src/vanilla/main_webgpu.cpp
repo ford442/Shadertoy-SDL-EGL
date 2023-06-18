2283d06a492a30ad6479ef8ea0ba55d27e02baca
Noah Cohn <noahc42@gmail.com>
2023-06-17 21:03:57 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -154,7 +154,7 @@ int raN=0;
 int raND=0;
 
 // int * WGPU_Result_Buffer[262144];
-int * WGPU_Result_Buffer[262144];
+int WGPU_Result_Buffer[262144];
 
 inline int rNd4(int randomMax){
 entropySeed=(randomMax)*randomizer();
@@ -164,12 +164,11 @@ return randomNumber;
 }
 
 WGpuBufferMapCallback mapCallbackStart=[](WGpuBuffer buffer,void * userData,WGPU_MAP_MODE_FLAGS mode,double_int53_t offset,double_int53_t size){
-
   
    uint64_t Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),uint32_t(0),BufferMapSize);
   //  WGPU_BufferMappedRange.at(0,0,0)=Range;
-  // WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
-   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),Range,uint32_t(0),WGPU_Result_Buffer,BufferMapSize);
+WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
+   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),Range,uint32_t(0),WGPU_ResultBuffer.at(0,0,0),BufferMapSize);
 
   
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
