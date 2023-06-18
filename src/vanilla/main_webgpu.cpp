4478dcc62ff17b29595423bdd6406ea0d0a9167f
Noah Cohn <noahc42@gmail.com>
2023-06-17 22:17:45 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -184,7 +184,7 @@ WGpuBufferMapCallback mapCallbackRun=[](WGpuBuffer buffer,void * userData,WGPU_M
    double WGPU_Map_Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),uint32_t(0),BufferMapSize);
 
    WGPU_BufferMappedRange.at(0,0,0)=WGPU_Map_Range;
-              std::cout << WGPU_BufferMappedRange.at(0,0,0)[0] << std::endl;
+              std::cout << WGPU_BufferMappedRange.at(0,0,0) << std::endl;
 
    wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),&WGPU_Map_Range,uint32_t(0),&WGPU_ResultBuffer.at(0,0,0),BufferMapSize);
 
