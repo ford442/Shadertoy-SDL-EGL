2ca74011ed886dfc7101b9759b776e308220926a
Noah Cohn <noahc42@gmail.com>
2023-06-17 21:11:34 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -169,7 +169,7 @@ WGpuBufferMapCallback mapCallbackStart=[](WGpuBuffer buffer,void * userData,WGPU
    uint64_t Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),uint32_t(0),BufferMapSize);
   //  WGPU_BufferMappedRange.at(0,0,0)=Range;
 WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Buffer;
-   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),Range,uint32_t(0),WGPU_ResultBuffer.at(0,0,0),BufferMapSize);
+   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,uint32_t(0),WGPU_Result_Buffer,BufferMapSize);
 
   
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
@@ -180,8 +180,8 @@ WGpuBufferMapCallback mapCallbackRun=[](WGpuBuffer buffer,void * userData,WGPU_M
 
   
    double WGPU_Map_Range=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),uint32_t(0),BufferMapSize);
-//    WGPU_BufferMappedRange.at(0,0,0)=WGPU_Map_Range;
-   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,uint32_t(0),WGPU_ResultBuffer.at(0,0,0),BufferMapSize);
+   WGPU_BufferMappedRange.at(0,0,0)=WGPU_Map_Range;
+   wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,uint32_t(0),&WGPU_ResultBuffer.at(0,0,0),BufferMapSize);
 
   
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
