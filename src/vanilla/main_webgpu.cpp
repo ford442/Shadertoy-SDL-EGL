8ab3f8f659a78cfe33d14e1b0e43580a25c78dd2
Noah Cohn <noahc42@gmail.com>
2023-06-17 23:34:49 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -155,7 +155,7 @@ int raND=0;
 
 // int * WGPU_Result_Buffer[262144];
 int holder[262144];
-int WGPU_Result_Buffer[262144]=new int[262144];
+int WGPU_Result_Buffer=new int[262144];
 int * locate=&WGPU_Result_Buffer[0];
 
 inline int rNd4(int randomMax){
