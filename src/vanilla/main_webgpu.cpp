5d514a14df326b85849c412e08babed99630e740
Noah Cohn <noahc42@gmail.com>
2023-06-18 17:32:17 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -182,9 +182,11 @@ wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,uintOutputBufferSize);
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1),0,0,&outputd,uintOutputBufferSize);
  
 std::cout << "COMPUTE read map: " << std::endl;
-std::cout << outputd[0] << std::endl;
- 
+// std::cout << outputd[0] << std::endl;
+ outpute[0]=outputd[0];
 wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
+ std::cout << outpute[0] << std::endl;
+
 return;
 };
 
