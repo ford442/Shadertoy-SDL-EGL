6c286db224c94120c611de72829f3ecfa642dae0
Noah Cohn <noahc42@gmail.com>
2023-06-20 17:58:45 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -7,9 +7,8 @@ inline char wgl_cmp_src[2000]=
 "@compute@workgroup_size(256,1,1)\n"
 "fn computeStuff(@builtin(global_invocation_id)global_id:vec3<u32>){"
 "let f:u32=global_id.x;"
-"let flo: vec4<f32> = vec4<f32>(0.42,0.42,0.42,0.42);"
 "let coord: vec2<u32>=vec2<u32>(0,0);"
-// "textureStore(textureA,coord,flo);"
+"textureStore(textureA,coord,vec4<r32float>(0.42,0.42,0.42,0.42));"
 "outputBuffer[f]=42;"
 "}";
 
