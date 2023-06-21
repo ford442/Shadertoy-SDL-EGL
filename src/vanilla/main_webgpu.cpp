daf5b6ba0aa7f094259378ae6e536dcf35ca4041
Noah Cohn <noahc42@gmail.com>
2023-06-21 01:13:44 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -9,7 +9,7 @@ inline char wgl_cmp_src[2000]=
 "let f:u32=global_id.x;"
 "let coord:vec2<u32>=vec2<u32>(0,0);"
 "let flo:vec4<f32>=vec4<f32>(0.42,0.0,0.0,1.0);"
-let u0:u32=0;
+"let u0:u32=0;"
 "let clr:f32=textureLoad(textureA:texture_storage_2d<f32>,coord,u0).r;"
 // "let flt:u32=u32(clr:f32);"
 // "textureStore(textureA,coord,vec4<f32>(0.42,0.0,0.0,1.0));"
