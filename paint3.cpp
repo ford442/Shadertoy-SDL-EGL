167e1fd6beb03da525124bc0827955c6bf87aa7f
Noah Cohn <noahc42@gmail.com>
2023-06-21 20:39:29 -0700

    Update paint3.cpp

--- paint3.cpp
+++ paint3.cpp
@@ -176,8 +176,8 @@ contx.getExtension('GL_NV_memory_attachment');
   
   contx.disable(gl.DITHER);
 
-const g=new GPU({canvas:bcanvas,webGl:contx});
-const g2=new GPU();
+const g=new GPUX({canvas:bcanvas,webGl:contx});
+const g2=new GPUX();
 const glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 const glslAlphe=`float Alphe(float a,float b,float c,float d,float e,float f,float g){return((0.7+(3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-e)*((1.0-g)*0.1))))))/4.0);}`;
 const glslAveg=`float Aveg(float a,float b){return(1.0-(((a)-(b))*((a)*(1.0/(1.0-b)))));}`;
