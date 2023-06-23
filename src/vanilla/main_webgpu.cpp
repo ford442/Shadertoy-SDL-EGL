c5a4bf1bd68351d99d5296442673ad0959cc5121
Noah Cohn <noahc42@gmail.com>
2023-06-23 02:15:37 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -13,7 +13,17 @@ inline char wgl_cmp_src[2000]=
 // "let u0:u32=0;"
 // "let clr:f32=textureLoad(textureA:texture_storage_2d<u32>,coord,u0);"
 // "textureStore(textureA,coord,vec4<u32>(24,24,24,255));"
+"let h:u32=f*g;"
+
+"var i: i32;"
+"loop {"
 "outputBuffer[f*g]=42;"
+"outputBuffer[f*g]=42;"
+"outputBuffer[f*g]=42;"
+"outputBuffer[f*g]=1;"
+"  i+=4;"
+"  if i == 256 { break; }"
+"}"
 "}";
 
 using mouse_tensor=boost::numeric::ublas::tensor<float>;
