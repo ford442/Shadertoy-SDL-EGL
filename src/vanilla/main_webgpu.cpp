fc81d1726770fdce7f7dfa3a041fb8af366ecf20
Noah Cohn <noahc42@gmail.com>
2023-06-23 02:25:00 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -250,28 +250,7 @@ return;
 };
 
 WGpuOnSubmittedWorkDoneCallback onComputeDoneStart=[](WGpuQueue queue,void *userData){
-wgpu_buffer_map_sync(WGPU_Buffers.at(1,0,1),mode1,0,OutputBufferBytes);
-double_int53_t WGPU_Range_Pointer=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,OutputBufferBytes);
-WGPU_BufferRange.at(0,0,0)=WGPU_Range_Pointer;
-wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1), 0 ,0,&WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
-std::cout << "Mapped range of result buffer:" << std::endl;
-std::cout << "\n" << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
-std::cout << "\n" << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[1] << std::endl;
-std::cout << "\n" << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[2] << std::endl;
-std::cout << "\n" << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[3] << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[4] << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[5] << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[6] << std::endl;
-std::cout << WGPU_ResultBuffer.at(0,0,0)[7] << std::endl;
-  sleep(1);
-  WGPU_BUFFER_MAP_STATE state=wgpu_buffer_map_state(WGPU_Buffers.at(1,0,1));
- if(state=='mapped'){
-wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
- }
+
   /*
 wgpu_buffer_map_sync(WGPU_Buffers.at(2,0,2),mode1,0,OutputBufferBytes);
 double_int53_t WGPU_Range_PointerB=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
@@ -405,7 +384,29 @@ wgpu_encoder_end(WGPU_ComputePassCommandEncoder.at(0,0,0));
 // wgpu_command_encoder_copy_texture_to_buffer(WGPU_CommandEncoder.at(0,0,0),&WGPU_Output_Image,&WGPU_Mapped_Buffer,1,1,1);
 
 wgpu_command_encoder_copy_buffer_to_buffer(WGPU_CommandEncoder.at(0,0,2),WGPU_Buffers.at(0,0,0),0,WGPU_Buffers.at(1,0,1),0,OutputBufferBytes);
-WGPU_CommandBuffer.at(0,0,0)=wgpu_encoder_finish(WGPU_CommandEncoder.at(0,0,0));
+wgpu_buffer_map_sync(WGPU_Buffers.at(1,0,1),mode1,0,OutputBufferBytes);
+double_int53_t WGPU_Range_Pointer=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(1,0,1),0,OutputBufferBytes);
+WGPU_BufferRange.at(0,0,0)=WGPU_Range_Pointer;
+wgpu_buffer_read_mapped_range(WGPU_Buffers.at(1,0,1), 0 ,0,&WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
+std::cout << "Mapped range of result buffer:" << std::endl;
+std::cout << "\n" << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
+std::cout << "\n" << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[1] << std::endl;
+std::cout << "\n" << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[2] << std::endl;
+std::cout << "\n" << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[3] << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[4] << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[5] << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[6] << std::endl;
+std::cout << WGPU_ResultBuffer.at(0,0,0)[7] << std::endl;
+//  sleep(1);
+//  WGPU_BUFFER_MAP_STATE state=wgpu_buffer_map_state(WGPU_Buffers.at(1,0,1));
+// if(state=='mapped'){
+wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
+// }
+  WGPU_CommandBuffer.at(0,0,0)=wgpu_encoder_finish(WGPU_CommandEncoder.at(0,0,0));
 
   // WGPU_ComputeDoneCallback.at(0,0,0)=onComputeDoneStart;
 wgpu_queue_set_on_submitted_work_done_callback(WGPU_Queue.at(0,0,0),WGPU_ComputeDoneCallback.at(0,0,0),0);
