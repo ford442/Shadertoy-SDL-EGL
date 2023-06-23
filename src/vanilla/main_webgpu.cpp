31837a1d1e861a0ddf917681e0930381c92baac4
Noah Cohn <noahc42@gmail.com>
2023-06-22 22:34:31 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -311,11 +311,11 @@ WGPU_Buffers.at(1,0,1)=wgpu_device_create_buffer(WGPU_Device.at(0,0,0),&WGPU_Buf
 WGPU_Buffers.at(2,0,2)=wgpu_device_create_buffer(WGPU_Device.at(0,0,0),&WGPU_BufferDescriptor.at(0,0,3));
  // wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
 WGPU_Output_Buffer.buffer=WGPU_Buffers.at(0,0,0);
-// WGPU_Output_Buffer.bytesPerRow=1024;
-// WGPU_Output_Buffer.rowsPerImage=0;
+WGPU_Output_Buffer.bytesPerRow=16;
+WGPU_Output_Buffer.rowsPerImage=0;
 WGPU_Mapped_Buffer.buffer=WGPU_Buffers.at(2,0,2);
-// WGPU_Mapped_Buffer.bytesPerRow=1024;
-// WGPU_Mapped_Buffer.rowsPerImage=0;
+WGPU_Mapped_Buffer.bytesPerRow=16;
+WGPU_Mapped_Buffer.rowsPerImage=0;
 raN=rNd4(1024);
 input[0]=raN;
 WGPU_InputBuffer.at(0,0,0)[0]=raN;
