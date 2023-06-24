589a7879cafefe6e4ec9a8502ae8e4e8aa6cf70b
Noah Cohn <noahc42@gmail.com>
2023-06-23 19:58:30 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -271,7 +271,7 @@ std::cout << "\n" << std::endl;
 std::cout << WGPU_ResultBuffer.at(0,0,0)[3] << std::endl;
 stateb=wgpu_buffer_map_state(WGPU_Buffers.at(1,0,1));
 // if(stateb=='mapped'){
-wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
+// wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 // }
 // wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 // wgpu_buffer_map_async(WGPU_Buffers.at(1,0,1),mapCallbackStart,&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
@@ -386,6 +386,8 @@ return;
 }
 
 static void WGPU_Run(){
+wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
+  
 WGPU_Queue.at(0,0,0)=wgpu_device_get_queue(WGPU_Device.at(0,0,0));
 WGPU_CommandEncoder.at(0,0,1)=wgpu_device_create_command_encoder_simple(WGPU_Device.at(0,0,0));
 /*
@@ -401,8 +403,8 @@ wgpu_command_encoder_copy_buffer_to_buffer(WGPU_CommandEncoder.at(0,0,1),WGPU_Bu
 WGPU_CommandBuffer.at(0,0,1)=wgpu_encoder_finish(WGPU_CommandEncoder.at(0,0,1));
 wgpu_queue_submit_one(WGPU_Queue.at(0,0,0),WGPU_CommandBuffer.at(0,0,1));
 */
-double_int53_t WGPU_Range_Pointer=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
-wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2),WGPU_Range_Pointer,0,WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
+// double_int53_t WGPU_Range_Pointer=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
+// wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2),WGPU_Range_Pointer,0,WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
 std::cout << "End Buffer:" << std::endl;
 std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
 std::cout << "----------" << std::endl;
@@ -487,7 +489,7 @@ document.getElementById('circle').height=window.innerHeight;
 document.getElementById('di').click();
 Module.ccall("startWebGPU",{async:true});
 setTimeout(function(){
-// Module.ccall("runWebGPU");
+Module.ccall("runWebGPU");
 },1500);
 setTimeout(function(){
 // Module.ccall("runWebGPU2");
