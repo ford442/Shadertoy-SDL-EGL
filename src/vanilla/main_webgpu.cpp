6bd54563aeaff14396184e73ed440594e11939d8
Noah Cohn <noahc42@gmail.com>
2023-06-24 00:45:45 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -300,6 +300,7 @@ std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
 std::cout << "----------" << std::endl;
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 // wgpu_buffer_map_async(WGPU_Buffers.at(2,0,2),WGPU_MapCallback.at(0,0,2),&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
+WGPU_Run();
 return;
 };
 
