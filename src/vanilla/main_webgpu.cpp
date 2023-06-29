4923a4da15022b6d09eee8891b8b75eb3c075f4c
Noah Cohn <noahc42@gmail.com>
2023-06-29 09:22:03 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -258,8 +258,8 @@ if(WGPU_BufferStatus.at(0,0,0)==3){
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 }
 // wgpu_buffer_map_async(WGPU_Buffers.at(1,0,1),WGPU_MapCallback.at(0,0,1),&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
-sleep(1);
-WGPU_Run();
+// sleep(1);
+// WGPU_Run();
 return;
 };
 
