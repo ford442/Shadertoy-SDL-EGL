aaef17255d43b997875c5459d0b967e223bdd903
Noah Cohn <noahc42@gmail.com>
2023-06-29 16:18:59 -0700

    Update pagec.js

--- pagec.js
+++ pagec.js
@@ -213,8 +213,11 @@ if(lockVid!=1){
 loadV.addEventListener('canplay',function(){
 loadV.width=this.videoWidth;
 loadV.height=this.videoHeight;
-document.getElementById('wid').innerHTML=this.videoWidth;
-document.getElementById('hig').innerHTML=this.videoHeight;
+// document.getElementById('wid').innerHTML=this.videoWidth;
+// document.getElementById('hig').innerHTML=this.videoHeight;
+document.getElementById('wid').innerHTML=parseInt(window.innerHeight,10);
+document.getElementById('hig').innerHTML=parseInt(window.innerHeight,10);
+ 
 var $sc=this.duration;
 var mic=Math.round($sc*1000000);
 $pt=Math.random()*mic;
