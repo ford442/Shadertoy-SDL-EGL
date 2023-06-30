b1cd7f7ed23e396ba0fd2f56d763eae55bc9e9d2
Noah Cohn <noahc42@gmail.com>
2023-06-30 02:44:05 -0700

    Update video_jebus.hpp

--- include/video/video_jebus.hpp
+++ include/video/video_jebus.hpp
@@ -15,17 +15,9 @@ void renderFrame();
 
 void strt();
 
-void plt();
-
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 
 // void SDLCALL bfr(void *unused,Uint8* stm,int len);
-
-void opn_aud();
-
-// void qu(int rc);
-
-// void cls_aud();
 
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 
