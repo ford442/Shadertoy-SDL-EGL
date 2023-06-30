039dd01772aa2f0c0e1ea6ad6f2411de19063888
Noah Cohn <noahc42@gmail.com>
2023-06-30 02:37:59 -0700

    Update video_jebus.hpp

--- include/video/video_jebus.hpp
+++ include/video/video_jebus.hpp
@@ -7,9 +7,9 @@ void avgFrm(int Fnum,int leng,float *ptr,float *aptr);
 
 void clrclr(GLfloat rlc,GLfloat alc,GLfloat avr);
 
-void uni(float xx,float yy,GLfloat time,EGLint fram);
+// void uni(float xx,float yy,GLfloat time,EGLint fram);
 
-GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
+// GLuint compile_shader(GLenum type,GLsizei nsources,const char **dsources);
 
 void renderFrame();
 
@@ -17,13 +17,15 @@ void strt();
 
 void plt();
 
-void SDLCALL bfr(void *unused,Uint8* stm,int len);
+EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
+
+// void SDLCALL bfr(void *unused,Uint8* stm,int len);
 
 void opn_aud();
 
-void qu(int rc);
+// void qu(int rc);
 
-void cls_aud();
+// void cls_aud();
 
 EM_BOOL mouse_call(int eventType,const EmscriptenMouseEvent *e,void *userData);
 
