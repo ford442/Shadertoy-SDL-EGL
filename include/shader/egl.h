3ff0edc7e0d5af0aa736d085b2cb79db2f06c90e
Noah Cohn <noahc42@gmail.com>
2023-07-02 07:15:34 -0700

    Update egl.h

--- include/shader/egl.h
+++ include/shader/egl.h
@@ -1,18 +1,18 @@
 #pragma once
 
 // #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-// #include <webgl/webgl2.h>  //  much better performance without
+#include <webgl/webgl2.h>  //  much better performance without
 
 #define GL_GLES_PROTOTYPES 1
+#include <GLES3/gl3platform.h>
 #include <GLES3/gl3.h>
 // #include <GLES3/gl31.h>
 // #include <GLES3/gl32.h>
-// #include <GLES3/gl3platform.h>
 #define EGL_EGL_PROTOTYPES 1
 #define EGL_EGLEXT_PROTOTYPES 1
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-// #include <EGL/eglplatform.h>
-// #include <KHR/khrplatform.h>
+#include <EGL/eglplatform.h>
+#include <KHR/khrplatform.h>
 
 // #include <emscripten/html5_webgl.h>
