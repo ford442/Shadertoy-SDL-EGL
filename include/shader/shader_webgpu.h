a901b21739dc0e9b254d97ae322a294df40b58f4
Noah Cohn <noahc42@gmail.com>
2023-07-01 17:48:33 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -415,6 +415,7 @@ double_int53_t WGPU_Range_PointerB=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(
 WGPU_BufferRange.at(0,0,1)=WGPU_Range_PointerB;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2), WGPU_BufferRange.at(0,0,1) ,0,WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
+}
 if(WGPU_BufferStatus.at(0,0,0)!=1){
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 }
