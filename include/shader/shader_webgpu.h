8f7ed2ffd29bc9c0d66799e1b7246dfddd273b74
Noah Cohn <noahc42@gmail.com>
2023-07-01 21:04:19 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -126,7 +126,7 @@ inline char cm_hdr_src[2300]=
 // "#define HW_PERFORMANCE 0\n"
 "precision highp int;\n"
 "precision highp float;\n"
-"precision lowp sampler3D;precision highp sampler2D;"
+"precision highp sampler3D;precision highp sampler2D;"
 "precision highp samplerCube;precision highp sampler2DArray;precision highp sampler2DShadow;"
 "precision highp isampler2D;precision highp isampler3D;precision highp isamplerCube;"
 "precision highp isampler2DArray;precision highp usampler2D;precision highp usampler3D;"
