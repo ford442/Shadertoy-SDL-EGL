03afda2eb7767db751816c02e27ccf0458ff53d1
Noah Cohn <noahc42@gmail.com>
2023-07-03 23:40:13 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1319,7 +1319,7 @@ glBindFramebuffer(GL_DRAW_FRAMEBUFFER,frameBuffer);
 glClear(GL_COLOR_BUFFER_BIT);
 glBindFramebuffer(GL_FRAMEBUFFER,0);
 glClearColor(0.0,0.0,0.0,1.0);
-glClear(GL_COLOR_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT|GL_STENCIL_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 glFlush();
 
 glUseProgram(S1.at(0,0,0));
