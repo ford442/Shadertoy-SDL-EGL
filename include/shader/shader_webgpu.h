8ae83c824e20c5b304c23b113d2db3f596d2f426
Noah Cohn <noahc42@gmail.com>
2023-07-08 08:13:47 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1229,12 +1229,12 @@ glBindBuffer(GL_ARRAY_BUFFER,Sh.at(2,1));
 glBufferData(GL_ARRAY_BUFFER,sizeof(vrt),vrt,GL_STATIC_DRAW);
   
 auto CLdevice=boost::compute::system::default_device();
+auto CLcontext=boost::compute::interop::opengl::context(CLdevice);
+auto command_queue=boost::compute::command_queue(CLcontext,CLdevice);
 
  /*   // Boost Compute / Interop / OpenGL
 boost::compute::interop::opengl::buffer_object_interface buffer_object(CLdevice,GL_ARRAY_BUFFER);
 boost::compute::vertex_array_object vertex_array(CLdevice);
-auto CLcontext=boost::compute::interop::opengl::context(CLdevice);
-auto command_queue=boost::compute::command_queue(CLcontext,CLdevice);
 std::cout << "Got OpenCL context:" << CLcontext << '\n'; 
 vertex_array.bind_buffer(GL_ARRAY_BUFFER,buffer_object);
 vertex_array.set_attribute_pointer(0,4,GL_FLOAT,GL_FALSE,0,0);
