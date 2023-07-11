19d58187a4bf058e7ab8a5287842643287a07b6c
Noah Cohn <noahc42@gmail.com>
2023-07-11 10:26:20 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -133,16 +133,16 @@ inline char frg_hdr_src[1000]=
 
 inline char frg_ftr_src[420]=
 "void main(){mainImage(fragColor,gl_FragCoord.xy);}\n"
-// "#define mainImage mainImage0(out dvec4 O,dvec2 U);"
-"#define mainImage mainImage0(out vec4 O,vec2 U);"
-// "int _N=3;void mainImage(out dvec4 O,dvec2 U){"
-"int _N=3;void mainImage(out vec4 O,vec2 U){"
-// "dvec4 o;O=dvec4(0);"
-"vec4 o;O=vec4(0);"
-// "mainImage0(o,U+dvec2(k%_N-_N/2,k/_N-_N/2)/double(_N));"
-"mainImage0(o,U+vec2(k%_N-_N/2,k/_N-_N/2)/float(_N));"
-// "O += o;}O /= double(_N*_N);O=pow(O,dvec4(2.077038lf/1.0lf,2.184228lf/1.0lf,2.449715lf/1.0lf,1.0lf));}"
-"O += o;}O /= float(_N*_N);O=pow(O,vec4(2.077038f/1.0f,2.184228f/1.0f,2.449715f/1.0f,1.0f));}"
+"#define mainImage mainImage0(out dvec4 O,dvec2 U);"
+// "#define mainImage mainImage0(out vec4 O,vec2 U);"
+"int _N=3;void mainImage(out dvec4 O,dvec2 U){"
+// "int _N=3;void mainImage(out vec4 O,vec2 U){"
+"dvec4 o;O=dvec4(0);"
+// "vec4 o;O=vec4(0);"
+"mainImage0(o,U+dvec2(k%_N-_N/2,k/_N-_N/2)/double(_N));"
+// "mainImage0(o,U+vec2(k%_N-_N/2,k/_N-_N/2)/float(_N));"
+"O += o;}O /= double(_N*_N);O=pow(O,dvec4(2.077038lf/1.0lf,2.184228lf/1.0lf,2.449715lf/1.0lf,1.0lf));}"
+// "O += o;}O /= float(_N*_N);O=pow(O,vec4(2.077038f/1.0f,2.184228f/1.0f,2.449715f/1.0f,1.0f));}"
 // "O += o;}O /= double(_N*_N);O=pow(O,dvec4(1.0f,1.0f,1.0f,1.0f));}"
 "void mainImage0\n\0";
 
