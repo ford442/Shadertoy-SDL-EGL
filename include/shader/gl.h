19b3547b2b46870525a8b0ee301ca3ebc8e95cc4
Noah Cohn <noahc42@gmail.com>
2023-07-13 13:29:20 -0700

    Update gl.h

--- include/shader/gl.h
+++ include/shader/gl.h
@@ -7,13 +7,13 @@
 #endif
 
 // #include <GL/gl3w.h>
-#include <GL/gl.h>
 
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES 1
 #endif
 
 #include <GL/glext.h>
+#include <GL/gl.h>
 
 // #define GLFW_INCLUDE_GLEXT 1
 // #include <GLFW/glfw3.h>
