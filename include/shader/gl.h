e69d5381bfd130a9fdcc5d541900faf8d0e4f6f9
Noah Cohn <noahc42@gmail.com>
2023-07-13 13:30:02 -0700

    Update gl.h

--- include/shader/gl.h
+++ include/shader/gl.h
@@ -2,18 +2,19 @@
 
 // #define GL_SILENCE_DEPRECATION
 
-#ifndef GL_FRAGMENT_PRECISION_HIGH
+// #ifndef GL_FRAGMENT_PRECISION_HIGH
 #define GL_FRAGMENT_PRECISION_HIGH 1
-#endif
+// #endif
 
 // #include <GL/gl3w.h>
 
-#ifndef GL_GLEXT_PROTOTYPES
+// #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES 1
-#endif
+//  #endif
 
-#include <GL/glext.h>
 #include <GL/gl.h>
+#include <GL/glext.h>
+
 
 // #define GLFW_INCLUDE_GLEXT 1
 // #include <GLFW/glfw3.h>
