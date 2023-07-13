58339b7c726b4821e60fad76f52cd12379e42f0e
Noah Cohn <noahc42@gmail.com>
2023-07-13 12:52:31 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -44,7 +44,7 @@ typedef ResultType result_type;
 #include <functional>
 #include <cassert>
 // #include "../../include/shader/gl.h"
-// #include "../../include/shader/egl.h"
+#include "../../include/shader/egl.h"
 // #include "../../include/shader/intrins.h"
 #include "../../lib/lib_webgpu.h"
 #include <emscripten/html5.h>
