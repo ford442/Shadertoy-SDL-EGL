a3e1edb117a67af0983be9e13f9aad75cfe0d51f
Noah Cohn <noahc42@gmail.com>
2023-07-13 10:49:15 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1069,8 +1069,10 @@ float f=77.77;
 register __m64 m={f,f};
 
 register __m64 m1=m;
-int f1 = _m_to_int_(m1);
-std::cout << f1 << std::endl;
+__m128 m2=_mm_castpd_ps(m1);
+float ff=_mm_cvtps_pd(m2);
+// int f1 = _m_to_int_(m1);
+std::cout << ff << std::endl;
 
 
 
