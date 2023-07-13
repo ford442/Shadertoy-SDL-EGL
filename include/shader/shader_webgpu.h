cc795c867e9ba08267977488c8482303c3b6d0d7
Noah Cohn <noahc42@gmail.com>
2023-07-13 10:11:10 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1064,8 +1064,9 @@ return nullptr;
 
 void strt(){
 float f=77.77;
-  __m64 m;
-m = _mm_set_ps1(f);
+//  __m64 m;
+// m = _mm_set_ps1(f);
+register __m64 m={a,a};
 
 register __m64 m1=m;
 int f1 = _m_to_int_(m1);
