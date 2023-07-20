78f7fd01ab81a8fad6801fc83ec06578fc27f330
Noah Cohn <noahc42@gmail.com>
2023-07-19 18:57:00 -0700

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -24,7 +24,7 @@ void ObtainedWebGpuDeviceStart(WGpuDevice result, void *userData){
 device=result;
 queue=wgpu_device_get_queue(device);
 canvasContext=wgpu_canvas_get_webgpu_context("canvas");
-WGpuCanvasConfiguration config={device,navigator_gpu_get_preferred_canvas_format(),WGPU_TEXTURE_USAGE_RENDER_ATTACHMENT};
+WGpuCanvasConfiguration config={device,23,WGPU_TEXTURE_USAGE_RENDER_ATTACHMENT};
 
 wgpu_canvas_context_configure(canvasContext,&config);
 const char *vertexShader=
