f6347cbc267db3172c05e726a841ecedbb8cf89b
Noah Cohn <noahc42@gmail.com>
2023-07-20 01:03:36 -0700

    Update gl.h

--- include/vanilla/gl.h
+++ include/vanilla/gl.h
@@ -16,3 +16,16 @@
 
 // #define GLFW_INCLUDE_GLEXT 1
 // #include <GLFW/glfw3.h>
+#include <webgl/webgl2.h>  //  much better performance without
+
+#define GL_GLES_PROTOTYPES 1
+#include <GLES3/gl3platform.h>
+#include <GLES3/gl3.h>
+// #include <GLES3/gl31.h>
+// #include <GLES3/gl32.h>
+#define EGL_EGL_PROTOTYPES 1
+#define EGL_EGLEXT_PROTOTYPES 1
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+#include <KHR/khrplatform.h>
