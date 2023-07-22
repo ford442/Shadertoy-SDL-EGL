cc8cdf3f53a4c39e4d85bfcc75092ef949cec157
Noah Cohn <noahc42@gmail.com>
2023-07-21 20:44:43 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -60,9 +60,8 @@ using floaT=std::experimental::native_simd<float>;
 #include <emscripten.h>
 
 #include "/content/RAMDRIVE2/tbb/include/tbb/tbb.h"
-#include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/tick_count.h"
+// #include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/tick_count.h"
 #include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/task_group.h"
-#include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/global_control.h"
 
 #include "/content/RAMDRIVE2/level-zero/include/ze_api.h"
 #include "/content/RAMDRIVE2/level-zero/include/zet_api.h"
