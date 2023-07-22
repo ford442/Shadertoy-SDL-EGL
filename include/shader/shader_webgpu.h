97fc24eb76a9d31e803959a36cf09f8e87421d6d
Noah Cohn <noahc42@gmail.com>
2023-07-22 02:14:00 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -18,7 +18,7 @@ using floaT=std::experimental::native_simd<float>;
 
 #define register
 
-// #include <SYCL/sycl.hpp>
+#include <SYCL/sycl.hpp>
 
 #include <boost/compute/cl.hpp> 
 #include <boost/compute/core.hpp> 
@@ -62,9 +62,9 @@ using floaT=std::experimental::native_simd<float>;
 // #include "/content/RAMDRIVE2/tbb/include/tbb/tbb.h"
 // #include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/tick_count.h"
 // #include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/tbb_allocator.h"
-#include "/content/RAMDRIVE2/tbb/include/tbb/task.h"
-#include "/content/RAMDRIVE2/tbb/include/tbb/detail/_task.h"
-#include "/content/RAMDRIVE2/tbb/include/tbb/task_group.h"
+#include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/task.h"
+#include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/detail/_task.h"
+#include "/content/RAMDRIVE2/tbb/include/oneapi/tbb/task_group.h"
 // #include "/content/RAMDRIVE2/tbb/include/tbb/task_scheduler_observer.h"
 
 #include "/content/RAMDRIVE2/level-zero/include/ze_api.h"
