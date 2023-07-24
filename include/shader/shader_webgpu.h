4a0eee558e7fc7fcb943ebdcc9b7013ddab01279
Noah Cohn <noahc42@gmail.com>
2023-07-24 02:18:30 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -15,7 +15,6 @@ typedef ResultType result_type;
 using floaT=std::experimental::native_simd<float>;
 
 #include <boost/function.hpp>
-#include <boost/context/detail/invoke.hpp>
 #include <boost/context/detail/tuple.hpp>
 #include <boost/context/fiber.hpp>
 
