e14d682556ba752b4cd244958ad45cb18dc2134d
Noah Cohn <noahc42@gmail.com>
2023-07-25 00:07:50 -0700

    Create boost_includes.hpp

---
+++ include/shader/boost_includes.hpp
@@ -0,0 +1,32 @@
+// using unary_function = std::unary_function;
+
+template<class ArgumentType,class ResultType>
+
+struct unary_function{
+typedef ArgumentType argument_type;
+typedef ResultType result_type;
+};
+
+// using floaT=std::experimental::native_simd<float>;
+
+#include <boost/function.hpp>
+#include <boost/context/detail/tuple.hpp>
+
+#include <boost/lockfree/spsc_queue.hpp>
+#include <boost/atomic.hpp>
+#include <boost/context/fiber.hpp>
+
+#include <boost/compute/cl.hpp> 
+#include <boost/compute/core.hpp> 
+
+// #include <boost/compute/algorithm.hpp>
+
+#include <boost/compute/interop/opengl.hpp>
+
+#include <boost/integer.hpp>
+#include <boost/atomic.hpp>
+#include <boost/numeric/ublas/tensor.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/chrono.hpp>
