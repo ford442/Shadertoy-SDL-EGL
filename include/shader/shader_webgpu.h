3d6b23305ea2693c843994a02d189dbaf3238956
Noah Cohn <noahc42@gmail.com>
2023-07-24 23:52:10 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1124,12 +1124,12 @@ tbb::task_arena m_arena(1,1,tbb::task_arena::priority::high);
  //     m_arena = std::make_unique<tbb::task_arena>(int(tbb::task_arena::automatic), 0);
 
 //  m_arena.initialize();
-  /*
+
 m_arena.enqueue([&]() {
     // Do some work.
 std::cout << "Hello world!" << std::endl;
 });
-*/
+
 emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
 emscripten_set_mousedown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
 emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_mv);
