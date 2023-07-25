96e0583fe006bf24eac4d92cfc66fc5ae65347d5
Noah Cohn <noahc42@gmail.com>
2023-07-24 23:48:37 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1123,7 +1123,7 @@ void strt(){
 tbb::task_arena m_arena(1,1,tbb::task_arena::priority::high);
  //     m_arena = std::make_unique<tbb::task_arena>(int(tbb::task_arena::automatic), 0);
 
-  m_arena.initialize();
+//  m_arena.initialize();
   /*
 m_arena.enqueue([&]() {
     // Do some work.
