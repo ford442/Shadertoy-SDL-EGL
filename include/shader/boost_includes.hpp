d2cb21f8b2fcb9e928c5dd29184f7dd7efe50d6a
Noah Cohn <noahc42@gmail.com>
2023-07-25 00:22:49 -0700

    Update boost_includes.hpp

--- include/shader/boost_includes.hpp
+++ include/shader/boost_includes.hpp
@@ -2,13 +2,4 @@
 
 // using floaT=std::experimental::native_simd<float>;
 
-#include <boost/context/detail/tuple.hpp>
-
-#include <boost/lockfree/spsc_queue.hpp>
-#include <boost/atomic.hpp>
-#include <boost/context/fiber.hpp>
-
-#include <boost/atomic.hpp>
-
-#include <boost/tuple/tuple.hpp>
 
