07b17e41d29406ebcad71e17b2a8ec47d0091530
Noah Cohn <noahc42@gmail.com>
2023-08-03 03:07:10 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -800,7 +800,7 @@ GPU gpu;
 
 public:
 
-static auto PRGin=std::function<void(register boost::uint_t<64>::exact)>;
+static auto PRGin=std::function<void(register boost::uint_t<64>::exact)>{};
 PRGin=[](register boost::uint_t<64>::exact m1){
 // static void PRGin(register boost::uint_t<64>::exact m1){
 sse4.at(0,0)=wasm_i64x2_splat(m1);
