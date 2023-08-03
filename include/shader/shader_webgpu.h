901c2cc3f661ee9d147ed8e36fe3c30a787d4912
Noah Cohn <noahc42@gmail.com>
2023-08-03 03:02:22 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -800,12 +800,12 @@ GPU gpu;
 
 public:
 
-static std::function<void(register boost::uint_t<64>::exact)>PRGin=[](register boost::uint_t<64>::exact m1){
+const static std::function<void(register boost::uint_t<64>::exact)>PRGin=[](register boost::uint_t<64>::exact m1){
 // static void PRGin(register boost::uint_t<64>::exact m1){
 sse4.at(0,0)=wasm_i64x2_splat(m1);
 S1.at(0,0,0)=wasm_i64x2_extract_lane(sse4.at(0,0),0);
 return;
-}
+};
 
 static void u_iTime_set(register boost::compute::double_ m80){
 // d_time.at(0,0)=set;
