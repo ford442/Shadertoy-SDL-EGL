7fc87552af2cde7c7cf24e75d0040fa998cc05e7
Noah Cohn <noahc42@gmail.com>
2023-08-06 04:42:49 -0700

    Update resurection_jebus_webgpu.cpp

--- resurection_jebus_webgpu.cpp
+++ resurection_jebus_webgpu.cpp
@@ -324,7 +324,7 @@ if(WGPU_BufferStatus.at(0,0,0)==3){
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 }
 // wgpu_buffer_map_async(WGPU_Buffers.at(1,0,1),WGPU_MapCallback.at(0,0,1),&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
-// usleep(15);
+usleep(10);
 // WGPU_Run();
 return;
 };
