5bf3312d63216c2b398fc4f56a270e2a36b872ce
Noah Cohn <noahc42@gmail.com>
2023-08-05 21:40:19 -0700

    Update resurection_jebus_webgpu.cpp

--- resurection_jebus_webgpu.cpp
+++ resurection_jebus_webgpu.cpp
@@ -294,9 +294,9 @@ if(WGPU_BufferStatus.at(0,0,0)==3){
 double_int53_t WGPU_Range_PointerB=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
 WGPU_BufferRange.at(0,0,1)=WGPU_Range_PointerB;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2), WGPU_BufferRange.at(0,0,1) ,0,WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
-EM_ASM({
-document.getElementById('outText').innerHTML=$0;
-},WGPU_ResultBuffer.at(0,0,0)[0]);
+// EM_ASM({
+// document.getElementById('outText').innerHTML=$0;
+// },WGPU_ResultBuffer.at(0,0,0)[0]);
 // std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
 }
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
@@ -315,9 +315,9 @@ if(WGPU_BufferStatus.at(0,0,0)==3){
 double_int53_t WGPU_Range_PointerC=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
 WGPU_BufferRange.at(0,0,0)=WGPU_Range_PointerC;
 wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2),WGPU_BufferRange.at(0,0,0),0,WGPU_ResultBuffer.at(0,0,0),OutputBufferBytes);
-EM_ASM({
-document.getElementById('outText').innerHTML=$0;
-},WGPU_ResultBuffer.at(0,0,0)[0]);
+// EM_ASM({
+// document.getElementById('outText').innerHTML=$0;
+// },WGPU_ResultBuffer.at(0,0,0)[0]);
 }
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
 if(WGPU_BufferStatus.at(0,0,0)==3){
