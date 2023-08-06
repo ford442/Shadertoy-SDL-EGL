d13609ef3236a97571022fdc8e2bc372bb8d7e3c
Noah Cohn <noahc42@gmail.com>
2023-08-06 04:11:24 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -1,6 +1,6 @@
 #include "../../include/vanilla/main_webgpu.h"
-#include <openvino/runtime/runtime.hpp>
-#include <openvino/runtime/remote_context.hpp>
+#include "include/vanilla/openvino/runtime/runtime.hpp"
+#include "include/vanilla/openvino/runtime/remote_context.hpp"
 
 
 // namespace compute = boost::compute;
