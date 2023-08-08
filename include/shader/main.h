630e33165576e2e720a28e85b1725c39ccee6e52
Noah Cohn <noahc42@gmail.com>
2023-08-08 05:03:04 -0700

    Update main.h

--- include/shader/main.h
+++ include/shader/main.h
@@ -1,11 +1,12 @@
 #pragma once
 // #pragma pack(4)
-#pragma fenv_access(on)        // enable environment sensitivity
+// #pragma fenv_access(on)        // enable environment sensitivity
 #include <boost/cstdfloat.hpp>  // must be first include
 
 #define _XOPEN_SOURCE 700
-// #undef _FLT_EVAL_METHOD
-// #define _FLT_EVAL_METHOD -1
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD -1
+/*
 #pragma STDC CX_LIMITED_RANGE ON
 #pragma STDC FP_CONTRACT ON
 #define _XOPEN_REALTIME 1
@@ -17,7 +18,7 @@
 #undef _FLT_ROUNDS
 #define _FLT_ROUNDS 1
 #define _POSIX_REGEXP	1
-
+*/
 
 #include "../../include/shader/intrins.h"
 #include "boost/tuple/tuple.hpp"
