901e95fe9650d6abd9c015bc9b3d2cbb18dfd0b9
Noah Cohn <noahc42@gmail.com>
2023-08-08 05:03:38 -0700

    Update defs.h

--- include/shader/defs.h
+++ include/shader/defs.h
@@ -8,19 +8,21 @@
 #endif
 */
 #define _XOPEN_SOURCE 700
-// #undef _FLT_EVAL_METHOD
-// #define _FLT_EVAL_METHOD -1
-#pragma STDC FP_CONTRACT ON
+#undef _FLT_EVAL_METHOD
+#define _FLT_EVAL_METHOD -1
+/*
 #pragma STDC CX_LIMITED_RANGE ON
+#pragma STDC FP_CONTRACT ON
 #define _XOPEN_REALTIME 1
 #define _POSIX_ASYNC_IO 1
 #define _POSIX_PRIO_IO 1
 #define _POSIX_SYNC_IO 1
-#define _XOPEN_SHM 1
+#define	_XOPEN_SHM 1
 #define _POSIX_PRIORITIZED_IO 1
 #undef _FLT_ROUNDS
 #define _FLT_ROUNDS 1
-#define _POSIX_REGEXP 1
+#define _POSIX_REGEXP	1
+*/
 
 // #pragma pack(4)
 // #pragma fenv_access(on)             // enable environment sensitivity
