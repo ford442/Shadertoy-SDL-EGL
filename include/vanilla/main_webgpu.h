e00045c8fc206a6b105a4e4e0ecdef0d84995da7
Noah Cohn <noahc42@gmail.com>
2023-08-08 04:52:39 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -66,14 +66,12 @@ typedef ResultType result_type;
 
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 
-#define CL_HPP_TARGET_OPENCL_VERSION 210
-#define CL_HPP_MINIMUM_OPENCL_VERSION 200
+#define CL_HPP_TARGET_OPENCL_VERSION 300
+#define CL_HPP_MINIMUM_OPENCL_VERSION 300
 
 #include <CL/opencl.hpp>
-#include <CL/cl_platform.h>
-#include <CL/cl_version.h>
-// #include "../../include/CL/opencl.hpp"
-// #include "../../include/CL/cl_platforms.h"
+// #include <CL/cl.h>
+#include "../../include/CL/opencl.h"
 
 #include <CL/cl.h>
 
