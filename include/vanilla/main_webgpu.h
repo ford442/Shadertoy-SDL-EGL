60fc5dd4f80538d4100cc81185032ec9dae408e7
Noah Cohn <noahc42@gmail.com>
2023-08-07 17:47:10 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -66,7 +66,7 @@ typedef ResultType result_type;
     #define CL_HPP_TARGET_OPENCL_VERSION 200
 
 #include <CL/opencl.hpp>
-#include <CL/cl.h>
+// #include <CL/cl.h>
 
 #include <boost/compute/cl.hpp> 
 #include <boost/compute/core.hpp>
