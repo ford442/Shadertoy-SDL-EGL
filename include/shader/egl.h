58e2f446b113cb6744c7bfaa7faf3c60940c3c95
Noah Cohn <noahc42@gmail.com>
2023-08-09 06:51:35 -0700

    Update egl.h

--- include/shader/egl.h
+++ include/shader/egl.h
@@ -7,15 +7,15 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
-#include <GLES3/gl3platform.h>
+// #include <GLES3/gl3platform.h>
 #include <GLES3/gl3.h>
-// #include <GLES3/gl31.h>
-// #include <GLES3/gl32.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
 #define EGL_EGL_PROTOTYPES 1
 #define EGL_EGLEXT_PROTOTYPES 1
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
-#include <KHR/khrplatform.h>
+// #include <KHR/khrplatform.h>
 
-// #include <emscripten/html5_webgl.h>
+#include <emscripten/html5_webgl.h>
