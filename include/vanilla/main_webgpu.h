3740292f465019bafef4eed5ef9a79eb353d655a
Noah Cohn <noahc42@gmail.com>
2023-08-09 05:18:26 -0700

    Update main_webgpu.h

--- include/vanilla/main_webgpu.h
+++ include/vanilla/main_webgpu.h
@@ -33,10 +33,10 @@
 
 #include <thrust/device_vector.h>
 #include <thrust/transform.h>
-#include <thrust/sequence.h>
-#include <thrust/copy.h>
-#include <thrust/fill.h>
-#include <thrust/replace.h>
+// #include <thrust/sequence.h>
+// #include <thrust/copy.h>
+// #include <thrust/fill.h>
+// #include <thrust/replace.h>
 #include <thrust/functional.h>
 
 // #include "../../include/vanilla/openvino/runtime/runtime.hpp"
