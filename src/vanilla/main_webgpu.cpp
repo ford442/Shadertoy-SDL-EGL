9d0ba30c12b351cd4d83f979af3199d7322cd843
Noah Cohn <noahc42@gmail.com>
2023-08-09 06:01:17 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -1,13 +1,7 @@
 #include "../../include/vanilla/main_webgpu.h"
 
 int cltest(){
-  // generate random data serially
-  thrust::host_vector<int> h_vec(100);
-  std::generate(h_vec.begin(), h_vec.end(), rand);
-  // transfer to device and compute sum
-thrust::device_vector<int> d_vec = h_vec;
-// int x = thrust::reduce(d_vec.begin(), d_vec.end(), 0, thrust::plus<int>());
-  return 0;
+
 }
 
 extern"C"{
