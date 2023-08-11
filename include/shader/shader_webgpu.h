0f9915b8ef76ae16e877ccc0405f4b8e75c53788
Noah Cohn <noahc42@gmail.com>
2023-08-10 20:44:51 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1581,7 +1581,7 @@ emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms
 emscripten_set_mouseup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,(EM_BOOL)0,ms_clk);
 // glBindVertexArray(0);
 glEnableVertexAttribArray(0);
-emscripten_set_main_loop((void(*)())Run::Rend,0,0);
+emscripten_set_main_loop((EM_BOOL(*)())Run::Rend,0,0);
 return 1;
 };
 
