dd2345568dabd0fcd12782b9784c3a9b00ff986d
Noah Cohn <noahc42@gmail.com>
2023-08-12 11:31:47 -0700

    Update gl.h

--- include/shader/gl.h
+++ include/shader/gl.h
@@ -1,32 +1,10 @@
 #pragma once
 
-#include "../../include/shader/boost_defs.h"
-
-#include <functional>
-
-template<class ArgumentType,class ResultType>
-
-struct unary_function{
-typedef ArgumentType argument_type;
-typedef ResultType result_type;
-};
-
-#define register
-
-#include <boost/compute/command_queue.hpp>
-#include <boost/compute/kernel.hpp>
-#include <boost/compute/program.hpp>
-#include <boost/compute/source.hpp>
-#include <boost/compute/system.hpp>
-#include <boost/compute/interop/opengl.hpp>
-
 // #define GL_SILENCE_DEPRECATION
 
 #ifndef GL_FRAGMENT_PRECISION_HIGH
 #define GL_FRAGMENT_PRECISION_HIGH 1
 #endif
-
-// #include <GL/gl3w.h>
 
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES 1
@@ -34,7 +12,3 @@ typedef ResultType result_type;
 
 #include <GL/gl.h>
 #include <GL/glext.h>
-
-
-// #define GLFW_INCLUDE_GLEXT 1
-// #include <GLFW/glfw3.h>
