13591678abd2d410cbd7f0f265ca3cac26322401
Noah Cohn <noahc42@gmail.com>
2023-08-12 00:32:35 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -791,7 +791,7 @@ EGLSurface surface=nullptr;
 EGLContext ctxegl=nullptr;
 EGLConfig eglconfig=nullptr;
 EGLint config_size=0,major,minor=0;
-static const char * Fnm=reinterpret_cast<const char *>("/shader/shader.glsl");
+const char * Fnm=reinterpret_cast<const char *>("/shader/shader.glsl");
 const char * src[4];
 char * vrt_bdy=vrt_bdy_src;
 char * frg_hdr=frg_hdr_src;
