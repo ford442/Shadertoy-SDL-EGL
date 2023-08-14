adf1b75de3fd965637bd114b4aefe8cbda2cda92
Noah Cohn <noahc42@gmail.com>
2023-08-14 13:19:48 -0700

    Update shader_webgpu.h

--- include/shader/shader_webgpu.h
+++ include/shader/shader_webgpu.h
@@ -1119,7 +1119,8 @@ mms.at(1,0)=0.5*t_size.at(0,0);
 mms.at(1,1)=0.5*t_size.at(0,0);
 eglBindAPI(EGL_OPENGL_API);
 emscripten_webgl_enable_extension(cntxi.at(0,0),"GL_ALL_EXTENSIONS");
-  
+emscripten_webgl_enable_extension(cntxi.at(0,0),"EXT_color_buffer_float");
+   
 // eglBindAPI(EGL_OPENGL_ES_API);
  ///glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_FASTEST);
 glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_NICEST);
