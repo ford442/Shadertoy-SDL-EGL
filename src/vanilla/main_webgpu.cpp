15c1ca48d9245268e6e53b51b108d5166287e98b
Noah Cohn <noahc42@gmail.com>
2023-08-14 06:53:50 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -8,7 +8,7 @@ extern"C"{
 
 void testtr(){
       // Create an OpenVINO Runtime object.
-InferenceEngine::Core::InferenceEngineRuntime runtime;
+  InferenceEngine::InferenceEngine engine;
     
     /*
     unsigned char* img_data = NULL;
