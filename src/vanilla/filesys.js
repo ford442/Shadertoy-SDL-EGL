fffe360de600a26da1126d343feb2c470b991766
Noah Cohn <noahc42@gmail.com>
2023-08-16 15:00:38 -0700

    Create filesys.js

---
+++ src/vanilla/filesys.js
@@ -0,0 +1,12 @@
+let pth="https://wasm.noahcohn.com/b3hd/vae_decoder.onnx";
+const ff=new XMLHttpRequest();
+ff.open('GET',pth,true);
+ff.responseType='arraybuffer';
+ff.onload=function(oEvent){
+const sarrayBuffer=ff.response;
+if(sarrayBuffer){
+const sfil=new Uint8ClampedArray(sarrayBuffer);
+setTimeout(function(){
+FS.writeFile('/model.onnx',sfil);
+},1150);
+}
