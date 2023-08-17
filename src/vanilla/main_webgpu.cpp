e8d1594d16d82fa927cbe12e36e47363db78c921
Noah Cohn <noahc42@gmail.com>
2023-08-16 17:08:21 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -520,19 +520,6 @@ return;
 };
 
 EM_JS(void,js_main,(),{
-FS.mkdir('/');
-var pth='https://wasm.noahcohn.com/b3hd/vae_decoder.onnx';
-const ff=new XMLHttpRequest();
-ff.open('GET',pth,true);
-ff.responseType='arraybuffer';
-ff.onload=function(oEvent){
-const sarrayBuffer=ff.response;
-if(sarrayBuffer){
-const sfil=new Uint8ClampedArray(sarrayBuffer);
-setTimeout(function(){
-FS.writeFile('/model.onnx',sfil);
-},1150);
-}};
 
 function strr(){
 Module.ccall("runWebGPU");
