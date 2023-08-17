9b82488da1affaaf26cd8096cafa677787ff8a69
Noah Cohn <noahc42@gmail.com>
2023-08-17 05:17:29 -0700

    Update icc.cpp

--- src/vanilla/icc.cpp
+++ src/vanilla/icc.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+
 void testicc(){
 std::cout << "Running ICPX compiled object." << std::endl;
 }
+
 int main(){
 // testicc();
 return 1;
+}
+
+extern "C"{
+
+void icctest(){
+testicc();
+}
+
 }
