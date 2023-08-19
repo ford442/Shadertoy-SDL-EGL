05c21ed018e0aee13fd7353edd35bbf60e40fd25
Noah Cohn <noahc42@gmail.com>
2023-08-19 12:43:57 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -877,8 +877,8 @@ static EM_BOOL i_iSize_set(int set){
 sse3.at(0,0)=wasm_i32x4_splat(set);
 i_size.at(0,0)=wasm_i32x4_extract_lane(sse3.at(0,0),0);
 i_size.at(0,1)=wasm_i32x4_extract_lane(sse3.at(0,0),0);
-i_size.at(1,0)=wasm_i32x4_extract_lane(sse3.at(0,0),0)*1.15;
-i_size.at(1,1)=wasm_i32x4_extract_lane(sse3.at(0,0),0)*1.15;
+i_size.at(1,0)=wasm_i32x4_extract_lane(sse3.at(0,0),0)*0.85;
+i_size.at(1,1)=wasm_i32x4_extract_lane(sse3.at(0,0),0)*0.85;
 return EM_TRUE;
 }
 
