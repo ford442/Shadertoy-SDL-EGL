622e2525faefc6730d586dd79e1b0675e83cd247
Noah Cohn <noahc42@gmail.com>
2023-08-20 11:09:35 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -121,6 +121,8 @@ boost::function<const EM_BOOL(boost::uint_t<24>::fast)>VCOin;
 
 boost::function<const EM_BOOL(boost::uint_t<24>::fast)>VBOin;
 
+static boost::function<const EM_BOOL()>setFloats;
+
 static inline boost::function<const register float()>gF;
 
 static inline boost::function<const register float()>gF5;
@@ -705,8 +707,6 @@ class GPU{
 private:
 
 public:
-
-static boost::function<EM_BOOL()>setFloats;
 
 boost::function<const EM_BOOL(boost::uint_t<24>::fast)>EBOin=[](boost::uint_t<24>::fast EBO){
 Sh.at(1,0)=EBO;
