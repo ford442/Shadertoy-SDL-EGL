53ca47c4dd7e505889d34aa6cf9017c8b09c35be
Noah Cohn <noahc42@gmail.com>
2023-08-22 13:12:28 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -45,7 +45,8 @@ typedef ResultType result_type;
 #include <boost/regex.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/thread/thread.hpp>
-#include <boost/fiber/fiber.hpp>
+#include <boost/fiber/all.hpp>
+#include <thread>
 
 #define BOOST_HAS_TR1
 
