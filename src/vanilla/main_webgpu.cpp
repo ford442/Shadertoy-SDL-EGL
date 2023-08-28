4717640a58dd45308619bd99754f6e99f1f24f08
Noah Cohn <noahc42@gmail.com>
2023-08-28 04:07:11 -0700

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -14,7 +14,7 @@ std::copy(infos[i].begin(),infos[i].end(),char_array+i*infos[0].size());
 std::cout << char_array << std::endl;
 Ort::Env ort_env;
 const char model_path[12]="/model.onnx";
-const int64_t batchSize=2;
+const int64_t batchSize=1;
 Ort::SessionOptions sessionOptions;
 sessionOptions.SetIntraOpNumThreads(1);
 	
