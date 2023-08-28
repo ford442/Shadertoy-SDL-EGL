3f29ebca2560f284c2af4d4feb33afe5ad300229
Noah Cohn <noahc42@gmail.com>
2023-08-28 10:08:16 -0700

    Update filesys.js

--- src/vanilla/filesys.js
+++ src/vanilla/filesys.js
@@ -4,6 +4,11 @@ console.log("Getting ONNX model...");
 const ff=new XMLHttpRequest();
 ff.open('GET',pth,true);
 ff.responseType='arraybuffer';
+ff.addEventListener("progress",function(event){
+if(event.lengthComputable){
+var complete=(event.loaded/event.total*100|0);
+document.getElementById('outText').innerHTML=complete;
+};};);
 ff.onload=function(oEvent){
 const sarrayBuffer=ff.response;
 if(sarrayBuffer){
@@ -12,11 +17,6 @@ setTimeout(function(){
 console.log("Got ONNX model.");
 FS.writeFile('/model.onnx',sfil);
 },1150);
-};};
-ff.addEventListener("progress",function(event){
-if(event.lengthComputable){
-var complete=(event.loaded/event.total*100|0);
-document.getElementById('outText').innerHTML=complete;
 };};);
 ff.send(null);
 };
