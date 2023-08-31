13469514bf116b79eb46e52a324fee2f479c1157
Noah Cohn <noahc42@gmail.com>
2023-08-31 00:31:53 -0700

    Update defs.hpp

--- include/shader/defs.hpp
+++ include/shader/defs.hpp
@@ -10,7 +10,7 @@
 #define _XOPEN_SOURCE 700
 #undef _FLT_EVAL_METHOD
 #define _FLT_EVAL_METHOD -1
-/*
+
 #pragma STDC CX_LIMITED_RANGE ON
 #pragma STDC FP_CONTRACT ON
 #define _XOPEN_REALTIME 1
@@ -22,7 +22,7 @@
 #undef _FLT_ROUNDS
 #define _FLT_ROUNDS 1
 #define _POSIX_REGEXP	1
-*/
+
 
 // #pragma pack(4)
 // #pragma fenv_access(on)             // enable environment sensitivity
