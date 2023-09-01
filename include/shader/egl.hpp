4b3fe17d3b219b68eb48a78fff762347574ec2da
Noah Cohn <noahc42@gmail.com>
2023-08-31 23:27:16 -0700

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -12,7 +12,7 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
-// #include <GLES3/gl3platform.h>
+#include <GLES3/gl3platform.h>
 #include <GLES3/gl3.h>
 #include <GLES3/gl31.h>
 #include <GLES3/gl32.h>
@@ -21,7 +21,7 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
-// #include <KHR/khrplatform.h>
+#include <KHR/khrplatform.h>
 
 // #include <emscripten/html5_webgl.h>
 
