992702086c23c6c4cc0954dad4eda372015a3184
Noah Cohn <noahc42@gmail.com>
2023-09-03 20:28:03 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -1325,7 +1325,7 @@ eglChooseConfig(display,att_lst,&eglconfig,1,&config_size);
 eglInitialize(display,&major,&minor);
 ctxegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,ctx_att);
 cntx.at(0,0)=ctxegl;
-eglSwapBuffers(display,surface)
+eglSwapBuffers(display,surface);
 // eglMakeCurrent(display,surface,surface,cntx.at(0,0));
 emscripten_webgl_make_context_current(cntxi.at(0,0));
 glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE);
