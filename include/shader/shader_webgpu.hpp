c19e05287223c1148ea03e53393280e3e2b45c41
Noah Cohn <noahc42@gmail.com>
2023-09-04 09:14:16 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -48,7 +48,8 @@ typedef ResultType result_type;
 #include <boost/system/error_code.hpp>
 #include <boost/thread/thread.hpp>
 
-#include <boost/context/fiber_fcontext.hpp>
+// #include <boost/context/fiber_fcontext.hpp>
+#include <boost/context/fiber.hpp>
 #include <boost/fiber/all.hpp>
 #include <thread>
 
