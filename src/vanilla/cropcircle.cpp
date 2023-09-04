2053e65cb56fec121479daaadbd4a2338ca3fd27
Noah Cohn <noahc42@gmail.com>
2023-09-04 10:59:05 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -1,91 +1,73 @@
 #include "../../include/vanilla/cropcircle.hpp"
 
 EM_JS(void, ma, (), {
-  var dis = set();
+  let dis = set();
   if (dis) dis();dis = set();
-
+  var $, $r, z, w, R, h, ww, o, l, r, m, rotm, rotmb, rottm, kna, knab, knb, knbb, knc, kncb, knd, kndb, rott, rottb, rottc;
   function set() {
-    var i;
-    var c = document.getElementById("masque");
-    var cb = document.getElementById("orig");
-    var cc = document.getElementById("temp");
+    ww = document.getElementById("iwid").innerHTML;
+    h = document.getElementById("ihig").innerHTML;
+    ow = document.getElementById("wid").innerHTML;
+    oh = document.getElementById("hig").innerHTML;
+    let cnP = document.getElementById("cp");
+    let flP = document.getElementById("flip");
+    let vd = document.getElementById("myvideo");
+    var c = document.getElementById("imag");
     var ctx = c.getContext("2d");
-    var ctxb = cb.getContext("2d");
-    var ctxc = cc.getContext("2d");
-    var cnP = document.getElementById("cp");
-    var flP = document.getElementById("flip");
-    var thr = document.getElementById("thr").innerHTML;
-    var ra = document.getElementById("pckr").innerHTML;
-    var ga = document.getElementById("pckg").innerHTML;
-    var ba = document.getElementById("pckb").innerHTML;
-    var rb = document.getElementById("pckbr").innerHTML;
-    var gb = document.getElementById("pckbg").innerHTML;
-    var bb = document.getElementById("pckbb").innerHTML;
-    var alf = document.getElementById("alf").innerHTML;
-    var alfb = document.getElementById("alfb").innerHTML;
-    var ww = document.getElementById("iwid").innerHTML;
-    var h = document.getElementById("ihig").innerHTML;
-    var vd = document.getElementById("myvideo");
-  
-    ctxc.drawImage(vd, 0, 0, ww, h);
-    var imgData = ctxc.getImageData(0, 0, ww, h);
-    ctxb.drawImage(vd, 0, 0, ww, h);
-    var rgbdat = new ImageData(ww, h);
+    ctx.drawImage(vd, 0, 0, ww, h);
+    var imgData = ctx.getImageData(0, 0, ww, h);
+    var rgbdat = ctx.createImageData(ww, h);
     var rgbd = rgbdat.data;
     var imgg = imgData.data;
+    var i;
     for (i = 0; i < (ww * h * 4); i = i + 4) {
       var rgb = (imgg[i] * 0.2126) + (imgg[i + 1] * 0.7152) + (imgg[i + 2] * 0.0722);
-      if (rgb > thr) {
-        rgbd[i] = ra;
-        rgbd[i + 1] = ga;
-        rgbd[i + 2] = ba;
-        rgbd[i + 3] = alf;
+      if (rgb > 128) {
+        rgbd[i] = 0;
+        rgbd[i + 1] = 255;
+        rgbd[i + 2] = 0;
+        rgbd[i + 3] = 255;
       } else {
-        rgbd[i] = rb;
-        rgbd[i + 1] = gb;
-        rgbd[i + 2] = bb;
-        rgbd[i + 3] = alfb;
+        rgbd[i] = 255;
+        rgbd[i + 1] = 255;
+        rgbd[i + 2] = 255;
+        rgbd[i + 3] = 255;
       }
     }
-    ctx.putImageData(rgbdat, 0, 0);
-
+        ctx.putImageData(rgbdat, 0, 0);
     function Ra() {
       flP.setAttribute("style", "transform: scaleX(1);");
       cnP.setAttribute("style", "transform: scaleY(1);");
     }
-
     function Rb() {
       flP.setAttribute("style", "transform: scaleX(-1);");
       cnP.setAttribute("style", "transform: scaleY(-1);");
     }
-    var knb = document.getElementById("rra");
-    var kna = document.getElementById("mainr");
-    var knc = document.getElementById("rrb");
-    var knd = document.getElementById("rrc");
-    var knbb = document.getElementById("rrab");
-    var kncb = document.getElementById("rrbb");
-    var kndb = document.getElementById("rrcb");
-    var rott = 0;
-    var rottb = 0;
-    var rottc = 0;
-    var dsd = false;
-
+    knb = document.getElementById("rra");
+    kna = document.getElementById("mainr");
+    knc = document.getElementById("rrb");
+    knd = document.getElementById("rrc");
+    knbb = document.getElementById("rrab");
+    kncb = document.getElementById("rrbb");
+    kndb = document.getElementById("rrcb");
+    rate = (kna.innerHTML);
+    rott = 0;
+    rottb = 0;
+    rottc = 0;
+    let dur = document.getElementById("temptime").innerHTML / 10;
+    let dsd = false;
     function $rn() {
       if (dsd) {
         return;
-      };
-      var rate = (kna.innerHTML);
-
-      function rrra(rta) {
-        cnP.setAttribute("style", "transform:rotate(" + rta + "deg);");
       }
-
+      function rrra(rta) {
+        cnP.setAttribute("style", "transform: rotate(" + rta + "deg);");
+      }
       function rrrb(rtb) {
         cnP.setAttribute("style", "transform:rotate(" + rtb + "deg);");
       }
-
       function rrrc(rtc) {
-        cnP.setAttribute("style", "transform:rotate(" + rtc + "deg);");
+        cnP.setAttribute("style", "transform: rotate(" + rtc + "deg);");
       }
       if ((rott - knd.innerHTML) < 0) {
         rott = (rott + 360 - knd.innerHTML);
@@ -125,7 +107,6 @@ EM_JS(void, ma, (), {
     };
   }
 });
-
 int main() {
   ma();
   return 1;
