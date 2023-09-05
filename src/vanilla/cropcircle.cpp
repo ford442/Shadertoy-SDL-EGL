0c5200fac2e8ec8fa21c0f86b1c4f37fef875af0
Noah Cohn <noahc42@gmail.com>
2023-09-05 16:44:14 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -160,7 +160,7 @@ rgbd[i + 2] = 0;
 rgbd[i + 3] =255- (rgb-128)*16;
 }
 }
- Module.HEAPF32.set(rgbdat);
+ Module.HEAPF32.set(rgbdat.data);
 
 // ctx.putImageData(new Float32Array($H,pointb,la), 0, 0);
 ctx.putImageData(rgbdat, 0, 0);
