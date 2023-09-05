6048d1eb6e63941886d7b522cb443b77362be86c
Noah Cohn <noahc42@gmail.com>
2023-09-04 20:26:26 -0700

    Update cropcircle.hpp

--- include/vanilla/cropcircle.hpp
+++ include/vanilla/cropcircle.hpp
@@ -1,2 +1,19 @@
 #include <emscripten.h>
 #include <webgl/webgl2.h>
+#include <functional>
+
+template<class ArgumentType,class ResultType>
+
+struct unary_function{
+typedef ArgumentType argument_type;
+typedef ResultType result_type;
+};
+
+#include <boost/function.hpp>
+
+float max;
+float min;
+float sum;
+float avgSum;
+float minSum;
+float maxSum;
