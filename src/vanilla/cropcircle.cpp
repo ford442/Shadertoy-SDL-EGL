ab308c0dc4cd0a2a4a5b06e68ec6b0d099e37b04
Noah Cohn <noahc42@gmail.com>
2023-09-04 20:58:34 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -76,9 +76,6 @@ antialias:false
 };
 
 const ctx = scanvas.getContext('2d',contxVars);
-// ctx.getExtension('GL_ALL_EXTENSIONS');
-ctx.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.NICEST);
-ctx.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
 const gpu = new GPUX({mode:'gpu', canvas:scanvas, webGl:ctx });
 let dis = set();
 if (dis) dis();dis = set();
