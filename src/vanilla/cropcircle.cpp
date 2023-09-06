de0c011e2768a31c0fa5bb526f4d26ffe859ab6f
Noah Cohn <noahc42@gmail.com>
2023-09-05 22:50:12 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -214,7 +214,7 @@ Module.ccall("nano",null,["Number","Number","Number"],[la,pointa,pointc]);
 var agav=new Float32Array($H,pointc,1);
 console.log(agav[0]);
 for(i=0;i<(ww*h*4);i=i+4){
-var rgb=(imgg[i]*0.2126/agav[0])+(imgg[i+1]*0.7152/agav[0])+(imgg[i+2]*0.0722/agav[0]);
+var rgb=(imgg[i]*0.2126*(agav[0])/0.5))+(imgg[i+1]*0.7152*(agav[0])/0.5)+(imgg[i+2]*0.0722*(agav[0])/0.5));
 var rgbm=(128-rgb)/2;
 if(rgb>126){
 if(rgb>209){    // orange
