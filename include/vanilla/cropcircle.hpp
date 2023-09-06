5a5586eeeb24cf93e7e2abf0b77204af754f9223
Noah Cohn <noahc42@gmail.com>
2023-09-05 20:09:02 -0700

    Update cropcircle.hpp

--- include/vanilla/cropcircle.hpp
+++ include/vanilla/cropcircle.hpp
@@ -1,6 +1,14 @@
 #include <emscripten.h>
+#include <emscripten/html5.h>
 #include <webgl/webgl2.h>
 #include <functional>
+
+#define GL_FRAGMENT_PRECISION_HIGH 1
+
+#include <GL/gl.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl3.h>
 
 template<class ArgumentType,class ResultType>
 
@@ -17,3 +25,14 @@ float sum;
 float avgSum;
 float minSum;
 float maxSum;
+
+EGLDisplay display;
+EGLContext contextegl;
+EGLSurface surface;
+EGLint config_size,major,minor;
+
+double wi,hi;
+
+int Size;
+GLfloat S;
+
