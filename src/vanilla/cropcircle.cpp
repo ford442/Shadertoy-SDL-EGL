da3f7571763788c418fa0b4ffacc7fda2325b152
Noah Cohn <noahc42@gmail.com>
2023-09-05 23:23:47 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -214,7 +214,7 @@ Module.ccall("nano",null,["Number","Number","Number"],[la,pointa,pointc]);
 var agav=new Float32Array($H,pointc,1);
 console.log(agav[0]);
 for(i=0;i<(ww*h*4);i=i+4){
-var rgb=(imgg[i]*0.2126+(imgg[i+1]*0.7152)+(imgg[i+2]*0.0722);
+var rgb=(imgg[i]*0.2126)+(imgg[i+1]*0.7152)+(imgg[i+2]*0.0722);
 
  //  but run past a lighter pixel if the avg is darker 
  //  i.e.  pixel is 110/255 or 0.431  ->  avg is 77/255 or 0.302
