efe2912a31038f02df71e3e25da061106c8ff4e3
Noah Cohn <noahc42@gmail.com>
2023-09-08 15:53:35 -0700

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -8,9 +8,7 @@
 #define GL_CONTEXT_COMPATIBILITY_PROFILE_BIT 0x00000002
 #define GL_CONTEXT_CORE_PROFILE_BIT 0x00000001
 
-// #define GLFW_INCLUDE_GLEXT 1
-// #define GLFW_INCLUDE_ES3
-#include <GLFW/glfw3.h>
+
 
 #define GL_GLES_PROTOTYPES 1
 #define EGL_EGL_PROTOTYPES 1
@@ -34,7 +32,9 @@
 
 // #include <emscripten/html5_webgl.h>
 
-
+// #define GLFW_INCLUDE_GLEXT 1
+// #define GLFW_INCLUDE_ES3
+#include <GLFW/glfw3.h>
 
 
 //
