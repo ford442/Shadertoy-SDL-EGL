f01b693d741e9c561eccc9c1c94d44345be7c714
Noah Cohn <noahc42@gmail.com>
2023-09-10 01:43:57 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -1347,6 +1347,7 @@ emscripten_webgl_enable_extension(cntxi.at(0,0),"EXT_color_buffer_half_float");
 emscripten_webgl_enable_extension(cntxi.at(0,0),"EGL_ANGLE_d3d_texture_client_buffer");
 emscripten_webgl_enable_extension(cntxi.at(0,0),"EGL_ANGLE_direct3d_display");
 emscripten_webgl_enable_extension(cntxi.at(0,0),"EGL_ANGLE_robust_resource_initialization");
+emscripten_webgl_enable_extension(cntxi.at(0,0),"WEBGL_color_buffer_float");
 
 surface=eglCreateWindowSurface(display,eglconfig,(NativeWindowType)0,att_lst2);
 eglChooseConfig(display,att_lst,&eglconfig,1,&config_size);
