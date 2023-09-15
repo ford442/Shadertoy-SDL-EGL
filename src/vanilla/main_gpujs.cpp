c7ca35c4355991b1815911428177d1a61ec5ed93
Noah Cohn <noahc42@gmail.com>
2023-09-14 21:30:27 -0700

    Update main_gpujs.cpp

--- src/vanilla/main_gpujs.cpp
+++ src/vanilla/main_gpujs.cpp
@@ -1,7 +1,7 @@
 #include "../../include/vanilla/main_gpujs.hpp"
 
 extern "C"{
-  
+
 }
 
 EM_JS(void,js_main,(),{
@@ -30,7 +30,8 @@ scanvas.style.height='100vh';
 scanvas.style.width='100vh';
 scanvas.style.backgroundColor='rgba(7,233,7,255)';
 document.getElementById("contain1").appendChild(scanvas);
-const contxVars={preferLowPowerToHighPerformance:false,logarithmicDepthBuffer:true,colorSpace:'display-p3',alpha:true,depth:true,stencil:true,preserveDrawingBuffer:true,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',antialias:false};
+/*
+  const contxVars={preferLowPowerToHighPerformance:false,logarithmicDepthBuffer:true,colorSpace:'display-p3',alpha:true,depth:true,stencil:true,preserveDrawingBuffer:true,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',antialias:false};
 const context = scanvas.getContext('webgl2',contxVars);
 context.getExtension('GL_ALL_EXTENSIONS');
 context.getExtension('GL_KHR_no_error');
@@ -38,12 +39,113 @@ context.getExtension('GL_REGAL_enable');
 context.getExtension('GL_ARB_spirv_extensions');
 context.getExtension('GL_ARB_ES2_compatibility');
 context.getExtension('GL_ARB_direct_state_access');
-// context.disable(gl.DITHER);
-// context.hint(gl.FRAGMENT_SHADER_DERIVATIVE_HINT,gl.NICEST);
-// context.hint(gl.GENERATE_MIPMAP_HINT,gl.NICEST);
-const gpu=new GPUX({mode:'gpu',webGl:context });
+*/
+var vv=document.getElementById("mv");
+var w$=parseInt(document.getElementById("wid").innerHTML,10);
+var h$=parseInt(document.getElementById("hig").innerHTML,10);
+const $H=Module.HEAPF32.buffer;
+var la=h$*h$*4;
+var pointa=77*la;
+const bcanvas=document.getElementById("zcanvas");
+const gl=bcanvas.getContext("webgl2",{colorType:'float32',preferLowPowerToHighPerformance:false,precision:'highp',logarithmicDepthBuffer:true,colorSpace:'display-p3',alpha:true,depth:false,stencil:true,imageSmoothingEnabled:true,preserveDrawingBuffer:true,premultipliedAlpha:false,desynchronized:false,lowLatency:true,powerPreference:'high-performance',antialias:true,willReadFrequently:true,majorVersion:2,minorVersion:0});
+const g=new GPU({canvas:bcanvas,webGl:gl});
+const t=g.createKernel(function(v){
+const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
+return[P[0],P[1],P[2],P[3]];
+}).setTactic("precision").setPipeline(true).setArgumentTypes(["HTMLVideo"]).setDynamicOutput(true).setOutput([w$,h$]);
+const r=g.createKernel(function(f){
+const p=f[this.thread.y][this.thread.x-this.constants.nblnk-this.constants.blnk];
+this.color(p[0],p[1],p[2],p[3]);
+}).setTactic("precision").setGraphical(true).setArgumentTypes(['HTMLVideo']).setDynamicOutput(true).setOutput([w$,h$]);
+w$=parseInt(document.getElementById("wid").innerHTML,10);
+h$=parseInt(document.getElementById("hig").innerHTML,10);
+vv=document.getElementById("mv");
+var blank$=Math.max((((w$-h$)*1)/2),0);
+var nblank$=Math.max((((h$-w$)*1)/2),0);
+var l=w$*h$*16;
+la=h$*h$*4;
+var al=w$*h$*8;
+pointa=77*la;
+for(var i=0;i<65;i++){
+var j=i+1;
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+};
+var $F=1;
+var $Bu=33;
+r.setConstants({nblnk:nblank$,blnk:blank$});
+t.setConstants({nblnk:nblank$,blnk:blank$});
+var $$1=t(vv);
+for (i=0;i<65;i++){
+var j=i+1;
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);$"+j+".set($$1);");
+};
+var d=S();if(d)d();d=S();function S(){
+w$=parseInt(document.getElementById("wid").innerHTML,10);
+h$=parseInt(document.getElementById("hig").innerHTML,10);
+var blank$=Math.max((((w$-h$)*0)/2),0);
+var nblank$=Math.max((((h$-w$)*0)/2),0);
+l=w$*h$*16;
+la=h$*h$*4;
+al=w$*h$*8;
+pointa=77*la;
+for(var i=0;i<65;i++){
+var j=i+1;
+eval("var point"+j+"="+i+"*la;var $"+j+"=new Float32Array($H,point"+j+",la);");
+};
+pointb=66*la;
+var $B=new Float32Array($H,pointb,sz);
+r.setConstants({nblnk:nblank$,blnk:blank$});
+t.setConstants({nblnk:nblank$,blnk:blank$});
+var T=false;
+function M(){
+vv=document.getElementById("mv");
+t.setConstants({nblnk:nblank$,blnk:blank$});
+r.setConstants({nblnk:nblank$,blnk:blank$});
+if(T){return;}
+for(var i=64;i>0;i--){
+var loca=$F+1;
+if(loca>64){loca=1;}
+var locb=$Bu+1;
+if(locb>64){locb=1;}
+eval("if ($F=="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");}");
+eval("if ($F=="+i+"){var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
+};
+if($F%4==0){
+var $bb=R(vv);
+};
+setTimeout(function(){
+M();
+if(loopLoop==true){
+if(f==true){
+if(vv.currentTime>a){
+vv.currentTime-=0.016;
+}else{
+f=false;
+if(vv.currentTime<b){
+vv.currentTime+=0.016;
+}else{
+f=true;
+}}};
+if(f==false){
+if(vv.currentTime<b){
+vv.currentTime+=0.016;
+}else{
+f=true;
+if(vv.currentTime>a){
+vv.currentTime-=0.016;
+}else{
+f=false;
+}}}};
+},16.66)};
+M();
+document.getElementById("di").onclick=function(){
+T=true;
+S();
+};
+return()=>{
+T=true;
+}};
 }
-  
 document.getElementById('pmhig').innerHTML=parseInt(window.innerHeight,10);
 document.getElementById('ihig').innerHTML=parseInt(window.innerHeight,10);
 document.getElementById('scanvas').height=parseInt(window.innerHeight,10);
@@ -52,11 +154,9 @@ const tem=document.getElementById('tim');
 const ban=document.getElementById('menuBtn');
 const sfr=document.getElementById('slideframe');
 var $lt=tem.innerHTML;
-
 function grab$lt(){
 $lt=Math.round($lt);
 }
-
 $lt=tem.innerHTML;
 grab$lt();
 const slo=new Slideout({'panel':document.getElementById('panel'),'menu':document.getElementById('menu'),'padding':384,'tolerance':70,'easing':'cubic-bezier(.32,2,.55,.27)'});
@@ -74,7 +174,6 @@ slt=slt*100;
 slt=Math.round(slt);
 slt=slt/100;
 tsl.setValues(slt);
-
 document.getElementById('menu').addEventListener('click',function(){
 $ll=tsl.getValue();
 $ll=$ll*100;
@@ -82,17 +181,14 @@ $ll=Math.round($ll);
 $ll=$ll/100;$ll=($ll*1000);
 tem.innerHTML=$ll;
 });
-
 setTimeout(function(){
 slt=tem.innerHTML;
 },8);
 },16)
 ;});
-
 document.getElementById('startBtn').addEventListener('click',function(){
 normalResStart();
 });
-
 setTimeout(function(){
 document.getElementById('circle').width=window.innerWidth;
 document.getElementById('circle').height=window.innerHeight;
@@ -100,7 +196,7 @@ document.getElementById('di').click();
 },500);
 
 });
-  
+
 int main(void){
 js_main();
 return 0;
