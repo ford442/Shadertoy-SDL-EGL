6f994d63e33961c138a686f07e01277031eb88d6
Noah Cohn <noahc42@gmail.com>
2023-09-15 03:17:54 -0700

    Update gpujsx.js

--- js/gpujsx.js
+++ js/gpujsx.js
@@ -17375,6 +17375,7 @@ __MAIN_RESULT__;
 
             initContext() {
                 const settings={alpha:true,depth:true,stencil:true,imageSmoothingEnabled:true,preserveDrawingBuffer:false,premultipliedAlpha:false,desynchronized:false,powerPreference:'high-performance',antialias:true,majorVersion:2,minorVersion:0};
+
                 return this.canvas.getContext('webgl2', settings);
             }
 
