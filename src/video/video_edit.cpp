e74033014d131e9316c61588b6297d03b11782a1
Noah Cohn <noahc42@gmail.com>
2023-09-15 00:33:59 -0700

    Update video_edit.cpp

--- src/video/video_edit.cpp
+++ src/video/video_edit.cpp
@@ -95,8 +95,8 @@ function doKey(e) {
     agav.fill(max,200,33);
     const bcanvas=document.getElementById("bcanvas");
     const gl=bcanvas.getContext("webgl2",{colorType:'float32',preferLowPowerToHighPerformance:false,precision:'highp',logarithmicDepthBuffer:true,colorSpace:'display-p3',alpha:true,depth:false,stencil:true,imageSmoothingEnabled:true,preserveDrawingBuffer:true,premultipliedAlpha:false,desynchronized:false,lowLatency:true,powerPreference:'high-performance',antialias:true,willReadFrequently:true,majorVersion:2,minorVersion:0});
-    const g=new GPU({canvas:bcanvas,webGl:gl});
-    const g2=new GPU();
+    const g=new GPUX({canvas:bcanvas,webGl:gl});
+    const g2=new GPUX();
     const glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 /// const glslAlphe=`float Alphe(float a,float b,float c,float d,float e,float f,float g){return((0.7+(3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-e)*((1.0-g)*0.1))))))/4.0);}`;
 // const glslAlphe=`float Alphe(float a,float b,float c,float d,float e,float f,float g){return((g+(3.0*((1.0-b)-(((((1.0-f)-(a)+b)*1.5)/2.0)+((f-0.5)*((1.0-f)*0.25))-((0.5-f)*(f*0.25))-((g-f)*((1.0-g)*0.1))))))/4.0);}`;
