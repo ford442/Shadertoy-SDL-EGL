30ec5a8ba0939973d35f3d035c4537e2243efa92
Noah Cohn <noahc42@gmail.com>
2023-09-17 13:37:58 -0700

    Update video_edit.cpp

--- src/combine/video_edit.cpp
+++ src/combine/video_edit.cpp
@@ -95,7 +95,7 @@ avgFrm(Fnum,leng,ptr,aptr);
 }
 
 EM_JS(void,ma,(),{
-// "use strict";
+ "use strict";
 const pnnl=document.body;
 var vv=document.getElementById("mv");
 var intervalBackward;
@@ -266,7 +266,7 @@ var av$=Ave(P[0],P[1],P[2]);
 var minuss=(av$-0.9)*(av$/(av$-0.9));
 av$=av$-(minuss*(av$*0.01));
 return[P[0],P[1],P[2],av$];
-}).setTactic("precision").setDynamicOutput(true).setPipeline(true).setOutput([w$,h$]);
+}).setTactic("precision").setDynamicOutput(true).setPipeline(true).setOutput([h$,h$]);
 
   //castle way
 // var P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
@@ -342,7 +342,7 @@ bb=GoldB(eulb);
 this.color(GoldR(p[0]),GoldG(p[1]),GoldB(p[2]),aveg);
  */
 this.color(p[0],p[1],p[2],aveg);
-}).setTactic("precision").setDynamicOutput(true).setGraphical(true).setOutput([w$,h$]);
+}).setTactic("precision").setDynamicOutput(true).setGraphical(true).setOutput([h$,h$]);
 
 w$=parseInt(document.getElementById("wid").innerHTML,10);
 h$=parseInt(document.getElementById("hig").innerHTML,10);
