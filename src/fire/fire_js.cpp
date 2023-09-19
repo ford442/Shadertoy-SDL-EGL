6f0f560545e1667668f0f96f5a6d8ce01d5eb040
Noah Cohn <noahc42@gmail.com>
2023-09-19 01:16:05 -0700

    Update fire_js.cpp

--- src/fire/fire_js.cpp
+++ src/fire/fire_js.cpp
@@ -474,7 +474,7 @@ const checkerboardShader=compileShader(gl.FRAGMENT_SHADER,`
  varying vec2 vUv;
  uniform sampler2D uTexture;
  uniform float aspectRatio;
- #define SCALE 25.0
+ #define SCALE 25.0;
  void main () {
  vec2 uv=floor(vUv * SCALE * vec2(aspectRatio,1.0));
  float v=mod(uv.x + uv.y,2.0);
