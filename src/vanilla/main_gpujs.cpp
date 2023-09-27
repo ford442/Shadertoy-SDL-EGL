6009787123c4897790131d6ab957912a7825580a
Noah Cohn <noahc42@gmail.com>
2023-09-27 12:36:48 -0700

    Update main_gpujs.cpp

--- src/vanilla/main_gpujs.cpp
+++ src/vanilla/main_gpujs.cpp
@@ -51,7 +51,7 @@ const $H=Module.HEAPF32.buffer;
 var la=h$*h$;
 const bcanvas=document.getElementById("bcanvas");
 const gl=bcanvas.getContext("webgl2",{preferLowPowerToHighPerformance:false,precision:'highp',logarithmicDepthBuffer:true,colorSpace:'display-p3',alpha:true,depth:false,stencil:true,imageSmoothingEnabled:true,preserveDrawingBuffer:true,premultipliedAlpha:false,desynchronized:false,lowLatency:true,powerPreference:'high-performance',antialias:true,willReadFrequently:true,majorVersion:2,minorVersion:0});
-const g=new GPUX({canvas:bcanvas,webGl:gl});
+const g=new GPUX({webGl:gl});
 const t=g.createKernel(function(v){
 const P=v[this.thread.y][this.thread.x-this.constants.blnk-this.constants.nblnk];
 return[P[0],P[1],P[2],P[3]];
@@ -122,10 +122,12 @@ S();
 };
 return()=>{
 T=true;
+};}
 }
-S();
+
 };
 }
+
 document.getElementById('pmhig').innerHTML=parseInt(window.innerHeight,10);
 document.getElementById('ihig').innerHTML=parseInt(window.innerHeight,10);
 document.getElementById('scanvas').height=parseInt(window.innerHeight,10);
