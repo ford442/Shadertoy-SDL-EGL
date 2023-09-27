9f28b17588e4df159acab73bb5d1f42554789067
Noah Cohn <noahc42@gmail.com>
2023-09-27 12:24:55 -0700

    Update main_gpujs.cpp

--- src/vanilla/main_gpujs.cpp
+++ src/vanilla/main_gpujs.cpp
@@ -85,7 +85,7 @@ eval("var point"+j+"=mem;var $"+j+"=new Float32Array($H,point"+j+",memr);$"+j+".
 };
   
 // var d=S();if(d)d();d=S();function S(){
-var d=S();M();d=S();function S(){
+var d=S();function S(){
 w$=parseInt(document.getElementById("wid").innerHTML,10);
 h$=parseInt(document.getElementById("hig").innerHTML,10);
 var blank$=Math.max((((w$-h$)*0)/2),0);
