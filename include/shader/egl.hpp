7d82f7f49c4666c1e159a55b2bc761a6de7ffb24
Noah Cohn <noahc42@gmail.com>
2023-10-07 18:02:48 -0700

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -4,6 +4,8 @@
 // #include <webgl/webgl2.h>  //  much better performance without
 // #include <webgl/webgl2_ext.h>  //  much better performance without
 
+#define GL_FRAGMENT_PRECISION_HIGH 1
+
 #define GL_CONTEXT_PROFILE_MASK 0x9126
 #define GL_CONTEXT_COMPATIBILITY_PROFILE_BIT 0x00000002
 #define GL_CONTEXT_CORE_PROFILE_BIT 0x00000001
@@ -27,14 +29,14 @@
 #include <GLES/glext.h>
 
 // #include <GLES3/gl3platform.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
+// #include <GLES3/gl31.h>
+// #include <GLES3/gl32.h>
 
-// #include <emscripten/html5_webgl.h>
+#include <emscripten/html5_webgl.h>
 
-#define GLFW_INCLUDE_GLEXT 1
-#define GLFW_INCLUDE_ES3
-#include <GLFW/glfw3.h>
+// #define GLFW_INCLUDE_GLEXT 1
+// #define GLFW_INCLUDE_ES3
+// #include <GLFW/glfw3.h>
 
 //
 // Copyright 2017 The ANGLE Project Authors. All rights reserved.
