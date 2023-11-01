ac13111a68515938bb08b72a85375088b365ae14
Noah Cohn <noahc42@gmail.com>
2023-11-01 06:09:05 -0700

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -1446,7 +1446,8 @@ glGenBuffers((GLsizei)1,&shad.VBO);
 gpu.VBOin(shad.VBO);
 glBindBuffer(GL_ARRAY_BUFFER,Sh.at(2,1));
 // glBufferData(GL_ARRAY_BUFFER,sizeof(vrt),vrt,GL_STATIC_DRAW);
-glBufferData(GL_ARRAY_BUFFER,len(vrt)*4,vrt,GL_STREAM_DRAW);
+// glBufferData(GL_ARRAY_BUFFER,len(vrt)*4,vrt,GL_STREAM_DRAW);
+glBufferData(GL_ARRAY_BUFFER,len(vrt)*4,vrt,GL_DYNAMIC_DRAW);
  /* 
 auto CLdevice=boost::compute::system::default_device();
 auto CLcontext=boost::compute::context(CLdevice);
