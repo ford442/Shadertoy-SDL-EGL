6a817b0e603c73f18452428fc645dad4e445cbfe
Noah Cohn <noahc42@gmail.com>
2023-11-07 07:19:22 -0800

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -24,7 +24,7 @@ WGpuRenderPassTimestampWrite renderPassTimestampWrite;
 WGPURenderPassTimestampWrites *ptrTimestampWrites = &timestampWrites;
 WGpuRenderPassTimestampWrite *ptrRenderPassTimestampWrite = &renderPassTimestampWrite;
 
-passDesc.timestampWrites = ptrTimestampWrites->next;
+passDesc.timestampWrites = (WGPU_RENDER_PASS_TIMESTAMP_WRITE_NEXT)ptrTimestampWrites;
 
 
 WGpuRenderPassEncoder pass=wgpu_command_encoder_begin_render_pass(encoder,&passDesc);
