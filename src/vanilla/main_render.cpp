2927b3c729f45910bad989d7b46f918d0afad8db
Noah Cohn <noahc42@gmail.com>
2023-11-07 06:56:26 -0800

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -29,7 +29,7 @@ const WGpuRenderPassTimestampWrite timestampWrite = {
   _end: 1
 };
 WGpuRenderPassDescriptor passDesc={1,&colorAttachment};
-  passDesc.timestampWrites = timestampWrite;
+  passDesc.timestampWrites = &timestampWrite;
 
 
 WGpuRenderPassEncoder pass=wgpu_command_encoder_begin_render_pass(encoder,&passDesc);
