99df7d8ae78e184299bc631a5b3fc041343e9c92
Noah Cohn <noahc42@gmail.com>
2023-11-09 11:12:41 -0800

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -356,7 +356,7 @@ static inline char frg_hdr_src[1000]=
 "out vec4 fragColor;\n";
 
 static inline char frg_ftr_src[1420]=
-"void main(){mainImage(fragColor,gl_FragCoord.xy);fragColor.rgb=(fragColor.rgb-0.5)*contrastAmount+0.5;}\n\0";
+"void main(){vec2 fragCoord=gl_FragCoord.xy;vec4 fragColor;mainImage(fragColor,gl_FragCoord.xy);fragColor.rgb=(fragColor.rgb-0.5)*contrastAmount+0.5;}\n\0";
 
 /*
 "#define mainImage mainImage0(out dvec4 O,dvec2 U);"
