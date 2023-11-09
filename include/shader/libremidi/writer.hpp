552a7e58d9a859aeb1139d4d043973208f4609a7
Noah Cohn <noahc42@gmail.com>
2023-11-08 20:28:42 -0800

    Update writer.hpp

--- include/shader/libremidi/writer.hpp
+++ include/shader/libremidi/writer.hpp
@@ -24,7 +24,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #pragma once
-#include <libremidi/message.hpp>
+#include "message.hpp"
 
 #include <cstdint>
 #include <vector>
