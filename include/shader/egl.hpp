70b1e964c50b31a798514490d7c5c847f3abd79c
Noah Cohn <noahc42@gmail.com>
2023-11-10 06:31:16 -0800

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -25,8 +25,8 @@
 // #include <GLES2/gl2.h>
 // #include <GLES2/gl2ext.h>
 
-// #include <GLES/gl.h>
-// #include <GLES/glext.h>
+#include <GLES/gl.h>
+#include <GLES/glext.h>
 
 // #include <GLES3/gl3platform.h>
 // #include <GLES3/gl31.h>
