3d13a8183e13f48c75efcbb1dd2bb5e1fd92fcf6
Noah Cohn <noahc42@gmail.com>
2023-11-12 05:55:45 -0800

    Update video_test.cpp

--- src/video/video_test.cpp
+++ src/video/video_test.cpp
@@ -758,7 +758,7 @@ display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
 // surface=eglCreateWindowSurface(display,eglconfig,0,attribut_list);
 // eglMakeCurrent(display,surface,surface,contextegl);
 emscripten_webgl_make_context_current(ctx);
-glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_NICEST);
+// glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_NICEST);
 // nanosleep(&req,&rem);
 glGenBuffers(1,&VBO);
 glBindBuffer(GL_ARRAY_BUFFER,VBO);
