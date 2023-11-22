16dc68fd0cfebb241c3d2103e8cb8aa028e44802
Noah Cohn <noahc42@gmail.com>
2023-11-21 17:34:37 -0800

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -29,6 +29,7 @@ passDesc.colorAttachments=&colorAttachment;
 // renderPassTimestampWrites.beginningOfPassWriteIndex=-1;
 // renderPassTimestampWrites.endOfPassWriteIndex=-1;
 // passDesc.timestampWrites=renderPassTimestampWrites;
+passDesc.timestampWrites=0;
 WGpuRenderPassEncoder pass=wgpu_command_encoder_begin_render_pass(encoder,&passDesc);
 wgpu_render_pass_encoder_set_pipeline(pass,renderPipeline);
 wgpu_render_pass_encoder_set_viewport(pass, 0.0, 0.0, sze.at(0,0), sze.at(0,0), 0.0, 1.0);
@@ -36,7 +37,9 @@ wgpu_render_pass_encoder_draw(pass,3,1,0,0);
 wgpu_render_pass_encoder_end(pass);
 WGpuCommandBuffer commandBuffer=wgpu_command_encoder_finish(encoder);
 wgpu_queue_submit_one_and_destroy(queue,commandBuffer);
-  
+  EM_ASM({
+  console.log("test");
+});
 return EM_FALSE;
 }
 
