badc08efc63fe44e9c69fac6403601c94efdf616
Noah Cohn <noahc42@gmail.com>
2023-11-22 16:12:51 -0800

    Update main_webgpu.hpp

--- include/vanilla/main_webgpu.hpp
+++ include/vanilla/main_webgpu.hpp
@@ -1,4 +1,4 @@
-// #include <boost/cstdfloat.hpp>  // must be first include
+#include <boost/cstdfloat.hpp>  // must be first include
 #include "../../include/vanilla/avx.hpp"
 #include "../../include/vanilla/defs.hpp"
 #include "../../include/vanilla/gl.hpp"
@@ -81,50 +81,3 @@ void startWebGPU();
 void runWebGPU();
 
 }
-
-using mouse_tensor=boost::numeric::ublas::tensor<float>;
-using shad_tensor=boost::numeric::ublas::tensor<boost::uint_t<32>::exact>;
-using prg_tensor=boost::numeric::ublas::tensor<boost::uint_t<64>::exact>;
-using sz_tensor=boost::numeric::ublas::tensor<boost::int_t<64>::exact>;
-using f_tensor=boost::numeric::ublas::tensor<float>;
-using d_tensor=boost::numeric::ublas::tensor<double>;
-using di_tensor=boost::numeric::ublas::tensor<boost::uint_t<64>::exact>;
-using v_tensor=boost::numeric::ublas::tensor<v128_t>;
-using i_tensor=boost::numeric::ublas::tensor<boost::int_t<64>::exact>;
-using iptr_tensor=boost::numeric::ublas::tensor<int *>;
-using fptr_tensor=boost::numeric::ublas::tensor<float *>;
-using uiptr_tensor=boost::numeric::ublas::tensor<uint32_t *>;
-using gi_tensor=boost::numeric::ublas::tensor<GLint>;
-using li_tensor=boost::numeric::ublas::tensor<long>;
-using void_tensor=boost::numeric::ublas::tensor<void *>;
-using bgle_tensor=boost::numeric::ublas::tensor<WGpuBindGroupLayoutEntry *>;
-using bge_tensor=boost::numeric::ublas::tensor<WGpuBindGroupEntry *>;
-using bmc_tensor=boost::numeric::ublas::tensor<WGpuBufferMapCallback>;
-using wdc_tensor=boost::numeric::ublas::tensor<WGpuOnSubmittedWorkDoneCallback>;
-using oac_tensor=boost::numeric::ublas::tensor<WGpuRequestAdapterCallback>;
-using odc_tensor=boost::numeric::ublas::tensor<WGpuRequestDeviceCallback>;
-using bbl_tensor=boost::numeric::ublas::tensor<WGpuBufferBindingLayout>;
-using bd_tensor=boost::numeric::ublas::tensor<WGpuBufferDescriptor>;
-using md_tensor=boost::numeric::ublas::tensor<WGpuShaderModuleDescriptor>;
-using dd_tensor=boost::numeric::ublas::tensor<WGpuDeviceDescriptor>;
-using rao_tensor=boost::numeric::ublas::tensor<WGpuRequestAdapterOptions>;
-using wa_tensor=boost::numeric::ublas::tensor<WGpuAdapter>;
-using wq_tensor=boost::numeric::ublas::tensor<WGpuQueue>;
-using cb_tensor=boost::numeric::ublas::tensor<WGpuCommandBuffer>;
-using wb_tensor=boost::numeric::ublas::tensor<WGpuBuffer>;
-using ce_tensor=boost::numeric::ublas::tensor<WGpuCommandEncoder>;
-using wd_tensor=boost::numeric::ublas::tensor<WGpuDevice>;
-using cpe_tensor=boost::numeric::ublas::tensor<WGpuComputePassEncoder>;
-using cp_tensor=boost::numeric::ublas::tensor<WGpuComputePipeline>;
-using pl_tensor=boost::numeric::ublas::tensor<WGpuPipelineLayout>;
-using cm_tensor=boost::numeric::ublas::tensor<WGpuShaderModule>;
-using bg_tensor=boost::numeric::ublas::tensor<WGpuBindGroup>;
-using bgl_tensor=boost::numeric::ublas::tensor<WGpuBindGroupLayout>;
-using i53_tensor=boost::numeric::ublas::tensor<double_int53_t>;
-using tex_tensor=boost::numeric::ublas::tensor<WGpuTexture>;
-using td_tensor=boost::numeric::ublas::tensor<WGpuTextureDescriptor>;
-using stbl_tensor=boost::numeric::ublas::tensor<WGpuStorageTextureBindingLayout>;
-using tv_tensor=boost::numeric::ublas::tensor<WGpuTextureView>;
-using tvd_tensor=boost::numeric::ublas::tensor<WGpuTextureViewDescriptor>;
-using ced_tensor=boost::numeric::ublas::tensor<WGpuCommandEncoderDescriptor>;
-using bms_tensor=boost::numeric::ublas::tensor<WGPU_BUFFER_MAP_STATE>;
