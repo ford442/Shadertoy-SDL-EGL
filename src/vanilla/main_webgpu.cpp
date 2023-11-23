e2b6e7c797374eac5fce199699aae56dc74271d8
Noah Cohn <noahc42@gmail.com>
2023-11-22 16:00:26 -0800

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -3,7 +3,7 @@
 EmscriptenWebGLContextAttributes attr;
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=0;
 
-double wi,hi;
+double wi,hi,szh,szw;
 
 using namespace std;
 
@@ -31,53 +31,6 @@ inline char wgl_cmp_src[2000]=
 // "if i==256{break;}"
 // "}"
 "}";
-
-using mouse_tensor=boost::numeric::ublas::tensor<float>;
-using shad_tensor=boost::numeric::ublas::tensor<boost::uint_t<32>::exact>;
-using prg_tensor=boost::numeric::ublas::tensor<boost::uint_t<64>::exact>;
-using sz_tensor=boost::numeric::ublas::tensor<boost::int_t<64>::exact>;
-using f_tensor=boost::numeric::ublas::tensor<float>;
-using d_tensor=boost::numeric::ublas::tensor<double>;
-using di_tensor=boost::numeric::ublas::tensor<boost::uint_t<64>::exact>;
-using v_tensor=boost::numeric::ublas::tensor<v128_t>;
-using i_tensor=boost::numeric::ublas::tensor<boost::int_t<64>::exact>;
-using iptr_tensor=boost::numeric::ublas::tensor<int *>;
-using fptr_tensor=boost::numeric::ublas::tensor<float *>;
-using uiptr_tensor=boost::numeric::ublas::tensor<uint32_t *>;
-using gi_tensor=boost::numeric::ublas::tensor<GLint>;
-using li_tensor=boost::numeric::ublas::tensor<long>;
-using void_tensor=boost::numeric::ublas::tensor<void *>;
-using bgle_tensor=boost::numeric::ublas::tensor<WGpuBindGroupLayoutEntry *>;
-using bge_tensor=boost::numeric::ublas::tensor<WGpuBindGroupEntry *>;
-using bmc_tensor=boost::numeric::ublas::tensor<WGpuBufferMapCallback>;
-using wdc_tensor=boost::numeric::ublas::tensor<WGpuOnSubmittedWorkDoneCallback>;
-using oac_tensor=boost::numeric::ublas::tensor<WGpuRequestAdapterCallback>;
-using odc_tensor=boost::numeric::ublas::tensor<WGpuRequestDeviceCallback>;
-using bbl_tensor=boost::numeric::ublas::tensor<WGpuBufferBindingLayout>;
-using bd_tensor=boost::numeric::ublas::tensor<WGpuBufferDescriptor>;
-using md_tensor=boost::numeric::ublas::tensor<WGpuShaderModuleDescriptor>;
-using dd_tensor=boost::numeric::ublas::tensor<WGpuDeviceDescriptor>;
-using rao_tensor=boost::numeric::ublas::tensor<WGpuRequestAdapterOptions>;
-using wa_tensor=boost::numeric::ublas::tensor<WGpuAdapter>;
-using wq_tensor=boost::numeric::ublas::tensor<WGpuQueue>;
-using cb_tensor=boost::numeric::ublas::tensor<WGpuCommandBuffer>;
-using wb_tensor=boost::numeric::ublas::tensor<WGpuBuffer>;
-using ce_tensor=boost::numeric::ublas::tensor<WGpuCommandEncoder>;
-using wd_tensor=boost::numeric::ublas::tensor<WGpuDevice>;
-using cpe_tensor=boost::numeric::ublas::tensor<WGpuComputePassEncoder>;
-using cp_tensor=boost::numeric::ublas::tensor<WGpuComputePipeline>;
-using pl_tensor=boost::numeric::ublas::tensor<WGpuPipelineLayout>;
-using cm_tensor=boost::numeric::ublas::tensor<WGpuShaderModule>;
-using bg_tensor=boost::numeric::ublas::tensor<WGpuBindGroup>;
-using bgl_tensor=boost::numeric::ublas::tensor<WGpuBindGroupLayout>;
-using i53_tensor=boost::numeric::ublas::tensor<double_int53_t>;
-using tex_tensor=boost::numeric::ublas::tensor<WGpuTexture>;
-using td_tensor=boost::numeric::ublas::tensor<WGpuTextureDescriptor>;
-using stbl_tensor=boost::numeric::ublas::tensor<WGpuStorageTextureBindingLayout>;
-using tv_tensor=boost::numeric::ublas::tensor<WGpuTextureView>;
-using tvd_tensor=boost::numeric::ublas::tensor<WGpuTextureViewDescriptor>;
-using ced_tensor=boost::numeric::ublas::tensor<WGpuCommandEncoderDescriptor>;
-using bms_tensor=boost::numeric::ublas::tensor<WGPU_BUFFER_MAP_STATE>;
 
 static v_tensor sse=v_tensor{2,2};
 static v_tensor sse2=v_tensor{2,2};
@@ -154,9 +107,9 @@ GLsizei width=256;
 GLsizei height=256;
 GLuint wtexture[4];
 WGpuTexture textureA;
-WGpuAdapter adapter=0;
-WGpuDevice device=0;
-WGpuQueue queue=0;
+// WGpuAdapter adapter=0;
+// WGpuDevice device=0;
+// WGpuQueue queue=0;
 WGpuBindGroupLayout bindGroupLayout=0;
 WGpuBindGroupLayout bindGroupLayoutB=0;
 WGpuComputePipeline computePipeline=0;
@@ -422,14 +375,14 @@ return;
 }
 
 static void ObtainedWebGpuDeviceStart2(WGpuDevice result,void * userData){
-device=result;
+// device=result;
 WGPU_Device.at(0,0,0)=result;
 raf();
 return;
 }
 
 static void ObtainedWebGpuDeviceStart(WGpuDevice result,void * userData){
-device=result;
+// device=result;
 WGPU_Device.at(0,0,0)=result;
 wgpu_adapter_request_device_async(WGPU_Adapter.at(0,0,1),&WGPU_DeviceDescriptor.at(0,0,0),WGPU_ObtainedDeviceCallback.at(0,0,1),&WGPU_UserData.at(0,0,1));
 return;
@@ -441,7 +394,7 @@ wgpu_adapter_request_device_async(WGPU_Adapter.at(0,0,0),&WGPU_DeviceDescriptor.
 }
 
 static void ObtainedWebGpuAdapterStart(WGpuAdapter result,void * userData){
-adapter=result;
+// adapter=result;
 WGPU_Adapter.at(0,0,0)=result;
     wgpu_adapter_request_device_async(WGPU_Adapter.at(0,0,0),&WGPU_DeviceDescriptor.at(0,0,0),WGPU_ObtainedDeviceCallback.at(0,0,1),&WGPU_UserData.at(0,0,0));
 // navigator_gpu_request_adapter_async(&WGPU_RequestAdapterOptions.at(0,0,0),WGPU_ObtainedAdapterCallback.at(0,0,1),&WGPU_UserData.at(0,0,1));
