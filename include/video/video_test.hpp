96dd36cd0c7a20040cf4a76df9ab32c25b8e5ba8
Noah Cohn <noahc42@gmail.com>
2023-11-27 13:26:20 -0800

    Update video_test.hpp

--- include/video/video_test.hpp
+++ include/video/video_test.hpp
@@ -18,17 +18,3 @@
 #include <functional>
 
 #include <boost/function.hpp>
-
-EMSCRIPTEN_RESULT ret;
-
-EGLDisplay display;
-EGLContext contextegl;
-EGLSurface surface;
-EGLint config_size,major,minor;
-
-float max;
-float min;
-float sum;
-float avgSum;
-float minSum;
-float maxSum;
