66cb2b647dc32d91ffe2bdf9485f2436e9763a27
Noah Cohn <noahc42@gmail.com>
2023-11-26 22:45:08 -0800

    Update resurection_jebus_nowebgpu.cpp

--- resurection_jebus_nowebgpu.cpp
+++ resurection_jebus_nowebgpu.cpp
@@ -1,9 +1,9 @@
 #include <boost/cstdfloat.hpp>  // must be first include
-#include "include/vanilla/avx.h"
+#include "include/vanilla/avx.hpp"
 #include <immintrin.h> 
-#include "include/vanilla/defs.h"
-#include "include/vanilla/gl.h"
-#include "include/vanilla/boost_defs.h"
+#include "include/vanilla/defs.hpp"
+#include "include/vanilla/gl.hpp"
+#include "include/vanilla/boost_defs.hpp"
 #include <boost/integer.hpp>
 #include <boost/atomic.hpp>
 #include <boost/numeric/ublas/tensor.hpp>
