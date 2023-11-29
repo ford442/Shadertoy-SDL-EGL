2aa4c19188b5cece901cbbbf0a4d01ea631c2f05
Noah Cohn <noahc42@gmail.com>
2023-11-28 17:49:18 -0800

    Update audio.hpp

--- include/audio/audio.hpp
+++ include/audio/audio.hpp
@@ -155,13 +155,16 @@ snd_pos(0);
 SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
 SDL_Init(SDL_INIT_AUDIO);
 // SDL_LoadWAV(flnm,&request,&wave.snd,&wave.slen);
-Mix_LoadWAV(flnm);
+Mix_Chunk *chunk = Mix_LoadWAV(flnm);
 sound.at(0,1,0)=wave.snd;
 snd_pos_u(wave.slen);
 request.callback=bfr;
 // wave.dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&request,NULL,0);
 wave.dev=Mix_OpenAudioDevice(request.freq,request.format,request.channels,request.samples,0,SDL_AUDIO_ALLOW_FORMAT_CHANGE|SDL_AUDIO_ALLOW_SAMPLES_CHANGE);
-SDL_PauseAudioDevice(wave.dev,SDL_FALSE);
+Mix_PlayChannel(-1, chunk, 0);
+
+  
+  SDL_PauseAudioDevice(wave.dev,SDL_FALSE);
 return EM_TRUE;
 };
 
