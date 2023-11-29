178ba8fc96488549288f56aef4a777f19c204443
Noah Cohn <noahc42@gmail.com>
2023-11-28 18:22:19 -0800

    Update audio.hpp

--- include/audio/audio.hpp
+++ include/audio/audio.hpp
@@ -162,7 +162,7 @@ soundp.at(0,1,0)=&sound.at(0,1,0);
 snd_pos_u(wave.slen);
 // request.callback=bfr;
 // wave.dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&request,NULL,0);
-wave.dev=Mix_OpenAudioDevice(request.freq,request.format,request.channels,request.samples,bfr(),SDL_AUDIO_ALLOW_FORMAT_CHANGE|SDL_AUDIO_ALLOW_SAMPLES_CHANGE);
+wave.dev=Mix_OpenAudioDevice(request.freq,request.format,request.channels,request.samples,bfr,SDL_AUDIO_ALLOW_FORMAT_CHANGE|SDL_AUDIO_ALLOW_SAMPLES_CHANGE);
 Mix_PlayMusic(sound.at(0,1,0),1);
   
 //  SDL_PauseAudioDevice(wave.dev,SDL_FALSE);
