1a5fabe7e3f9d2e654f5c5fc9d074fb5094e291e
Noah Cohn <noahc42@gmail.com>
2023-12-02 03:07:24 -0800

    Update egl.hpp

--- include/shader/egl.hpp
+++ include/shader/egl.hpp
@@ -6,14 +6,14 @@
 
 #include <GL/gl.h>
 #include <GL/glext.h>
-#include <GLES3/gl32.h>
+// #include <GLES3/gl32.h>
 #include <GLES3/gl31.h>
-#include <GLES2/gl2.h>
+// #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #include <GLES3/gl3.h>
 // #include <GLES/gl.h>
 // #include <GLES/glext.h>
-// #include <GLES3/gl3platform.h>
+#include <GLES3/gl3platform.h>
 #include <emscripten/html5_webgl.h>
 // #define GLFW_INCLUDE_GLEXT 1
 // #define GLFW_INCLUDE_ES3
