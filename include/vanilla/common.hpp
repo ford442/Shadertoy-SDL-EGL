cc99c4f6b4a1897f5dc64f29332613ee3e9446fa
Noah Cohn <noahc42@gmail.com>
2023-12-02 18:56:36 -0800

    Update common.hpp

--- include/vanilla/common.hpp
+++ include/vanilla/common.hpp
@@ -1,3 +1,7 @@
+
+#include <boost/numeric/ublas/tensor.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 using void_tensor=boost::numeric::ublas::tensor<void *>;
 static void_tensor bin=void_tensor{1,1};
 
