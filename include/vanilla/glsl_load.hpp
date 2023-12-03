5b294e445e423eff0136162fddf6ea5cce471679
Noah Cohn <noahc42@gmail.com>
2023-12-02 19:50:20 -0800

    Update glsl_load.hpp

--- include/vanilla/glsl_load.hpp
+++ include/vanilla/glsl_load.hpp
@@ -1746,10 +1746,8 @@ glBindBuffer(GL_UNIFORM_BUFFER,0);
   
 GLint binarySize;
 glGetProgramiv(S1.at(0,0,0), GL_PROGRAM_BINARY_LENGTH, &binarySize);
-
 glGetProgramBinary(S1.at(0,0,0),sizeof(GLbin)*64,binLength,binaryFormat,&GLbin);
 bin.at(0,0)=GLbin;
-std::vector<GLchar> binary(binarySize);
 glGetProgramBinary(S1.at(0,0,0), binarySize, NULL, binaryFormat, binary.data());
   
   
