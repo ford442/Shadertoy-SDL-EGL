b2efaf49704ec29e8727f64acc851e16e8573a72
Noah Cohn <noahc42@gmail.com>
2023-12-02 23:18:41 -0800

    Update common.hpp

--- include/vanilla/common.hpp
+++ include/vanilla/common.hpp
@@ -3,12 +3,12 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 using void_tensor=boost::numeric::ublas::tensor<void *>;
-using wuint_tensor=boost::numeric::ublas::tensor<std::vector<uint32_t> *>;
+using wuint_tensor=boost::numeric::ublas::tensor<std::vector<GLchar>>;
+using gli_tensor=boost::numeric::ublas::tensor<GLint>;
 
 static void_tensor bin=void_tensor{1,1};
 static wuint_tensor wbin=wuint_tensor{1,1};
+static gli_tensor wbin_size=gli_tensor{1,1};
 
 void gl_js();
- 
-GLint binarySize;
-std::vector<GLchar> binary(binarySize);
+
