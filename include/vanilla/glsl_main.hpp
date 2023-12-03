5d89df407d98416b4af3f7fde33ae5c908cc9a7a
Noah Cohn <noahc42@gmail.com>
2023-12-02 18:51:28 -0800

    Update glsl_main.hpp

--- include/vanilla/glsl_main.hpp
+++ include/vanilla/glsl_main.hpp
@@ -22,7 +22,14 @@ typedef ResultType result_type;
 #include <boost/config.hpp>
 #include <boost/preprocessor.hpp>
 #include <boost/function.hpp>
-#include "boost/tuple/tuple.hpp"
+#include <boost/integer.hpp>
+#include <boost/atomic.hpp>
+#include <boost/numeric/ublas/tensor.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/chrono.hpp>
+
 #include <random>
 #include <cstdlib>
 #include <emscripten.h>
