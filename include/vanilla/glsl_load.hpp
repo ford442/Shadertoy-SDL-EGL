7c1757cddee091a19bf11d14ea79303c81ca8230
Noah Cohn <noahc42@gmail.com>
2023-12-02 21:09:54 -0800

    Update glsl_load.hpp

--- include/vanilla/glsl_load.hpp
+++ include/vanilla/glsl_load.hpp
@@ -1743,8 +1743,7 @@ glBindBuffer(GL_UNIFORM_BUFFER,0);
 */
 // glDetachShader(S1.at(0,0,0),frag);
 // glDetachShader(S1.at(0,0,0),vtx);
-  
-GLint binarySize;
+ 
 glGetProgramiv(S1.at(0,0,0), GL_PROGRAM_BINARY_LENGTH, &binarySize);
 glGetProgramBinary(S1.at(0,0,0),sizeof(GLbin)*64,binLength,binaryFormat,&GLbin);
 bin.at(0,0)=GLbin;
