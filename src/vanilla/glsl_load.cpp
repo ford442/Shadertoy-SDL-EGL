dd0125cb3dae1ba00e5131b4d38a4847f53dae0f
Noah Cohn <noahc42@gmail.com>
2023-12-03 06:15:19 -0800

    Update glsl_load.cpp

--- src/vanilla/glsl_load.cpp
+++ src/vanilla/glsl_load.cpp
@@ -59,9 +59,13 @@ std::vector<GLchar> binr(wbin_size.at(0,0));
 std::vector<uint32_t> ubinr(wbin_size.at(0,0));
 binr=wbin.at(0,0);
 ubinr=wubin.at(0,0);
+std::memcpy(ubinr.data(),wubin.at(0,0).data(),wubin.at(0,0).size()*sizeof(uint32_t));
+  
 spirv_cross::CompilerGLSL glsl(reinterpret_cast<const uint32_t*>(binr.data()),binr.size());
 spirv_cross::ShaderResources resources=glsl.get_shader_resources();
-tint::Program * Tprogram = std::make_unique<tint::Program>(tint::reader::spirv::Parse(ubinr.data()));
+
+
+tint::Program Tprogram = std::make_unique<tint::Program>(tint::reader::spirv::Parse(ubinr.data()));
 
 // tint::Module module = tint::Parser.ParseSpirv(ubinr);
 // tint::Program program = tint::Program::FromModule(module);
