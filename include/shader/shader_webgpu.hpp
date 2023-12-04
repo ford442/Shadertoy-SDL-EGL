9da61ec757c781731f99b72c97ae915030fec222
Noah Cohn <noahc42@gmail.com>
2023-12-03 22:28:48 -0800

    Update shader_webgpu.hpp

--- include/shader/shader_webgpu.hpp
+++ include/shader/shader_webgpu.hpp
@@ -211,8 +211,7 @@ return shader;
 
 };
 
-static inline char32_t wgl_cmp_src[2000]=L
-"@group(0)@binding(0)var<storage,read>inputBuffer:array<u32,262144>;"
+static inline char32_t wgl_cmp_src[2000]=L"@group(0)@binding(0)var<storage,read>inputBuffer:array<u32,262144>;"
 "@group(0)@binding(1)var<storage,read_write>outputBuffer:array<u32,262144>;"
 "@group(0)@binding(2)var textureA:texture_storage_2d<rgba32uint,write>;"
 "@compute@workgroup_size(4,1,64)"
@@ -227,8 +226,7 @@ static inline char32_t wgl_cmp_src[2000]=L
 "}"
 "}";
 
-static inline char32_t cm_hdr_src[3300]=L
-"#version 300 es\n"
+static inline char32_t cm_hdr_src[3300]=L"#version 300 es\n"
 "#extension GL_EXTENSIONS : enable\n"
 "#pragma target(glsl_es)\n"
 "#pragma enable_bitfield_ops\n"
@@ -307,8 +305,7 @@ static inline char32_t cm_hdr_src[3300]=L
 "precision highp samplerCube;"
 */
 
-static inline char32_t vrt_bdy_src[420]=L
-"precision highp int;\n"
+static inline char32_t vrt_bdy_src[420]=L"precision highp int;\n"
 "precision highp sampler2D;"
 "precision highp samplerCube;"
 "layout(location=0)in vec4 iPosition;void main(){gl_Position=iPosition;}\n\0";
@@ -324,8 +321,7 @@ static inline char32_t frg_hdr_src[1000]=L
 "uniform sampler2D iChannel3;"
 "out vec4 fragColor;\n";
 
-static inline char32_t frg_ftr_src[2420]=L
-"void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
+static inline char32_t frg_ftr_src[2420]=L"void main(){mainImage(fragColor,gl_FragCoord.xy);}\n\0";
 /*
 "#define mainImage mainImage0(out vec4 O, vec2 U);\n"
 "int _N=3;void mainImage(out vec4 O,vec2 U){\n"
