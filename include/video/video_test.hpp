9a5be0f12feb39c9fdcb605611786435a071c12e
Noah Cohn <noahc42@gmail.com>
2023-12-03 16:22:47 -0800

    Update video_test.hpp

--- include/video/video_test.hpp
+++ include/video/video_test.hpp
@@ -1,19 +1,20 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
-#define GL_GLEXT_PROTOTYPES 1
-#define GL_FRAGMENT_PRECISION_HIGH 1
-#define GL3_PROTOTYPES 1
-
-#include <GLES3/gl3.h>
-// #include <GLES3/gl31.h>
-// #include <GLES3/gl32.h>
-// #include <GLES3/gl3platform.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-// #include <EGL/eglplatform.h>
 #include <GL/gl.h>
 #include <GL/glext.h>
+// #include <GLES3/gl32.h>
+#include <GLES3/gl31.h>
+// #include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <GLES3/gl3.h>
+// #include <GLES/gl.h>
+// #include <GLES/glext.h>
+#include <GLES3/gl3platform.h>
+#include <emscripten/html5_webgl.h>
+// #define GLFW_INCLUDE_GLEXT 1
+// #define GLFW_INCLUDE_ES3
+// #include <GLFW/glfw3.h>
 
 #include <functional>
 
