c853f58cce4e910f9765b6b9fb1961d44b08b58c
Noah Cohn <noahc42@gmail.com>
2023-12-03 23:38:38 -0800

    Update gl.hpp

--- include/vanilla/gl.hpp
+++ include/vanilla/gl.hpp
@@ -8,9 +8,9 @@
 
 #include <GL/gl.h>
 
-#ifndef GL_GLEXT_PROTOTYPES
-#define GL_GLEXT_PROTOTYPES 1
-#endif
+// #ifndef GL_GLEXT_PROTOTYPES
+// #define GL_GLEXT_PROTOTYPES 1
+// #endif
 
 #include <GL/glext.h>
 
@@ -21,11 +21,12 @@
 #define GL_GLES_PROTOTYPES 1
 #include <GLES3/gl3platform.h>
 #include <GLES3/gl3.h>
+#include <GLES2/gl2ext.h>
 // #include <GLES3/gl31.h>
 // #include <GLES3/gl32.h>
 #define EGL_EGL_PROTOTYPES 1
 #define EGL_EGLEXT_PROTOTYPES 1
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
-#include <KHR/khrplatform.h>
+//  #include <EGL/eglplatform.h>
+// #include <KHR/khrplatform.h>
