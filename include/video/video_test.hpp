e1b41ebb7e5a152b4a01799844350fc7661f4f0d
Noah Cohn <noahc42@gmail.com>
2023-12-05 09:53:12 -0800

    Update video_test.hpp

--- include/video/video_test.hpp
+++ include/video/video_test.hpp
@@ -24,8 +24,8 @@
 
 #include <boost/function.hpp>
 
-EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
-static const char8_t *read_file(const char *);
-GLuint compile_shader(GLenum,GLsizei,const char **);
+// EM_BOOL mouse_call(int,const EmscriptenMouseEvent *,void *);
+// static const char8_t *read_file(const char *);
+// GLuint compile_shader(GLenum,GLsizei,const char **);
 
 // #include "../../include/video/common.hpp"
