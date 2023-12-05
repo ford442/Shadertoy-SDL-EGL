2eb76fa2b7fa772b9b8be0ff0612f9d778ee9d46
Noah Cohn <noahc42@gmail.com>
2023-12-05 05:50:29 -0800

    Update video_google_street.hpp

--- include/video/video_google_street.hpp
+++ include/video/video_google_street.hpp
@@ -26,34 +26,34 @@
 
 #include "../../include/video/egl.hpp"
 
-void avgFrm(int,int,float *,float *);
+EM_BOOL avgFrm(int,int,float *,float *);
 
 extern "C"{
 
-void b3();
+EM_BOOL b3();
 
-void nano(int,int,float *,float *);
+EM_BOOL nano(int,int,float *,float *);
 
 }
 
-void uni(GLfloat,GLfloat,GLfloat,GLint,GLfloat);
+EM_BOOL uni(GLfloat,GLfloat,GLfloat,GLint,GLfloat);
 
 GLuint compile_shader(GLenum,GLsizei,const GLchar **);
 
 char32_t * read_file(const GLchar *);
 
-void renderFrame();
+EM_BOOL renderFrame();
 
 EM_BOOL mouse_call_click(int,const EmscriptenMouseEvent *,void *);
 
 static EM_BOOL mouse_call_move(int,const EmscriptenMouseEvent *,void *);
 
-void clrclr(GLfloat,GLfloat,GLfloat);
+EM_BOOL clrclr(GLfloat,GLfloat,GLfloat);
 
 extern "C"{
 
-void clr(GLfloat,GLfloat,GLfloat);
+EM_BOOL clr(GLfloat,GLfloat,GLfloat);
 
-void str();
+EM_BOOL str();
 
 }
