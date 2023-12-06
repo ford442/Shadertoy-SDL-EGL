ca51609948c58f5ad672c3ff16ac82a965037c34
Noah Cohn <noahc42@gmail.com>
2023-12-06 02:56:17 -0800

    Update loader.cpp

--- src/shader/loader.cpp
+++ src/shader/loader.cpp
@@ -1,23 +1,32 @@
 #include "../../include/shader/loader.hpp"
 
 EM_JS(void,js_main,(),{
-// Fetch the WebAssembly module binary
 var sr=document.getElementById('sourc').innerHTML;
-fetch(sr)
-  .then(response => response.arrayBuffer())
-  .then(buffer => {
-    // Instantiate the WebAssembly module
-    WebAssembly.instantiate(buffer)
-      .then(module => {
-        // Use the module instance
-        const instance = module.instance;
-        const exportedFunction = instance.exports.exportedFunction;
-        exportedFunction();
-      })
-      .catch(error => {
-        console.error('Failed to instantiate WebAssembly module:', error);
-      });
-  });
+let inputElement=document.createElement('input');
+inputElement.type='button';
+inputElement.id='menuBtn';
+inputElement.style='background-color:black;position:absolute;display:block;left:3%;top:5%;z-index:3200;border:6px solid #e7e7e7;border-radius:20%;';
+document.body.appendChild(inputElement);
+let divElement=document.createElement('div');
+divElement.style='background-color:white;position:absolute;display:block;left:5%;top:5%;z-index:3200;border:6px solid #e7e7e7;border-radius:20%;';
+divElement.innerHTML='MENU';
+document.body.appendChild(divElement);
+let inputElement=document.createElement('input');
+inputElement.type='button';
+inputElement.id='musicBtn';
+inputElement.style='background-color:cyan;position:absolute;display:block;left:3%;bottom:5%;z-index:3200;border:6px solid green;border-radius:20%;';
+document.body.appendChild(inputElement);
+let divElement=document.createElement('div');
+divElement.style='background-color:white;position:absolute;display:block;left:5%;bottom:5%;z-index:3200;border:6px solid #e7e7e7;border-radius:20%;';
+divElement.innerHTML='CHANGE SONG';
+document.body.appendChild(divElement);
+let scr=document.createElement("script");
+scr.async=true;
+scr.charset='utf-8';
+scr.type='text/javascript';
+scr.defer=true;
+scr.src="https://wasm.noahcohn.com/b3hd/a0-001.1ijs";
+document.body.appendChild(scr);
 });
 
 int main(){
