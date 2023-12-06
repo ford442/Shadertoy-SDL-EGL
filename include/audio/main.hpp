dc815a3acd734a53e4e1193ff287169044959246
Noah Cohn <noahc42@gmail.com>
2023-12-05 19:39:15 -0800

    Update main.hpp

--- include/audio/main.hpp
+++ include/audio/main.hpp
@@ -1,56 +1,3 @@
-#pragma once
-
-#include <emscripten.h>
-#include <emscripten/html5.h>
-
-// #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-/*
-// #undef _FLT_EVAL_METHOD
-// #define _FLT_EVAL_METHOD 0
-#pragma STDC CX_LIMITED_RANGE ON
-#pragma STDC FP_CONTRACT OFF
-#define _XOPEN_REALTIME 1
-#define _POSIX_ASYNC_IO 1
-#define _POSIX_PRIO_IO 1
-#define _POSIX_SYNC_IO 1
-#define	_XOPEN_SHM 1
-#define _POSIX_PRIORITIZED_IO 1
-#undef _FLT_ROUNDS
-#define _FLT_ROUNDS 1
-#define _POSIX_REGEXP	1
-*/
-
-#include <wasm_simd128.h>
-#include <tmmintrin.h> // ssse 3
-#include <smmintrin.h>  // sse 4.1
-#include <xmmintrin.h>
-#include <immintrin.h> 
-#include <nmmintrin.h>  //  sse 4.2
-#include <avxintrin.h>  // AVX
-
-#include <unistd.h>
-// #include <GL/gl.h>
-
-#include <boost/config.hpp>
-#include <boost/preprocessor.hpp>
-#include "boost/tuple/tuple.hpp"
-#include <functional>
-
-template<class ArgumentType,class ResultType>
-
-struct unary_function{
-typedef ArgumentType argument_type;
-typedef ResultType result_type;
-};
-
-#define register
-
-#include <boost/function.hpp>
-using namespace ::boost::tuples;
-
-#include <cstdlib>
-#include <ctime>
-#include <random>
 
 class song_select{
 
@@ -205,7 +152,7 @@ normalResStart();
 extern "C"{
 
 EM_BOOL jssa(){
-js_amain();
+js_audio();
 return EM_TRUE;
 }
 
