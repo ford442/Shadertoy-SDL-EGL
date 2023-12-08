11b9d8b967767eb70340ef8eb0a6acfa2a9ec574
Noah Cohn <noahc42@gmail.com>
2023-12-08 14:23:59 -0800

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -70,7 +70,7 @@ const char *vertexShader =
 "var pos = array<vec2<f32>, 3>(\n"
 "vec2<f32>(0.0, 0.5),\n"
 "vec2<f32>(-0.5, -0.5),\n"
-"vec2<f32>(0.5, -0.5)\n"
+"vec2<f32>(0.5, -0.5),\n"
 "vec2<f32>(0.5, 0.5)\n"
 ");\n"
 "return vec4<f32>(pos[vertexIndex], 0.0, 1.0);\n"
