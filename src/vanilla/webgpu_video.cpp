28d01c7464dc064689a09e919b552d47ef9dc9b5
Noah Cohn <noahc42@gmail.com>
2023-12-10 19:17:42 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -449,7 +449,7 @@ ctx.drawImage(vv,0,0);
 let imageData=ctx.getImageData(0,0,cnv.width,cnv.height);
 let pixelData=new Uint8ClampedArray(imageData.data);
 let heapArray=new Uint8ClampedArray(H,0,dataSize);
-heapArray.set(pixelData,0,dataSize);
+// heapArray.set(pixelData,0,dataSize);
 // Module.ccall("frm",null,["Number"],[0]);
 },16);
 }
