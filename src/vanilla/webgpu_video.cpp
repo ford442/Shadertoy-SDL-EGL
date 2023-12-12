a01f331bfffc0266572f171fa49456f0799f56ec
Noah Cohn <noahc42@gmail.com>
2023-12-12 11:05:38 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -537,7 +537,7 @@ return EM_TRUE;
 extern "C"{
 
 void frm(uint8_t * h){
-// framm(h);
+framm(h);
 return;
 }
 
@@ -551,17 +551,15 @@ let vv=document.getElementById('mv');
 let cnv=document.getElementById('bcanvas');
 let ctx=cnv.getContext('2d',{willReadFrequently:true});
 let H=Module.HEAPU8.buffer;
-let dataSize=cnv.width*cnv.height*4;
+let dataSize=cnv.width*cnv.height;
 setInterval(function(){
 ctx.drawImage(vv,0,0);
-
-  
 let imageData=ctx.getImageData(0,0,cnv.width,cnv.height);
 let pixelData=new Uint8ClampedArray(imageData.data);
 let heapArray=new Uint8ClampedArray(H,0,dataSize);
-// heapArray.set(pixelData,0,dataSize);
-// Module.ccall("frm",null,["Number"],[0]);
-},16);
+heapArray.set(pixelData,0,dataSize);
+Module.ccall("frm",null,["Number"],[0]);
+},100);
 }
   
 function normalResStart(){
