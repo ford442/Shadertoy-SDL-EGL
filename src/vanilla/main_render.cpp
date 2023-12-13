cfc5051749e3cea94a63e4dddf1b3f857e0f0757
Noah Cohn <noahc42@gmail.com>
2023-12-13 00:06:41 -0800

    Update main_render.cpp

--- src/vanilla/main_render.cpp
+++ src/vanilla/main_render.cpp
@@ -197,19 +197,11 @@ wgpu_queue_write_buffer(wq.at(0,0),wb.at(0,0),0,&u64_uni.at(0,0),sizeof(uint64_t
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(1,1),0,&u64_uni.at(1,1),sizeof(uint64_t));
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(2,2),0,&u64_uni.at(2,2),sizeof(uint64_t));
 wgpu_render_pass_encoder_set_viewport(wrpe.at(0,0),0.0,0.0,sze.at(0,0),sze.at(0,0),0.0f,1.0f);
+wgpu_render_pass_encoder_draw(wrbe.at(0,0),6,1,0,0);
 wgpu_render_pass_encoder_end(wrpe.at(0,0));
 wcb.at(0,0)=wgpu_command_encoder_finish(wce.at(0,0));
 wgpu_queue_submit_one_and_destroy(wq.at(0,0),wcb.at(0,0));
 wq.at(0,0)=wgpu_device_get_queue(wd.at(0,0));
-
-wrbe.at(0,0)=wgpu_command_encoder_begin_render_pass(wce.at(0,0),&wrbd.at(0,0));
-wgpu_render_pass_encoder_set_pipeline(wrbe.at(0,0),wrp.at(0,0));
-wgpu_encoder_set_bind_group(wrbe.at(0,0),0,wbg.at(0,0),0,0);
-wgpu_render_pass_encoder_draw(wrbe.at(0,0),6,1,0,0);
-wgpu_render_pass_encoder_end(wrbe.at(0,0));
-wcb.at(0,0)=wgpu_command_encoder_finish(wce.at(0,0));
-wgpu_queue_submit_one_and_destroy(wq.at(0,0),wcb.at(0,0));
-
 return;
 }
 
