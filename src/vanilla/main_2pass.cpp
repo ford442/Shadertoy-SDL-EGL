24b811a391a26a9377c3c5e2a6edf58c46e5c06a
Noah Cohn <noahc42@gmail.com>
2023-12-15 07:39:57 -0800

    Update main_2pass.cpp

--- src/vanilla/main_2pass.cpp
+++ src/vanilla/main_2pass.cpp
@@ -52,6 +52,8 @@ WGpuBufferBindingLayout bufferBindingLayout1={WGPU_BUFFER_BINDING_LAYOUT_DEFAULT
 WGpuTextureBindingLayout textureBindingLayout={};
 WGpuColorTargetState colorTarget={};
 WGpuColorTargetState colorTarget2={};
+WGpuCommandEncoder wceA={};
+WGpuCommandEncoder wceB={};
 
 double szh,szw;
 int szhI,szwI;
@@ -175,8 +177,8 @@ wq.at(0,0)=wgpu_device_get_queue(wd.at(0,0));
 // wTime.iTime=get_current_time_in_milliseconds();
 bindgroup=wgpu_device_create_bind_group(wd.at(0,0),wbgl.at(0,0),wbge.at(0,0),3);
 wbg.at(0,0)=bindgroup;
-wce.at(0,0)=wgpu_device_create_command_encoder(wd.at(0,0),0);
-wce.at(1,1)=wgpu_device_create_command_encoder(wd.at(0,0),0);
+  
+
 colorAttachment={WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER};
 // colorTexture=wgpu_device_create_texture(wd.at(1,1),&wtd.at(1,1));
 colorTexture=wgpu_canvas_context_get_current_texture(wcc.at(0,0));
@@ -213,6 +215,8 @@ passDesc2={};
 passDesc2.numColorAttachments=0;
 passDesc2.depthStencilAttachment=wrpdsa.at(0,0);
 wrpd.at(1,1)=passDesc2;
+wceA=wgpu_device_create_command_encoder(wd.at(0,0),0);
+wce.at(0,0)=wceA;
 wrpe.at(0,0)=wgpu_command_encoder_begin_render_pass(wce.at(0,0),&wrpd.at(0,0));
 wgpu_render_pass_encoder_set_pipeline(wrpe.at(0,0),wrp.at(0,0));
 wgpu_encoder_set_bind_group(wrpe.at(0,0),0,wbg.at(0,0),0,0);
@@ -224,7 +228,9 @@ wgpu_render_pass_encoder_draw(wrpe.at(0,0),6,1,0,0);
 wgpu_render_pass_encoder_end(wrpe.at(0,0));
 wcb.at(0,0)=wgpu_command_encoder_finish(wce.at(0,0));
 wgpu_queue_submit_one_and_destroy(wq.at(0,0),wcb.at(0,0));
-wrpe.at(0,0).destroy();
+wceA.destroy();
+wceB=wgpu_device_create_command_encoder(wd.at(0,0),0);
+wce.at(1,1)=wceB;
 wrpe.at(1,1)=wgpu_command_encoder_begin_render_pass(wce.at(1,1),&wrpd.at(1,1));
 wgpu_render_pass_encoder_set_pipeline(wrpe.at(1,1),wrp.at(1,1));
 wgpu_encoder_set_bind_group(wrpe.at(1,1),0,wbg.at(0,0),0,0);
@@ -233,7 +239,7 @@ wgpu_render_pass_encoder_draw(wrpe.at(1,1),6,1,0,0);
 wgpu_render_pass_encoder_end(wrpe.at(1,1));
 wcb.at(1,1)=wgpu_command_encoder_finish(wce.at(1,1));
 wgpu_queue_submit_one_and_destroy(wq.at(0,0),wcb.at(1,1));
-wrpe.at(1,1).destroy();
+wceB.destroy();
 return;
 }
 
