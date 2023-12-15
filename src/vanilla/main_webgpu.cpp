6b5afca1ee1b511e1999cf43b24a1686a81599ca
Noah Cohn <noahc42@gmail.com>
2023-12-15 01:57:32 -0800

    Update main_webgpu.cpp

--- src/vanilla/main_webgpu.cpp
+++ src/vanilla/main_webgpu.cpp
@@ -267,8 +267,8 @@ if(WGPU_BufferStatus.at(0,0,0)==3){
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 }
 // wgpu_buffer_map_async(WGPU_Buffers.at(1,0,1),WGPU_MapCallback.at(0,0,1),&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
-usleep(42000);
-runWebGPU();
+// usleep(42000);
+// runWebGPU();
 return;
 };
 
@@ -537,7 +537,7 @@ normalResStart();
 
 document.getElementById('startBtn2').addEventListener('click',function(){
 // myStopFunction();
-Module.ccall("runWebGPU");
+// Module.ccall("runWebGPU");
 	
 // Module.ccall("runWebGPU2");
 for(var i=0;i<420;i++){
