915349a4900a092e0c8b1a55ef10f0562d61866a
Noah Cohn <noahc42@gmail.com>
2023-12-17 19:34:14 -0800

    Update main_128f.cpp

--- src/vanilla/main_128f.cpp
+++ src/vanilla/main_128f.cpp
@@ -138,6 +138,15 @@ const char * vertexShader =
 "return vec4<f32>(pos[vertexIndex], 0.0f, 1.0f);"
 "}\n";
 
+const char * fragHeader =
+"#define UNPACK_UINT8(u32Value) ((u32Value & 0xff) / 255.0)\n"
+"@group(0)@binding(0)var<uniform>Time:u32;"
+"@group(0)@binding(1)var<uniform>iFrame:u32;"
+"@group(0)@binding(2)var<uniform>Resolution:u32;"
+"var<private> iTime: f32 = UNPACK_UINT8(Time);"
+"var<private> iResolution: vec3<f32> = vec3<f32>(vec2<f32>(UNPACK_UINT8(Resolution)),1.0f);";
+
+
 const char * fragEntry="main_1";
 
 const char * Fnm=reinterpret_cast<const char *>("/shader/shader.glsl");
@@ -250,6 +259,10 @@ void ObtainedWebGpuDeviceStart(WGpuDevice result, void *userData){
 wd.at(0,0)=result;
 wcc.at(0,0)=wgpu_canvas_get_webgpu_context("canvas");
 const char * frag_body=(char*)rd_fl(Fnm);
+      #include <string.h>
+char full_frag_body[strlen(frag_body) + strlen(fragHeader) + 1];
+strcpy(full_frag_body, fragHeader);
+strcat(full_frag_body, frag_body);
 // canvasFormat=navigator_gpu_get_preferred_canvas_format();
 // wtf.at(0,0)=WGPU_TEXTURE_FORMAT_BGRA8UNORM;
 // wtf.at(0,0)=WGPU_TEXTURE_FORMAT_RGB10A2UNORM;
@@ -302,7 +315,7 @@ wcts.at(1,1)=colorTarget2;
 shaderModuleDescV={};
 shaderModuleDescF={};
 fragHint.entryPointName=fragEntry;
-shaderModuleDescF.code=frag_body;
+shaderModuleDescF.code=full_frag_body;
 // shaderModuleDescF.hint=fragHint;
 shaderModuleDescV.code=vertexShader;
 vs=wgpu_device_create_shader_module(wd.at(0,0),&shaderModuleDescV);
