fc783f5657e82cc0c035a1b110f0ace047d042e1
Noah Cohn <noahc42@gmail.com>
2023-12-18 14:45:15 -0800

    Update main_128f.cpp

--- src/vanilla/main_128f.cpp
+++ src/vanilla/main_128f.cpp
@@ -626,7 +626,7 @@ let strng=event.data;
 console.log('String: ',strng);
 // strng=unescape(encodeURIComponent(strng));
 console.log('String encodeURIComponent: ',strng);
-let cfil=new Uint8ClampedArray(strng);
+let cfil=new Uint8Array(strng);
 // for(let i=0;i<strng.length;){
 // cfil[i] = strng.charCodeAt(i);
 // i++;
