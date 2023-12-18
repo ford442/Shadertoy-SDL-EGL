bd4bc765b06b14e07823b2e4c735169fb4a2331d
Noah Cohn <noahc42@gmail.com>
2023-12-18 07:35:53 -0800

    Update main_128f.cpp

--- src/vanilla/main_128f.cpp
+++ src/vanilla/main_128f.cpp
@@ -260,7 +260,7 @@ wd.at(0,0)=result;
 wcc.at(0,0)=wgpu_canvas_get_webgpu_context("canvas");
 const char * frag_body=(char*)rd_fl(Fnm);
       #include <string.h>
-char full_frag_body[strlen(frag_body) + strlen(fragHeader) + 1];
+char full_frag_body[strlen(fragHeader) + strlen(frag_body) + 1];
 strcpy(full_frag_body, fragHeader);
 strcat(full_frag_body, frag_body);
 // canvasFormat=navigator_gpu_get_preferred_canvas_format();
