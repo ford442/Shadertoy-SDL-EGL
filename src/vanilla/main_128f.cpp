ce3a59c996f0726710c9f3daa36574654ecf7b62
Noah Cohn <noahc42@gmail.com>
2023-12-18 16:00:26 -0800

    Update main_128f.cpp

--- src/vanilla/main_128f.cpp
+++ src/vanilla/main_128f.cpp
@@ -624,7 +624,7 @@ let codeMessage=new BroadcastChannel('codeMessage');
 codeMessage.addEventListener('message',function(event){
 let strng=event.data;
 console.log('String: ',strng);
-let cfil=new Int8Array(strng);
+let cfil=new Uint8ClampedArray(strng);
 for (let i = 0; i < strng.length; i++) {
 cfil[i] = strng.charCodeAt(i);
 }
