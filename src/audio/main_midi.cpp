3df2a6e046fe6250e52a30806c7e2778362e0408
Noah Cohn <noahc42@gmail.com>
2023-12-27 02:19:55 -0800

    Update main_midi.cpp

--- src/audio/main_midi.cpp
+++ src/audio/main_midi.cpp
@@ -66,7 +66,7 @@ normalResStart();
 static inline boost::function<void()>jss=[](){js_main();};
 
 int main(){
-emscripten_async_load_script("./test.js",null,null);
+emscripten_async_load_script("./test.js",nullptr,nullptr);
 tie(jss,js_main);
 jss();
 return 0;
