a6ec123a9d45d4d418a3fb034031db20de2b30ee
Noah Cohn <noahc42@gmail.com>
2023-12-31 22:41:02 -0800

    Update drum.js

--- js/drum.js
+++ js/drum.js
@@ -5,7 +5,7 @@ window.open('./drum/o8o.htm');
 let drumTempo=new BroadcastChannel('tempo');
 
 drumTempo.addEventListener('message',ea=>{
-var tmp0=ea.data.data;
+var tmp0=parseInt(ea.data.data);
 Module.ccall('tempoSet',null,["Number"],[tmp0]);
 });
 
