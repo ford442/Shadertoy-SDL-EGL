b0da03beba98247ad77c6b05c5c75b1618de9c50
Noah Cohn <noahc42@gmail.com>
2024-01-03 00:30:28 -0800

    Update main_compute.hpp

--- include/vanilla/main_compute.hpp
+++ include/vanilla/main_compute.hpp
@@ -12,7 +12,7 @@ using namespace std;
 
 
 #include "/content/RAMDRIVE2/aubio/src/aubio.h"
-#include "/content/RAMDRIVE2/aubio/examples/utils.c"
+#include "/content/RAMDRIVE2/aubio/src/utils/parameter.c"
 #include "/content/RAMDRIVE2/aubio/src/types.h"
 #include "/content/RAMDRIVE2/aubio/src/fvec.c"
 #include "/content/RAMDRIVE2/aubio/src/lvec.c"
