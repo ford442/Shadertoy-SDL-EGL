dee69dfd7933d64ba72720236f83febd4fefb80b
Noah Cohn <noahc42@gmail.com>
2024-01-05 01:10:16 -0800

    Update main_onnx.hpp

--- include/vanilla/main_onnx.hpp
+++ include/vanilla/main_onnx.hpp
@@ -89,7 +89,7 @@ void runWebGPU();
 }
 
 
-using dv_tensor=boost::numeric::ublas::tensor<SDL_AudioDeviceID>;
+// using dv_tensor=boost::numeric::ublas::tensor<SDL_AudioDeviceID>;
 using void_tensor=boost::numeric::ublas::tensor<boost::atomic<void *>>;
 using gi_tensor=boost::numeric::ublas::tensor<boost::atomic<long>>;
 using ub_tensor=boost::numeric::ublas::tensor<boost::atomic<unsigned char *>>;
