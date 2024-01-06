a5bf5b7f0d68a10ba1c311923f70dee4b8981dca
Noah Cohn <noahc42@gmail.com>
2024-01-05 23:00:20 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -668,7 +668,7 @@ setInterval(function(){
 gl2.drawImage(vv,0,0);
 imageData=gl2.getImageData(0,0,cnv.height,cnv.height);
 let pixelData=new Uint8Array(imageData.data.buffer);
-let heapArray=new Uint8Array(H,0,imageData.data.length);
+let heapArray=new Uint8Array(H,0,cnv.height*cnv.height*4);
 heapArray.set(pixelData);
 // Module.ccall("frm",null,["Number"],[0]);
 },50);
