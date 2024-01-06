869fe6c8d619071d9ddea387b34591ac999c6f3c
Noah Cohn <noahc42@gmail.com>
2024-01-06 11:32:33 -0800

    Update main_compute.cpp

--- src/vanilla/main_compute.cpp
+++ src/vanilla/main_compute.cpp
@@ -136,6 +136,7 @@ static wsd_tensor wsd=wsd_tensor{2,2};
 static ws_tensor wgpu_sampler=ws_tensor{2,2};
 static wsbl_tensor wsbl=wsbl_tensor{2,2};
 static wvbl_tensor wvbl=wvbl_tensor{2,2};
+static wict_tensor wict=wict_tensor{2,2};
 
 /*
 static mouse_tensor mms=mouse_tensor{2,2};
@@ -507,7 +508,7 @@ wgpu_encoder_end(WGPU_ComputePassCommandEncoder.at(0,0,0));
 wgpu_command_encoder_copy_buffer_to_texture(WGPU_CommandEncoder.at(0,0,0),&WGPU_Output_Buffer,&WGPU_Output_Image,256,256,1);
 // wgpu_command_encoder_copy_buffer_to_buffer(WGPU_CommandEncoder.at(0,0,0),WGPU_Buffers.at(0,0,0),0,WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
 wgpu_command_encoder_copy_texture_to_buffer(WGPU_CommandEncoder.at(0,0,0),&WGPU_Output_Image,&WGPU_Mapped_Buffer,256,256,1);
-wgpu_command_encoder_copy_texture_to_buffer(WGPU_CommandEncoder.at(0,0,0),&WGPU_Output_Image,&wb.at(3,3),1,64,1);
+// wgpu_command_encoder_copy_texture_to_buffer(WGPU_CommandEncoder.at(0,0,0),&WGPU_Output_Image,&wb.at(3,3),1,64,1);
 // WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
 WGPU_CommandBuffer.at(0,0,0)=wgpu_encoder_finish(WGPU_CommandEncoder.at(0,0,0));
 // if(WGPU_BufferStatus.at(0,0,0)!=1){
@@ -526,7 +527,7 @@ wce.at(0,0)=wceA;
 wrpe.at(0,0)=wgpu_command_encoder_begin_render_pass(wce.at(0,0),&wrpd.at(0,0));
 wgpu_render_pass_encoder_set_pipeline(wrpe.at(0,0),wrp.at(0,0));
 wgpu_encoder_set_bind_group(wrpe.at(0,0),0,wbg.at(0,0),0,0);
-// wgpu_command_encoder_copy_texture_to_buffer(wrpe.at(0,0),&WGPU_Output_Image,&wb.at(3,3),1,64,1);
+wgpu_command_encoder_copy_texture_to_buffer(wrpe.at(0,0),&wict.at(0,0),&wb.at(3,3),1,64,1);
 // wgpu_command_encoder_copy_buffer_to_buffer(wrpe.at(0,0),WGPU_Buffers.at(2,0,2),0,wb.at(3,3),0,64);
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(0,0),0,&u64_uni.at(0,0),sizeof(uint64_t));
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(1,1),0,&u64_uni.at(1,1),sizeof(uint64_t));
@@ -564,6 +565,7 @@ WGPU_Texture.at(0,0,0)=wgpu_device_create_texture(wd.at(0,0),&WGPU_TextureDescri
 WGPU_Texture.at(0,0,1)=wgpu_device_create_texture(wd.at(0,0),&WGPU_TextureDescriptor.at(0,0,0));
 WGPU_Input_Image.texture=WGPU_Texture.at(0,0,0);
 WGPU_Output_Image.texture=WGPU_Texture.at(0,0,1);
+wict.at(0,0)=WGPU_Output_Image;
 WGPU_TextureViewDescriptor.at(0,0,0)=textureViewDescriptorA;
 WGPU_ResultBuffer.at(0,0,0)=WGPU_Result_Array;
 WGPU_InputBuffer.at(0,0,0)=WGPU_Input_Array;
