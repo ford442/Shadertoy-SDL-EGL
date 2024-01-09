d57d5042506e5f2e8c250b019642dcb42afd27de
Noah Cohn <noahc42@gmail.com>
2024-01-09 05:15:39 -0800

    Update video_jebus2.cpp

--- src/video/video_jebus2.cpp
+++ src/video/video_jebus2.cpp
@@ -17,15 +17,17 @@
 #define GL_FRAGMENT_PRECISION_HIGH 1
 #define GL3_PROTOTYPES 1
 
+#include <GLES3/gl31.h>
+// #include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 #include <GLES3/gl3.h>
-// #include <GLES3/gl31.h>
-// #include <GLES3/gl32.h>
 #include <GLES3/gl3platform.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
+// #include <EGL/eglplatform.h>
 #include <GL/gl.h>
 #include <GL/glext.h>
+#include <emscripten/html5_webgl.h>
 
 void avgFrm(int Fnum,int leng,float *ptr,float *aptr){
 float max=0.0f;
