2215c88bc12462b34800c649957c1b876846c199
Noah Cohn <noahc42@gmail.com>
2024-01-09 05:03:50 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -172,7 +172,9 @@ const char * frag_body=
 "@group(0) @binding(2) var myTexture : texture_2d <f32>;\n"
 "@fragment\n"
 "fn main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {\n"
-"return textureSample(myTexture, mySampler, fragUV);\n"
+"var sample1 = textureSample( tex, tex_sampler, uv );"
+"var outColor: vec4<f32> = sample1;"
+"return vec4<f32>(outColor);"
 "}\n";
 
 const char * Fnm=reinterpret_cast<const char *>("/shader/shader.glsl");
