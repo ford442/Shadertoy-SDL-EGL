801452b909114895676d5787703e6a8692d6f40c
Noah Cohn <noahc42@gmail.com>
2024-01-17 13:45:04 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -210,7 +210,7 @@ const char * frag_body=
 "@group(0) @binding(0) var <uniform> iTime : u32;\n"
 "@group(0) @binding(1) var mySampler : sampler;\n"
 "@group(0) @binding(2) var myTexture : texture_2d <f32>;\n"
-"@group(0) @binding(3) var extTexture : texture_external <f32>;\n"
+"@group(0) @binding(3) var extTexture : texture_external;\n"
 "@fragment\n"
 "fn main(@location(0) fragUV : vec2<f32>) ->\n"
 "@location(0) vec4<f32> {\n"
