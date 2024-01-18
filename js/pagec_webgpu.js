ff7fbc6c17df652e2a8ae741b47cfb763319b9d0
Noah Cohn <noahc42@gmail.com>
2024-01-18 11:12:30 -0800

    Update pagec_webgpu.js

--- js/pagec_webgpu.js
+++ js/pagec_webgpu.js
@@ -225,7 +225,7 @@ pnnl.addEventListener('keydown',spKey);
 
 function loada(){
 if(lockVid!=1){
-// document.getElementById('ldv').height=document.innerHeight;
+document.getElementById('ldv').height=document.innerHeight;
 loadV.addEventListener('canplay',function(){
 loadV.width=this.videoWidth;
 loadV.height=this.videoHeight;
@@ -271,7 +271,7 @@ vide[0].id=lo;
 vide[1].id=mv;
 document.getElementById('mv').play();
 document.getElementById('ldv').src=document.getElementById('isrc').innerHTML;
-// document.getElementById('mv').height=document.innerHeight;
+document.getElementById('mv').height=document.innerHeight;
 document.getElementById('ldv').currentTime=document.getElementById('itim').innerHTML;
 document.getElementById('ldv').load();
 $iwid.innerHTML=parseInt($w,10);
