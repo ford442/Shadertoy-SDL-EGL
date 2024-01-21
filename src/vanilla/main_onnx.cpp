e03e67b9cf3c54f12a945f4f83b7ceb8cce49929
Noah Cohn <noahc42@gmail.com>
2024-01-21 15:00:49 -0800

    Update main_onnx.cpp

--- src/vanilla/main_onnx.cpp
+++ src/vanilla/main_onnx.cpp
@@ -58,7 +58,7 @@ char model_path[12]="/model.onnx";
 const int64_t batchSize=2;
  Ort::SessionOptions sessionOptions;
 
-sessionOptions.SetIntraOpNumThreads(1);
+// sessionOptions.SetIntraOpNumThreads(1);
 		 // Sets graph optimization level
     // Available levels are
     // ORT_DISABLE_ALL -> To disable all optimizations
@@ -66,7 +66,7 @@ sessionOptions.SetIntraOpNumThreads(1);
     // removals) ORT_ENABLE_EXTENDED -> To enable extended optimizations
     // (Includes level 1 + more complex optimizations like node fusions)
     // ORT_ENABLE_ALL -> To Enable All possible optimizations
-sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_DISABLE_ALL);
+// sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_DISABLE_ALL);
 std::cout << "got ORT session/options" << std::endl;
 		std::cout << "got ORT SessionOptions" << std::endl;
 // Ort::Session sesh=session(ort_env,model_path,sessionOptions );
