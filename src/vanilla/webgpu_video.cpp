77a63e4a766baf011a68728159258c3b527cb34a
Noah Cohn <noahc42@gmail.com>
2024-01-21 16:17:10 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -715,7 +715,7 @@ setInterval(function(){
 gl2.drawImage(vvi,0,0);
 image=gl2.getImageData(0,0,cnv.width,cnv.height);
 imageData=image.data;
-let pixelData=new Uint8Array(imageData);
+let pixelData=new Uint8ClampedArray(imageData);
 FS.writeFile('/video/frame.gl',pixelData);
 },16.666);
 /*
