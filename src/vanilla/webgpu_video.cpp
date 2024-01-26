6d77ea46893ef6116b249b731ef01300cd68d235
Noah Cohn <noahc42@gmail.com>
2024-01-25 16:26:44 -0800

    Update webgpu_video.cpp

--- src/vanilla/webgpu_video.cpp
+++ src/vanilla/webgpu_video.cpp
@@ -789,20 +789,19 @@ FS.writeFile('/video/frame.gl',frrm);
 let cnv=document.querySelector('#bcanvas');
 cnv.height=h$;
 cnv.width=h$;
-let offS=0.0-(Math.floor((w$-h$)/2.0));
-const gl2=cnv.getContext('2d',{willReadFrequently:false,alpha:true}); // colorType:'float32',
-// gl2.drawImage(vvi,offS,0,h$,h$,0,0,h$,h$);
-gl2.drawImage(vvi,0,0,h$,h$,0,0,h$,h$);
+let offS=Math.floor((w$-h$)/2.0);
+const gl2=cnv.getContext('2d',{colorType:'float32',willReadFrequently:false,alpha:true}); // 
+gl2.drawImage(vvi,offS,0,h$,h$,0,0,h$,h$);
 let image=gl2.getImageData(0,0,cnv.height,cnv.height);
 let imageData=image.data;
 // Module.ccall("frm",null,['Number'],['Number'],h$,h$);
 setInterval(function(){
-// gl2.drawImage(vvi,offS,0,h$,h$,0,0,h$,h$);
-gl2.drawImage(vvi,0,0,h$,h$,0,0,h$,h$);
+gl2.drawImage(vvi,offS,0,h$,h$,0,0,h$,h$);
 image=gl2.getImageData(0,0,cnv.height,cnv.height);
 imageData=image.data;
 // imageData=cropFrameToSquare(imageData,w$,h$);
 // let pixelData=new Uint8ClampedArray(imageData);
+let pixelData=new Float32Array(imageData);
 FS.writeFile('/video/frame.gl',imageData);
 },16.6);
 /*
