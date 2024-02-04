9ce99b9ae634eac9bb095a0ee2e8c5b91dbdba0b
Noah Cohn <noahc42@gmail.com>
2024-02-03 19:13:05 -0800

    Update main_onnx.cpp

--- src/vanilla/main_onnx.cpp
+++ src/vanilla/main_onnx.cpp
@@ -142,7 +142,7 @@ std::vector<const char*>outputNames={"last_hidden_state","pooler_output"};
 std::cout << "Establishing tensor names" << std::endl;
 Ort::MemoryInfo memoryInfo=Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator,OrtMemTypeCPU);
 std::cout << "Establishing memoryInfo" << std::endl;
-std::vector<Ort::Value> inputTensors;
+std::array<Ort::Value,12> inputTensors;
 Ort::Value outputTensors{nullptr};
 inputTensors.push_back(Ort::Value::CreateTensor<int32_t>(memoryInfo,inputTensorValues.data(),inputTensorSize,&inputDims.at(0),6));
 	
