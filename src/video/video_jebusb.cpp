6dd5c070e3ac926ef9a311622ae914ec412181c1
Noah Cohn <noahc42@gmail.com>
2024-02-08 10:58:09 -0800

    Update video_jebusb.cpp

--- src/video/video_jebusb.cpp
+++ src/video/video_jebusb.cpp
@@ -482,7 +482,7 @@ for(i=64;i>0;i--){
 var loca=$F+1;if(loca>64){loca=1;}
 var locb=$Bu+1;if(locb>64){locb=1;}
 // eval("if ($F==="+i+"){var $r"+i+"=t($"+i+");r($r"+i+");var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
-eval("if ($F==="+i+"){var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");FS.writeFile('/video/frame"+$Bu+".gl',$"+$Bu+");$F="+loca+";$Bu="+locb+";var ff=new Float32Array(FS.readFile('/video/frame"+$Bu+".gl'));var $r"+i+"=t(ff);r($r"+i+");}");
+eval("if ($F==="+i+"){var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");FS.writeFile('/video/frame"+$Bu+".gl',$"+$Bu+");$F="+loca+";$Bu="+locb+";var ff=new Float32Array(FS.readFile('/video/frame"+$Bu+".gl',{encoding:'binary'}));var $r"+i+"=t(ff);r($r"+i+");}");
 }
 var $bb=R(vv);
 $B.set($bb,0,sz);
