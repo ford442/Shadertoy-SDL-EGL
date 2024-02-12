019050b36822601151fd195a5f5b5b60afd1e729
Noah Cohn <noahc42@gmail.com>
2024-02-12 10:14:27 -0800

    Update webgpu_misr.cpp

--- src/vanilla/webgpu_misr.cpp
+++ src/vanilla/webgpu_misr.cpp
@@ -665,7 +665,7 @@ wgpu_encoder_set_bind_group(wrpe.at(0,0),0,wbg.at(0,0),0,0);
 /*
 void wgpu_command_encoder_copy_texture_to_texture(WGpuCommandEncoder commandEncoder, const WGpuImageCopyTexture *source NOTNULL, const WGpuImageCopyTexture *destination NOTNULL, uint32_t copyWidth, uint32_t copyHeight _WGPU_DEFAULT_VALUE(1), uint32_t copyDepthOrArrayLayers _WGPU_DEFAULT_VALUE(1));
 */
-  wgpu_command_encoder_copy_texture_to_texture(wrpe.at(0,0),&wict.at(2,2),&wict.at(0,0),sze.at(0,0),sze.at(0,0),1);
+  wgpu_command_encoder_copy_texture_to_texture(wrpe.at(0,0),&wict.at(1,1),&wict.at(0,0),sze.at(0,0),sze.at(0,0),1);
 
   // wgpu_queue_write_texture(wq.at(0,0),&wict.at(1,1),&frame_tensorGL.at(0,0),sze.at(1,1)*4,sze.at(1,1),sze.at(1,1),sze.at(1,1),1);
 
