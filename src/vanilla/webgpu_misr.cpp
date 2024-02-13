1e8c8175e9a67e67038cd9b3e0e6a019f375fac0
Noah Cohn <noahc42@gmail.com>
2024-02-13 02:07:57 -0800

    Update webgpu_misr.cpp

--- src/vanilla/webgpu_misr.cpp
+++ src/vanilla/webgpu_misr.cpp
@@ -223,7 +223,8 @@ inline char wgl_cmp_src[2000]=
 "for(var x=0u;x<sizeOUTu;x++){\n"
 // "if(x*y<=sizeOUTu*sizeOUTu){\n"
 "var INtexCoord:vec2<u32>=vec2<u32>(x,y)/vec2<u32>(sizeINu.x,sizeINu.y);\n"
-"var color:vec4<f32>=textureLoad(textureIN,INtexCoord,0);\n"
+// "var color:vec4<f32>=textureLoad(textureIN,INtexCoord,0);\n"
+"var color:vec4<f32>=vec4<f32>(inputBuffer[2],inputBuffer[3],inputBuffer[4],1.0f);\n"
 // "let color32u:vec4<f32>=clamp(vec4<f32>(round(color*255.0)),vec4<u32>(0u,0u,0u,0u),vec4<u32>(255u,255u,255u,255u));\n"
 // "outputBuffer[0]=0.7777-(0.0001*);\n"
 "textureStore(textureOUT,vec2<u32>(u32(x),u32(y)),color);\n"
@@ -581,8 +582,8 @@ wtv.at(1,1)=colorTextureView;
 // colorAttachment.view=wgpu_texture_create_view(wgpu_canvas_context_get_current_texture(wcc.at(0,0)),0);
 colorAttachment.view=wtv.at(1,1);
 colorAttachment.storeOp=WGPU_STORE_OP_STORE;
-// colorAttachment.loadOp=WGPU_LOAD_OP_LOAD;
-colorAttachment.loadOp=WGPU_LOAD_OP_CLEAR;
+colorAttachment.loadOp=WGPU_LOAD_OP_LOAD;
+// colorAttachment.loadOp=WGPU_LOAD_OP_CLEAR;
 colorAttachment.clearValue.r=0.8;
 colorAttachment.clearValue.g=0.0;
 colorAttachment.clearValue.b=0.3;
@@ -633,6 +634,9 @@ wgpu_queue_write_texture(wq.at(0,0),&wict.at(2,2),&frame_tensorf.at(0,0),sze.at(
 // raN=rNd4(256);
 WGPU_InputBuffer.at(0,0,0)[0]=szef.at(1,1);
 WGPU_InputBuffer.at(0,0,0)[1]=szef.at(0,0);
+WGPU_InputBuffer.at(0,0,0)[2]=u64_uni.at(0,0)/22.0f;
+WGPU_InputBuffer.at(0,0,0)[3]=u64_uni.at(1,1)*0.001f;
+WGPU_InputBuffer.at(0,0,0)[4]=u64_uni.at(1,1)%1.0f;
 wgpu_queue_write_buffer(WGPU_Queue.at(0,0,0),WGPU_Buffers.at(1,1,1),0,WGPU_InputBuffer.at(0,0,0),InputBufferBytes);
 // wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&WGPU_Input_Image,&WGPU_ColorBuffer.at(0,0,0),1024,0,1,1,1);
   
