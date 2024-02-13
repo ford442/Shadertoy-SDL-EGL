1349b471d7605be41e2b535d96d51c89c1056350
Noah Cohn <noahc42@gmail.com>
2024-02-12 16:42:15 -0800

    Update webgpu_misr.cpp

--- src/vanilla/webgpu_misr.cpp
+++ src/vanilla/webgpu_misr.cpp
@@ -225,7 +225,7 @@ inline char wgl_cmp_src[2000]=
 "let INtexCoord:vec2<u32>=vec2<u32>(u32(x),u32(y))/vec2<u32>(sizeINu.x);\n"
 "let color:vec4<f32>=textureLoad(textureIN,INtexCoord,0);\n"
 "let color32u:vec4<u32>=clamp(vec4<u32>(round(color*255.0)),vec4<u32>(0u,0u,0u,0u),vec4<u32>(255u,255u,255u,255u));\n"
-"textureStore(textureOUT,vec2<u32>(u32(x),u32(y)),color32u);\n"
+"textureStore(textureOUT,vec2<u32>(x,y),color32u);\n"
 // "}"
 "}"
 "}"
