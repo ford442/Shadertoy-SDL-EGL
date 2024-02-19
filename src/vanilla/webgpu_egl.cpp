d0ed06b01a67b996a57169c26b8ef4d61c508a9f
Noah Cohn <noahc42@gmail.com>
2024-02-19 00:45:16 -0800

    Update webgpu_egl.cpp

--- src/vanilla/webgpu_egl.cpp
+++ src/vanilla/webgpu_egl.cpp
@@ -575,7 +575,7 @@ wgpu_queue_write_buffer(wq.at(0,0),wb.at(1,1),0,&u64_uni.at(1,1),sizeof(uint64_t
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(2,2),0,&u64_uni.at(2,2),sizeof(uint64_t));
 wgpu_render_pass_encoder_set_index_buffer(wrpe.at(0,0),wb.at(4,4),WGPU_INDEX_FORMAT_UINT32,0,36*sizeof(uint32_t));
 wgpu_render_pass_encoder_set_vertex_buffer(wrpe.at(0,0),0,wb.at(3,3),0,sizeof(vertices));
-wgpu_render_pass_encoder_set_viewport(wrpe.at(0,0),0.0f,0.0f,szef.at(0,0),szef.at(0,0),0.0f,1.0f);
+wgpu_render_pass_encoder_set_viewport(wrpe.at(0,0),szef.at(1,1),szef.at(1,1),szef.at(0,0),szef.at(0,0),0.0f,1.0f);
 wgpu_render_pass_encoder_set_scissor_rect(wrpe.at(0,0),0.0f,0.0f,sze.at(0,0),sze.at(0,0));
 wgpu_render_pass_encoder_draw_indexed(wrpe.at(0,0),36,1,0,0,0);
 wgpu_render_pass_encoder_end(wrpe.at(0,0));
@@ -590,7 +590,7 @@ wgpu_render_pass_encoder_set_pipeline(wrpe.at(1,1),wrp.at(1,1));
 wgpu_encoder_set_bind_group(wrpe.at(1,1),0,wbg.at(0,0),0,0);
 wgpu_render_pass_encoder_set_index_buffer(wrpe.at(1,1),wb.at(4,4),WGPU_INDEX_FORMAT_UINT32,0,36*sizeof(uint32_t));
 wgpu_render_pass_encoder_set_vertex_buffer(wrpe.at(1,1),0,wb.at(3,3),0,sizeof(vertices));
-wgpu_render_pass_encoder_set_viewport(wrpe.at(1,1),0.0f,0.0f,szef.at(0,0),szef.at(0,0),0.0f,1.0f);
+wgpu_render_pass_encoder_set_viewport(wrpe.at(1,1),szef.at(1,1),szef.at(1,1),szef.at(0,0),szef.at(0,0),0.0f,1.0f);
 wgpu_render_pass_encoder_set_scissor_rect(wrpe.at(1,1),0.0f,0.0f,sze.at(0,0),sze.at(0,0));
 wgpu_render_pass_encoder_draw_indexed(wrpe.at(1,1),36,1,0,0,0);
 wgpu_render_pass_encoder_end(wrpe.at(1,1));
@@ -741,6 +741,7 @@ emscripten_get_canvas_element_size("canvas",&szhI,&szwI);
 u64_siz.at(0,0)=szh;
 sze.at(0,0)=szh;
 szef.at(0,0)=floor(sze.at(0,0));
+szef.at(1,1)=0.0f;
 sze.at(0,1)=szhI;
 multiSamp={};
 multiSamp.count=4;
@@ -1058,6 +1059,7 @@ colorAttachment={WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER};
 u64_uni.at(0,0)=0;
 u64_uni.at(1,1)=0;
 u64_uni.at(2,2)=u64_siz.at(0,0);
+u64_uni.at(3,3)=u64_siz.at(0,0);
 u_time.t1=boost::chrono::high_resolution_clock::now();
 u_time.t2=boost::chrono::high_resolution_clock::now();
 u_time.t3=boost::chrono::high_resolution_clock::now();
@@ -1390,12 +1392,14 @@ document.querySelector('#di').click();
 });
 
 void uniUp(){
-u64_uni.at(2,2)=int(u64_uni.at(2,2)*1.01);
+u64_uni.at(2,2)=u64_uni.at(2,2)+int(u64_uni.at(3,3)*0.01);
+resf.at(1,1)=resf.at(1,1)+float(u64_uni.at(3,3)*0.01);
 return;
 }
 
 void uniDown(){
-u64_uni.at(2,2)=int(u64_uni.at(2,2)*0.99);
+u64_uni.at(2,2)=u64_uni.at(2,2)-int(u64_uni.at(3,3)*0.01);
+resf.at(1,1)=resf.at(1,1)-float(u64_uni.at(3,3)*0.01);
 return;
 }
 
