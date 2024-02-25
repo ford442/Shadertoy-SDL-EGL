2bb1605d125ff45d8817b81380c2043f7e2c9ee3
Noah Cohn <noahc42@gmail.com>
2024-02-24 21:54:33 -0800

    Update webgpu_computeTest.cpp

--- src/vanilla/webgpu_computeTest.cpp
+++ src/vanilla/webgpu_computeTest.cpp
@@ -659,15 +659,15 @@ wgpu_command_encoder_copy_buffer_to_buffer(WGPU_CommandEncoder.at(0,0,0),WGPU_Bu
 // WGPU_ResultBuffer.at(0,0,0)[1]=111.111;
 WGPU_Range_PointerB=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,64*sizeof(float));
 WGPU_BufferRange.at(0,0,1)=WGPU_Range_PointerB;
-wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2),WGPU_BufferRange.at(0,0,1),0,WGPU_ResultBuffer.at(0,0,0),64*sizeof(float));
+wgpu_buffer_read_mapped_range(WGPU_Buffers.at(2,0,2),WGPU_Range_PointerB,0,WGPU_ResultBuffer.at(0,0,0),64*sizeof(float));
 WGPU_ResultBuffer.at(0,0,0)[2]=1024;
 WGPU_ResultBuffer.at(0,0,0)[3]=1024.1024;
 
 EM_ASM({
 document.querySelector('#outText').innerHTML='Buffer at [0]:'+$0;
 document.querySelector('#outText').innerHTML+='Buffer at [1]:'+$1;
-document.querySelector('#outText').innerHTML+='Buffer at [2] (int):'+$2;
-document.querySelector('#outText').innerHTML+='Buffer at [3] (float):'+$3;
+// document.querySelector('#outText').innerHTML+='Buffer at [2] (int):'+$2;
+// document.querySelector('#outText').innerHTML+='Buffer at [3] (float):'+$3;
 },WGPU_ResultBuffer.at(0,0,0)[0],WGPU_ResultBuffer.at(0,0,0)[1],WGPU_ResultBuffer.at(0,0,0)[2],WGPU_ResultBuffer.at(0,0,0)[3]);
 
  // wgpu_buffer_unmap(WGPU_Buffers.at(1,0,1));
