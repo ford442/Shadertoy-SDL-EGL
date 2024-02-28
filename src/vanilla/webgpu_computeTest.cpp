8643cfd84d98a058ee79b3df3da5bcf9ebc5461f
Noah Cohn <noahc42@gmail.com>
2024-02-27 18:40:32 -0800

    Update webgpu_computeTest.cpp

--- src/vanilla/webgpu_computeTest.cpp
+++ src/vanilla/webgpu_computeTest.cpp
@@ -516,9 +516,9 @@ document.querySelector('#outText').innerHTML=$0.toFixed(2)+', '+$1.toFixed(2);
 // std::cout << WGPU_ResultBuffer.at(0,0,0)[0] << std::endl;
 // }
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
-// if(WGPU_BufferStatus.at(0,0,0)!=1){
+if(WGPU_BufferStatus.at(0,0,0)!=1){
 wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
-// }
+}
 // wgpu_buffer_map_async(WGPU_Buffers.at(1,0,1),mapCallbackStart,&WGPU_UserData.at(0,0,0),mode1,0,WGPU_InputRangeSize);
 
 return;
