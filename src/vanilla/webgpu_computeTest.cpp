ee6397795695cffc8dd57ec1010150f7476ef60a
Noah Cohn <noahc42@gmail.com>
2024-03-02 01:38:55 -0800

    Update webgpu_computeTest.cpp

--- src/vanilla/webgpu_computeTest.cpp
+++ src/vanilla/webgpu_computeTest.cpp
@@ -231,6 +231,7 @@ inline char wgl_cmp_src[2000]=
 "var INtexCoord:vec2<u32>=vec2<u32>(vec2<u32>(u32(x),u32(y))*(sizeINu/sizeOUTu));\n"
 // "var colorTest:vec4<f32>=textureLoad(textureIN,INtexCoord,0);\n"
 "var colorTest:vec4<f32>=vec4<f32>(0.7,0.0,0.88,1.0);\n"
+"inputBuffer[1]=inputBuffer[1]+1.0;\n"
 "textureStore(textureOUT,vec2<u32>(u32(x),u32(y)),colorTest);\n"
 "}"
 "}"
