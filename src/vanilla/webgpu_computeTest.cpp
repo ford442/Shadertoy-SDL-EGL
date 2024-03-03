d5a0b55436f4555fc48d0ecbf95eaddf75fb568e
Noah Cohn <noahc42@gmail.com>
2024-03-02 16:17:28 -0800

    Update webgpu_computeTest.cpp

--- src/vanilla/webgpu_computeTest.cpp
+++ src/vanilla/webgpu_computeTest.cpp
@@ -978,7 +978,7 @@ wbbl.at(0,0)=bufferBindingLayoutR;
 // wgpu_buffer_unmap(WGPU_Buffers.at(2,0,2));
 // }
 WGPU_Output_Buffer.buffer=WGPU_Buffers.at(0,0,0);
-WGPU_Output_Buffer.bytesPerRow=floor((sze.at(0,0)*4)/256)*256;
+WGPU_Output_Buffer.bytesPerRow=(floor((sze.at(0,0)*4)/256)+1)*256;
 WGPU_Output_Buffer.rowsPerImage=sze.at(0,0);
 WGPU_Mapped_Buffer.buffer=WGPU_Buffers.at(2,0,2);
 WGPU_Mapped_Buffer.bytesPerRow=512;
