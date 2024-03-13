c828e76504eb74b5e8abdf3c9249f013108bd015
Noah Cohn <noahc42@gmail.com>
2024-03-13 13:58:19 -0700

    Update webgpu_computeFix.cpp

--- src/vanilla/webgpu_computeFix.cpp
+++ src/vanilla/webgpu_computeFix.cpp
@@ -651,6 +651,13 @@ render();
 }
 
 void ObtainedWebGpuDeviceStart(WGpuDevice result,void *userData){
+workgroupSize=1;
+OutputBufferUnits=33000000;
+OutputBufferBytes=33000000*4;
+InputBufferUnits=4665600;
+InputBufferBytes=4665600*4;
+float * WGPU_Result_Array=new float[OutputBufferBytes];
+float * WGPU_Input_Array=new float[InputBufferBytes];
 wd.at(0,0)=result;
 js_data_pointer.at(0,0)=0;
 fjs_data_pointer.at(0,0)=0;
