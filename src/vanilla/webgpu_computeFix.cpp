9067405fc685296001fd6c166f1866e45c6ea185
Noah Cohn <noahc42@gmail.com>
2024-03-17 01:02:20 -0700

    Update webgpu_computeFix.cpp

--- src/vanilla/webgpu_computeFix.cpp
+++ src/vanilla/webgpu_computeFix.cpp
@@ -616,7 +616,7 @@ wgpu_command_encoder_copy_buffer_to_buffer(WGPU_CommandEncoder.at(0,0,0),WGPU_Bu
     //  non-callback mapping
 
 EM_ASM({
-document.querySelector('#outText').innerHTML='Buffer at [0]:'+$0.toFixed(2);
+document.querySelector('#outText').innerHTML='Buffer at [0]:'+$0;
 },WGPU_ResultBuffer.at(0,0,0)[0]);
 if(on.at(1,1)==3){
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
