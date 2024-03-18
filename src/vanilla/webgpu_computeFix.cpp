894e12ac54a2cab0c9f0df31d2960e062d458d05
Noah Cohn <noahc42@gmail.com>
2024-03-18 00:16:30 -0700

    Update webgpu_computeFix.cpp

--- src/vanilla/webgpu_computeFix.cpp
+++ src/vanilla/webgpu_computeFix.cpp
@@ -1268,7 +1268,7 @@ let tstSiZ=h$;
 // document.querySelector("#mvi").height=h$;
 // document.querySelector("#mvi").width=w$;
 if(running==0){
-// Module.ccall("frm",null,['Number'],['Number'],h$,h$);
+Module.ccall("frm",null,['Number'],['Number'],h$,h$);
 setTimeout(function(){
 Module.ccall("startWebGPU",null,"Number",h$);
 console.log('Starting..');
