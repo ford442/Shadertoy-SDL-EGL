796a84ced9ce70e91e4afdcc0d5f727df20e803f
Noah Cohn <noahc42@gmail.com>
2024-03-17 19:20:16 -0700

    Update webgpu_computeFix.cpp

--- src/vanilla/webgpu_computeFix.cpp
+++ src/vanilla/webgpu_computeFix.cpp
@@ -10,6 +10,21 @@ inline char wgl_cmp_src[2000]=
 // "@group(0)@binding(6)var videoTexture: texture_2d <f32>;\n"
 "@compute@workgroup_size(1,1,64)\n"
 "fn computeStuff(@builtin(global_invocation_id)global_id:vec3<u32>){\n"
+"outputBuffer[0]=f32(3.33f);\n"
+"outputBuffer[1]=4.44f;\n"
+
+"}";
+
+inline char wgl_cmp_srcAA[2000]=
+"@group(0)@binding(0)var <storage,read> inputBuffer: array<f32,64>;\n"
+"@group(0)@binding(1)var <storage,read_write> outputBuffer: array<f32,64>;\n"
+"@group(0)@binding(2)var textureIN: texture_2d <f32>;\n"
+"@group(0)@binding(3)var textureOUT: texture_storage_2d <rgba8unorm,write>;\n"
+"@group(0)@binding(4)var resizeSampler: sampler;\n"
+"@group(0)@binding(5)var <uniform> iResolution: u32;\n"
+// "@group(0)@binding(6)var videoTexture: texture_2d <f32>;\n"
+"@compute@workgroup_size(1,1,64)\n"
+"fn computeStuff(@builtin(global_invocation_id)global_id:vec3<u32>){\n"
 "var sizeINf=inputBuffer[0];\n"
 "var loopx:u32=u32(sizeINf);\n"
 "var sizeINu:u32=u32(sizeINf);\n"
