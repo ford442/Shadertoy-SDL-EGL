f546ec436d0c278d7d6b025463066b0c62af4869
Noah Cohn <noahc42@gmail.com>
2024-03-24 19:31:40 -0700

    Update video_jebusa.cpp

--- src/video/video_jebusa.cpp
+++ src/video/video_jebusa.cpp
@@ -57,7 +57,7 @@ maxSum+=aptr[i+200];
 }
 aptr[200]=maxSum/32;
  EM_ASM({
-document.querySelector('#dbg_fmax').innerHTML='test';
+document.querySelector('#dbg_fmax').innerHTML=$0;
 },aptr[0]);
 return;
 }
