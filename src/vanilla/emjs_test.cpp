5b4dcadd7a6b5cfa9b6071adf87e61664a7b4834
Noah Cohn <noahc42@gmail.com>
2024-03-27 04:10:15 -0700

    Update emjs_test.cpp

--- src/vanilla/emjs_test.cpp
+++ src/vanilla/emjs_test.cpp
@@ -50,11 +50,13 @@ document.querySelector('#btn1').addEventListener('click',function(){
 let buffer=new ArrayBuffer(8);
 let view=new Float32Array(buffer);
 view[0]=42.42;
-Module.ccall('emjs',null,["Number"],[view]);
+// Module.ccall('emjs',null,["Number"],[view]);
 
 //  JS to C passing HEAP array
 let H1=Module.HEAPF32.buffer;
 let viewH=new Float32Array(H1);
+viewH[0]=42.42;
+Module.ccall('emjs',null,["Number"],[H1]);
 
 });
 document.querySelector('#btn2').addEventListener('click',function(){
