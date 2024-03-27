a8950ec5bf920e8537e9a2b191f34275b6373e70
Noah Cohn <noahc42@gmail.com>
2024-03-27 05:55:32 -0700

    Update emjs_test.cpp

--- src/vanilla/emjs_test.cpp
+++ src/vanilla/emjs_test.cpp
@@ -21,15 +21,15 @@ static fp_tensor fltp=fp_tensor{5,5};
 float * C_Array=new float[16];
 
 EM_JS(void,emjs_,(float * fPointer),{
-console.log(fPointer[0]);
+console.log(fPointer[8]);
 })
 
 
 void c_(float val){
 flt.at(0,0)=val;
 fltp.at(0,0)=C_Array;
-// fltp.at(0,0)[0]=flt.at(0,0);
-// emjs_(C_Array);
+fltp.at(0,0)[8]=flt.at(0,0);
+emjs_(fltp.at(0,0));
 }
 
 extern "C"{
@@ -47,24 +47,19 @@ c_(a);
 EM_JS(void,setup_js,(),{
 document.querySelector('#btn1').addEventListener('click',function(){
 //  JS to C passing array
-let buffer=new ArrayBuffer(16);
-// let view=new Float32Array(buffer,0,16);
-// var rr=new Array(1);
-// rr[0]=42.42;
-// view.set(rr,0,1);
-// Module.ccall('emjs',null,["Number"],[view]);
+let buffer=new ArrayBuffer(8);
+let view=new Float32Array(buffer);
+view[8]=42.42;
+Module.ccall('emjs',null,["Number"],[buffer]);
 
 //  JS to C passing HEAP array
 let H1=Module.HEAPF32.buffer;
-// let viewH=new Float32Array(H1,0,16);
-// rr[0]=42.42;
-// viewH.set(rr,0,1);
-// Module.ccall('emjs',null,["Number"],[H1]);
+let viewH=new Float32Array(H1);
 
 });
 document.querySelector('#btn2').addEventListener('click',function(){
 //  C to JS passing array
-// Module.ccall('cfunc',null,["Number"],[42.42]);
+Module.ccall('cfunc',null,["Number"],[42.42]);
 
 //  C to JS passing HEAP array
 
