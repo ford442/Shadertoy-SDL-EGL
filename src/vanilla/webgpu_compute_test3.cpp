e424d23cfe4a5a2660c1088594f4f95df7342fc3
Noah Cohn <noahc42@gmail.com>
2024-03-30 14:06:24 -0700

    Update webgpu_compute_test3.cpp

--- src/vanilla/webgpu_compute_test3.cpp
+++ src/vanilla/webgpu_compute_test3.cpp
@@ -585,9 +585,9 @@ xy.y=0;
 videoFrm.origin=xy;
 videoFrm.flipY=EM_FALSE;
   */
-videoSamplerDescriptor.addressModeU=WGPU_ADDRESS_MODE_LINEAR;
-videoSamplerDescriptor.addressModeV=WGPU_ADDRESS_MODE_LINEAR;
-videoSamplerDescriptor.addressModeW=WGPU_ADDRESS_MODE_LINEAR;
+videoSamplerDescriptor.addressModeU=WGPU_ADDRESS_MODE_CLAMP_TO_EDGE;
+videoSamplerDescriptor.addressModeV=WGPU_ADDRESS_MODE_CLAMP_TO_EDGE;
+videoSamplerDescriptor.addressModeW=WGPU_ADDRESS_MODE_CLAMP_TO_EDGE;
 videoSamplerDescriptor.magFilter=WGPU_FILTER_MODE_LINEAR;
 videoSamplerDescriptor.minFilter=WGPU_FILTER_MODE_LINEAR;
   // videoSamplerDescriptor.mipmapFilter=WGPU_MIPMAP_FILTER_MODE_LINEAR;
