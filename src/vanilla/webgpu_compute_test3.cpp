5634ff6e9bbdeb88313187d52a23a4a0181779f7
Noah Cohn <noahc42@gmail.com>
2024-04-01 23:26:28 -0700

    Update webgpu_compute_test3.cpp

--- src/vanilla/webgpu_compute_test3.cpp
+++ src/vanilla/webgpu_compute_test3.cpp
@@ -9,7 +9,7 @@ char wgl_cmp_src[2000]=
 "@group(0)@binding(5)var <uniform> iTime: u32;\n"
 "@group(0)@binding(6)var videoOUT: texture_storage_2d <rgba8unorm,write>;\n"
 "@compute@workgroup_size(8,1,8)\n"
-"fn mainImage(@builtin(global_invocation_id)global_id:vec3<u32>){\n"
+"fn main_image(@builtin(global_invocation_id)global_id:vec3<u32>){\n"
 "var outSizeU:u32=textureDimensions(textureOUT).x;\n"
 "var inSizeU:u32=textureDimensions(textureIN).x;\n"
 "var sizeRatio:f32=f32(inSizeU)/f32(outSizeU);\n"
