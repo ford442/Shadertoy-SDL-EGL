3bbc393d4ff792d01628e3515dec1e6d3847b7dd
Noah Cohn <noahc42@gmail.com>
2024-04-02 04:38:29 -0700

    Update webgpu_tex_render.cpp

--- src/vanilla/webgpu_tex_render.cpp
+++ src/vanilla/webgpu_tex_render.cpp
@@ -6,7 +6,7 @@ char wgl_cmp_src[2000]=
 "@group(0)@binding(2)var textureIN: texture_2d <f32>;\n"
 // "@group(0)@binding(3)var textureOUT: texture_storage_2d <rgba32float,write>;\n"
 // /"@group(0)@binding(4)var resizeSampler: sampler;\n"
-"@group(0)@binding(5)var <uniform> iTime: u32;\n"
+// "@group(0)@binding(5)var <uniform> iTime: u32;\n"
 "@group(0)@binding(6)var videoOUT: texture_storage_2d <rgba32float,write>;\n"
 // "@group(0)@binding(7)var colorOUT: texture_storage_2d <rgba8unorm,write>;\n"
 "@compute@workgroup_size(1,1,1)\n"
