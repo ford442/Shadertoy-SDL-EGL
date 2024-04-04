bc6aaee9dc583639c6cc546f4e48cdf5451ee943
Noah Cohn <noahc42@gmail.com>
2024-04-04 00:20:30 -0700

    Update webgpu_egl_tex.cpp

--- src/vanilla/webgpu_egl_tex.cpp
+++ src/vanilla/webgpu_egl_tex.cpp
@@ -37,49 +37,6 @@ randomNumber=std::rand()%randomMax;
 return randomNumber;
 }
 
-const char * frag_body25=
-"@group(0)@binding(7)var <uniform> iTime : u32;\n"
-"@group(0)@binding(6)var <uniform> iFrame : u32;\n"
-"@group(0)@binding(5)var <uniform> iResolution : u32;\n"
-"@group(0)@binding(0)var videoSampler: sampler;\n"
-"@group(0)@binding(1)var textureIN: texture_storage_2d <rgba32float,write>;\n"
-"@group(0)@binding(2)var videoOUT: texture_2d <f32>;\n"
-// "@group(0) @binding(3) var extTexture : texture_external;\n"
-"@fragment\n"
-"fn main(@location(0) fragUV : vec2<f32>) ->\n"
-"@location(0) vec4<f32> {\n"
-"var tstclr:vec4<f32>=vec4<f32>(0.444,0.0,0.444,1.0);\n"
-"return tstclr;\n"
-// "return textureSample(videoOUT,videoSampler,fragUV);\n"
-"}\n";
-
-const char * frag_body24 =
-"@group(0)@binding(7)var <uniform> iTime : u32;\n"
-"@group(0)@binding(6)var <uniform> iFrame : u32;\n"
-"@group(0)@binding(5)var <uniform> iResolution : u32;\n"
-"@group(0)@binding(0)var videoSampler: sampler;\n"
-"@group(0)@binding(1)var textureIN: texture_storage_2d <rgba32float,write>;\n"
-"@group(0)@binding(2)var videoOUT: texture_2d <f32>;\n"
-"var<private> fragColor_1 : vec4<f32>;\n"
-"var<private> gl_FragCoord : vec4<f32>;\n"
-"var<private> iMouse : vec4<f32>;\n"
-"var<private> iPosition : vec4<f32>;\n"
-"struct main_out {\n"
-"@location(0)\n"
-"fragColor_1_1 : vec4<f32>,\n"
-"@location(1)\n"
-"iPosition_1 : vec4<f32>,\n"
-"}\n"
-"@fragment\n"
-"fn main(@builtin(position) gl_FragCoord_param : vec4<f32>) -> main_out {\n"
-// "fn main(@location(0) fragUV : vec2<f32>) ->\n"
-// "@location(0) vec4<f32> {\n"
-"gl_FragCoord = gl_FragCoord_param;\n"
-"fragColor_1=vec4<f32>(0.0,0.733,0.723,1.0);\n"
-// "fragColor_1 =  vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<u32>(iResolution,iResolution))));\n"
-"return main_out(fragColor_1, iPosition);\n"
-"}";
-
 const char * frag_body2 = R"delimiter(
   //   //
 @group(0)@binding(0)var videoSampler: sampler;
@@ -92,35 +49,35 @@ var<private> fragColor_1 : vec4<f32>;
 var<private> gl_FragCoord : vec4<f32>;
 var<private> iMouse : vec4<f32>;
 var<private> iPosition : vec4<f32>;
-fn mainImage_vf4_vf2_(fragColor : ptr<function, vec4<f32>>, fragCoord : ptr<function, vec2<f32>>) {
+fn mainImage_vf4_vf2_(fragColor: ptr<function, vec4<f32>>,fragCoord: ptr<function, vec2<f32>>) {
 var col : vec3<f32>;
 col = vec3<f32>(0.40000000596046447754f, 0.0f, 0.5f);
 let x_24 : vec3<f32> = col;
-  *(fragColor) = vec4<f32>(x_24.x, x_24.y, x_24.z, 1.0f);
+*(fragColor) = vec4<f32>(x_24.x, x_24.y, x_24.z, 1.0f);
 //  let b3_col : vec4<f32> = *(fragColor);textureStore(textureIN,vec2<u32>(gl_FragCoord.xy),vec4<f32>(b3_col.rgb,1.0f));
-  return;}
+return;}
 fn main_1() {
-  var param : vec4<f32>;
-  var param_1 : vec2<f32>;
-  let x_36 : vec4<f32> = gl_FragCoord;
-  param_1 = vec2<f32>(x_36.x, x_36.y);
-  mainImage_vf4_vf2_(&(param), &(param_1));
-  let x_39 : vec4<f32> = param;
-  let tstcr:vec4<f32>=vec4<f32>(0.3,0.0,0.44,1.0);
- fragColor_1 =vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy)); // /vec2<f32>(vec2<u32>(iResolution,iResolution))));
+var param : vec4<f32>;
+var param_1 : vec2<f32>;
+let x_36 : vec4<f32> = gl_FragCoord;
+param_1 = vec2<f32>(x_36.x, x_36.y);
+mainImage_vf4_vf2_(&(param), &(param_1));
+let x_39 : vec4<f32> = param;
+let tstcr:vec4<f32>=vec4<f32>(0.3,0.0,0.44,1.0);
+fragColor_1 =vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy*vec2<f32>(vec2<u32>(iResolution,iResolution))));
 return;
 }
 struct main_out {
-  @location(0)
-  fragColor_1_1 : vec4<f32>,
-  @location(1)
-  iPosition_1 : vec4<f32>,
+@location(0)
+fragColor_1_1 : vec4<f32>,
+@location(1)
+iPosition_1 : vec4<f32>,
 }
- @fragment
- fn main(@builtin(position) gl_FragCoord_param : vec4<f32>) -> main_out {
-  gl_FragCoord = gl_FragCoord_param;
-  main_1();
-  return main_out(fragColor_1, iPosition);
+@fragment
+fn main(@builtin(position) gl_FragCoord_param : vec4<f32>) -> main_out {
+gl_FragCoord = gl_FragCoord_param;
+main_1();
+return main_out(fragColor_1, iPosition);
 }
   //   //
 )delimiter";
