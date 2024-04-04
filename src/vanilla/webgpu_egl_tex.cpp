5f31637e96e026fef904f40506ee4f8985240a04
Noah Cohn <noahc42@gmail.com>
2024-04-04 00:25:00 -0700

    Update webgpu_egl_tex.cpp

--- src/vanilla/webgpu_egl_tex.cpp
+++ src/vanilla/webgpu_egl_tex.cpp
@@ -64,7 +64,7 @@ param_1 = vec2<f32>(x_36.x, x_36.y);
 mainImage_vf4_vf2_(&(param), &(param_1));
 let x_39 : vec4<f32> = param;
 let tstcr:vec4<f32>=vec4<f32>(0.3,0.0,0.44,1.0);
-fragColor_1 =vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy*vec2<f32>(vec2<u32>(iResolution,iResolution))));
+fragColor_1 =vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<u32>(iResolution,iResolution))));
 return;
 }
 struct main_out {
