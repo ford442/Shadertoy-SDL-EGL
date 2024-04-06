b4bd3d1c1f3c6a11d011c29b2f121897e0b15e9f
Noah Cohn <noahc42@gmail.com>
2024-04-06 09:49:03 -0700

    Update webgpu_egl_tex.cpp

--- src/vanilla/webgpu_egl_tex.cpp
+++ src/vanilla/webgpu_egl_tex.cpp
@@ -111,13 +111,18 @@ passDesc.numColorAttachments=1;
 passDesc.colorAttachments=&wrpca.at(1,1); // &wrpca.at(0,0); // 
 // passDesc.depthStencilAttachment=wrpdsa.at(1,1);  //  wrpdsa.at(0,0); //
 passDesc.occlusionQuerySet=0;
-// passDesc.maxDrawCount=100;
+passDesc.maxDrawCount=100;
+renderTimestampWrites.querySet=0;
+renderTimestampWrites..beginningOfPassWriteIndex=-1;
+renderTimestampWrites.endOfPassWriteIndex=-1;
+passDesc.timestampWrites=renderTimestampWrites;
 wrpd.at(0,0)=passDesc;
 passDesc2.numColorAttachments=1;
 passDesc2.colorAttachments=&wrpca.at(0,0); // &wrpca.at(1,1); //
 // passDesc2.depthStencilAttachment=wrpdsa.at(0,0);
 passDesc2.occlusionQuerySet=0;
-// passDesc2.maxDrawCount=100;
+passDesc2.maxDrawCount=100;
+passDesc2.timestampWrites=renderTimestampWrites;
 wrpd.at(1,1)=passDesc2;
 /*       //  Frame Data
 std::ifstream fram(Fnm2,std::ios::binary);
