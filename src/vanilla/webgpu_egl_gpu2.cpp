ce6c8171aa6360b943c3818588dda65eabc3ba14
Noah Cohn <noahc42@gmail.com>
2024-04-09 09:43:17 -0700

    Update webgpu_egl_gpu2.cpp

--- src/vanilla/webgpu_egl_gpu2.cpp
+++ src/vanilla/webgpu_egl_gpu2.cpp
@@ -129,7 +129,7 @@ std::ifstream fram(Fnm2,std::ios::binary);
 std::vector<uint8_t> data((std::istreambuf_iterator<char>(fram)),(std::istreambuf_iterator<char>()));
 frame_tensor.at(0,0)=data;
   // wetd.at(0,0).source=texid.at(0,0);
-wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&wict.at(2,2),&frame_tensor.at(0,0),sze.at(1,1)*4,sze.at(1,1),sze.at(1,1),sze.at(1,1),1);
+wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&wict.at(2,2),&frame_tensor.at(0,0),sze.at(1,1)*8,sze.at(1,1),sze.at(1,1),sze.at(1,1),1);
 /*   //  Render Pass
 wceA=wgpu_device_create_command_encoder(wd.at(0,0),0);
 wce.at(0,0)=wceA;
