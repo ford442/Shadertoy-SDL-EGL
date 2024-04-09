ab3d6621b120293634e4737525a33afc1e2761bc
Noah Cohn <noahc42@gmail.com>
2024-04-09 09:10:04 -0700

    Update webgpu_compute_js_gpu.cpp

--- src/vanilla/webgpu_compute_js_gpu.cpp
+++ src/vanilla/webgpu_compute_js_gpu.cpp
@@ -170,7 +170,7 @@ document.querySelector('#bcanvas').height=parseInt(window.innerHeight,10);
 document.querySelector('#canvas').width=parseInt(window.innerHeight,10);
 document.querySelector('#bcanvas').width=parseInt(window.innerHeight,10);
 document.querySelector('#di').click();
-videoFramesGpu();
+videoFrames();
 // let vsiz=document.querySelector('#vsiz').innerHTML;
 // Module.ccall("startWebGPUi",null,"Number",[vsiz]);
 },1500);
