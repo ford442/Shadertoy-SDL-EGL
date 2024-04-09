687fc7a4b44e7f824b7cbb3aef22b7c7106c7afc
Noah Cohn <noahc42@gmail.com>
2024-04-09 08:45:09 -0700

    Update webgpu_egl_gpu2.cpp

--- src/vanilla/webgpu_egl_gpu2.cpp
+++ src/vanilla/webgpu_egl_gpu2.cpp
@@ -861,7 +861,7 @@ navigator_gpu_request_adapter_async(&wao.at(0,0),ObtainedWebGpuAdapterStart,0);
 return EM_TRUE;
 }
 
-#include "../../src/vanilla/webgpu_compute_js_tex.cpp"
+#include "../../src/vanilla/webgpu_compute_js_gpu.cpp"
 
 extern"C"{
 
