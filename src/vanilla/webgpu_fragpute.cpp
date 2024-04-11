0be5ff13a2a052a8c8045362dcef5da3acf2e779
Noah Cohn <noahc42@gmail.com>
2024-04-11 02:26:17 -0700

    Update webgpu_fragpute.cpp

--- src/vanilla/webgpu_fragpute.cpp
+++ src/vanilla/webgpu_fragpute.cpp
@@ -862,7 +862,7 @@ navigator_gpu_request_adapter_async(&wao.at(0,0),ObtainedWebGpuAdapterStart,0);
 return EM_TRUE;
 }
 
-#include "../../src/vanilla/webgpu_compute_js_gpu2.cpp"
+#include "../../src/vanilla/webgpu_compute_js_frag.cpp"
 
 extern"C"{
 
