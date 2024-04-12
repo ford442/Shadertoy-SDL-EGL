e86c80c35a23153066ad90bf07d7bf427ed880a4
Noah Cohn <noahc42@gmail.com>
2024-04-12 06:07:28 -0700

    Update webgpu_audio.hpp

--- include/audio/webgpu_audio.hpp
+++ include/audio/webgpu_audio.hpp
@@ -6,27 +6,6 @@
 #include "../../lib/lib_webgpu_cpp20.cpp"
 #include <SDL2/SDL.h>
 
-#include <math.h>
-class Oscillator {
-public:
-Oscillator(float frequency) {
-this->frequency = frequency;
-this->phase = 0.0f;
-}
-
-float generate() {
-  
-    float sample = sin(2.0f * M_PI * this->frequency * this->phase);
-    this->phase += 1.0f / 44100.0f;
-  
-    return sample;
-}
-
-private:
-float frequency;
-float phase;
-};
-
 using namespace std;
 #include <cstdio> // C++ style
 #include <cstdarg> // C++ style
@@ -116,14 +95,163 @@ using namespace std;
 
 template<class ArgumentType,class ResultType>
 
+
+#include <boost/preprocessor.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/tensor.hpp>
+#include <boost/atomic.hpp>
+#include <boost/tuple/tuple.hpp>
+// #include <boost/integer.hpp>
+#include <boost/bind/bind.hpp>
 #include <boost/function.hpp>
+
 
 struct unary_function{
 typedef ArgumentType argument_type;
 typedef ResultType result_type;
 };
 
-#include <boost/function.hpp>
+#define register
+
+#define BOOST_CHRONO_HEADER_ONLY 1
+#define BOOST_ERROR_CODE_HEADER_ONLY 1
+#define BOOST_UBLAS_MOVE_SEMANTICS
+#define BOOST_UBLAS_TYPE_CHECK 0
+#define BOOST_UBLAS_USE_LONG_DOUBLE
+// #define BOOST_NO_EXCEPTIONS
+
+
+
+
+extern "C"{  
+  
+EM_BOOL pl();
+  
+}
+
+
+
+
+using void_tensor=boost::numeric::ublas::tensor<boost::atomic<void *>>;
+using gi_tensor=boost::numeric::ublas::tensor<boost::atomic<long>>;
+using ub_tensor=boost::numeric::ublas::tensor<boost::atomic<unsigned char *>>;
+using lu_tensor=boost::numeric::ublas::tensor<boost::atomic<unsigned long>>;
+using v_tensor=boost::numeric::ublas::tensor<v128_t>;
+
+ub_tensor sound=ub_tensor{1,1,1};
+gi_tensor sound_pos=gi_tensor{1,1};
+gi_tensor sound_lft=gi_tensor{1,1};
+lu_tensor sound_pos_u=lu_tensor{1,1};
+v_tensor sse=v_tensor{1,2};
+v_tensor sse2=v_tensor{1,1};
+v_tensor sse3=v_tensor{1,1};
+
+struct{
+register GLubyte * snd;
+register long pos=0;
+SDL_AudioDeviceID dev;
+register GLuint slen=0;
+register GLubyte * wptr;
+}wave;
+
+
+class Audio{
+
+private:
+
+GLchar flnm[24];
+SDL_AudioSpec request;
+
+public:
+
+// static EM_BOOL snd_pos(boost::atomic<short int> set){
+static EM_BOOL snd_pos(GLint set){
+sse3.at(0,0)=wasm_i64x2_splat(set);
+sound_pos.at(0,0)=wasm_i64x2_extract_lane(sse3.at(0,0),0);
+return EM_TRUE;
+}
+
+static EM_BOOL snd_lft(long long set){
+sse.at(0,1)=wasm_i64x2_splat(set);
+sound_lft.at(0,0)=wasm_i64x2_extract_lane(sse.at(0,1),0);
+return EM_TRUE;
+}
+
+static EM_BOOL snd_pos_u(unsigned long long set){
+sse2.at(0,0)=wasm_u64x2_splat(set);
+sound_pos_u.at(0,0)=wasm_u64x2_extract_lane(sse2.at(0,0),0);
+return EM_TRUE;
+}
+
+static void SDLCALL bfr(void * unused,GLubyte * stm,GLint len){
+::boost::tuples::tie(stm,len);
+wave.wptr=sound.at(0,1,0)+sound_pos.at(0,0);
+snd_lft(sound_pos_u.at(0,0)-sound_pos.at(0,0));
+while(sound_lft.at(0,0)<=len){
+SDL_UnlockAudioDevice(wave.dev);
+SDL_memcpy(stm,wave.wptr,sound_lft.at(0,0));
+stm+=sound_lft.at(0,0);
+len-=sound_lft.at(0,0);
+wave.wptr=sound.at(0,1,0);
+snd_lft(sound_pos_u.at(0,0));
+snd_pos(0);
+SDL_LockAudioDevice(wave.dev);
+}
+SDL_memcpy(stm,wave.wptr,len);
+snd_pos(sound_pos.at(0,0)+len);
+return;
+}
+
+boost::function<EM_BOOL()>plt=[this](){
+::boost::tuples::tie(sound,sound_pos,sound_pos_u);
+::boost::tuples::tie(wave,sse,sse2);
+::boost::tuples::tie(bfr,request);
+request.freq=44100;
+request.format=AUDIO_S32;
+request.channels=2;
+request.samples=128;
+SDL_memset(&request,0,sizeof(request));
+snd_pos(0);
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+SDL_Init(SDL_INIT_AUDIO);
+SDL_LoadWAV(flnm,&request,&wave.snd,&wave.slen);
+sound.at(0,1,0)=wave.snd;
+snd_pos_u(wave.slen);
+request.callback=bfr;
+wave.dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&request,NULL,0);
+SDL_PauseAudioDevice(wave.dev,SDL_FALSE);
+return EM_TRUE;
+};
+
+};
+
+#include <math.h>
+
+class Oscillator {
+public:
+Oscillator(float frequency) {
+this->frequency = frequency;
+this->phase = 0.0f;
+}
+
+float generate() {
+  
+    float sample = sin(2.0f * M_PI * this->frequency * this->phase);
+    this->phase += 1.0f / 44100.0f;
+  
+    return sample;
+}
+
+private:
+float frequency;
+float phase;
+};
+
+
+
+
+
 
 inline int rNd4(int);
 // static void WGPU_Run();
