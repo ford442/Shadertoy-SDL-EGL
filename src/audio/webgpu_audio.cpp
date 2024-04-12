73e1f7b30ae8883cb3a2bfd4e8181d32de12ab1f
Noah Cohn <noahc42@gmail.com>
2024-04-12 06:41:47 -0700

    Update webgpu_audio.cpp

--- src/audio/webgpu_audio.cpp
+++ src/audio/webgpu_audio.cpp
@@ -971,7 +971,7 @@ navigator_gpu_request_adapter_async(&wao.at(0,0),ObtainedWebGpuAdapterStart,0);
 return EM_TRUE;
 }
 
-#include "../../src/vanilla/webgpu_compute_js_tex.cpp"
+#include "../../src/audio/webgpu_compute_js_audio.cpp"
 
 extern"C"{
 
