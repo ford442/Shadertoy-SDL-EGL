efaeadb327e24b250b3b119989f07c86161384dc
Noah Cohn <noahc42@gmail.com>
2024-04-15 00:49:19 -0700

    Update webgpu_vid2.cpp

--- src/vanilla/webgpu_vid2.cpp
+++ src/vanilla/webgpu_vid2.cpp
@@ -940,7 +940,7 @@ navigator_gpu_request_adapter_async(&wao.at(0,0),ObtainedWebGpuAdapterStart,0);
 return EM_TRUE;
 }
 
-#include "../../src/vanilla/webgpu_compute_js_vid.cpp"
+#include "../../src/vanilla/webgpu_compute_js_vid2.cpp"
 
 extern"C"{
 
