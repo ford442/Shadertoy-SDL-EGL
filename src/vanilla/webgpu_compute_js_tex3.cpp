0c671bd4e50bd6c8d9c1c45b1396ff9a3d51eb80
Noah Cohn <noahc42@gmail.com>
2024-04-21 15:25:45 -0700

    Update webgpu_compute_js_tex3.cpp

--- src/vanilla/webgpu_compute_js_tex3.cpp
+++ src/vanilla/webgpu_compute_js_tex3.cpp
@@ -45,25 +45,25 @@ const gl2=cnv.getContext('2d',{colorType:'float32',willReadFrequently:false,alph
 const gl3=cnv.getContext('2d',{willReadFrequently:false,alpha:true}); // 
 // const gl2=cnv.getContext('2d',{alpha:true}); // 
 gl2.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
-let image=gl2.getImageData(0,0,w$,h$);
-let imageData=image.data;
-let pixelData=new Float32Array(imageData);
+var image=gl2.getImageData(0,0,w$,h$);
+var imageData=image.data;
+var pixelData=new Float32Array(imageData);
 FS.writeFile('/video/frame.gl',pixelData);
 setInterval(function(){
 gl2.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
 image=gl2.getImageData(0,0,w$,h$);
-let image2=gl3.getImageData(0,0,w$,h$);
+var image2=gl3.getImageData(0,0,w$,h$);
 imageData=image.data;
-let imageData2=image2.data;
+var imageData2=image2.data;
 pixelData=new Float32Array(imageData);
 imageData=image.data;
   console.log('getImageData.data: ',imageData[5]);
   console.log('pixelData=new Float32Array: ',pixelData[5]);
-let pixelData2=new Uint8ClampedArray(imageData);
+var pixelData2=new Uint8ClampedArray(imageData);
   console.log('pixelData=new Uint8ClampedArray: ',pixelData2[5]);
-let pixelData4=new Float32Array(imageData2);
+var pixelData4=new Float32Array(imageData2);
     console.log('pixelData3=new Float32Array: ',pixelData4[5]);
-let pixelData5=new Uint8ClampedArray(imageData2);
+var pixelData5=new Uint8ClampedArray(imageData2);
   console.log('pixelData3=new Uint8ClampedArray: ',pixelData5[5]);
 pixelData=new Float32Array(imageData,0,la);
 FS.writeFile('/video/frame.gl',pixelData);
