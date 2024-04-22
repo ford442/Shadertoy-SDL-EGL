a5753ba02ea8d4c55466339281083ca427dbe074
Noah Cohn <noahc42@gmail.com>
2024-04-22 11:32:10 -0700

    Update webgpu_tex3.cpp

--- src/vanilla/webgpu_tex3.cpp
+++ src/vanilla/webgpu_tex3.cpp
@@ -142,13 +142,14 @@ passDesc2.occlusionQuerySet=0;
 passDesc2.maxDrawCount=6;
 passDesc2.timestampWrites=renderTimestampWrites;
 wrpd.at(1,1)=passDesc2;
-      //  Frame Data
+      //  Frame Data 
 std::ifstream fram(Fnm2,std::ios::binary);
 std::vector<uint8_t>data((std::istreambuf_iterator<char>(fram)),(std::istreambuf_iterator<char>()));
 // frame_tensor.at(0,0)=data;
 std::vector<float>floatData(data.size());
 std::transform(data.begin(), data.end(), floatData.begin(), 
-[](uint8_t val) { return val / 255.0f; });
+// [](uint8_t val) { return val / 255.0f; });  // for RGBA32FLOAT
+[](uint8_t val) { return static_cast<float>(val); });  //  for RGBA16FLOAT
 const size_t bytesPerRow=sze.at(6,6) * 4 * sizeof(float);
 // fjs_data_pointer.at(0,0)=floatData.data();
 // frame_tensorGL.at(0,0)=data;
