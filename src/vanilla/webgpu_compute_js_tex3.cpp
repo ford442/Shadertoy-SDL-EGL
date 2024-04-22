e8a320f797b4d71b66a2d8c1ac09ad5885b1c246
Noah Cohn <noahc42@gmail.com>
2024-04-21 22:43:27 -0700

    Update webgpu_compute_js_tex3.cpp

--- src/vanilla/webgpu_compute_js_tex3.cpp
+++ src/vanilla/webgpu_compute_js_tex3.cpp
@@ -57,36 +57,36 @@ let la=nearestPowerOf2(((w$*h$*4)/4)*4);
 // const gl3=cnv.getContext('2d',{colorType:'float64',alpha:true}); // 
   
 const gl3=cnv.getContext('2d',{
-colorType:'float32',
+colorType:'float64',
 alpha:true,
 willReadFrequently:false,
 stencil:false,
 depth:false,
-colorSpace:"display-p3",
+// colorSpace:"display-p3",
 desynchronized:false,
 antialias:true,
 powerPreference:"high-performance",
 premultipliedAlpha:true,
-preserveDrawingBuffer:true
+// preserveDrawingBuffer:true
 }); // 
 gl3.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
 // var image=flipImageData(gl3.getImageData(0,0,w$,h$));
-var image=gl3.getImageData(0,0,w$,h$);
-var imageData=image.data;
+let image=gl3.getImageData(0,0,w$,h$);
+let imageData=image.data;
 // var pixelData=new Uint8ClampedArray(imageData);
-// var pixelData=new Float64Array(imageData);
+let pixelData=new Float64Array(imageData);
 // var pixelData=new Float64Array(imageData,0,la);
-FS.writeFile('/video/frame.gl',new Float32Array(imageData));
+FS.writeFile('/video/frame.gl',pixelData);
 setInterval(function(){
 gl3.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
 // image=flipImageData(gl3.getImageData(0,0,w$,h$));
 image=gl3.getImageData(0,0,w$,h$);
 imageData=image.data;
-// pixelData=new Float64Array(imageData);
+pixelData=new Float64Array(imageData);
  //  const externalTexture = gpuDevice.createTexture({size: [imageWidth, imageHeight, 1],format: 'rgba8unorm',usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST });
 // gpuQueue.writeTexture({ texture }, pixelData, { bytesPerRow }, { width: w$, height: h$ } );
 // pixelData=new Float64Array(imageData,0,la);
-FS.writeFile('/video/frame.gl',new Float32Array(imageData));
+FS.writeFile('/video/frame.gl',pixelData);
 },16.666);
 }
   
