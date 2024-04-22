1309631023cef8634e4536786e06f5d81fc1dc3c
Noah Cohn <noahc42@gmail.com>
2024-04-21 23:40:51 -0700

    Update webgpu_tex3.cpp

--- src/vanilla/webgpu_tex3.cpp
+++ src/vanilla/webgpu_tex3.cpp
@@ -147,10 +147,9 @@ std::ifstream fram(Fnm2,std::ios::binary);
 std::vector<uint8_t> data((std::istreambuf_iterator<char>(fram)),(std::istreambuf_iterator<char>()));
 // std::vector<GLubyte> data((std::istreambuf_iterator<char>(fram)),(std::istreambuf_iterator<char>()));
 // frame_tensor.at(0,0)=data;
-std::vector<float> floatData(data.size());
-std::transform(data.begin(),data.end(),floatData.begin(),[](uint8_t val){
-return val / 255.0f;
-});
+  std::vector<float> floatData(data.size());
+std::transform(data.begin(), data.end(), floatData.begin(), 
+[](uint8_t val) { return val / 255.0f; });
 const size_t bytesPerRow=sze.at(6,6) * 4 * sizeof(float);
 // fjs_data_pointer.at(0,0)=floatData.data();
 // frame_tensorGL.at(0,0)=data;
