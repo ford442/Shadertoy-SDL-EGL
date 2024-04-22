333ab7a5122c4af8dd1bb79c3b4f285c54b2115e
Noah Cohn <noahc42@gmail.com>
2024-04-21 23:46:03 -0700

    Update webgpu_compute_js_tex3.cpp

--- src/vanilla/webgpu_compute_js_tex3.cpp
+++ src/vanilla/webgpu_compute_js_tex3.cpp
@@ -51,15 +51,14 @@ cnv.height=h$;
 cnvb.height=SiZ;
 cnv.width=w$;
 cnvb.width=SiZ;
-var ratio=SiZ/h$;
 let offS=Math.floor((w$-h$)/2);
 let la=nearestPowerOf2(((w$*h$*4)/4)*4);
 // const gl3=cnv.getContext('2d',{colorType:'float64',alpha:true}); // 
   
 const gl3=cnv.getContext('2d',{
-colorType:'float64',
+colorType:'float32',
 alpha:true,
-willReadFrequently:true,
+willReadFrequently:false,
 stencil:false,
 depth:false,
 colorSpace:"display-p3",
@@ -74,7 +73,7 @@ gl3.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
 let image=gl3.getImageData(0,0,w$,h$);
 let imageData=image.data;
 // var pixelData=new Uint8ClampedArray(imageData);
-let pixelData=new Float64Array(imageData);
+let pixelData=new Float32Array(imageData);
 // var pixelData=new Float64Array(imageData,0,la);
 FS.writeFile('/video/frame.gl',pixelData);
 setInterval(function(){
@@ -82,7 +81,7 @@ gl3.drawImage(vvi,0,0,w$,h$,0,0,w$,h$);
 // image=flipImageData(gl3.getImageData(0,0,w$,h$));
 image=gl3.getImageData(0,0,w$,h$);
 imageData=image.data;
-pixelData=new Float64Array(imageData);
+pixelData=new Float32Array(imageData);
  //  const externalTexture = gpuDevice.createTexture({size: [imageWidth, imageHeight, 1],format: 'rgba8unorm',usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST });
 // gpuQueue.writeTexture({ texture }, pixelData, { bytesPerRow }, { width: w$, height: h$ } );
 // pixelData=new Float64Array(imageData,0,la);
