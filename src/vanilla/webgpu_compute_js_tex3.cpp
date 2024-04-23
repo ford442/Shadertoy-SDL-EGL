7efafd47fa9ce9ec526cbb83a583d408a1703455
Noah Cohn <noahc42@gmail.com>
2024-04-23 16:43:21 -0700

    Update webgpu_compute_js_tex3.cpp

--- src/vanilla/webgpu_compute_js_tex3.cpp
+++ src/vanilla/webgpu_compute_js_tex3.cpp
@@ -226,7 +226,7 @@ videoStart();
 document.querySelector('#startBtn2').addEventListener('click',function(){
 var pth=document.querySelector('#path').innerHTML;
 getShader(pth,'shader.wgsl');
-var pth2=document.querySelector('#computePath').innerHTML;
+var pth2=document.querySelector('#computePathNovid').innerHTML;
 getShader(pth2,'compute.wgsl');
 normalResSetup();
 regularStart();
