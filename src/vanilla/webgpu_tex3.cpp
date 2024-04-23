e7564f72c8ae4dabce495a387fd18d47fe47ea7e
Noah Cohn <noahc42@gmail.com>
2024-04-23 16:21:13 -0700

    Update webgpu_tex3.cpp

--- src/vanilla/webgpu_tex3.cpp
+++ src/vanilla/webgpu_tex3.cpp
@@ -149,11 +149,9 @@ passDesc2.timestampWrites=renderTimestampWrites;
 wrpd.at(1,1)=passDesc2;
   
    if(on.at(4,4)==1){
-  
       //  Frame Data 
 std::ifstream fram(Fnm2,std::ios::binary);
 std::vector<uint8_t>data((std::istreambuf_iterator<char>(fram)),(std::istreambuf_iterator<char>()));
-
 std::vector<float>floatData(data.size());
 std::transform(data.begin(),data.end(),floatData.begin(), 
 [](uint8_t val){return val/255.0f;});  // for RGBA32FLOAT
@@ -171,11 +169,9 @@ const size_t bytesPerRow=sze.at(6,6) * 4 * sizeof(float);
 // wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&wict.at(4,4),&frame_tensor.at(0,0),sze.at(6,6)*4,sze.at(7,7),sze.at(6,6),sze.at(7,7),1);
 
 //   wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&wict.at(4,4),&frame_tensor.at(0,0),bytesPerRow,sze.at(7,7),sze.at(6,6),sze.at(7,7),1);
-
-     on.at(4,4)=0;
-     
-   }   // end if on 4,4
     wgpu_queue_write_texture(WGPU_Queue.at(0,0,0),&wict.at(4,4),floatData.data(),bytesPerRow,sze.at(7,7),sze.at(6,6),sze.at(7,7),1);
+     on.at(4,4)=0;
+   }   // end if on 4,4
 
 // void wgpu_queue_copy_external_image_to_texture(WGpuQueue queue, const WGpuImageCopyExternalImage *source NOTNULL, const WGpuImageCopyTextureTagged *destination NOTNULL, uint32_t copyWidth, uint32_t copyHeight _WGPU_DEFAULT_VALUE(1), uint32_t copyDepthOrArrayLayers _WGPU_DEFAULT_VALUE(1));
 // wgpu_queue_copy_external_image_to_texture(WGPU_Queue.at(0,0,0), ,&wictt.at(0,0) ,sze.at(7,7),sze.at(6,6),sze.at(7,7),1);
@@ -266,7 +262,7 @@ return;
 
 void ObtainedWebGpuDeviceStart(WGpuDevice result,void *userData){
 if(on.at(0,0)==0){wd.at(0,0)=result;}
-on.at(4,4)=0;
+on.at(4,4)=1;
 js_data_pointer.at(0,0)=0;
 fjs_data_pointer.at(0,0)=0;
 wcc.at(0,0)=wgpu_canvas_get_webgpu_context("canvas");
