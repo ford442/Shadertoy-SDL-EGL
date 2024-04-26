d11ef398d334ceb8d5e7f4a832b88b97295a947e
Noah Cohn <noahc42@gmail.com>
2024-04-25 17:34:42 -0700

    Update webgpu_compute_vars.cpp

--- src/vanilla/webgpu_compute_vars.cpp
+++ src/vanilla/webgpu_compute_vars.cpp
@@ -216,6 +216,65 @@ let x_39 : vec4<f32> = param;
 let tstcr:vec4<f32>=vec4<f32>(0.3,0.0,0.44,1.0);
 let ress:u32=u32(textureDimensions(videoOUT).x);
 // fragColor_1=vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<u32>(ress,ress))));
+fragColor_1=vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<f32>(iResolution,iResolution))));
+return;
+}
+struct main_out {
+@location(0)
+fragColor_1_1 : vec4<f32>,
+@location(1)
+iPosition_1 : vec4<f32>,
+}
+@fragment
+fn main(@builtin(position) gl_FragCoord_param : vec4<f32>) -> main_out {
+gl_FragCoord = gl_FragCoord_param;
+main_1();
+return main_out(fragColor_1, iPosition);
+}
+  //   //
+)delimiter";
+
+const char * frag_body3 = R"delimiter(
+  //   //
+@group(0)@binding(0)var videoSampler: sampler;
+// @group(0)@binding(1)var textureIN: texture_storage_2d <rgba32float,write>;
+@group(0)@binding(2)var videoOUT: texture_2d <f32>;
+@group(0)@binding(5)var<uniform> iResolution : f32;
+@group(0)@binding(6)var<uniform> iFrame : u32;
+@group(0)@binding(7)var<uniform> iTime : f32;
+    @group(0)@binding(8)var<uniform> iRotation : f32;
+var<private> fragColor_1 : vec4<f32>;
+var<private> gl_FragCoord : vec4<f32>;
+var<private> iMouse : vec4<f32>;
+var<private> iPosition : vec4<f32>;
+fn mainImage_vf4_vf2_(fragColor: ptr<function, vec4<f32>>,fragCoord: ptr<function, vec2<f32>>) {
+let radians = radians(   iRotation   );
+  let cos_theta = cos(radians);
+    let sin_theta = sin(radians);
+    let offsetCoord = texCoord -    iCenter    ; 
+let rotatedCoord = vec2<f32>(
+        offsetCoord.x * cos_theta - offsetCoord.y * sin_theta,
+        offsetCoord.x * sin_theta + offsetCoord.y * cos_theta
+    );
+    let finalCoord = rotatedCoord +    iCenter    ;
+
+var col : vec3<f32>;
+col = vec3<f32>(0.40000000596046447754f, 0.0f, 0.5f);
+let x_24 : vec3<f32> = col;
+*(fragColor) = vec4<f32>(x_24.x, x_24.y, x_24.z, 1.0f);
+//  let b3_col : vec4<f32> = *(fragColor);
+// textureStore(textureIN,    finalCoord    ,vec4<f32>(b3_col.rgb,1.0f));
+return;}
+fn main_1() {
+var param : vec4<f32>;
+var param_1 : vec2<f32>;
+let x_36 : vec4<f32> = gl_FragCoord;
+param_1 = vec2<f32>(x_36.x, x_36.y);
+mainImage_vf4_vf2_(&(param), &(param_1));
+let x_39 : vec4<f32> = param;
+let tstcr:vec4<f32>=vec4<f32>(0.3,0.0,0.44,1.0);
+let ress:u32=u32(textureDimensions(videoOUT).x);
+// fragColor_1=vec4<f32>(textureSample(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<u32>(ress,ress))));
 fragColor_1=vec4<f32>(textureSampleBaseClampToEdge(videoOUT,videoSampler,gl_FragCoord.xy/vec2<f32>(vec2<f32>(iResolution,iResolution))));
 return;
 }
