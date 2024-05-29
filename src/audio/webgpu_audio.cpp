2b5eb41b4895689a1213977c9a96e2e9af80606d
Noah Cohn <noahc42@gmail.com>
2024-05-29 16:04:26 -0700

    Update webgpu_audio.cpp

--- src/audio/webgpu_audio.cpp
+++ src/audio/webgpu_audio.cpp
@@ -190,6 +190,9 @@ on.at(1,1)=1;
 wgpu_buffer_map_sync(WGPU_Buffers.at(2,0,2),mode1,0,OutputBufferBytes);  
 // wgpu_buffer_map_async(WGPU_Buffers.at(2,0,2),WGPU_MapCallback.at(0,0,0),&WGPU_UserData.at(0,0,0),mode1,0,OutputBufferBytes);
 }
+if(on.at(1,1)==3){
+on.at(1,1)=0;
+}
 WGPU_BufferStatus.at(0,0,0)=wgpu_buffer_map_state(WGPU_Buffers.at(2,0,2));
 if(WGPU_BufferStatus.at(0,0,0)==3){
 WGPU_Range_PointerB=wgpu_buffer_get_mapped_range(WGPU_Buffers.at(2,0,2),0,OutputBufferBytes);
