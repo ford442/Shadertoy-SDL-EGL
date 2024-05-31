c6595f5d71d876245d1db8bfc97d8776252b5f4c
Noah Cohn <noahc42@gmail.com>
2024-05-30 20:49:14 -0700

    Update webgpu_compute_js_tex3.cpp

--- src/vanilla/webgpu_compute_js_tex3.cpp
+++ src/vanilla/webgpu_compute_js_tex3.cpp
@@ -1,7 +1,10 @@
 EM_JS(void,js_main,(),{
 
 FS.mkdir('/shader');
+  FS.mount(MEMFS, {}, '/shader');
 FS.mkdir('/video');
+  FS.mount(MEMFS, {}, '/video');
+
 let running=0;
 
 function flipImageData(imageData){
