b636c85104897bee01d8a87eeb332308635f665d
Noah Cohn <noahc42@gmail.com>
2024-07-04 14:19:43 -0700

    Update webgpu_em.hpp

--- include/vanilla/webgpu_em.hpp
+++ include/vanilla/webgpu_em.hpp
@@ -60,7 +60,8 @@ using namespace std;
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/chrono.hpp>
-#include <boost/compute/core.hpp>
+
+// #include <boost/compute/core.hpp>
 
 #include <array>
 #include <chrono>
