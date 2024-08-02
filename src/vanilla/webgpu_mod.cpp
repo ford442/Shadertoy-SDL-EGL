668f326a13c99e500fad0d80ad882927fc51d638
Noah Cohn <noahc42@gmail.com>
2024-08-02 16:05:38 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -94,12 +94,12 @@ return EM_TRUE;
 }
 
 EM_BOOL ZoomIn(){
-u64_uni.at(4,4)++;
+u64_uni.at(4,4)[0]++;
 return EM_TRUE;
 }
 
 EM_BOOL ZoomOut(){
-u64_uni.at(4,4)--;
+u64_uni.at(4,4)[0]--;
 return EM_TRUE;
 }
 
@@ -258,7 +258,7 @@ wgpu_encoder_set_bind_group(wrpe.at(0,0),0,wbg.at(0,0),0,0);
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(2,2),0,&f32_uniform.at(2,2),sizeof(emscripten_align1_float));
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(1,1),0,&u64_uni.at(3,3),sizeof(uint64_t));
 wgpu_queue_write_buffer(wq.at(0,0),wb.at(0,0),0,&f32_uniform.at(0,0),sizeof(emscripten_align1_float));
-wgpu_queue_write_buffer(wq.at(0,0),wb.at(8,8),0,&u64_uni.at(4,4),sizeof(uint64_t));
+wgpu_queue_write_buffer(wq.at(0,0),wb.at(8,8),0,&u64_uni.at(4,4[0],sizeof(uint64_t));
   //  wgpu_queue_write_buffer(wq.at(0,0),wb.at(0,0),0,&v4f32_uniform.at(0,0),sizeof(emscripten_align1_float)*4);
 wgpu_render_pass_encoder_set_index_buffer(wrpe.at(0,0),wb.at(7,7),WGPU_INDEX_FORMAT_UINT32,0,36*sizeof(uint32_t));
 wgpu_render_pass_encoder_set_vertex_buffer(wrpe.at(0,0),0,wb.at(6,6),0,sizeof(vertices));
@@ -557,7 +557,7 @@ Mapped_Image_Buffer.buffer=WGPU_Buffers.at(2,0,2);
 // wicb.at(0,0)=Mapped_Image_Buffer;
 WGpuBufferDescriptor bufferDescriptorIn={u64_bfrSze.at(1,1),WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_DST,EM_FALSE};
 WGpuBufferDescriptor bufferDescriptorOut={u64_bfrSze.at(0,0),WGPU_BUFFER_USAGE_STORAGE|WGPU_BUFFER_USAGE_COPY_SRC,EM_FALSE};
-WGpuBufferDescriptor bufferDescriptorZoom={8,WGPU_BUFFER_USAGE_UNIFORM|WGPU_BUFFER_USAGE_COPY_DST,EM_FALSE};
+WGpuBufferDescriptor bufferDescriptorZoom={24,WGPU_BUFFER_USAGE_UNIFORM|WGPU_BUFFER_USAGE_COPY_DST,EM_FALSE};
 wbd.at(3,3)=bufferDescriptorIn;
 wbd.at(4,4)=bufferDescriptorOut;
 wbd.at(8,8)=bufferDescriptorZoom;
@@ -806,7 +806,7 @@ Compute_Bindgroup_Entries[8].resource=wtv.at(6,6);
 Compute_Bindgroup_Entries[9].binding=9;
 Compute_Bindgroup_Entries[9].resource=wb.at(8,8);
 Compute_Bindgroup_Entries[9].bufferBindOffset=0;
-Compute_Bindgroup_Entries[9].bufferBindSize=sizeof(uint64_t);
+Compute_Bindgroup_Entries[9].bufferBindSize=sizeof(uint64_t)*3;
 WGPU_BindGroupEntries.at(0,0,0)=Compute_Bindgroup_Entries;
 WGPU_BindGroup.at(0,0,0)=wgpu_device_create_bind_group(wd.at(0,0),WGPU_BindGroupLayout.at(0,0,0),WGPU_BindGroupEntries.at(0,0,0),10);
 WGpuComputePassTimestampWrites computePassTimestampWrites={};
