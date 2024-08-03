d4db821c6b2bbac8c0aa16fe2d9b412af7a47920
Noah Cohn <noahc42@gmail.com>
2024-08-03 14:32:33 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -396,7 +396,7 @@ emscripten_get_canvas_element_size("canvas",&szwI,&szhI);
 emscripten_get_element_css_size("canvas",&szw,&szh);
 // u64_siz.at(3,3)=sze.at(1,1);
 sze.at(0,0)=static_cast<emscripten_align1_int>(szhI);
-sze.at(3,3)=static_cast<emscripten_align1_int>(std::max(sze.at(0,0),sze.at(1,1))*(u64_uni.at(4,4)/1000));
+sze.at(3,3)=static_cast<emscripten_align1_int>(std::max(sze.at(0,0),sze.at(1,1))*(float(u64_uni.at(4,4)/1000.0f)));
 // u64_siz.at(2,2)=static_cast<emscripten_align1_int>(szhI);
 f32_uniform.at(1,1)=szhI;
 f32_uniform.at(2,2)=static_cast<emscripten_align1_float>(sze.at(1,1));
