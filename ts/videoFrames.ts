d2d8857c605e74e9c064eafe03b146cf355bfb52
Noah Cohn <noahc42@gmail.com>
2024-08-03 01:54:30 -0700

    Update videoFrames.ts

--- ts/videoFrames.ts
+++ ts/videoFrames.ts
@@ -1,14 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-
-async function loadWasmModule() {
-    const wasmFiles = fs.readdirSync('./').filter(file => file.match(/^w0-.*\.js$/)); 
-    const wasmModulePath = path.join('./', wasmFiles[0]); // Get the first (and only) matching file
-    const { init, FS } = await import(wasmModulePath); 
-    await init();
-}
-
-loadWasmModule();
 
 const videoElement = document.getElementById("mvi") as HTMLVideoElement;
 const canvas = new OffscreenCanvas(videoElement.videoWidth,videoElement.videoHeight); 
@@ -18,7 +7,7 @@ function processFrame() {
   context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
   const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
   const frameData = imageData.data.buffer; // Raw pixel data (Uint8ClampedArray)
-  FS.writeFile("/video/frame.gl", new Uint8Array(frameData));
+ // FS.writeFile("/video/frame.gl", new Uint8Array(frameData));
 
   requestAnimationFrame(processFrame);
 }
