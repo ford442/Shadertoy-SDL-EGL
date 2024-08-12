a3c01d53114b80fb7ae8f4193b078151af924cd1
Noah Cohn <noahc42@gmail.com>
2024-08-12 15:08:24 -0700

    Create apng.cpp

---
+++ src/vanilla/apng.cpp
@@ -0,0 +1,51 @@
+#include <emscripten.h>
+#include <emscripten/bind.h>
+int main(){
+EM_ASM({
+let scr=document.createElement("script");
+scr.async=true;
+scr.charset='utf-8';
+scr.type='text/javascript';
+scr.defer=true;
+scr.src="https://js.1ink.us/canvas2apng.js";
+document.body.appendChild(scr);
+document.getElementById("apngBtn").addEventListener('click',function(){
+var acanvas = document.querySelector("#scanvas");  // Animation drawn on this canvas
+window.encoder = new APNGencoder(acanvas);
+encoder.setRepeat(0);    // auto-loop is 0
+encoder.setDelay(25);    // 1/100 sec  // really ms ?
+encoder.setDispose(0);
+encoder.setBlend(1);
+var ii=0;
+encoder.start();
+function render() {
+ii++;
+encoder.addFrame(acanvas, { delay: 25 }); // Capture the frame from your main canvas
+if (ii>10) {
+encoder.finish(); // Finalize encoding when done
+var out = encoder.stream();
+var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
+var timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Format: YYYYMMDDTHHMMSS
+var filename = `APNG_${timestamp}.png`;
+var link = document.createElement('a');
+link.download = filename;  // Set the desired filename
+link.href = href;
+link.click();
+window.open(href);
+console.log('finished');
+} else {
+setTimeout(function(){
+render();
+},250);
+} // Continue the animation
+}
+render();
+var base64Out = bytesToBase64(encoder.stream().bin);     // ANIMATION
+var img = document.getElementById("imgAnimPNG");
+img.style.width = acanvas.width;
+img.style.height = acanvas.height;
+img.src = "data:image/png;base64," + base64Out;
+});
+});
+return 0;
+}
