04b6bf0d99bac9736147d0c9b4f93421d439be43
Noah Cohn <noahc42@gmail.com>
2024-08-26 02:45:04 -0700

    Update webgpu_onnx.cpp

--- src/vanilla/webgpu_onnx.cpp
+++ src/vanilla/webgpu_onnx.cpp
@@ -1482,7 +1482,6 @@ return;
 
 void startWebGPUi(emscripten_align1_int sz,emscripten_align1_int sr){
 WGPU_Start(sz,sr);
-cltest();
 return;
 }
 
@@ -1499,6 +1498,7 @@ return;
 }
 
 int main(){
+cltest();
 on.at(0,0)=0;
 js_main();
 return 0;
