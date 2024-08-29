fcd0c69d9698a0bd584988d59433ee3ad14a8b4f
Noah Cohn <noahc42@gmail.com>
2024-08-29 06:29:28 -0700

    Create externs.js

---
+++ externs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} type
+ * @param {function(!Event)} listener
+ * @param {boolean=} opt_useCapture
+ */
+Window.prototype.addEventListener = function(type, listener, opt_useCapture) {};
+
+/**
+ * @constructor
+ */
+function Event() {}
