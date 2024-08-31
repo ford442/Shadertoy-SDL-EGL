cc37f0036d0ad110d4496ca544558fd817a6f58e
Noah Cohn <noahc42@gmail.com>
2024-08-31 02:45:34 -0700

    Update apng.cpp

--- src/vanilla/apng.cpp
+++ src/vanilla/apng.cpp
@@ -14,11 +14,8 @@ document.body.appendChild(scr);
   
 document.getElementById("apngBtn").addEventListener('click',function(){
 const acanvas = document.querySelector("#scanvas");  // Animation drawn on this canvas
-const bcanvas = document.querySelector("#bcanvas");  // Animation drawn on this canvas
-const context=acanvas.getContext('2d');
 const siz=parseInt(acanvas.height);
 window.encoder = new APNGencoder(acanvas);
-window.encoder2 = new APNGencoder(bcanvas);
 encoder.setRepeat(0);    // auto-loop is 0
 encoder.setDelay(100);    // 1/100 sec  // really ms ?
 encoder.setDispose(0);
@@ -26,69 +23,11 @@ encoder.setBlend(1);
 var ii=0;
 encoder.start();
 
-  //  test
-console.log('frame ',ii);
-context.getImageData(0,0,siz,siz).data;
-encoder.addFrame(context);
-encoder.finish(); // Finalize encoding when done
-var out = encoder.stream();
-var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
-var timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Format: YYYYMMDDTHHMMSS
-var filename = `APNG_${timestamp}.png`;
-var link = document.createElement('a');
-link.download = filename;  // Set the desired filename
-link.href =href;
-link.click();
-console.log('finished a');
-console.log('frame ',ii);
-encoder.start();
-
-encoder.addFrame(acanvas);
-encoder.finish(); // Finalize encoding when done
-var out = encoder.stream();
-var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
-var timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Format: YYYYMMDDTHHMMSS
-var filename = `APNG_${timestamp}.png`;
-var link = document.createElement('a');
-link.download = filename;  // Set the desired filename
-link.href =href;
-link.click();
-console.log('finished b');
-
-encoder2.start();
-
-console.log('frame ',ii);
-context.getImageData(0,0,siz,siz).data;
-encoder2.addFrame(context);
-encoder2.finish(); // Finalize encoding when done
-var out = encoder.stream();
-var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
-var timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Format: YYYYMMDDTHHMMSS
-var filename = `APNG_${timestamp}.png`;
-var link = document.createElement('a');
-link.download = filename;  // Set the desired filename
-link.href =href;
-link.click();
-console.log('finished c');
-console.log('frame ',ii);
-encoder2.start();
-
-encoder2.addFrame(acanvas);
-encoder2.finish(); // Finalize encoding when done
-var out = encoder.stream();
-var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
-var timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Format: YYYYMMDDTHHMMSS
-var filename = `APNG_${timestamp}.png`;
-var link = document.createElement('a');
-link.download = filename;  // Set the desired filename
-link.href =href;
-link.click();
-console.log('finished d');
-
-/*
 function render() {
-setTimeout(function(){
-if (ii>40) {
+if (ii>21) {
+return;
+}
+if (ii>20) {
 encoder.finish(); // Finalize encoding when done
 var out = encoder.stream();
 var href= URL.createObjectURL(new Blob([new Uint8Array(out.bin)], {type : "image/png" } ));
@@ -106,19 +45,16 @@ img.style.width = acanvas.width;
 img.style.height = acanvas.height;
 img.src = "data:image/png;base64," + base64Out;
 } else {
-if(ii<41){
 ii++;
 console.log('frame ',ii);
-context.getImageData(0,0,siz,siz);
-encoder.addFrame(context);
+encoder.addFrame(acanvas);
+setTimeout(function(){
 render();
-}
 }
 },100);
 }
-  
+
 render();
-*/
 
 });
 
