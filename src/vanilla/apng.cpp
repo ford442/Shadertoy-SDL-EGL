d48eeec81f7bae7d8368f9c8412658b13da4d8c6
Noah Cohn <noahc42@gmail.com>
2024-09-12 14:46:50 -0700

    Update apng.cpp

--- src/vanilla/apng.cpp
+++ src/vanilla/apng.cpp
@@ -30,7 +30,7 @@ row_pointers[(CframeCount - 1) * height + y] = frame_data[CframeCount - 1] + y *
 void read_png(FILE *fp, int sig_read) {
  png_structp png_ptr;
  png_infop info_ptr;
- png_uint_32 width, height;
+ png_uint_16 width, height;
  int bit_depth, color_type, interlace_type;// After reading the image information
 png_read_info(png_ptr, info_ptr);
 png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,&interlace_type, NULL,NULL);
