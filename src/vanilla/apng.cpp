ec6a4d680aba1fcc92311400078dc441b885fb28
Noah Cohn <noahc42@gmail.com>
2024-09-13 14:29:59 -0700

    Update apng.cpp

--- src/vanilla/apng.cpp
+++ src/vanilla/apng.cpp
@@ -63,7 +63,7 @@ FILE* fp = fopen(fileName.c_str(), "r");
     rewind(fp);// Read the PNG file
 read_png(fp, 0);
 png_set_next_frame_fcTL(png_ptr_write, info_ptr_write, decoded_png_data.width, decoded_png_data.height, 0, 0, 
-static_cast<png_uint_16>(delays[i]), 1000, PNG_DISPOSE_OP_BACKGROUND, PNG_BLEND_OP_SOURCE); 
+static_cast<png_uint_16>(delay), 1000, PNG_DISPOSE_OP_BACKGROUND, PNG_BLEND_OP_SOURCE); 
 png_write_image(png_ptr_write, decoded_png_data.rows);
 fclose(fp);
 for (int y = 0; y < decoded_png_data.height; y++) {
