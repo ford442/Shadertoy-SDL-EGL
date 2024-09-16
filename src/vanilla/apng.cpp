1f331cd45bd3bc4922269a7f7d39ee1a2cb68c9f
Noah Cohn <noahc42@gmail.com>
2024-09-16 16:39:10 -0700

    Update apng.cpp

--- src/vanilla/apng.cpp
+++ src/vanilla/apng.cpp
@@ -23,58 +23,9 @@ png_bytepp row_pointers=nullptr;
 int CframeCount=10;
 int num_frames=10;
 
-void read_png(FILE *fp, int sig_read) {
-png_structp png_ptr;
-png_infop info_ptr;
-png_ptr=png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-info_ptr=png_create_info_struct(png_ptr);
-png_init_io(png_ptr, fp);
-png_set_sig_bytes(png_ptr, sig_read);
-png_read_info(png_ptr, info_ptr);
-png_uint_32 width, height;
-int bit_depth, color_type, interlace_type;
-png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL,  NULL);
-decoded_png_data.width=width;
-decoded_png_data.height=height;
-decoded_png_data.rows=(png_bytep*) malloc(sizeof(png_bytep) * height);
-for (int y=0; y < height; y++) {
-decoded_png_data.rows[y]=(png_byte*) malloc(png_get_rowbytes(png_ptr, info_ptr));
-}
-png_read_image(png_ptr, decoded_png_data.rows);
-png_read_end(png_ptr, NULL);
-png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
-return;
-}
-
-void runApngC() {
-int delay=500, num_frames=10, size=1024;
-png_ptr_write=png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-info_ptr_write=png_create_info_struct(png_ptr_write);
-png_set_IHDR(png_ptr_write, info_ptr_write, size, size, 8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-png_set_acTL(png_ptr_write, info_ptr_write, 10, 0); 
-for (int i=0; i < 10; ++i) {
-std::stringstream ss;
-ss << "/frames/frame" << (i + 1) << ".png";
-std::string fileName=ss.str();
-FILE* fp=fopen(fileName.c_str(), "r");
-read_png(fp, 0);
-png_set_next_frame_fcTL(png_ptr_write,info_ptr_write,decoded_png_data.width,decoded_png_data.height,0,0,500,1000, PNG_DISPOSE_OP_BACKGROUND, PNG_BLEND_OP_SOURCE); 
-png_write_image(png_ptr_write, decoded_png_data.rows);
-fclose(fp);
-for (int y=0; y < decoded_png_data.height; y++) {
-free(decoded_png_data.rows[y]);
-}
-free(decoded_png_data.rows);
-}
-png_write_end(png_ptr_write, info_ptr_write);
-png_destroy_write_struct(&png_ptr_write, &info_ptr_write);
-return; 
-}
-
 extern "C" {
 
 void runApng() {
-runApngC();
 return;
 }
 
@@ -158,7 +109,8 @@ return;
 ii++;
 console.log('Frame: ', ii);
 const image = ctx.getImageData(0, 0, siz, siz);
-Module.ccall('writePngFrame', null, ['array', 'number', 'number'], [image.data, siz, siz]);
+    const pixelData = new Float32Array(imageData.data);
+Module.ccall('writePngFrame', null, ['array', 'number', 'number'], [pixelData, siz, siz]);
 }
 setTimeout(function(){
 render();
