2611785da1f4f19229f7e95184277037dd6d7d1a
Noah Cohn <noahc42@gmail.com>
2024-10-08 17:17:12 -0700

    Update apng_py.cpp

--- src/vanilla/apng_py.cpp
+++ src/vanilla/apng_py.cpp
@@ -75,7 +75,6 @@ await micropip.install('numpy')
 await micropip.install('cython')
 await micropip.install('apng')
 await micropip.install('pillow')
-await micropip.install('essentia')
 import os
 import js
 import base64
@@ -84,7 +83,7 @@ from PIL import Image
 import io
 import numpy as np
 from apng import APNG, PNG
-frames_data = []  # List to store decoded frame data
+frames_data = []
 frames_data = []
 apng_animation = APNG()
 
@@ -105,16 +104,12 @@ for frame_data in frames_data:
 apng_bytes = apng_animation.to_bytes()
 apng_base64 = base64.b64encode(apng_bytes).decode('ascii')
 apng_base64`,{globals}).then(result => {
-    // Get the base64 data back in JavaScript
-  //  const apngBase64 = pyodide.globals.get("apngBase64");
 
-    // Create a data URL
     const apngDataURL = "data:image/apng;base64," + result;
     const img = document.createElement('img');
     img.src = apngDataURL;
     document.body.appendChild(img);
 
-    // Example: Triggering a download
     const a = document.createElement('a');
     a.href = apngDataURL;
     a.download = 'animation.apng';
