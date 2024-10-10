9fe5a6c363784f8c4f8f517cb0511392f369d245
Noah Cohn <noahc42@gmail.com>
2024-10-10 12:17:28 -0700

    Update depth.cpp

--- src/vanilla/depth.cpp
+++ src/vanilla/depth.cpp
@@ -34,9 +34,10 @@ return nullptr;
 int main(){
   
 const char * Fnm=reinterpret_cast<const char *>("depth.js");
+const char * scr=reinterpret_cast<const char *>("console.log('From async run.');");
 const char * js_script=(char*)rd_fl(Fnm);
-emscripten_async_run_script("console.log('From async run.');", 100); // 1 for async
-emscripten_async_run_script(&js_script, 100); // 1 for async
+emscripten_async_run_script(scr, 100); // 1 for async
+emscripten_async_run_script(js_script, 100); // 1 for async
 
 return 0;
 }
